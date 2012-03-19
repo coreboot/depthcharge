46a4ba08ca24f922d3736cc439bdea2d9045bf96
Gabe Black <gabeblack@google.com>
2012-03-19 03:45:06 -0700

    Define a structure which can describe block devices generically.
    This will give the vboot interfacing code something more consistent to work
    with so that it doesn't have to be specialized for each type of disk we may
    need in the future.
    
    BUG=chrome-os-partner:8339
    TEST=With this and other changes, built and ran on Stumpy and accessed the
    SSD successfully.
    BRANCH=None
    
    Change-Id: I8c8209169d490bde696d9c4b2ac8ac349b1f5832
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: https://gerrit-int.chromium.org/24871
    Reviewed-by: Stefan Reinauer <reinauer@google.com>

---
+++ include/blockdev.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2012 The Chromium OS Authors.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __BLOCKDEV_H__
+#define __BLOCKDEV_H__
+
+#include <stdint.h>
+
+typedef uint64_t lba_t;
+
+typedef struct BlockDev {
+	int removable;
+	unsigned block_size;
+	lba_t block_count;
+	lba_t (*read)(struct BlockDev *dev, lba_t start, lba_t count,
+		      void *buffer);
+	lba_t (*write)(struct BlockDev *dev, lba_t start, lba_t count,
+		       const void *buffer);
+	void *dev_data;
+} BlockDev;
+
+#endif
