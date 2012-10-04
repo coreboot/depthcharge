ac5f32a79f03ba0e23b1bdbec89575b06078bfe3
Gabe Black <gabeblack@google.com>
2012-10-04 01:31:20 -0700

    depthcharge: Implement bitmap support.
    This change adds some data structures which help decode bitmaps, and some code
    that actually draws the bitmaps into the coreboot framebuffer. That support
    augments the corebootfb support in libpayload and, to avoid namespace issues,
    uses the dc_corebootfb prefix for its functions.
    
    Only palettized version 3 bitmaps are supported, and that limits the bits per
    pixel to 8. 8 bits per pixel has been tested, but other color depths
    (specifically 1 and 4) have support implemented.
    
    The code which extracts the palette index for a pixel has support for pulling
    out values larger than 8 bits, but that would appear to be unnecessary because
    the palette isn't used with those color depths. I chose to leave that support
    in so that the code that pulls pixels out of the bitmap can be pulled out and
    used both to pull out indices and to pull out unencoded data.
    
    BUG=chrome-os-partner:8339
    TEST=With this and another change that hooks up VbExDisplayImage, built and
    booted on Link, halted the CPU after all bitmaps were drawn, and inspected the
    image on screen as compared to one drawn by U-Boot. Verified visually that they
    were the same. Booted into ChromeOS.
    BRANCH=None
    
    Change-Id: I20565814dbc86ed741e6fbd9811b7e588f88a28b
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: https://gerrit-int.chromium.org/26534
    Reviewed-by: Bill Richardson <wfrichar@google.com>
    Reviewed-by: Stefan Reinauer <reinauer@google.com>

---
+++ src/base/bitmap.h
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2011-2012 The Chromium OS Authors.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __BASE_BITMAP_H__
+#define __BASE_BITMAP_H__
+
+#include <stdint.h>
+
+/* Main header */
+
+typedef struct __attribute__ ((__packed__)) BitmapFileHeader {
+	uint8_t signature[2];
+	uint32_t file_size;
+	uint16_t reserved[2];
+	uint32_t bitmap_offset;
+} BitmapFileHeader;
+
+
+/* Bitmap version 2 */
+
+typedef struct __attribute__ ((__packed__)) BitmapHeaderV2 {
+	uint32_t header_size;
+	int16_t width;
+	int16_t height;
+	uint16_t planes;
+	uint16_t bits_per_pixel;
+} BitmapHeaderV2;
+
+typedef struct __attribute__ ((__packed__)) BitmapPaletteElementV2 {
+	uint8_t blue;
+	uint8_t green;
+	uint8_t red;
+} BitmapPaletteElementV2;
+
+
+/* Bitmap version 3 */
+
+typedef struct __attribute__ ((__packed__)) BitmapHeaderV3 {
+	uint32_t header_size;
+	int32_t width;
+	int32_t height;
+	uint16_t planes;
+	uint16_t bits_per_pixel;
+	uint32_t compression;
+	uint32_t size;
+	int32_t h_res;
+	int32_t v_res;
+	uint32_t colors_used;
+	uint32_t colors_important;
+} BitmapHeaderV3;
+
+typedef struct __attribute__ ((__packed__)) BitmapPaletteElementV3 {
+	uint8_t blue;
+	uint8_t green;
+	uint8_t red;
+	uint8_t reserved;
+} BitmapPaletteElementV3;
+
+typedef struct __attribute__ ((__packed__)) BitmapBitfieldMasks {
+	uint32_t red;
+	uint32_t green;
+	uint32_t blue;
+} BitmapBitfieldMasks;
+
+
+/* Bitmap version 4 */
+
+typedef struct __attribute__ ((__packed__)) BitmapHeaderV4 {
+	uint32_t header_size;
+	int32_t width;
+	int32_t height;
+	uint16_t planes;
+	uint16_t bits_per_pixel;
+	uint32_t compression;
+	uint32_t size;
+	int32_t h_res;
+	int32_t v_res;
+	uint32_t colors_used;
+	uint32_t colors_important;
+	uint32_t red_mask;
+	uint32_t green_mask;
+	uint32_t blue_mask;
+	uint32_t alpha_mask;
+	uint32_t cs_type;
+	int32_t red_x;
+	int32_t red_y;
+	int32_t red_z;
+	int32_t green_x;
+	int32_t green_y;
+	int32_t green_z;
+	int32_t blue_x;
+	int32_t blue_y;
+	int32_t blue_z;
+	uint32_t gamma_red;
+	uint32_t gamma_green;
+	uint32_t gamma_blue;
+} BitmapHeaderV4;
+
+#endif /* __BASE_MEMORY_H__ */
---
+++ src/drivers/coreboot_fb.c
@@ -0,0 +1,177 @@
+/*
+ * Copyright (c) 2012 The Chromium OS Authors.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <libpayload.h>
+#include <stdint.h>
+#include <sysinfo.h>
+
+#include "base/bitmap.h"
+#include "drivers/coreboot_fb.h"
+
+static inline void dc_corebootfb_draw_pixel(uint32_t x, uint32_t y,
+					    uint32_t red, uint32_t green,
+					    uint32_t blue,
+					    struct cb_framebuffer *fbinfo,
+					    unsigned char *fbaddr)
+{
+	const uint32_t xres = fbinfo->x_resolution;
+	const int bpp = fbinfo->bits_per_pixel;
+
+	uint32_t color = 0;
+	color |= (red >> (8 - fbinfo->red_mask_size))
+		<< fbinfo->red_mask_pos;
+	color |= (green >> (8 - fbinfo->green_mask_size))
+		<< fbinfo->green_mask_pos;
+	color |= (blue >> (8 - fbinfo->blue_mask_size))
+		<< fbinfo->blue_mask_pos;
+
+	uint8_t *pixel = fbaddr + (x + y * xres) * bpp / 8;
+	for (int i = 0; i < bpp / 8; i++)
+		pixel[i] = (color >> (i * 8));
+}
+
+static int dc_corebootfb_draw_bitmap_v2(uint32_t x, uint32_t y,
+					void *bitmap,
+					struct cb_framebuffer *fbinfo,
+					unsigned char *fbaddr)
+{
+	printf("Bitmap version 2 not supported.\n");
+	return -1;
+}
+
+static int dc_corebootfb_draw_bitmap_v3(uint32_t x, uint32_t y,
+					void *bitmap,
+					struct cb_framebuffer *fbinfo,
+					unsigned char *fbaddr)
+{
+	BitmapFileHeader *file_header = (BitmapFileHeader *)bitmap;
+	BitmapHeaderV3 *header = (BitmapHeaderV3 *)(file_header + 1);
+	int bpp = header->bits_per_pixel;
+
+	// Check for things we don't support.
+	if (header->compression != 0 || bpp >= 16) {
+		printf("Non-palette bitmaps are not supported.\n");
+		return -1;
+	}
+	if (bpp != 1 && bpp != 4 && bpp != 8) {
+		printf("Unsupported bits per pixel.\n");
+		return -1;
+	}
+
+	BitmapPaletteElementV3 *palette =
+		(BitmapPaletteElementV3 *)(header +1);
+
+	int32_t width = header->width, height = header->height;
+	int extra = width % 4;
+	const int32_t padding = extra ? (4 - extra) : 0;
+	int32_t ystep = -1;
+	if (height < 0) {
+		height = -height;
+		ystep = -ystep;
+	} else {
+		y += height;
+	}
+	uint8_t *cur_data = (uint8_t *)bitmap + file_header->bitmap_offset;
+	uint32_t x_offset = 0, y_offset = 0;
+	int bit = 0;
+	// Loop over all the pixels in the image.
+	for (uint32_t pixel = 0; pixel < width * height; pixel++) {
+		int index = 0;
+		// Extract the index value for this pixel.
+		if (bpp >= 8) {
+			// For pixels one byte or larger, glue them together
+			// one byte at a time. Pixels are big endian.
+			for (int i = 0; i < bpp / 8; i++) {
+				index <<= 8;
+				index |= *cur_data++;
+			}
+		} else {
+			// For pixels smaller than a byte, extract some bits
+			// from the right byte. Bytes are big endian.
+			const uint8_t mask = (1 << bpp) - 1;
+			bit += bpp;
+			index = (*cur_data >> (8 - bit)) & mask;
+			if (bit == 8) {
+				bit = 0;
+				cur_data++;
+			}
+		}
+
+		// Actually draw that pixel on the display.
+		dc_corebootfb_draw_pixel(x + x_offset,
+					 y + y_offset,
+					 palette[index].red,
+					 palette[index].green,
+					 palette[index].blue,
+					 fbinfo,
+					 fbaddr);
+
+		// Keep track of position.
+		if (++x_offset == width) {
+			x_offset = 0;
+			y_offset += ystep;
+			cur_data += padding;
+		}
+	}
+	return 0;
+}
+
+static int dc_corebootfb_draw_bitmap_v4(uint32_t x, uint32_t y,
+					void *bitmap,
+					struct cb_framebuffer *fbinfo,
+					unsigned char *fbaddr)
+{
+	printf("Bitmap version 4 not supported.\n");
+	return -1;
+}
+
+int dc_corebootfb_draw_bitmap(uint32_t x, uint32_t y, void *bitmap)
+{
+	BitmapFileHeader *file_header = (BitmapFileHeader *)bitmap;
+
+	if (file_header->signature[0] != 'B' ||
+	    file_header->signature[1] != 'M') {
+		printf("Bitmap signature check failed.\n");
+		return -1;
+	}
+
+	struct cb_framebuffer *fbinfo = lib_sysinfo.framebuffer;
+	unsigned char *fbaddr =
+		(unsigned char *)(uintptr_t)(fbinfo->physical_address);
+
+	uint32_t header_size = *(uint32_t *)(file_header + 1);
+	switch (header_size) {
+	case 12:
+		return dc_corebootfb_draw_bitmap_v2(x, y, bitmap,
+						    fbinfo, fbaddr);
+	case 40:
+		return dc_corebootfb_draw_bitmap_v3(x, y, bitmap,
+						    fbinfo, fbaddr);
+	case 108:
+		return dc_corebootfb_draw_bitmap_v4(x, y, bitmap,
+						    fbinfo, fbaddr);
+	default:
+		printf("Unrecognized bitmap format.\n");
+		return -1;
+	}
+	return 0;
+}
