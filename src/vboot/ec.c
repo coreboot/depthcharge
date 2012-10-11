e8b714857d02a95aedc160fd4e65846b021bd47b
Gabe Black <gabeblack@google.com>
2012-10-10 18:58:48 -0700

    depthcharge: Implement the EC related vboot callbacks.
    These may not be implemented entirely correctly since they're not easy to test
    yet. They were heavily based on the U-Boot versions of the same functions and
    should at least be close.
    
    BUG=chrome-os-partner:8339
    TEST=Built and booted on Link. Called some of the EC related functions
    directly from main() and verified that they didn't do anything obviously
    wrong. We'll need the ability to boot an RW firmware to test things properly.
    BRANCH=None
    
    Change-Id: I3b3aae9729432184fc5a75ec5b920cbbc5799fff
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: https://gerrit-int.chromium.org/27006
    Reviewed-by: Stefan Reinauer <reinauer@google.com>

--- src/vboot/ec.c
+++ src/vboot/ec.c
@@ -24,6 +24,8 @@
 #include <vboot_api.h>
 
 #include "base/flag.h"
+#include "base/fmap.h"
+#include "drivers/ec/chromeos/mkbp.h"
 
 int VbExTrustEC(void)
 {
@@ -38,43 +40,199 @@ int VbExTrustEC(void)
 
 VbError_t VbExEcRunningRW(int *in_rw)
 {
-	printf("VbExEcRunningRW not implemented.\n");
-	return VBERROR_UNKNOWN;
+	if (!mkbp_ptr) {
+		printf("No MKBP device.\n");
+		return VBERROR_UNKNOWN;
+	}
+
+	enum ec_current_image image;
+	if (mkbp_read_current_image(mkbp_ptr, &image) < 0) {
+		printf("Failed to read current EC image.\n");
+		return VBERROR_UNKNOWN;
+	}
+	switch (image) {
+	case EC_IMAGE_RO:
+		*in_rw = 0;
+		break;
+	case EC_IMAGE_RW:
+		*in_rw = 1;
+		break;
+	default:
+		printf("Unrecognized EC image type %d.\n", image);
+		return VBERROR_UNKNOWN;
+	}
+
+	return VBERROR_SUCCESS;
 }
 
 VbError_t VbExEcJumpToRW(void)
 {
-	printf("VbExEcJumpToRw not implemented.\n");
-	return VBERROR_UNKNOWN;
+	if (!mkbp_ptr) {
+		printf("No MKBP device.\n");
+		return VBERROR_UNKNOWN;
+	}
+
+	if (mkbp_reboot(mkbp_ptr, EC_REBOOT_JUMP_RW, 0) < 0) {
+		printf("Failed to make the EC jump to RW.\n");
+		return VBERROR_UNKNOWN;
+	}
+
+	return VBERROR_SUCCESS;
 }
 
 VbError_t VbExEcStayInRO(void)
 {
-	printf("VbExEcStayInRO not implemented.\n");
-	return VBERROR_UNKNOWN;
+	if (!mkbp_ptr) {
+		printf("No MKBP device.\n");
+		return VBERROR_UNKNOWN;
+	}
+
+	if (mkbp_reboot(mkbp_ptr, EC_REBOOT_DISABLE_JUMP, 0) < 0) {
+		printf("Failed to make the EC disable jumping.\n");
+		return VBERROR_UNKNOWN;
+	}
+
+	return VBERROR_SUCCESS;
 }
 
 VbError_t VbExEcHashRW(const uint8_t **hash, int *hash_size)
 {
-	printf("VbExEcHashRW not implemented.\n");
-	return VBERROR_UNKNOWN;
+	if (!mkbp_ptr) {
+		printf("No MKBP device.\n");
+		return VBERROR_UNKNOWN;
+	}
+
+	struct ec_response_vboot_hash resp;
+	if (mkbp_read_hash(mkbp_ptr, &resp) < 0) {
+		printf("Failed to read EC hash.\n");
+		return VBERROR_UNKNOWN;
+	}
+
+	/*
+	 * TODO (rspangler@chromium.org): the code below isn't very tolerant
+	 * of errors.
+	 *
+	 * If the EC is busy calculating a hash, we should wait and retry
+	 * reading the hash status.
+	 *
+	 * If the hash is unavailable, the wrong type, or covers the wrong
+	 * offset/size (which we need to get from the FDT, since it's
+	 * board-specific), we should request a new hash and wait for it to
+	 * finish.  Also need a flag to force it to rehash, which we'll use
+	 * after doing a firmware update.
+	 */
+	if (resp.status != EC_VBOOT_HASH_STATUS_DONE) {
+		printf("EC hash wasn't finished.\n");
+		return VBERROR_UNKNOWN;
+	}
+	if (resp.hash_type != EC_VBOOT_HASH_TYPE_SHA256) {
+		printf("EC hash was the wrong type.\n");
+		return VBERROR_UNKNOWN;
+	}
+
+	*hash = resp.hash_digest;
+	*hash_size = resp.digest_size;
+
+	return VBERROR_SUCCESS;
 }
 
 VbError_t VbExEcGetExpectedRW(enum VbSelectFirmware_t select,
 			      const uint8_t **image, int *image_size)
 {
-	printf("VbExEcGetExpectedRW not implemented.\n");
+	const char *name;
+
+	switch (select) {
+	case VB_SELECT_FIRMWARE_A:
+		name = "EC_MAIN_A";
+		break;
+	case VB_SELECT_FIRMWARE_B:
+		name = "EC_MAIN_B";
+		break;
+	default:
+		printf("Unrecognized EC firmware requested.\n");
+		return VBERROR_UNKNOWN;
+	}
+
+	FmapArea *area = fmap_find_area(main_fmap, name);
+	if (!area) {
+		printf("Didn't find section %s in the fmap.\n", name);
+		return VBERROR_UNKNOWN;
+	}
+
+	uint32_t offset = area->area_offset;
+	uint32_t size = area->area_size;
+
+	printf("EC-RW image offset, size are %d, %d.\n", offset, size);
+
+	*image = (uint8_t *)(main_rom_base + offset);
+	*image_size = size;
+	return VBERROR_SUCCESS;
+}
+
+static VbError_t ec_protect_rw(int protect)
+{
+	struct ec_response_flash_protect resp;
+	uint32_t mask = EC_FLASH_PROTECT_RW_NOW | EC_FLASH_PROTECT_RW_AT_BOOT;
+
+	if (!mkbp_ptr) {
+		printf("No MKBP device.\n");
+		return VBERROR_UNKNOWN;
+	}
+
+	/* Update protection */
+	if (mkbp_flash_protect(mkbp_ptr, mask, protect ? mask : 0, &resp) < 0) {
+		printf("Failed to update EC flash protection.\n");
+		return VBERROR_UNKNOWN;
+	}
+
+	if (!protect) {
+		/* If protection is still enabled, need reboot */
+		if (resp.flags & EC_FLASH_PROTECT_RW_NOW)
+			return VBERROR_EC_REBOOT_TO_RO_REQUIRED;
+
+		return VBERROR_SUCCESS;
+	}
+
+	/*
+	 * If write protect and ro-at-boot aren't both asserted, don't expect
+	 * protection enabled.
+	 */
+	if ((~resp.flags) & (EC_FLASH_PROTECT_GPIO_ASSERTED |
+			     EC_FLASH_PROTECT_RO_AT_BOOT))
+		return VBERROR_SUCCESS;
+
+	/* If flash is protected now, success */
+	if (resp.flags & EC_FLASH_PROTECT_RW_NOW)
+		return VBERROR_SUCCESS;
+
+	/* If RW will be protected at boot but not now, need a reboot */
+	if (resp.flags & EC_FLASH_PROTECT_RW_AT_BOOT)
+		return VBERROR_EC_REBOOT_TO_RO_REQUIRED;
+
+	/* Otherwise, it's an error */
 	return VBERROR_UNKNOWN;
 }
 
 VbError_t VbExEcUpdateRW(const uint8_t *image, int image_size)
 {
-	printf("VbExEcUpdateRW not implemented.\n");
-	return VBERROR_UNKNOWN;
+	if (!mkbp_ptr) {
+		printf("No MKBP device.\n");
+		return VBERROR_UNKNOWN;
+	}
+
+	int rv = ec_protect_rw(0);
+	if (rv == VBERROR_EC_REBOOT_TO_RO_REQUIRED || rv != VBERROR_SUCCESS)
+		return rv;
+
+	if (mkbp_flash_update_rw(mkbp_ptr, image, image_size)) {
+		printf("Failed to update EC RW flash.\n");
+		return VBERROR_UNKNOWN;
+	}
+
+	return VBERROR_SUCCESS;
 }
 
 VbError_t VbExEcProtectRW(void)
 {
-	printf("VbExEcProtectRW not implemented.\n");
-	return VBERROR_UNKNOWN;
+	return ec_protect_rw(1);
 }
