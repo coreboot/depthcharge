1ff5b775c0c0d450de167dfaadf77c90ba989c64
Gabe Black <gabeblack@google.com>
2013-01-24 01:02:32 -0800

    depthcharge: Add a dummy sound driver for ARM.
    This will make it link, but sound still won't work. The driver will be
    unconditionally compiled in for ARM and nothing else. Once a real driver is
    available, this driver can be turned off for ARM or eliminated entirely.
    
    BUG=chrome-os-partner:17340
    TEST=Built for Daisy and saw errors about missing symbols from the sound
    driver go away.
    BRANCH=None
    
    Change-Id: I7d80172e97a710f00acda9591da005c34d9cb225
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: https://gerrit-int.chromium.org/31650

---
+++ src/drivers/sound/dummy.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * Alternatively, this software may be distributed under the terms of the
+ * GNU General Public License ("GPL") version 2 as published by the Free
+ * Software Foundation.
+ */
+
+#include <libpayload.h>
+
+#include "drivers/sound/sound.h"
+
+void enable_beep(uint32_t frequency)
+{
+	printf("Enable beep at %d Hz.\n", frequency);
+}
+
+void disable_beep(void)
+{
+	printf("Disable beep.\n");
+}
