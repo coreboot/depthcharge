1dae41a6508915f86542b75e6bdec782ff1d62d6
Gabe Black <gabeblack@google.com>
2013-01-23 23:34:44 -0800

    depthcharge: Add some functions needed by libgcc.
    These are:
    
    int raise(int sig)
    The standard library function which raises a signal. Since we don't have
    those, this prints a message saying what happened and then hangs. This
    function seems to only be called by the division function when it detects
    a division by zero. If that happens we want to know about it so we can fix it,
    and it should never happen in the absence of bugs.
    
    __aeabi_unwind_cpp_pr0
    A function that takes relatively complicated arguments and is used to unwind
    the stack in the event of a C++ exception. I think the pr0 specifies the
    personality. We're not in C++ and not using C++ exceptions, so we can just
    ignore these requests.
    
    BUG=chrome-os-partner:17340
    TEST=Built for Snow and saw more linker errors go away.
    BRANCH=None
    
    Change-Id: Ide76988d4b51962f3a37daf5525f980fce598de2
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: https://gerrit-int.chromium.org/31646

---
+++ src/arch/arm/base/gcc.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2013 The Chromium OS Authors.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <libpayload.h>
+#include <stdint.h>
+
+int raise(int sig);
+int raise(int sig)
+{
+	printf("libgcc raises signal ");
+	if (sig == 8)
+		printf("SIGFPE."); // Probably divide by zero.
+	else
+		printf("%d.", sig);
+	halt();
+}
+
+void __aeabi_unwind_cpp_pr0(void);
+void __aeabi_unwind_cpp_pr0(void)
+{
+}
