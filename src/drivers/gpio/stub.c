23898d8249050953f3b3a93dc41655c39b39beb3
Gabe Black <gabeblack@google.com>
2013-01-26 19:25:36 -0800

    depthcharge: Add a "stub" GPIO driver for when a real one isn't necessary.
    If, for instance, depthcharge is being ported to a platform that doesn't use
    EC software sync and has no other use for real GPIOs (excluding ones in the
    coreboot tables), then there's no reason to have to implement a GPIO driver
    that won't be used. Also, while support for a platform is being developed,
    this driver can be used to make sure the linker can resolve all references
    without having to have everything written up front.
    
    BUG=chrome-os-partner:17340
    TEST=Built and booted on Link. Built for Daisy and saw some linker errors go
    away.
    BRANCH=None
    
    Change-Id: Ia2b010ba3d2d3b40b439b7bffd5b7007f353ea45
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: https://gerrit-int.chromium.org/31669
    Reviewed-by: Ronald G. Minnich <rminnich@google.com>

---
+++ src/drivers/gpio/stub.c
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2013 The Chromium OS Authors.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <libpayload.h>
+
+#include "drivers/gpio/gpio.h"
+
+int gpio_use(unsigned num, unsigned use)
+{
+	printf("%s(%d, %d) is unimplemented.\n", __func__, num, use);
+	halt();
+}
+
+int gpio_direction(unsigned num, unsigned input)
+{
+	printf("%s(%d, %d) is unimplemented.\n", __func__, num, input);
+	halt();
+}
+
+int gpio_get_value(unsigned num)
+{
+	printf("%s(%d) is unimplemented.\n", __func__, num);
+	halt();
+}
+
+int gpio_set_value(unsigned num, unsigned value)
+{
+	printf("%s(%d, %d) is unimplemented.\n", __func__, num, value);
+	halt();
+}
