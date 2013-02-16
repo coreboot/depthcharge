17649711654a10664e0b0696d55f207b0e8acdc1
Gabe Black <gabeblack@google.com>
2013-02-16 11:38:12 -0800

    netboot: Add a driver for ASIX based USB network dongles.
    This driver will attach to dongles that have vendor and product IDs which
    exactly match the dongle I had at my desk. It might need to accept more IDs in
    the future, but I'll wait and see what's needed.
    
    BUG=chrome-os-partner:16688
    TEST=Built and booted into the netboot payload on Link. Saw that the device
    was probed successfully, initialized without error, that it returned the
    correct MAC address, that it waited for a link correctly, and that the LEDs on
    the dongle were active once the driver was loaded.
    BRANCH=None
    
    Change-Id: I08a806a01a62944bd0baab13ea2755aeee029431
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: https://gerrit-int.chromium.org/32736

---
+++ src/drivers/net/asix.c
@@ -0,0 +1,434 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <assert.h>
+#include <endian.h>
+#include <libpayload.h>
+#include <stdint.h>
+#include <usb/usb.h>
+
+#include "base/init_funcs.h"
+#include "drivers/bus/usb/usb.h"
+#include "drivers/net/asix.h"
+#include "drivers/net/net.h"
+
+
+/*
+ * Utility functions.
+ */
+
+static int send_control_dev_req(usbdev_t *dev, dev_req_t *req, void *data)
+{
+	return dev->controller->control(
+		dev, req->data_dir == device_to_host ? IN : OUT,
+		sizeof(dev_req_t), req, req->wLength, (uint8_t *)data);
+}
+
+static int asix_ctrl_read(usbdev_t *dev, uint8_t request, uint16_t value,
+		    uint16_t index, uint16_t length, void *data)
+{
+	dev_req_t dev_req;
+	dev_req.req_recp = dev_recp;
+	dev_req.req_type = vendor_type;
+	dev_req.data_dir = device_to_host;
+	dev_req.bRequest = request;
+	dev_req.wValue = value;
+	dev_req.wIndex = index;
+	dev_req.wLength = length;
+
+	return send_control_dev_req(dev, &dev_req, data);
+}
+
+static int asix_ctrl_write(usbdev_t *dev, uint8_t request, uint16_t value,
+		    uint16_t index, uint16_t length, void *data)
+{
+	dev_req_t dev_req;
+	dev_req.req_recp = dev_recp;
+	dev_req.req_type = vendor_type;
+	dev_req.data_dir = host_to_device;
+	dev_req.bRequest = request;
+	dev_req.wValue = value;
+	dev_req.wIndex = index;
+	dev_req.wLength = length;
+
+	return send_control_dev_req(dev, &dev_req, data);
+}
+
+static int asix_write_gpio(usbdev_t *dev, uint16_t value)
+{
+	if (asix_ctrl_write(dev, GpioRegWrite, value, 0, 0, NULL)) {
+		printf("ASIX: Failed to set up GPIOs.\n");
+		return 1;
+	}
+	return 0;
+}
+
+static int asix_sw_reset(usbdev_t *dev, uint8_t bits)
+{
+	if (asix_ctrl_write(dev, SoftResetRegWrite, bits, 0, 0, NULL)) {
+		printf("ASIX: Software reset failed.\n");
+		return 1;
+	}
+	return 0;
+}
+
+static int asix_phy_addr(usbdev_t *dev)
+{
+	uint8_t phy_addr[2];
+	if (asix_ctrl_read(dev, PhyAddrRegRead, 0, 0, sizeof(phy_addr),
+			phy_addr)) {
+		printf("ASIX: Reading PHY address register failed.\n");
+		return -1;
+	}
+	return phy_addr[1];
+}
+
+static int asix_write_rx_ctl(usbdev_t *dev, uint16_t val)
+{
+	if (asix_ctrl_write(dev, RxCtrlRegWrite, val, 0, 0, NULL)) {
+		printf("ASIX: Setting the rx control reg failed.\n");
+		return 1;
+	}
+	return 0;
+}
+
+static int asix_set_sw_mii(usbdev_t *dev)
+{
+	if (asix_ctrl_write(dev, SoftSerMgmtCtrlReg, 0, 0, 0, NULL)) {
+		printf("ASIX: Failed to enabled software MII access.\n");
+		return 1;
+	}
+	return 0;
+}
+
+static int asix_set_hw_mii(usbdev_t *dev)
+{
+	if (asix_ctrl_write(dev, HardSerMgmtCtrlReg, 0, 0, 0, NULL)) {
+		printf("ASIX: Failed to enabled software MII access.\n");
+		return 1;
+	}
+	return 0;
+}
+
+static int asix_mdio_read(usbdev_t *dev, int phy_id, int loc, uint16_t *val)
+{
+	if (asix_set_sw_mii(dev) ||
+	    asix_ctrl_read(dev, PhyRegRead, phy_id, loc, sizeof(*val), val) ||
+	    asix_set_hw_mii(dev)) {
+		printf("ASIX: MDIO read failed.\n");
+		return 1;
+	}
+	*val = letohw(*val);
+	return 0;
+}
+
+static int asix_mdio_write(usbdev_t *dev, int phy_id, int loc, uint16_t val)
+{
+	val = htolew(val);
+	if (asix_set_sw_mii(dev) ||
+	    asix_ctrl_write(dev, PhyRegWrite, phy_id, loc, sizeof(val), &val) ||
+	    asix_set_hw_mii(dev)) {
+		printf("ASIX: MDIO write failed.\n");
+		return 1;
+	}
+	return 0;
+}
+
+static int mii_nway_restart(usbdev_t *dev, int phy_id)
+{
+	uint16_t bmcr;
+	if (asix_mdio_read(dev, phy_id, MiiBmcr, &bmcr))
+		return 1;
+
+	if (bmcr & BmcrAutoNegEnable) {
+		bmcr |= BmcrRestartAutoNeg;
+		asix_mdio_write(dev, phy_id, MiiBmcr, bmcr);
+		return 0;
+	} else {
+		return 1;
+	}
+}
+
+static int asix_write_medium_mode(usbdev_t *dev, uint16_t mode)
+{
+	if (asix_ctrl_write(dev, MedModeRegWrite, mode, 0, 0, NULL)) {
+		printf("ASIX: Failed to write medium mode %#x.\n", mode);
+		return 1;
+	}
+	return 0;
+}
+
+
+
+/*
+ * The main attraction.
+ */
+
+typedef struct AsixDev {
+	endpoint_t *bulk_in;
+	endpoint_t *bulk_out;
+	uip_eth_addr mac_addr;
+	int phy_id;
+} AsixDev;
+
+static int asix_init(GenericUsbDevice *gen_dev)
+{
+	AsixDev asix_dev;
+	usbdev_t *usb_dev = gen_dev->dev;
+
+	// Map out the endpoints.
+	asix_dev.bulk_in = &usb_dev->endpoints[2];
+	asix_dev.bulk_out = &usb_dev->endpoints[3];
+
+	if (!asix_dev.bulk_in || !asix_dev.bulk_out ||
+			asix_dev.bulk_in->type != BULK ||
+			asix_dev.bulk_out->type != BULK ||
+			asix_dev.bulk_in->direction != IN ||
+			asix_dev.bulk_out->direction != OUT) {
+		printf("ASIX: Problem with the endpoints.\n");
+		return 1;
+	}
+
+	if (asix_write_gpio(usb_dev, GpioGpo2En | GpioGpo2 | GpioRse))
+		return 1;
+
+	mdelay(5);
+
+	int phy_addr = asix_phy_addr(usb_dev);
+	if (phy_addr < 0)
+		return 1;
+	int embed_phy = (phy_addr & 0x1f) == 0x10 ? 1 : 0;
+	if (asix_ctrl_write(usb_dev, SoftPhySelRegWrite, embed_phy, 0, 0,
+			NULL)) {
+		printf("ASIX: Failed to select PHY.\n");
+		return 1;
+	}
+
+	if (asix_sw_reset(usb_dev, SoftResetPrl | SoftResetIppd))
+		return 1;
+	if (asix_sw_reset(usb_dev, 0))
+		return 1;
+	if (asix_sw_reset(usb_dev, embed_phy ? SoftResetIprl : SoftResetPrte))
+		return 1;
+
+	if (asix_write_rx_ctl(usb_dev, 0))
+		return 1;
+
+	asix_dev.phy_id = asix_phy_addr(usb_dev);
+
+	if (asix_sw_reset(usb_dev, SoftResetPrl))
+		return 1;
+	if (asix_sw_reset(usb_dev, SoftResetIprl | SoftResetPrl))
+		return 1;
+
+	if (asix_mdio_write(usb_dev, asix_dev.phy_id, MiiBmcr, BmcrReset))
+		return 1;
+	if (asix_mdio_write(usb_dev, asix_dev.phy_id, MiiAnar,
+			    AdvertiseAll | AdvertiseCsma))
+		return 1;
+	if (mii_nway_restart(usb_dev, asix_dev.phy_id))
+		return 1;
+
+	if (asix_write_medium_mode(usb_dev, MediumDefault))
+		return 1;
+
+	if (asix_ctrl_write(usb_dev, IpgRegWrite, Ipg0Default | Ipg1Default,
+			Ipg2Default, 0, NULL)) {
+		printf("ASIX: Failed to write IPG values.\n");
+		return 1;
+	}
+
+	if (asix_write_rx_ctl(usb_dev, RxCtrlDefault))
+		return 1;
+
+
+	gen_dev->dev_data = malloc(sizeof(AsixDev));
+	if (!gen_dev->dev_data) {
+		printf("ASIX: Failed to allocate AsixDev structure.\n");
+		return 1;
+	}
+	memcpy(gen_dev->dev_data, &asix_dev, sizeof(AsixDev));
+
+	return 0;
+}
+
+int asix_ready(NetDevice *net_dev, int *ready)
+{
+	GenericUsbDevice *gen_dev = (GenericUsbDevice *)net_dev->dev_data;
+	usbdev_t *usb_dev = gen_dev->dev;
+	AsixDev *asix_dev = gen_dev->dev_data;
+
+	uint16_t link_status = 0;
+	if (asix_mdio_read(usb_dev, asix_dev->phy_id, MiiBmsr, &link_status)) {
+		printf("ASIX: Failed to read BMSR.\n");
+		return 1;
+	}
+	*ready = (link_status & BmsrLstatus);
+	return 0;
+}
+
+int asix_send(NetDevice *net_dev, void *buf, uint16_t len)
+{
+	GenericUsbDevice *gen_dev = (GenericUsbDevice *)net_dev->dev_data;
+	usbdev_t *usb_dev = gen_dev->dev;
+	AsixDev *asix_dev = gen_dev->dev_data;
+
+	uint32_t packet_len;
+	static uint8_t msg[UIP_BUFSIZE + sizeof(packet_len)];
+
+	packet_len = ((len << 16) | (len << 0)) ^ 0xffff0000;
+	packet_len = htolel(packet_len);
+	memcpy(msg, &packet_len, sizeof(packet_len));
+	memcpy(msg + sizeof(packet_len), buf, len);
+
+	if (len & 1)
+		len++;
+
+	if (usb_dev->controller->bulk(asix_dev->bulk_out,
+			(len + sizeof(packet_len)) * 2, msg, 0)) {
+		printf("ASIX: Failed to send packet.\n");
+		return 1;
+	}
+
+	return 0;
+}
+
+int asix_recv(NetDevice *net_dev, void *buf, uint16_t *len, int maxlen)
+{
+	GenericUsbDevice *gen_dev = (GenericUsbDevice *)net_dev->dev_data;
+	usbdev_t *usb_dev = gen_dev->dev;
+	AsixDev *asix_dev = gen_dev->dev_data;
+
+	static uint32_t packet_len;
+	static uint8_t msg[RxUrbSize + sizeof(packet_len)];
+	static int offset;
+
+	assert(maxlen < RxUrbSize);
+
+	if (!packet_len) {
+		memset(msg, 0, RxUrbSize);
+		if (usb_dev->controller->bulk(asix_dev->bulk_in,
+				RxUrbSize, msg, 0)) {
+			*len = 0;
+			return 0;
+		}
+		offset = 0;
+		memcpy(&packet_len, msg, sizeof(packet_len));
+	}
+
+	if (!packet_len) {
+		*len = 0;
+		return 0;
+	}
+	*len = (packet_len & 0xffff);
+	packet_len = (packet_len >> 16) ^ 0xffff;
+	if (*len != packet_len) {
+		packet_len = 0;
+		offset = 0;
+		printf("ASIX: Malformed packet length.\n");
+		return 1;
+	}
+	if (packet_len & 1)
+		packet_len++;
+	if (packet_len > maxlen || offset + packet_len > RxUrbSize) {
+		packet_len = 0;
+		offset = 0;
+		printf("ASIX: Packet is too large.\n");
+		return 1;
+	}
+
+	memcpy(buf, msg + offset + sizeof(packet_len), packet_len);
+	offset += sizeof(packet_len) + packet_len;
+	memcpy(&packet_len, msg + offset, sizeof(packet_len));
+
+	return 0;
+}
+
+const uip_eth_addr *asix_get_mac(NetDevice *net_dev)
+{
+	GenericUsbDevice *gen_dev = (GenericUsbDevice *)net_dev->dev_data;
+	usbdev_t *usb_dev = gen_dev->dev;
+	AsixDev *asix_dev = gen_dev->dev_data;
+
+	if (asix_ctrl_read(usb_dev, NodeIdRead, 0, 0, sizeof(uip_eth_addr),
+			&asix_dev->mac_addr)) {
+		printf("ASIX: Failed to retrieve MAC address.\n");
+		return NULL;
+	} else {
+		return &asix_dev->mac_addr;
+	}
+}
+
+
+
+/*
+ * Code to plug the driver into the USB and network stacks.
+ */
+
+static NetDevice asix_network_device = {
+	&asix_ready,
+	&asix_recv,
+	&asix_send,
+	&asix_get_mac,
+	NULL
+};
+
+static const uint16_t AsixVendorId = 0x0b95;
+static const uint16_t AsixProductId = 0x772a;
+
+int asix_probe(GenericUsbDevice *dev)
+{
+	if (asix_network_device.dev_data)
+		return 0;
+
+	device_descriptor_t *dd = (device_descriptor_t *)dev->dev->descriptor;
+	if (dd->idVendor == AsixVendorId && dd->idProduct == AsixProductId) {
+		asix_network_device.dev_data = dev;
+		if (asix_init(dev)) {
+			return 0;
+		} else {
+			net_set_device(&asix_network_device);
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void asix_remove(GenericUsbDevice *dev)
+{
+	if (net_get_device() == &asix_network_device)
+		net_set_device(NULL);
+	free(dev->dev_data);
+}
+
+static GenericUsbDriver asix_usb_driver = {
+	&asix_probe,
+	&asix_remove,
+};
+
+static int asix_driver_register(void)
+{
+	list_insert_after(&asix_usb_driver.list_node, &generic_usb_drivers);
+	return 0;
+}
+
+INIT_FUNC(asix_driver_register);
---
+++ src/drivers/net/asix.h
@@ -0,0 +1,162 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __DRIVERS_NET_ASIX_H__
+#define __DRIVERS_NET_ASIX_H__
+
+enum {
+	SramRead = 0x2,
+	SramWrite = 0x3,
+	SoftSerMgmtCtrlReg = 0x6,
+	PhyRegRead = 0x7,
+	PhyRegWrite = 0x8,
+	SerMgmtStatusReg = 0x9,
+	HardSerMgmtCtrlReg = 0xa,
+	SromReadReg = 0xb,
+	SromWriteReg = 0xc,
+	SromWriteEnableReg = 0xd,
+	SromWriteDisableReg = 0xe,
+	RxCtrlRegRead = 0xf,
+	RxCtrlRegWrite = 0x10,
+	IpgRegRead = 0x11,
+	IpgRegWrite = 0x12,
+	NodeIdRead = 0x13,
+	NodeIdWrite = 0x14,
+	MulticastFilterRegRead = 0x15,
+	MulticastFilterRegWrite = 0x16,
+	TestRegWrite = 0x17,
+	PhyAddrRegRead = 0x19,
+	MedStatusRegRead = 0x1a,
+	MedModeRegWrite = 0x1b,
+	MonModeStatusRegRead = 0x1c,
+	MonModeRegWrite = 0x1d,
+	GpioStatusRegRead = 0x1e,
+	GpioRegWrite = 0x1f,
+	SoftResetRegWrite = 0x20,
+	SoftPhySelStatusRegRead = 0x21,
+	SoftPhySelRegWrite = 0x22
+};
+
+enum {
+	GpioGpo0En = 0x1 << 0,
+	GpioGpo0 = 0x1 << 1,
+	GpioGpo1En = 0x1 << 2,
+	GpioGpo1 = 0x1 << 3,
+	GpioGpo2En = 0x1 << 4,
+	GpioGpo2 = 0x1 << 5,
+	GpioRse = 0x1 << 7
+};
+
+enum {
+	SoftResetRr = 0x1 << 0,
+	SoftResetRt = 0x1 << 1,
+	SoftResetPrte = 0x1 << 2,
+	SoftResetPrl = 0x1 << 3,
+	SoftResetBz = 0x1 << 4,
+	SoftResetIprl = 0x1 << 5,
+	SoftResetIppd = 0x1 << 6
+};
+
+enum {
+	MiiBmcr = 0x0,
+	MiiBmsr = 0x1,
+	MiiPhyIdr1 = 0x2,
+	MiiPhyIdr2 = 0x3,
+	MiiAnar = 0x4,
+	MiiAnlpar = 0x5,
+	MiiAner = 0x6
+};
+
+enum {
+	BmcrCollisionTest = 0x1 << 7,
+	BmcrDuplexMode = 0x1 << 8,
+	BmcrRestartAutoNeg = 0x1 << 9,
+	BmcrIsolate = 0x1 << 10,
+	BmcrPowerDown = 0x1 << 11,
+	BmcrAutoNegEnable = 0x1 << 12,
+	BmcrSpeedSel = 0x1 << 13,
+	BmcrLoopback = 0x1 << 14,
+	BmcrReset = 0x1 << 15
+};
+
+enum {
+	BmsrErcap = 0x1 << 0,
+	BmsrJcd = 0x1 << 1,
+	BmsrLstatus = 0x1 << 2,
+	BmsrAnegCapable = 0x1 << 3,
+	BmsrRfault = 0x1 << 4,
+	BmsrAnegComplete = 0x1 << 5,
+	BmsrEstaten = 0x1 << 8,
+	Bmsr100Half2 = 0x1 << 9,
+	Bmsr100Full2 = 0x1 << 10,
+	Bmsr10Half = 0x1 << 11,
+	Bmsr10Full = 0x1 << 12,
+	Bmsr100Half = 0x1<< 13,
+	Bmsr100Full = 0x1 << 14,
+	Bmsr100Base4 = 0x1 << 15
+};
+
+enum {
+	AdvertiseSelect = 0x1f,
+	AdvertiseCsma = 0x1,
+	Advertise10Half = 0x1 << 5,
+	Advertise10Full = 0x1 << 6,
+	Advertise100Half = 0x1 << 7,
+	Advertise100Full = 0x1 << 8,
+
+	AdvertiseAll = Advertise10Half | Advertise10Full |
+		       Advertise100Half | Advertise100Full
+};
+
+enum {
+	MediumFd = 0x1 << 1,
+	MediumAc = 0x1 << 2,
+	MediumRfc = 0x1 << 4,
+	MediumTfc = 0x1 << 5,
+	MediumJfe = 0x1 << 6,
+	MediumPf = 0x1 << 7,
+	MediumRe = 0x1 << 8,
+	MediumPs = 0x1 << 9,
+	MediumSbp = 0x1 << 11,
+	MediumSm = 0x1 << 12,
+};
+
+static const uint16_t MediumDefault =
+	MediumPs | MediumFd | MediumAc | MediumRfc |
+	MediumTfc | MediumJfe | MediumRe;
+
+static const uint16_t Ipg0Default = 0x15;
+static const uint16_t Ipg1Default = 0x0c;
+static const uint16_t Ipg2Default = 0x12;
+
+enum {
+	RxCtrlSo = 0x1 << 15,
+	RxCtrlAb = 0x1 << 7
+};
+
+static const uint16_t RxCtrlDefault = RxCtrlSo | RxCtrlAb;
+
+enum {
+	RxUrbSize = 2048
+};
+
+#endif /* __DRIVERS_NET_ASIX_H__ */
