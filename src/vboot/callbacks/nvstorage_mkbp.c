bcca9b2ee41f2dce50daa3d266414a435d4eb1cc
Gabe Black <gabeblack@google.com>
2013-02-18 04:38:43 -0800

    depthcharge: Add an MKBP based nvstorage option for vboot.
    One of the ways of storing vboot's non-volatile data is on the ChromeOS EC,
    and that happens to be where it's being stored by the standard Snow firmware.
    This set of callbacks is mostly just a wrapper around commands provided by the
    MKBP driver itself.
    
    BUG=chrome-os-partner:17340
    TEST=Built for and booted into depthcharge on Snow. Saw it attempt to talk to
    the ChromeOS EC. Because there's no I2C driver yet that failed.
    BRANCH=None
    
    Change-Id: Icc0d3a489be827a33bf3918ffbf8e835e28c0dcd
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: https://gerrit-int.chromium.org/32769
    Reviewed-by: Ronald G. Minnich <rminnich@google.com>

---
+++ src/vboot/callbacks/nvstorage_mkbp.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2012 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <libpayload.h>
+#include <vboot_api.h>
+
+#include "drivers/ec/chromeos/mkbp.h"
+
+VbError_t VbExNvStorageRead(uint8_t* buf)
+{
+	if (!mkbp_ptr && !mkbp_init()) {
+		printf("No MKBP device.\n");
+		return VBERROR_UNKNOWN;
+	}
+
+	if (mkbp_read_vbnvcontext(mkbp_ptr, buf))
+		return VBERROR_UNKNOWN;
+
+	return VBERROR_SUCCESS;
+}
+
+VbError_t VbExNvStorageWrite(const uint8_t* buf)
+{
+	if (!mkbp_ptr && !mkbp_init()) {
+		printf("No MKBP device.\n");
+		return VBERROR_UNKNOWN;
+	}
+
+	if (mkbp_write_vbnvcontext(mkbp_ptr, buf))
+		return VBERROR_UNKNOWN;
+
+	return VBERROR_SUCCESS;
+}
