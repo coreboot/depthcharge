261ede5d4d30965697f2381762d440d38e42113c
Gabe Black <gabeblack@google.com>
2013-02-26 11:10:36 -0800

    depthcharge: Add a trampoline entry point for ARM.
    Without an entry point, the trampoline code was being garbage collected by the
    linker entirely, and when adding the empty region it would occupy to the code
    that tracks what memory is unused it triggered an assert. This code is not
    functionally tested yet.
    
    BUG=chrome-os-partner:17340
    TEST=Built and booted into depthcharge on Snow. With this change, saw an
    assert about an empty range go away. Checked the sections in the depthcharge
    ELF before and after this change. There were .tramp sections afterwords but
    not before.
    BRANCH=None
    
    Change-Id: Ica110558adb64c379bad2f01a63e92c2b5780f2d
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: https://gerrit-int.chromium.org/32955
    Reviewed-by: Stefan Reinauer <reinauer@google.com>

---
+++ src/arch/arm/trampoline.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <stdint.h>
+
+#include "base/elf.h"
+#include "image/symbols.h"
+#include "image/trampoline.h"
+
+uint8_t trampoline_stack[16384] __attribute__((aligned(16)));
+
+void ENTRY trampoline(Elf32_Ehdr *ehdr)
+{
+	void *new_stack = trampoline_stack + sizeof(trampoline_stack) -
+		sizeof(uint32_t);
+	__asm__ __volatile__(
+		"mov sp, %[new_stack]\n"
+		"mov r0, %[ehdr]\n"
+		"b load_elf\n"
+		:: [new_stack]"r"(new_stack), [ehdr]"r"(ehdr)
+		: "memory"
+	);
+}
