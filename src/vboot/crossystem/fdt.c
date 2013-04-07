8dd25883c8f330285d082c2e6fd992cb2b8b2a98
Gabe Black <gabeblack@chromium.org>
2013-04-07 02:02:26 -0700

    ARM: Add the crossystem data to the kernel's device tree when booting on ARM.
    This functionality was currently stubbed out and not implemented. Several
    values that are included in U-Boot are excluded here because the crossystem
    utility doesn't look at them.
    
    The ddr-type property is not set despite the fact that U-Boot sets it and
    crossystem reads it. There are two main reasons for that. First, the existence
    of the value is board specific. Support for it should go in a board specific
    place and not in the generic code added here.
    
    Second, there's no reason for the firmware to have to prepare this value. The
    different ddr types are selected based on whether certain resistors are
    stuffed. Since the very existence of this field is system specific, similarly
    system specific code could be added to the kernel, to crossystem, or to some
    unrelated utility to read the GPIOs those resistors are connected to and
    determine the ddr type.
    
    BUG=chrome-os-partner:18639
    TEST=Built and booted into ChromeOS on Snow. Ran crossystem and verified that
    the same values were successfully retrieved as under U-Boot. Verified that
    most values made sense, or at least were being passed through to crossystem in
    a sensible way. One value in particular, the write protect value read by
    coreboot, seems to be wrong. I believe that's because Coreboot is reporting it
    incorrectly.
    BRANCH=None
    
    Change-Id: I870aeeec15e82b59851d41a95e6ba9abffab38f6
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/47542
    Reviewed-by: Ronald G. Minnich <rminnich@chromium.org>
    Tested-by: Gabe Black <gabeblack@chromium.org>
    Commit-Queue: Gabe Black <gabeblack@chromium.org>

--- src/vboot/crossystem/fdt.c
+++ src/vboot/crossystem/fdt.c
@@ -20,11 +20,192 @@
  * MA 02111-1307 USA
  */
 
-#include "base/device_tree.h"
-#include "vboot/crossystem/crossystem.h"
+#include <assert.h>
+#include <endian.h>
+#include <gbb_header.h>
+#include <stdlib.h>
+#include <string.h>
+#include <vboot_api.h>
+#include <vboot_struct.h>
 
-static int install_crossystem_data(DeviceTreeFixup *fixup, DeviceTree *data)
+#include "base/device_tree.h"
+#include "config.h"
+#include "image/fmap.h"
+#include "vboot/crossystem/crossystem.h"
+#include "vboot/util/commonparams.h"
+#include "vboot/util/flag.h"
+
+enum {
+	VDAT_RW_A = 0,
+	VDAT_RW_B = 1,
+	VDAT_RECOVERY = 0xFF
+};
+
+static void bin_prop(ListNode *props, ListNode *old_props,
+		     char *name, void *data, int size)
 {
+	DeviceTreeProperty *prop;
+
+	// Check to see if the kernel already had a property with this name.
+	// To avoid unnecessary overhead, only look through properties that
+	// were already there under the assumption we won't be trying to
+	// overwrite something we just added.
+	if (old_props) {
+		list_for_each(prop, *old_props, list_node) {
+			if (!strcmp(prop->prop.name, name)) {
+				prop->prop.data = data;
+				prop->prop.size = size;
+				return;
+			}
+		}
+	}
+
+	prop = dt_new_property();
+	list_insert_after(&prop->list_node, props);
+	prop->prop.name = name;
+	prop->prop.data = data;
+	prop->prop.size = size;
+}
+
+static void string_prop(ListNode *props, ListNode *old_props,
+			char *name, char *str)
+{
+	bin_prop(props, old_props, name, str, strlen(str) + 1);
+}
+
+static void int_prop(ListNode *props, ListNode *old_props,
+		     char *name, uint32_t val)
+{
+	uint32_t *val_ptr = malloc(sizeof(val));
+	assert(val_ptr);
+	*val_ptr = htobel(val);
+	bin_prop(props, old_props, name, val_ptr, sizeof(*val_ptr));
+}
+
+static DeviceTreeNode *dt_find_chromeos_node(DeviceTree *tree)
+{
+	DeviceTreeNode *node;
+	DeviceTreeNode *firmware = NULL;
+	DeviceTreeNode *chromeos = NULL;
+
+	// Find the /firmware node, if one exists.
+	list_for_each(node, tree->root->children, list_node) {
+		if (!strcmp(node->name, "firmware")) {
+			firmware = node;
+			break;
+		}
+	}
+
+	// Make one if it didn't.
+	if (!firmware) {
+		firmware = dt_new_node();
+		node->name = "firmware";
+		list_insert_after(&firmware->list_node, &tree->root->children);
+	}
+
+	// Find the /firmware/chromeos node, if one exists
+	list_for_each(node, firmware->children, list_node) {
+		if (!strcmp(node->name, "chromeos")) {
+			chromeos = node;
+			break;
+		}
+	}
+
+	// Make one if it didn't.
+	if (!chromeos) {
+		chromeos = dt_new_node();
+		node->name = "chromeos";
+		list_insert_after(&chromeos->list_node, &firmware->children);
+	}
+
+	return chromeos;
+}
+
+static int install_crossystem_data(DeviceTreeFixup *fixup, DeviceTree *tree)
+{
+	DeviceTreeNode *chromeos = dt_find_chromeos_node(tree);
+
+	ListNode *props = &chromeos->properties;
+	ListNode *old = chromeos->properties.next;
+
+	string_prop(props, old, "compatible", "chromeos-firmware");
+
+	void *blob;
+	int size;
+	if (find_common_params(&blob, &size))
+		return 1;
+	bin_prop(props, old, "vboot-shared-data", blob, size);
+	VbSharedDataHeader *vdat = (VbSharedDataHeader *)blob;
+
+	if (CONFIG_NV_STORAGE_CMOS) {
+		string_prop(props, old, "nonvolatile-context-storage", "nvram");
+	} else if (CONFIG_NV_STORAGE_MKBP) {
+		string_prop(props, old, "nonvolatile-context-storage", "mkbp");
+	} else if (CONFIG_NV_STORAGE_DISK) {
+		string_prop(props, old, "nonvolatile-context-storage", "disk");
+		int_prop(props, old, "nonvolatile-context-lba",
+			CONFIG_NV_STORAGE_DISK_LBA);
+		int_prop(props, old, "nonvolatile-context-offset",
+			CONFIG_NV_STORAGE_DISK_OFFSET);
+		int_prop(props, old, "nonvolatile-context-size",
+			CONFIG_NV_STORAGE_DISK_SIZE);
+	}
+
+	int recovery = 0;
+	const char *fwid;
+	int fwid_size;
+	switch (vdat->firmware_index) {
+	case VDAT_RW_A:
+		fwid = fmap_rwa_fwid();
+		fwid_size = fmap_rwa_fwid_size();
+		break;
+	case VDAT_RW_B:
+		fwid = fmap_rwb_fwid();
+		fwid_size = fmap_rwb_fwid_size();
+		break;
+	case VDAT_RECOVERY:
+		recovery = 1;
+		fwid = fmap_ro_fwid();
+		fwid_size = fmap_ro_fwid_size();
+		break;
+	default:
+		printf("Unrecognized firmware index %d.\n",
+		       vdat->firmware_index);
+		return 1;
+	}
+	bin_prop(props, old, "firmware-version", (char *)fwid, fwid_size);
+
+	if (recovery)
+		string_prop(props, old, "firmware-type", "recovery");
+	else if (vdat->flags & VBSD_BOOT_DEV_SWITCH_ON)
+		string_prop(props, old, "firmware-type", "developer");
+	else
+		string_prop(props, old, "firmware-type", "normal");
+
+	int_prop(props, old, "fmap-offset", CONFIG_FMAP_OFFSET);
+
+	bin_prop(props, old, "readonly-firmware-version",
+		 (char *)fmap_ro_fwid(), fmap_ro_fwid_size());
+
+	GoogleBinaryBlockHeader *gbb = cparams.gbb_data;
+	if (memcmp(gbb->signature, GBB_SIGNATURE, GBB_SIGNATURE_SIZE)) {
+		printf("Bad signature on GBB.\n");
+		return 1;
+	}
+	char *hwid = (char *)((uintptr_t)cparams.gbb_data + gbb->hwid_offset);
+	bin_prop(props, old, "hardware-id", hwid, gbb->hwid_size);
+
+	if (CONFIG_EC_SOFTWARE_SYNC) {
+		int in_rw = 0;
+
+		if (VbExEcRunningRW(&in_rw)) {
+			printf("Couldn't tell if the EC firmware is RW.\n");
+			return 1;
+		}
+		string_prop(props, old, "active-ec-firmware",
+			    in_rw ? "RW" : "RO");
+	}
+
 	return 0;
 }
 
