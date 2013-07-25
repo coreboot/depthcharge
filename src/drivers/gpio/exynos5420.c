434f46777e65db2e362259d787b8c8fc85ac050c
Gabe Black <gabeblack@chromium.org>
2013-07-25 11:08:59 -0700

    gpio: Add a driver for exynos5420 gpios.
    BUG=chrome-os-partner:19420
    TEST=Built and booted on pit with this driver turned on and the EC-in-RW gpio
    configured.
    BRANCH=None
    
    Change-Id: Ib2daeccf6972a96692d089225049b0f3d48758d5
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/63428
    Reviewed-by: David Hendricks <dhendrix@chromium.org>
    Commit-Queue: Gabe Black <gabeblack@chromium.org>
    Tested-by: Gabe Black <gabeblack@chromium.org>

---
+++ src/drivers/gpio/exynos5420.c
@@ -0,0 +1,226 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <assert.h>
+#include <libpayload.h>
+#include <stdint.h>
+
+#include "base/list.h"
+#include "drivers/gpio/gpio.h"
+#include "drivers/gpio/exynos5420.h"
+
+typedef struct __attribute__((packed)) GpioRegs
+{
+	uint32_t con;
+	uint32_t dat;
+	uint32_t pud;
+	uint32_t drv;
+	uint32_t conpdn;
+	uint32_t pudpdn;
+} GpioRegs;
+
+typedef struct GpioBank
+{
+	int num_bits;
+	GpioRegs *regs;
+} GpioBank;
+
+typedef struct GpioGroup
+{
+	int num_banks;
+	GpioBank *banks[8];
+} GpioGroup;
+
+static GpioBank bank_a0 = { 8, (void *)(uintptr_t)0x14010000 };
+static GpioBank bank_a1 = { 6, (void *)(uintptr_t)0x14010020 };
+static GpioBank bank_a2 = { 8, (void *)(uintptr_t)0x14010040 };
+
+static GpioBank bank_b0 = { 5, (void *)(uintptr_t)0x14010060 };
+static GpioBank bank_b1 = { 5, (void *)(uintptr_t)0x14010080 };
+static GpioBank bank_b2 = { 4, (void *)(uintptr_t)0x140100a0 };
+static GpioBank bank_b3 = { 8, (void *)(uintptr_t)0x140100c0 };
+static GpioBank bank_b4 = { 2, (void *)(uintptr_t)0x140100e0 };
+
+static GpioBank bank_c0 = { 8, (void *)(uintptr_t)0x13410000 };
+static GpioBank bank_c1 = { 8, (void *)(uintptr_t)0x13410020 };
+static GpioBank bank_c2 = { 7, (void *)(uintptr_t)0x13410040 };
+static GpioBank bank_c3 = { 4, (void *)(uintptr_t)0x13410060 };
+static GpioBank bank_c4 = { 2, (void *)(uintptr_t)0x13410080 };
+
+static GpioBank bank_d1 = { 8, (void *)(uintptr_t)0x134100a0 };
+
+static GpioBank bank_e0 = { 8, (void *)(uintptr_t)0x14000000 };
+static GpioBank bank_e1 = { 2, (void *)(uintptr_t)0x14000020 };
+
+static GpioBank bank_f0 = { 6, (void *)(uintptr_t)0x14000040 };
+static GpioBank bank_f1 = { 8, (void *)(uintptr_t)0x14000060 };
+
+static GpioBank bank_g0 = { 8, (void *)(uintptr_t)0x14000080 };
+static GpioBank bank_g1 = { 8, (void *)(uintptr_t)0x140000a0 };
+static GpioBank bank_g2 = { 2, (void *)(uintptr_t)0x140000c0 };
+
+static GpioBank bank_h0 = { 8, (void *)(uintptr_t)0x14010100 };
+
+static GpioBank bank_j4 = { 4, (void *)(uintptr_t)0x140000e0 };
+
+static GpioBank bank_x0 = { 8, (void *)(uintptr_t)0x13400c00 };
+static GpioBank bank_x1 = { 8, (void *)(uintptr_t)0x13400c20 };
+static GpioBank bank_x2 = { 8, (void *)(uintptr_t)0x13400c40 };
+static GpioBank bank_x3 = { 8, (void *)(uintptr_t)0x13400c60 };
+
+static GpioBank bank_y0 = { 6, (void *)(uintptr_t)0x134100c0 };
+static GpioBank bank_y1 = { 4, (void *)(uintptr_t)0x134100e0 };
+static GpioBank bank_y2 = { 6, (void *)(uintptr_t)0x13410100 };
+static GpioBank bank_y3 = { 8, (void *)(uintptr_t)0x13410120 };
+static GpioBank bank_y4 = { 8, (void *)(uintptr_t)0x13410140 };
+static GpioBank bank_y5 = { 8, (void *)(uintptr_t)0x13410160 };
+static GpioBank bank_y6 = { 8, (void *)(uintptr_t)0x13410180 };
+static GpioBank bank_y7 = { 8, (void *)(uintptr_t)0x13400000 };
+
+static GpioBank bank_z =  { 7, (void *)(uintptr_t)0x03860000 };
+
+GpioGroup groups[] = {
+	[GPIO_A] = { 3, { &bank_a0, &bank_a1, &bank_a2 }},
+	[GPIO_B] = { 5, { &bank_b0, &bank_b1, &bank_b2, &bank_b3, &bank_b4 }},
+	[GPIO_C] = { 5, { &bank_c0, &bank_c1, &bank_c2, &bank_c3, &bank_c4 }},
+	[GPIO_D] = { 2, { NULL,     &bank_d1 }},
+	[GPIO_E] = { 2, { &bank_e0, &bank_e1 }},
+	[GPIO_F] = { 2, { &bank_f0, &bank_f1 }},
+	[GPIO_G] = { 3, { &bank_g0, &bank_g1, &bank_g2 }},
+	[GPIO_H] = { 1, { &bank_h0, }},
+	[GPIO_J] = { 5, { NULL,     NULL,     NULL,     NULL,     &bank_j4 }},
+	[GPIO_X] = { 4, { &bank_x0, &bank_x1, &bank_x2, &bank_x3 }},
+	[GPIO_Y] = { 8, { &bank_y0, &bank_y1, &bank_y2, &bank_y3, &bank_y4,
+			  &bank_y5, &bank_y6, &bank_y7 }},
+	[GPIO_Z] = { 1, { &bank_z }}
+};
+
+static GpioRegs *exynos5420_get_regs(Exynos5420Gpio *gpio)
+{
+	GpioGroup *group = &groups[gpio->group];
+	GpioBank *bank = group->banks[gpio->bank];
+	return bank->regs;
+}
+
+static int exynos5420_gpio_use(Exynos5420Gpio *me, unsigned use)
+{
+	GpioRegs *regs = exynos5420_get_regs(me);
+
+	uint32_t con = readl(&regs->con);
+	con &= ~(0xf << (me->bit * 4));
+	con |= ((use & 0xf) << (me->bit * 4));
+	writel(con, &regs->con);
+
+	return 0;
+}
+
+static int exynos5420_gpio_set_pud(Exynos5420Gpio *me, unsigned value)
+{
+	GpioRegs *regs = exynos5420_get_regs(me);
+
+	uint32_t pud = readl(&regs->pud);
+	pud &= ~(0x3 << (me->bit * 2));
+	pud |= ((value & 0x3) << (me->bit * 2));
+	writel(pud, &regs->pud);
+
+	return 0;
+}
+
+static int exynos5420_gpio_get_value(GpioOps *me)
+{
+	assert(me);
+	Exynos5420Gpio *gpio = container_of(me, Exynos5420Gpio, ops);
+	GpioRegs *regs = exynos5420_get_regs(gpio);
+
+	if (!gpio->dir_set) {
+		if (gpio->use(gpio, 0))
+			return -1;
+		gpio->dir_set = 1;
+	}
+
+	return (readl(&regs->dat) >> gpio->bit) & 0x1;
+}
+
+static int exynos5420_gpio_set_value(GpioOps *me, unsigned value)
+{
+	assert(me);
+	Exynos5420Gpio *gpio = container_of(me, Exynos5420Gpio, ops);
+	GpioRegs *regs = exynos5420_get_regs(gpio);
+
+	if (!gpio->dir_set) {
+		if (gpio->use(gpio, 1))
+			return -1;
+		gpio->dir_set = 1;
+	}
+
+	uint32_t dat = readl(&regs->dat);
+	dat &= ~(0x1 << gpio->bit);
+	dat |= ((value & 0x1) << gpio->bit);
+	writel(dat, &regs->dat);
+	return 0;
+}
+
+Exynos5420Gpio *new_exynos5420_gpio(unsigned group, unsigned bank, unsigned bit)
+{
+	if (group >= ARRAY_SIZE(groups) || bank >= groups[group].num_banks ||
+			groups[group].banks[bank] == NULL ||
+			bit >= groups[group].banks[bank]->num_bits) {
+		printf("GPIO parameters (%d, %d, %d) out of bounds.\n",
+			group, bank, bit);
+		return NULL;
+	}
+
+	Exynos5420Gpio *gpio = malloc(sizeof(*gpio));
+	if (!gpio) {
+		printf("Failed to allocate exynos 5420 gpio object.\n");
+		return NULL;
+	}
+	memset(gpio, 0, sizeof(*gpio));
+	gpio->use = &exynos5420_gpio_use;
+	gpio->pud = &exynos5420_gpio_set_pud;
+	gpio->group = group;
+	gpio->bank = bank;
+	gpio->bit = bit;
+	return gpio;
+}
+
+Exynos5420Gpio *new_exynos5420_gpio_input(unsigned group, unsigned bank,
+					  unsigned bit)
+{
+	Exynos5420Gpio *gpio = new_exynos5420_gpio(group, bank, bit);
+	if (!gpio)
+		return NULL;
+
+	gpio->ops.get = &exynos5420_gpio_get_value;
+	return gpio;
+}
+
+Exynos5420Gpio *new_exynos5420_gpio_output(unsigned group, unsigned bank,
+					   unsigned bit)
+{
+	Exynos5420Gpio *gpio = new_exynos5420_gpio(group, bank, bit);
+	if (!gpio)
+		return NULL;
+
+	gpio->ops.set = &exynos5420_gpio_set_value;
+	return gpio;
+}
---
+++ src/drivers/gpio/exynos5420.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __DRIVERS_GPIO_EXYNOS5420_H__
+#define __DRIVERS_GPIO_EXYNOS5420_H__
+
+#include "drivers/gpio/gpio.h"
+
+enum {
+	GPIO_A,
+	GPIO_B,
+	GPIO_C,
+	GPIO_D,
+	GPIO_E,
+	GPIO_F,
+	GPIO_G,
+	GPIO_H,
+	GPIO_J,
+	GPIO_X,
+	GPIO_Y,
+	GPIO_Z
+};
+
+typedef struct Exynos5420Gpio
+{
+	GpioOps ops;
+	int (*use)(struct Exynos5420Gpio *me, unsigned use);
+	int (*pud)(struct Exynos5420Gpio *me, unsigned value);
+	int dir_set;
+	unsigned group;
+	unsigned bank;
+	unsigned bit;
+} Exynos5420Gpio;
+
+Exynos5420Gpio *new_exynos5420_gpio(unsigned group, unsigned bank,
+				    unsigned bit);
+Exynos5420Gpio *new_exynos5420_gpio_input(unsigned group, unsigned bank,
+					  unsigned bit);
+Exynos5420Gpio *new_exynos5420_gpio_output(unsigned group, unsigned bank,
+					   unsigned bit);
+
+#endif /* __DRIVERS_GPIO_EXYNOS5420_H__ */
