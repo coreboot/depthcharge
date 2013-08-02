9a3fbc9e7b8df9124f7baa80fd7abc5870429ad6
Gabe Black <gabeblack@chromium.org>
2013-08-02 14:16:13 -0700

    exynos5: Generalize how graphics are disabled.
    The old implementation turned off the FIMD DMA by disabling window 0, but
    other boards like pit use window 3. Rather than turning off a particular
    window, we should disable video output at all.
    
    BUG=chrome-os-partner:19420
    TEST=Built and booted on snow and saw video from the kernel and no crash due
    to the sysmmu. Reproduced the sysmmu panic on pit and verified that this
    change makes it go away.
    BRANCH=None
    
    Change-Id: I6f6fc57ca3be638b31edf97671211b788ef58cfc
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: https://gerrit.chromium.org/gerrit/64384
    Reviewed-by: Ronald G. Minnich <rminnich@chromium.org>
    Tested-by: Gabe Black <gabeblack@chromium.org>
    Commit-Queue: Gabe Black <gabeblack@chromium.org>

--- src/drivers/video/exynos5.c
+++ src/drivers/video/exynos5.c
@@ -27,54 +27,19 @@
 #include "base/init_funcs.h"
 #include "config.h"
 
-// An incomplete FIMD register map.
-typedef struct Exynos5Fimd {
-	uint32_t vidcon0;
-	uint8_t _1[0x1c];
-	uint32_t wincon0;
-	uint32_t wincon1;
-	uint32_t wincon2;
-	uint32_t wincon3;
-	uint32_t wincon4;
-	uint32_t shadowcon;
-	uint8_t _2[0x8];
-	uint32_t vidosd0a;
-	uint32_t vidosd0b;
-	uint32_t vidosd0c;
-	uint8_t _3[0x54];
-	uint32_t vidw00add0b0;
-	uint8_t _4[0x2c];
-	uint32_t vidw00add1b0;
-	uint8_t _5[0x2c];
-	uint32_t vidw00add2;
-	uint8_t _6[0x3c];
-	uint32_t w1keycon0;
-	uint32_t w1keycon1;
-	uint32_t w2keycon0;
-	uint32_t w2keycon1;
-	uint32_t w3keycon0;
-	uint32_t w3keycon1;
-	uint32_t w4keycon0;
-	uint32_t w4keycon1;
-	uint8_t _7[0x20];
-	uint32_t win0map;
-	uint8_t _8[0xdc];
-	uint32_t blendcon;
-	uint8_t _9[0x18];
-	uint32_t dpclkcon;
-} Exynos5Fimd;
-
 enum {
-	Channel0En = 1 << 0
+	FIMD_VIDCON0_ENVID = 2,
+	FIMD_VIDCON0_ENVID_F = 1
 };
 
 int disable_fimd_dma(struct CleanupFunc *cleanup, CleanupType type)
 {
-	Exynos5Fimd *fimd = (Exynos5Fimd *)cleanup->data;
+	// The vidcon0 register is at the start of the register block.
+	uint32_t *fimd_vidcon0 = (uint32_t *)cleanup->data;
 
-	writel(0, &fimd->wincon0);
-	uint32_t shadowcon = readl(&fimd->shadowcon);
-	writel(shadowcon & ~Channel0En, &fimd->shadowcon);
+	uint32_t vidcon0 = readl(fimd_vidcon0);
+	vidcon0 &= ~(FIMD_VIDCON0_ENVID | FIMD_VIDCON0_ENVID_F);
+	writel(vidcon0, fimd_vidcon0);
 
 	return 0;
 }
