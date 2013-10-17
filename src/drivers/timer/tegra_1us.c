dae4bbc4b1018ddc8940f833accbfa8157524d22
Gabe Black <gabeblack@google.com>
2013-10-17 04:00:10 -0700

    tegra: Add a driver for the tegra 1us timer.
    We should use this as the system timer on tegra machines like nyan.
    
    BUG=None
    TEST=Built and booted on nyan with this driver enabled and saw that timer_us
    no longer caused a divide by zero error.
    BRANCH=None
    
    Change-Id: I45886f762ae3c1c09be565bc78ea796338fc9623
    Signed-off-by: Gabe Black <gabeblack@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/173493
    Reviewed-by: Ronald Minnich <rminnich@chromium.org>
    Tested-by: Ronald Minnich <rminnich@chromium.org>
    Commit-Queue: Ronald Minnich <rminnich@chromium.org>

---
+++ src/drivers/timer/tegra_1us.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <config.h>
+#include <libpayload.h>
+
+#include "drivers/timer/timer.h"
+
+static uint32_t * const tegra_tmrus =
+	(void *)CONFIG_DRIVER_TIMER_TEGRA_1US_ADDRESS;
+
+uint64_t timer_hz(void)
+{
+	return 1000 * 1000;
+}
+
+uint64_t timer_raw_value(void)
+{
+	return readl(tegra_tmrus);
+}
