dbddb7395deae6b1c968af98efcec0e881e47270
Shawn Nematbakhsh <shawnn@chromium.org>
2013-10-18 11:10:09 -0700

    bayleybay: Update board config
    - Update bayleybay board config for MOCK_TPM
    - Add board.c to init flash
    
    TEST=Manual. Boot to vboot handoff.
    BUG=chrome-os-partner:23326
    
    Change-Id: Iab621c088eb151261493c8d52535d0d118057dbb
    Reviewed-on: https://chromium-review.googlesource.com/173713
    Tested-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Reviewed-by: Gabe Black <gabeblack@chromium.org>
    Commit-Queue: David James <davidjames@chromium.org>

---
+++ src/board/bayleybay/board.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include "base/init_funcs.h"
+#include "drivers/flash/flash.h"
+#include "drivers/flash/memmapped.h"
+#include "drivers/power/pch.h"
+
+static int board_setup(void)
+{
+	MemMappedFlash *flash = new_mem_mapped_flash(0xff800000, 0x800000);
+	if (!flash || flash_set_ops(&flash->ops))
+		return 1;
+
+	if (power_set_ops(&pch_power_ops))
+		return 1;
+
+	return 0;
+}
+
+INIT_FUNC(board_setup);
