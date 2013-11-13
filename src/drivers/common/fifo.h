11e33be283699d5407847a8943aa4c1c0761f1f6
Hung-Te Lin <hungte@chromium.org>
2013-11-13 12:23:18 +0800

    drivers: Add a general FIFO structure for drivers.
    Adding TxFifoOps and RxFifoOps for drivers implementing (or using) FIFO
    peripherals, ex I2S.
    
    BUG=none
    TEST=emerge-nyan depthcharge
    BRANCH=none
    
    Change-Id: Ib4999abc359f8516a22c6f954adbf998e6bb2bd7
    Reviewed-on: https://chromium-review.googlesource.com/176620
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Gabe Black <gabeblack@chromium.org>
    Commit-Queue: Hung-Te Lin <hungte@chromium.org>

---
+++ src/drivers/common/fifo.h
@@ -0,0 +1,59 @@
+/*
+ * This file is part of the depthcharge project.
+ *
+ * Copyright 2013 Google Inc.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA, 02110-1301 USA
+ */
+
+#ifndef __DRIVERS_COMMON_FIFO_H__
+#define __DRIVERS_COMMON_FIFO_H__
+
+#include <stdint.h>
+
+typedef struct TxFifoOps {
+
+	// Sends arbitrary bytes into FIFO.
+	// Returns the actual bytes sent, or -1 on error.
+	ssize_t (*send)(struct TxFifoOps *me, const void *buf, size_t len);
+
+	// Returns 1 if the FIFO is full, otherwise 0.
+	int (*is_full)(struct TxFifoOps *me);
+
+	// (Optional) Returns the total capacity of FIFO in bytes.
+	size_t (*capacity)(struct TxFifoOps *me);
+
+	// (Optional) Returns the current size of data already in FIFO.
+	size_t (*size)(struct TxFifoOps *me);
+
+} TxFifoOps;
+
+typedef struct RxFifoOps {
+
+	// Receives arbitrary bytes from FIFO.
+	// Returns the actual bytes received, or -1 on error.
+	ssize_t (*recv)(struct RxFifoOps *me, void *buf, size_t len);
+
+	// Returns 1 if the FIFO is empty, otherwise 0.
+	int (*is_empty)(struct RxFifoOps *me);
+
+	// (Optional) Returns the total capacity of FIFO in bytes.
+	size_t (*capacity)(struct RxFifoOps *me);
+
+	// (Optional) Returns the current size of data available in FIFO.
+	size_t (*size)(struct RxFifoOps *me);
+
+} RxFifoOps;
+
+#endif /* __DRIVERS_COMMON_FIFO_H__ */
