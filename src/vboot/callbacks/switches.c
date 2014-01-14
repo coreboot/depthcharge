1592e53b3568308f922b6247e58e7fcd5df9368b
Luigi Semenzato <semenzato@chromium.org>
2014-01-13 18:29:10 -0800

    depthcharge: add functions to improve dev mode switch security
    Add VbExKeyboardReadWithFlags() and VbExGetSwitches()
    and read the recovery button state directly from hardware.
    Also pass the VB_INIT_FLAG_VIRTUAL_REC_SWITCH at init
    as appropriate.
    
    These changes make it possible for vboot to avoid some
    dangerous opportunistic exploits that could put the device
    in developer mode without the owner intending to do so.
    
    BUG=chrome-os-partner:21729
    TEST=compiles
    BRANCH=none
    CQ-DEPEND=CL:182241,CL:182946,CL:182357
    
    Change-Id: Ied1e59684517c0125be029f575be282fdb2db8a3
    Reviewed-on: https://chromium-review.googlesource.com/182420
    Reviewed-by: Luigi Semenzato <semenzato@chromium.org>
    Tested-by: Luigi Semenzato <semenzato@chromium.org>
    Commit-Queue: Luigi Semenzato <semenzato@chromium.org>
    (cherry picked from commit 5cef802050cabec41c719b7b0e2bb116432c1a4c)
    Reviewed-on: https://chromium-review.googlesource.com/261603
    Commit-Queue: Bernie Thompson <bhthompson@chromium.org>
    Tested-by: Bernie Thompson <bhthompson@chromium.org>

---
+++ src/vboot/callbacks/switches.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <libpayload.h>
+#include <vboot_api.h>
+#include <vboot/util/flag.h>
+
+/*
+ * Return the state of the switches specified in request_mask.
+ * TODO(semenzato): find a better interface than the INIT_FLAGS.
+ */
+uint32_t VbExGetSwitches(uint32_t request_mask)
+{
+	uint32_t result = 0;
+
+	if ((request_mask & VB_INIT_FLAG_DEV_SWITCH_ON) &&
+            flag_fetch(FLAG_DEVSW))
+		result |= VB_INIT_FLAG_DEV_SWITCH_ON;
+
+	if ((request_mask & VB_INIT_FLAG_REC_BUTTON_PRESSED) &&
+	    flag_fetch(FLAG_RECSW))
+		result |= VB_INIT_FLAG_REC_BUTTON_PRESSED;
+
+	if ((request_mask & VB_INIT_FLAG_WP_ENABLED) &&
+	    flag_fetch(FLAG_WPSW))
+		result |= VB_INIT_FLAG_WP_ENABLED;
+
+	return result;
+}
