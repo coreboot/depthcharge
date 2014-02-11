54a9fcec3f85be9fd6b8702296b85688e25f5402
Shawn Nematbakhsh <shawnn@chromium.org>
2014-02-11 14:45:43 -0800

    clapper/glimmer/rambi/squawks: Lookup I2C port base address from gnvs table
    Don't use a fixed I2C base address since it's subject to change.
    
    BUG=chrome-os-partner:24309
    TEST=Manual on Rambi. Boot FW in dev mode, verify beep is heard.
    BRANCH=Rambi.
    
    Change-Id: I776a1740b47f0e56509694968c7fc46861d547ac
    Original-Change-Id: Ife0a67278bc07c1a4e2b858905c0c87068894b5b
    Reviewed-on: https://chromium-review.googlesource.com/186136
    Reviewed-by: Shawn Nematbakhsh <shawnn@chromium.org>
    Commit-Queue: Shawn Nematbakhsh <shawnn@chromium.org>
    Tested-by: Shawn Nematbakhsh <shawnn@chromium.org>

--- src/board/clapper/board.c
+++ src/board/clapper/board.c
@@ -49,7 +49,8 @@ static const int emmc_clock_max = 200 * 1000 * 1000;
 
 static int board_setup(void)
 {
-        if (sysinfo_install_flags())
+	device_nvs_t *nvs = lib_sysinfo.acpi_gnvs + DEVICE_NVS_OFFSET;
+	if (sysinfo_install_flags())
 		return 1;
 
 	/* ECRW GPIO: SCGPIO59 */
@@ -65,12 +66,16 @@ static int board_setup(void)
 	if (flash_set_ops(&flash->ops))
 		return 1;
 
+	/* Setup sound components */
 	pcidev_t lpe_pcidev = PCI_DEV(0, 0x15, 0);
 	BytI2s *i2s = new_byt_i2s(
 		lpe_pcidev, &baytrail_max98090_settings, 16, 2, 4800000, 48000);
 	I2sSource *i2s_source = new_i2s_source(&i2s->ops, 48000, 2, 16000);
 	SoundRoute *sound_route = new_sound_route(&i2s_source->ops);
-	DesignwareI2c *i2c = new_designware_i2c(0xd0824000, 400000);
+
+	die_if(!nvs->lpss_en[LPSS_NVS_I2C2], "Codec I2C misconfigured\n");
+	DesignwareI2c *i2c = new_designware_i2c(
+		nvs->lpss_bar0[LPSS_NVS_I2C2], 400000);
 	Max98090Codec *codec = new_max98090_codec(
 		&i2c->ops, 0x10, 16, 48000, 400, 1);
 	list_insert_after(&codec->component.list_node,
@@ -86,7 +91,6 @@ static int board_setup(void)
 
 	/* Initialize eMMC port in ACPI or PCI mode */
 	SdhciHost *emmc;
-	device_nvs_t *nvs = lib_sysinfo.acpi_gnvs + DEVICE_NVS_OFFSET;
 	if (nvs->scc_en[SCC_NVS_MMC])
 		emmc = new_mem_sdhci_host((void *)nvs->scc_bar0[SCC_NVS_MMC],
 					  0, emmc_clock_min, emmc_clock_max);
