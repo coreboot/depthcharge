1b62833d391fb8880711354b3119460452e30976
ray <ray.liao@quantatw.com>
2014-05-09 19:36:59 +0800

    Tricky: Initial tricky depthcharge
    BUG=chrome-os-partner:28702
    BRANCH=tricky
    TEST=emerge-tricky depthcharge
    
    Change-Id: Ib9bf64cd33f9ca0cd7fa4156237167d05074a315
    Signed-off-by: Ray Liao <ray.liao@quantatw.com>
    Signed-off-by: Justin Tsai <Justin_Tsai@quantatw.com>
    Reviewed-by: Jean Zheng <jean@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/199050
    Reviewed-by: Shawn Nematbakhsh <shawnn@chromium.org>

---
+++ src/board/tricky/board.c
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <pci.h>
+
+#include "base/init_funcs.h"
+#include "base/list.h"
+#include "drivers/flash/flash.h"
+#include "drivers/flash/memmapped.h"
+#include "drivers/gpio/lynxpoint_lp.h"
+#include "drivers/gpio/sysinfo.h"
+#include "drivers/power/pch.h"
+#include "drivers/sound/pcat_beep.h"
+#include "drivers/sound/sound.h"
+#include "drivers/storage/ahci.h"
+#include "drivers/storage/blockdev.h"
+#include "drivers/tpm/lpc.h"
+#include "drivers/tpm/tpm.h"
+#include "vboot/util/flag.h"
+
+static int board_setup(void)
+{
+	sysinfo_install_flags();
+
+	flash_set_ops(&new_mem_mapped_flash(0xff800000, 0x800000)->ops);
+
+	sound_set_ops(&new_pcat_beep()->ops);
+
+	AhciCtrlr *ahci = new_ahci_ctrlr(PCI_DEV(0, 31, 2));
+	list_insert_after(&ahci->ctrlr.list_node, &fixed_block_dev_controllers);
+
+	power_set_ops(&pch_power_ops);
+
+	tpm_set_ops(&new_lpc_tpm((void *)0xfed40000)->ops);
+
+	return 0;
+}
+
+INIT_FUNC(board_setup);
