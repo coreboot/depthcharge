1d2b85dfcd98848382cbee10618775441623b15c
Sourabh Banerjee <sbanerje@codeaurora.org>
2014-06-03 22:14:22 +0530

    ipq806x: i2c, gsbi and qup driver
    This driver provides interfaces to initialize the GSBI QUP block for i2c
    protocol. I2C transfer interface can be used to read and write the slave.
    The driver sets up the QUP in FIFO mode for i2c transfers.
    
    BUG=chrome-os-partner:27944
    TEST=Tested I2C slave on GSBI4 for readb, writeb, read_raw and write_raw
         operations.
    
    Change-Id: Id602eac86a76ce1c92073c2ccb50fcc1ac110e87
    Signed-off-by: Sourabh Banerjee <sbanerje@codeaurora.org>
    Reviewed-on: https://chromium-review.googlesource.com/203723
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
    Commit-Queue: Vadim Bendebury <vbendeb@chromium.org>
    Tested-by: Vadim Bendebury <vbendeb@chromium.org>

---
+++ src/drivers/bus/i2c/ipq806x.h
@@ -0,0 +1,42 @@
+/*
+ * This file is part of the depthcharge project.
+ *
+ * Copyright (C) 2014 The Linux Foundation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef __DRIVERS_BUS_I2C_IPQ806X_H__
+#define __DRIVERS_BUS_I2C_IPQ806X_H__
+
+#include "drivers/bus/i2c/i2c.h"
+
+typedef struct {
+	I2cOps ops;
+	unsigned gsbi_id;
+	unsigned initialized;
+} Ipq806xI2c;
+
+Ipq806xI2c *new_ipq806x_i2c(unsigned gsbi_id);
+#endif
