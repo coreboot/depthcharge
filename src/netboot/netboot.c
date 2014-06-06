c6dbc2aca6e3c4823d29047fd16799c5760d970a
Vadim Bendebury <vbendeb@chromium.org>
2014-06-06 14:49:53 -0700

    cli: add command for Ethernet debugging
    This adds a CLI command to exercise basic Ethernet interface
    functions, check link status, try getting IP address over DHCP, send a
    packet.
    
    It first will try finding an Ethernet interface without initializing
    USB (in case the device has a built in interface), and in case one is
    not available, will try finding a port on a USB dongle.
    
    Refactoring of netboot.c makes it easier to invoke DHCP request from
    different places in the code.
    
    BRANCH=none
    BUG=chrome-os-partner:28915
    TEST=manual
      . ran the commands, observed them work with the USB dongle.
    
    Change-Id: I3479c5729e89cc78bf3d6a20b5bff65f55e21600
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/248401
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/netboot/netboot.c
+++ src/netboot/netboot.c
@@ -94,6 +94,36 @@ static const uint32_t MaxPayloadSize = CONFIG_KERNEL_SIZE;
 static char cmd_line[4096] = "lsm.module_locking=0 cros_netboot_ramfs "
 			     "cros_factory_install cros_secure cros_netboot";
 
+int try_dhcp(uip_ipaddr_t *my_ip,
+	     uip_ipaddr_t *next_ip,
+	     uip_ipaddr_t *server_ip,
+	     const char **dhcp_bootfile)
+{
+	static int mac_addr_set = 0;
+
+	if (!mac_addr_set) {
+		// Plug in the MAC address.
+		const uip_eth_addr *mac_addr = net_get_mac();
+		if (!mac_addr)
+			halt();
+		printf("MAC: ");
+		print_mac_addr(mac_addr);
+		printf("\n");
+		uip_setethaddr(*mac_addr);
+	}
+
+	if (dhcp_request(next_ip, server_ip, dhcp_bootfile))
+		return 1;
+
+	printf("My ip is ");
+	uip_gethostaddr(my_ip);
+	print_ip_addr(my_ip);
+	printf("\nThe DHCP server ip is ");
+	print_ip_addr(server_ip);
+	printf("\n");
+	return 0;
+}
+
 void netboot(uip_ipaddr_t *tftp_ip, char *bootfile, char *argsfile, char *args)
 {
 	dc_usb_initialize();
@@ -115,27 +145,11 @@ void netboot(uip_ipaddr_t *tftp_ip, char *bootfile, char *argsfile, char *args)
 	// Start up the network stack.
 	uip_init();
 
-	// Plug in the MAC address.
-	const uip_eth_addr *mac_addr = net_get_mac();
-	if (!mac_addr)
-		halt();
-	printf("MAC: ");
-	print_mac_addr(mac_addr);
-	printf("\n");
-	uip_setethaddr(*mac_addr);
-
 	// Find out who we are.
 	uip_ipaddr_t my_ip, next_ip, server_ip;
 	const char *dhcp_bootfile;
-	while (dhcp_request(&next_ip, &server_ip, &dhcp_bootfile))
+	while (try_dhcp(&my_ip, &next_ip, &server_ip, &dhcp_bootfile))
 		printf("Dhcp failed, retrying.\n");
-
-	printf("My ip is ");
-	uip_gethostaddr(&my_ip);
-	print_ip_addr(&my_ip);
-	printf("\nThe DHCP server ip is ");
-	print_ip_addr(&server_ip);
-	printf("\n");
 
 	if (!tftp_ip) {
 		tftp_ip = &next_ip;
