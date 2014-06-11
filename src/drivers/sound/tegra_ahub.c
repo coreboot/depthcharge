030f4d5625ca488c5635f0fba739782fe0007f73
Daisuke Nojiri <dnojiri@chromium.org>
2014-06-11 07:51:10 -0700

    Nyans: Use APBIF_APBDMA_LIVE_STATUS_0 to check I2S buffer status
    TEST=Rebooted Big 100+ times. Verified no hangs. Built Nyan and Nyan Blaze.
    BUG=chrome-os-partner:29418
    BRANCH=none
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Change-Id: I15a016e651926f160eadb031749bdf652ca2b767
    Reviewed-on: https://chromium-review.googlesource.com/203308
    Reviewed-by: David Hendricks <dhendrix@chromium.org>

--- src/drivers/sound/tegra_ahub.c
+++ src/drivers/sound/tegra_ahub.c
@@ -155,15 +155,7 @@ static size_t tegra_ahub_apbif_capacity(TxFifoOps *me)
 static int tegra_ahub_apbif_is_full(TxFifoOps *me)
 {
 	TegraAudioHubApbif *apbif = container_of(me, TegraAudioHubApbif, ops);
-	/* FIFO_FULL bit may not be set even when the buffer is full. You may
-	 * consider using _is_empty() instead. */
-	return readl(&apbif->regs->i2s_live_stat) & apbif->full_mask;
-}
-
-static int tegra_ahub_apbif_is_empty(TxFifoOps *me)
-{
-	TegraAudioHubApbif *apbif = container_of(me, TegraAudioHubApbif, ops);
-	return readl(&apbif->regs->i2s_live_stat) & apbif->empty_mask;
+	return readl(&apbif->regs->apbdma_live_stat) & apbif->full_mask;
 }
 
 static ssize_t tegra_ahub_apbif_send(TxFifoOps *me, const void *buf, size_t len)
@@ -177,7 +169,7 @@ static ssize_t tegra_ahub_apbif_send(TxFifoOps *me, const void *buf, size_t len)
 		return -1;
 	}
 	while (written < len) {
-		while (!tegra_ahub_apbif_is_empty(me));
+		while (tegra_ahub_apbif_is_full(me));
 		writel(*data++, &regs->channel0_txfifo);
 		written += sizeof(*data);
 	}
@@ -200,17 +192,10 @@ static void tegra_ahub_apbif_enable_channel0(TegraAudioHubApbif *apbif,
 	writel(ctrl, &apbif->regs->channel0_ctrl);
 }
 
-static int tegra_ahub_apbif_set_fifo_mask(TegraAudioHubApbif *apbif,
-					       int i2s_id)
+static int tegra_ahub_apbif_set_fifo_mask(TegraAudioHubApbif *apbif)
 {
-	if (i2s_id > 4) {
-		printf("%s: Invalid I2S component id: %d\n", __func__, i2s_id);
-		return 1;
-	}
-	apbif->full_mask = TEGRA_AHUB_I2S_LIVE_STATUS_I2S0_TX_FIFO_FULL <<
-			(2 * i2s_id);
-	apbif->empty_mask = TEGRA_AHUB_I2S_LIVE_STATUS_I2S0_TX_FIFO_EMPTY <<
-			(2 * i2s_id);
+	// We use APBIF channel0 as a sender
+	apbif->full_mask = TEGRA_AHUB_APBDMA_LIVE_STATUS_CH0_TX_CIF_FIFO_FULL;
 	return 0;
 }
 
@@ -255,7 +240,7 @@ static int tegra_audio_hub_enable(SoundRouteComponentOps *me)
 	tegra_ahub_apbif_set_cif(ahub->apbif, cif_ctrl);
 	tegra_ahub_apbif_enable_channel0(ahub->apbif, fifo_threshold);
 
-	if (tegra_ahub_apbif_set_fifo_mask(ahub->apbif, ahub->i2s->id) ||
+	if (tegra_ahub_apbif_set_fifo_mask(ahub->apbif) ||
 	    tegra_ahub_xbar_enable_i2s(ahub->xbar, ahub->i2s->id))
 		return 1;
 	return 0;
