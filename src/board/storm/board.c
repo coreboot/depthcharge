11fdf0e8e16c5f4b076acbd2435480ed9cea3f13
Vadim Bendebury <vbendeb@chromium.org>
2014-06-12 11:33:44 -0700

    storm: support vboot gpio callbacks
    During startup vboot collects information about state of different
    switches (a mix of physical and virtual sources, different on
    different systems). AP148 does not have neither any physical switches
    nor any provision for virtual switches.
    
    This patch adds some code to plug in into the vboot callbacks and
    provide some fixed settings for the few switches vboot cares about
    when the system is booted.
    
    BUG=chrome-os-partner:27784
    TEST=with this and following patches the system boots all the way up
         to starting the kernel
    
    Change-Id: I23989e4dd3a91149c2ff7e7765d6f4e0677f6efd
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/203690
    Reviewed-by: David Hendricks <dhendrix@chromium.org>

--- src/board/storm/board.c
+++ src/board/storm/board.c
@@ -25,9 +25,34 @@
 #include "base/init_funcs.h"
 #include "drivers/bus/spi/ipq806x.h"
 #include "drivers/bus/usb/usb.h"
+#include "drivers/gpio/gpio.h"
+#include "vboot/util/flag.h"
+
+typedef struct
+{
+	GpioOps fake_ops;
+	int	fake_value;
+} FakeGpio;
+
+static int get_fake_gpio(struct GpioOps *me)
+{
+	FakeGpio *fgpio = container_of(me, FakeGpio, fake_ops);
+
+	return fgpio->fake_value;
+}
+
+static FakeGpio fake_gpios[] = 	{
+	{ .fake_ops = { .get = get_fake_gpio, }, 1 }, /* FLAG_WPSW */
+	{ .fake_ops = { .get = get_fake_gpio, }, 0 }, /* FLAG_RECSW */
+	{ .fake_ops = { .get = get_fake_gpio, }, 1 }, /* FLAG_DEVSW */
+	{ .fake_ops = { .get = get_fake_gpio, }, 1 }, /* FLAG_LIDSW */
+	{ .fake_ops = { .get = get_fake_gpio, }, 0 }, /* FLAG_PWRSW */
+};
 
 static int board_setup(void)
 {
+	int i;
+
 	SpiController *spi = new_spi(0, 0);
 	flash_set_ops(&new_spi_flash(&spi->ops, 0x400000)->ops);
 
@@ -36,6 +61,9 @@ static int board_setup(void)
 
 	list_insert_after(&usb_host1->list_node, &usb_host_controllers);
 	list_insert_after(&usb_host2->list_node, &usb_host_controllers);
+
+	for (i = 0; i < ARRAY_SIZE(fake_gpios); i++)
+		flag_install(i, &fake_gpios[i].fake_ops);
 
 	return 0;
 }
