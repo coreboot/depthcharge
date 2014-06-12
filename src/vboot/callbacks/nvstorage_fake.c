43d794d3c3f8226888e7b59d650e9654dae5bea3
Vadim Bendebury <vbendeb@chromium.org>
2014-06-12 11:15:14 -0700

    vboot: provide fake nonvolatile storage support
    While the actual nonvolatile storage scheme for Storm is being
    discussed, it is necessary to provide some plug for vboot to be able
    to follow the actual Chrome OS boot sequence.
    
    This fake implementation storage will not survive target reset, but it
    is good enough for bringing up the storm target in dev mode.
    
    BUG=chrome-os-partner:27784,chrome-os-partner:29546
    TEST=with other patches applied this implementation allows booting of
         Chrome OS on storm
    
    Change-Id: I15bc138afe58b156a2c443b79afce97048388a3c
    Reviewed-on: https://chromium-review.googlesource.com/203669
    Tested-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Commit-Queue: Vadim Bendebury <vbendeb@chromium.org>

---
+++ src/vboot/callbacks/nvstorage_fake.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2012 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <libpayload.h>
+#include <vboot_api.h>
+
+static u8 fake_nvram[VBNV_BLOCK_SIZE];
+
+VbError_t VbExNvStorageRead(uint8_t* buf)
+{
+	memcpy(buf, fake_nvram, sizeof(VBNV_BLOCK_SIZE));
+	return VBERROR_SUCCESS;
+}
+
+VbError_t VbExNvStorageWrite(const uint8_t* buf)
+{
+	memcpy(fake_nvram, buf, sizeof(VBNV_BLOCK_SIZE));
+	return VBERROR_SUCCESS;
+}
