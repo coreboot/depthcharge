cfcdafbb67286729c2b7462a6290f6f33c1d97af
Julius Werner <jwerner@chromium.org>
2014-06-16 12:25:53 -0700

    devicetree: Move coreboot addresses from /firmware to /firmware/coreboot
    Olof suggested that using 'ranges' is the lesser evil after all, and
    that upstream is more likely to accept it that way. Let's give this
    another spin while we (hopeful) still can and pray that it will really
    stay final after that.
    
    The kernel driver will still work just as well since it goes by
    'compatible', and since the 'ranges' property now allows it to properly
    translate the address in the subnode.
    
    BRANCH=nyan
    BUG=chrome-os-partner:29311
    TEST=Booted, ensured /sys/firmware/log still works (with the other
    patch) and /proc/device-tree looks as expected.
    
    Change-Id: Ib2138fede9c654b46cc2f8f0bfb7a49171287a0c
    Signed-off-by: Julius Werner <jwerner@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/204020
    Reviewed-by: David Hendricks <dhendrix@chromium.org>
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-by: Olof Johansson <olofj@chromium.org>

--- src/boot/coreboot.c
+++ src/boot/coreboot.c
@@ -173,7 +173,15 @@ static int install_coreboot_data(DeviceTreeFixup *fixup, DeviceTree *tree)
 {
 	unsigned addr_cells = 1, size_cells = 1;
 	const char *firmware_path[] = { "firmware", NULL };
-	DeviceTreeNode *coreboot = dt_find_node(tree->root, firmware_path,
+	DeviceTreeNode *firmware = dt_find_node(tree->root, firmware_path,
+						&addr_cells, &size_cells, 1);
+
+	// Need to add 'ranges' to the intermediate node to make 'reg' work.
+	bin_prop(&firmware->properties, firmware->properties.next,
+		 "ranges", NULL, 0);
+
+	const char *coreboot_path[] = { "coreboot", NULL };
+	DeviceTreeNode *coreboot = dt_find_node(firmware, coreboot_path,
 						&addr_cells, &size_cells, 1);
 
 	ListNode *props = &coreboot->properties;
