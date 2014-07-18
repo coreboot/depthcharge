3294e574115dd71c6e77abb82fff11a2054cacbf
Furquan Shaikh <furquan@google.com>
2014-07-18 15:50:22 -0700

    rush: Pull in flash_set_ops from nyan
    Depthcharge on rush got stuck on flash_ops missing. Thus, pulled in
    flash_set_ops and required changes into rush.
    
    BUG=None
    BRANCH=None
    TEST=Compiles successfully and depthcharge booting goes ahead and stops on vboot
    handle is NULL
    
    Change-Id: I1ac5b12f01b161f648fbb49cefc6c96e2d934816
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/209006
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Tom Warren <twarren@nvidia.com>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Commit-Queue: Furquan Shaikh <furquan@chromium.org>

--- src/board/rush/board.c
+++ src/board/rush/board.c
@@ -20,12 +20,38 @@
  * MA 02111-1307 USA
  */
 
+#include <assert.h>
 #include <libpayload.h>
 
 #include "base/init_funcs.h"
+#include "boot/fit.h"
+#include "boot/ramoops.h"
+#include "config.h"
+#include "drivers/bus/spi/tegra.h"
+#include "drivers/gpio/sysinfo.h"
+#include "drivers/gpio/tegra.h"
+#include "drivers/dma/tegra_apb.h"
+#include "drivers/flash/spi.h"
 
 static int board_setup(void)
 {
+	sysinfo_install_flags();
+
+	void *dma_channel_bases[32];
+	for (int i = 0; i < ARRAY_SIZE(dma_channel_bases); i++)
+		dma_channel_bases[i] = (void *)((unsigned long)0x60021000 + 0x40 * i);
+
+	TegraApbDmaController *dma_controller =
+		new_tegra_apb_dma((void *)0x60020000, dma_channel_bases,
+				  ARRAY_SIZE(dma_channel_bases));
+
+	TegraSpi *spi4 = new_tegra_spi(0x7000da00, dma_controller,
+				       APBDMA_SLAVE_SL2B4);
+
+	flash_set_ops(&new_spi_flash(&spi4->ops, 0x400000)->ops);
+
+	ramoops_buffer(0x87f00000, 0x100000, 0x20000);
+
 	return 0;
 }
 
