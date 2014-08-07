0f7d7626906f5a9b064dd1c94dc2eab0f54da4ea
Aaron Durbin <adurbin@chromium.org>
2014-08-07 12:44:32 -0500

    ec/cros: add proto v3 handling over i2c
    This adds support for the proto v3 over i2c. It
    allows one to communicate with an EC on i2c that
    only speaks proto v3.
    
    BUG=chrome-os-partner:31148
    BRANCH=None
    TEST=Built and ran through depthcharge w/o any EC errors.
    
    Change-Id: I6fd8d567b1c7506f2336550976e4d8c6fb7b73ec
    Signed-off-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/211421
    Reviewed-by: Tom Warren <twarren@nvidia.com>
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>

--- src/drivers/ec/cros/i2c.c
+++ src/drivers/ec/cros/i2c.c
@@ -126,12 +126,99 @@ static int send_command(CrosEcBusOps *me, uint8_t cmd, int cmd_version,
 	return din_len;
 }
 
+static void proto3_fill_out_segs(I2cSeg *segs, CrosEcI2cBus *bus,
+				uint32_t out_bytes, uint32_t in_bytes)
+{
+	segs[0].read = 0;
+	segs[0].chip = bus->chip;
+	segs[0].buf = bus->buf;
+	segs[0].len = out_bytes;
+	segs[1].read = 1;
+	segs[1].chip = bus->chip;
+	segs[1].buf = bus->buf;
+	segs[1].len = in_bytes;
+}
+
+static int send_packet(CrosEcBusOps *me, const void *dout, uint32_t dout_len,
+		       void *din, uint32_t din_len)
+
+{
+	CrosEcI2cBus *bus = container_of(me, CrosEcI2cBus, ops);
+	uint8_t *bytes;
+	I2cSeg segs[2];
+
+	/* There is one framing byte at the beginning of the sent data. */
+	uint32_t out_bytes = dout_len + 1;
+	/* The recieved data contains a return code and size before the data. */
+	uint32_t in_bytes = din_len + 2;
+
+	if (!bus->buf)
+		bus->buf = xmalloc(MSG_BYTES);
+
+	/*
+	 * Sanity-check I/O sizes given transaction overhead in internal
+	 * buffers.
+	 */
+	if (out_bytes > MSG_BYTES) {
+		printf("%s: Cannot send %d bytes\n", __func__, dout_len);
+		return -1;
+	}
+	if (in_bytes > MSG_BYTES) {
+		printf("%s: Cannot receive %d bytes\n", __func__, din_len);
+		return -1;
+	}
+	assert(dout_len >= 0);
+
+	proto3_fill_out_segs(segs, bus, out_bytes, in_bytes);
+
+	/*
+	 * Copy command and data into output buffer so we can do a single I2C
+	 * burst transaction.
+	 */
+	bytes = bus->buf;
+	*bytes++ = EC_COMMAND_PROTOCOL_3;
+	memcpy(bytes, dout, dout_len);
+	bytes += dout_len;
+
+	assert(out_bytes == bytes - (uint8_t *)bus->buf);
+
+	/* Send output data */
+	cros_ec_dump_data("out", -1, bus->buf, out_bytes);
+
+	if (bus->bus->transfer(bus->bus, segs, ARRAY_SIZE(segs)))
+		return -1;
+
+	cros_ec_dump_data("in", -1, bus->buf, in_bytes);
+
+	bytes = bus->buf;
+
+	int result = *bytes++;
+	if (result != EC_RES_SUCCESS) {
+		printf("%s: Received bad result code %d\n", __func__, result);
+		return -result;
+	}
+
+	int len = *bytes++;
+	if (len + 2 > MSG_BYTES) {
+		printf("%s: Received length %#02x too large\n",
+		       __func__, len);
+		return -1;
+	}
+
+	din_len = MIN(din_len, len);
+	if (din)
+		memcpy(din, bytes, din_len);
+
+	return din_len;
+}
+
 CrosEcI2cBus *new_cros_ec_i2c_bus(I2cOps *i2c_bus, uint8_t chip)
 {
 	assert(i2c_bus);
 
 	CrosEcI2cBus *bus = xzalloc(sizeof(*bus));
 	bus->ops.send_command = &send_command;
+	bus->ops.send_packet = &send_packet;
 	bus->bus = i2c_bus;
 	bus->chip = chip;
 
