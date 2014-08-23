2fe999f33f23fd6e81e7cef48f9836b9e3bdc8f6
Vadim Bendebury <vbendeb@chromium.org>
2014-08-22 20:29:51 -0700

    cli: add SPI write support
    This patch refactors the CLI 'spi' command implementation and adds the
    ability to use the SPI flash 'write' interface.
    
    BUG=chrome-os-partner:29546
    TEST=ran the following CLI commands (comments after '#'):
    
    storm: md.b 40001000 110     # show contents of a DRAM area
    40001000: 28 00 a0 e4 a0 ff 25 01 a0 00 a0 f0 ca 24 04 01    (.....%......$..
    [...]
    400010f0: 2b 3e 1f d4 00 00 01 c9 08 00 00 c9 30 df ff d6    +>..........0...
    40001100: 04 61 ea 04 01 00 00 c9 00 00 01 c9 21 00 a0 e4    .a..........!...
    storm: spi dump 3f0800 110   # show contents of a SPI area before write
    00000000: ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff    ................
    [...]
    00000100: ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff    ................
    storm: spi write 3f0800 101 40001000 # write 0x101 bytes
    storm: spi dump 3f0800 110   # verify exactly 101 bytes written
    00000000: 28 00 a0 e4 a0 ff 25 01 a0 00 a0 f0 ca 24 04 01    (.....%......$..
    [...]
    000000f0: 2b 3e 1f d4 00 00 01 c9 08 00 00 c9 30 df ff d6    +>..........0...
    00000100: 04 ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff    ................
    storm: spi write 3f0901 1 40001101  # write one more byte
    storm: spi dump 3f0800 110    # verify one more byte written
    00000000: 28 00 a0 e4 a0 ff 25 01 a0 00 a0 f0 ca 24 04 01    (.....%......$..
    [...]
    00000100: 04 61 ff ff ff ff ff ff ff ff ff ff ff ff ff ff    .a..............
    storm:
    
    Change-Id: Id274733e07c5ed03b1d740c23dd9827c8bf18127
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/213921
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Stefan Reinauer <reinauer@chromium.org>

--- src/debug/cli/spi.c
+++ src/debug/cli/spi.c
@@ -7,31 +7,23 @@
 #include "common.h"
 #include "drivers/flash/flash.h"
 
-static int do_spi(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+static int do_spi_read(int argc, char * const argv[], int read_flag)
 {
-	unsigned offset, length, i = 2;
+	unsigned offset, length;
 	const char *buffer;
 	void *mem_addr = 0;
-	int read_flag = 0;	/* True if the subcommand is 'read' */
 
-	if (argc < 2)
+	if (!read_flag && (argc != 2))
 		return CMD_RET_USAGE;
 
-	if (!strcmp(argv[1], "dump")) {
-		if (argc != 4)
-			return CMD_RET_USAGE;
-	} else if (!strcmp(argv[1], "read")) {
-		if (argc != 5)
+	if (read_flag) {
+		if (argc != 3)
 			return CMD_RET_USAGE;
 		mem_addr = (void *) strtoul(argv[2], 0, 16);
-		i = 3;
-		read_flag = 1;
-	} else {
-		return CMD_RET_USAGE;
 	}
 
-	offset = strtoul(argv[i++], 0, 16);
-	length = strtoul(argv[i++], 0, 16);
+	offset = strtoul(argv[0], 0, 16);
+	length = strtoul(argv[1], 0, 16);
 
 	buffer = flash_read(offset, length);
 
@@ -49,6 +41,45 @@ static int do_spi(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	return 0;
 }
 
+static int do_spi_write(int argc, char * const argv[], int read_flag)
+{
+	unsigned offset, length;
+	const void *mem_addr = 0;
+
+	if (argc != 3)
+		return CMD_RET_USAGE;
+
+	mem_addr = (const void *) strtoul(argv[2], 0, 16);
+	offset = strtoul(argv[0], 0, 16);
+	length = strtoul(argv[1], 0, 16);
+
+	return length != flash_write(offset, length, mem_addr);
+}
+
+static int do_spi(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+{
+	struct {
+		const char *name;
+		int param;
+		int (*function)(int argc, char *const argv[], int flag);
+	} subcommands[] = {
+		{"read", 1, do_spi_read},
+		{"dump", 0, do_spi_read},
+		{"write", 0, do_spi_write}
+	};
+	int i;
+
+	if (argc < 2)
+		return CMD_RET_USAGE;
+
+	for (i = 0; i < ARRAY_SIZE(subcommands); i++)
+		if (!strcmp(subcommands[i].name, argv[1]))
+			return subcommands[i].function(argc -2,
+						       argv + 2,
+						       subcommands[i].param);
+	return CMD_RET_USAGE;
+}
+
 U_BOOT_CMD(
 	spi,	5,	1,
 	"SPI flash sub-system",
@@ -56,4 +87,6 @@ U_BOOT_CMD(
 	"                           at `offset'\n"
 	"spi read offset len addr - read 'len' bytes starting at 'offset'\n"
 	"                           into RAM starting at 'addr'\n"
+	"spi write offset len addr - write 'len' bytes starting at 'offset'\n"
+	"                           from RAM starting at 'addr'\n"
 );
