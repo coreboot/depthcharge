77baba9a314c9f93741cb9cebeebe1b1ed7202b4
Ionela Voinescu <ionela.voinescu@imgtec.com>
2014-12-08 13:20:26 +0000

    urara: add urara platform specific files
    So far this initializes and makes us of the IMGTEC SPFI
    driver, the SPI NAND driver and the USB driver.
    
    BUG=chrome-os-partner:31438
    TEST=tested on Pistachio FPGA and bring up board;
         this can be teted in CLI showing that both
         SPI NAND and USB are working properly.
    BRANCH=none
    
    Change-Id: Ib9d9bdee0459566c332a3f3fe0fdf0666940787c
    Signed-off-by: Ionela Voinescu <ionela.voinescu@imgtec.com>
    Reviewed-on: https://chromium-review.googlesource.com/233873
    Reviewed-by: David Hendricks <dhendrix@chromium.org>

---
+++ src/board/urara/board.c
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the depthcharge project.
+ *
+ * Copyright (C) 2014 Imagination Technologies
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <arch/cpu.h>
+#include "base/init_funcs.h"
+#include "drivers/flash/spi.h"
+#include "drivers/bus/usb/usb.h"
+#include "drivers/bus/spi/imgtec_spfi.h"
+#include "drivers/storage/mtd/mtd.h"
+#include "drivers/storage/mtd/stream.h"
+#include "drivers/storage/spi_gpt.h"
+#include "drivers/storage/mtd/nand/spi_nand.h"
+
+#define IMG_SPIM0_BASE_ADDRESS	0xB8100F00
+#define IMG_SPIM1_BASE_ADDRESS	0xB8101000
+
+
+static int board_setup(void)
+{
+	struct imgtec_spi *spfi;
+	MtdDevCtrlr *mtd;
+	SpiGptCtrlr *virtual_dev;
+	UsbHostController *usb_host;
+
+	usb_host = new_usb_hc(DWC2, 0xB8120000);
+	list_insert_after(&usb_host->list_node, &usb_host_controllers);
+
+	spfi = new_imgtec_spi(IMG_SPIM1_BASE_ADDRESS, 0);
+	flash_set_ops(&new_spi_flash(&spfi->ops)->ops);
+
+	spfi = new_imgtec_spi(IMG_SPIM1_BASE_ADDRESS, 1);
+	mtd = new_spi_nand(&spfi->ops);
+	virtual_dev = new_spi_gpt("RW_GPT", new_mtd_stream(mtd), NULL);
+	list_insert_after(&virtual_dev->block_ctrlr.list_node,
+				&fixed_block_dev_controllers);
+	return 0;
+}
+
+INIT_FUNC(board_setup);
