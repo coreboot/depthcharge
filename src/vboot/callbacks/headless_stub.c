c25927ccf543779ac5327b369a02bdae0bf9bd72
Vadim Bendebury <vbendeb@chromium.org>
2014-12-17 17:15:27 -0800

    headless: do not flood the console with debug messages
    The physical presence condition is polled pretty frequently, this
    results in a flood of console messages when the devices is booting in
    recovery mode.
    
    The solution is not to report the physical presence condition unless
    there has been a change.
    
    BRANCH=storm
    BUG=none
    TEST=verified that when storm is booting in recovery mode the message
         is printed only when condition changes.
    
    Change-Id: Idf636f5b418d42acacc28998716ae6af40d9b93e
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/236438
    Reviewed-by: David Hendricks <dhendrix@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/237828

--- src/vboot/callbacks/headless_stub.c
+++ src/vboot/callbacks/headless_stub.c
@@ -74,7 +74,14 @@ int video_console_init(void)
  */
 uint32_t VbExKeyboardRead(void)
 {
+	static uint32_t prev_rv;
 	uint32_t rv = flag_fetch(FLAG_PHYS_PRESENCE);
+
+	if (prev_rv != rv) {
+		printf("%s:%d phy presense %sasserted\n",
+		       __func__, __LINE__, rv ? "" : "de");
+		prev_rv = rv;
+	}
 
 	if (rv)
 		return 0x15; /* That's ^U, i.e. boot from USB. */
