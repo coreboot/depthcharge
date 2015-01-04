2844c31fde3267a979ea5006f0b0a5177ea00526
Vadim Bendebury <vbendeb@chromium.org>
2015-01-04 13:39:51 -0800

    arch/mips: implement FIT image support
    BUG=chrome-os-partner:31438
    TEST=tested on Pistachio bring up board; with these changes
         and the ones that follow it boots linux all the way
         to userspace.
    BRANCH=none
    
    Change-Id: Ide5a18f1e716c8f4ed31698015c60454622e5a01
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/238225
    Reviewed-by: David Hendricks <dhendrix@chromium.org>
    Commit-Queue: David Hendricks <dhendrix@chromium.org>
    Tested-by: David Hendricks <dhendrix@chromium.org>

---
+++ src/arch/mips/fit.c
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <libpayload.h>
+
+#include "base/device_tree.h"
+#include "boot/fit.h"
+#include "config.h"
+
+int boot(void *image, char *cmd_line, void *params, void *loader)
+{
+	DeviceTree *tree;
+	void (*kernel)(int, unsigned, DeviceTree *tree);
+	uint32_t kernel_size;
+
+	if (fit_load(image, cmd_line, (void **)(&kernel), &kernel_size, &tree))
+		return 1;
+
+	if (!tree) {
+		printf("A device tree is required to boot on MIPS.\n");
+		return 1;
+	}
+
+	if (dt_apply_fixups(tree))
+		return 1;
+
+	// Allocate a spot for the FDT in memory.
+	void *fdt = (void *)(uintptr_t)CONFIG_KERNEL_FIT_FDT_ADDR;
+	uint32_t size = dt_flat_size(tree);
+
+	// Reserve the spot the device tree will go.
+	DeviceTreeReserveMapEntry *entry = xzalloc(sizeof(*entry));
+	entry->start = (uintptr_t)fdt;
+	entry->size = size;
+	list_insert_after(&entry->list_node, &tree->reserve_map);
+
+	// Flatten it.
+	dt_flatten(tree, fdt);
+
+	/* Relocate and jump to kernel */
+	printf("Relocating kernel from %p to %#x\n", kernel,
+			CONFIG_KERNEL_START);
+	memmove((void *)CONFIG_KERNEL_START, kernel, kernel_size);
+	kernel = (void *)CONFIG_KERNEL_START;
+	printf("Starting kernel..\n");
+
+	cache_sync_instructions();
+	kernel(0, 0xffffffff, tree);
+	printf("Kernel returned!\n");
+
+	return 0;
+}
