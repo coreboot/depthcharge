da676ecf929ff3ef36e861ac23680afacde8e828
Duncan Laurie <dlaurie@chromium.org>
2015-01-05 09:21:53 -0800

    broadwell: Add I2S bus driver config for RT5677 codec
    Add the I2S bus configuration for the Realtek RT5677 codec.
    
    BUG=chrome-os-partner:29650
    BRANCH=samus
    TEST=boot on samus in developer mode, check for beep after 30 seconds
    
    Change-Id: I769ec2b0708af811c4aa3d1938d660fd8af2c91c
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/238352
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/drivers/bus/i2s/broadwell/broadwell-alc5677.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2015 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef __DRIVERS_BUS_I2S_BROADWELL_BROADWELL_ALC5677_H__
+#define __DRIVERS_BUS_I2S_BROADWELL_BROADWELL_ALC5677_H__
+
+#include "drivers/bus/i2s/broadwell/broadwell.h"
+
+extern const BdwI2sSettings broadwell_alc5677_settings;
+
+#endif
