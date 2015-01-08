3a2065e200b99464a996f9ac083a629f473d7c12
huang lin <hl@rock-chips.com>
2015-01-08 16:39:01 +0800

    veyron: stop display at the end of depthcharge
    we use npll as vop source clock in coreboot, and in kernel
    it will change the npll rate before vop initial, it may cause
    display corruption in some panel. so we reset the vop to stop
    display at the end of depthcharge.
    
    BUG=chrome-os-partner:34713
    TEST=Boot from jerry, and display normal
    BRANCH=None
    
    Change-Id: I2aabf5a47b8254355a7f0980dc98f6e76bb9c064
    Signed-off-by: huang lin <hl@rock-chips.com>
    Signed-off-by: zhengxing <zhengxing@rock-chips.com>
    Reviewed-on: https://chromium-review.googlesource.com/239470
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Commit-Queue: Daniel Kurtz <djkurtz@chromium.org>
    Trybot-Ready: Daniel Kurtz <djkurtz@chromium.org>
    (cherry picked from commit 0c2673e827e106b8c42e06b193eab651a32515fe)
    Signed-off-by: Julius Werner <jwerner@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/243110

---
+++ src/drivers/video/rockchip.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2014 Rockchip Electronics Co., Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <arch/io.h>
+#include <libpayload.h>
+
+#include "drivers/video/display.h"
+
+#define RK_CLRSETBITS(clr, set) ((((clr) | (set)) << 16) | set)
+#define RK_SETBITS(set) RK_CLRSETBITS(0, set)
+#define RK_CLRBITS(clr) RK_CLRSETBITS(clr, 0)
+
+#define VOP_STANDBY_EN    22
+
+static uint32_t *vop0_sys_ctrl = (uint32_t *)0xff930008;
+static uint32_t *vop1_sys_ctrl = (uint32_t *)0xff940008;
+
+static int rockchip_display_stop(void)
+{
+	/* set vop0 to standby */
+	writel(RK_SETBITS(1 << VOP_STANDBY_EN), vop0_sys_ctrl);
+
+	/* set vop1 to standby */
+	writel(RK_SETBITS(1 << VOP_STANDBY_EN), vop1_sys_ctrl);
+
+	/* wait frame complete (60Hz) to enter standby */
+	mdelay(17);
+
+	return 0;
+}
+
+static DisplayOps rockchip_display_ops = {
+	.stop = &rockchip_display_stop,
+};
+
+DisplayOps *new_rockchip_display(void)
+{
+	return &rockchip_display_ops;
+}
