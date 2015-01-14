191786a99327f2f006c95c391c3a97a3f6cc3c47
Duncan Laurie <dlaurie@chromium.org>
2015-01-14 12:24:41 -0800

    display: Separate the display binding code from vboot callback.
    This allows us to initialize the display in dev / netboot mode, irrespective of
    whether vboot is being used or not.
    
    BUG=chrome-os-partner:34078
    BRANCH=None
    TEST=Compiles and boots to kernel prompt. Display is verified in firmware and
    kernel for dev and netboot mode as well as in kernel for normal mode.
    
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/231891
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Commit-Queue: Furquan Shaikh <furquan@chromium.org>
    (cherry picked from commit 4fb0dc0769335c879a354d33c1b48e932e3111ba)
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    
    Conflicts:
    	src/board/rush_ryu/board.c
    
    Change-Id: Id401ea9f36fc4754f05c56ace392868d4b535634
    Reviewed-on: https://chromium-review.googlesource.com/241472
    Trybot-Ready: Duncan Laurie <dlaurie@chromium.org>
    Commit-Queue: Duncan Laurie <dlaurie@chromium.org>
    Tested-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

---
+++ src/drivers/video/display.c
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <libpayload.h>
+#include <stdint.h>
+
+#include "base/cleanup_funcs.h"
+#include "drivers/video/display.h"
+
+static DisplayOps *display_ops;
+
+static int display_cleanup(struct CleanupFunc *cleanup, CleanupType type)
+{
+	if (display_ops != NULL && display_ops->stop != NULL)
+		return display_ops->stop();
+	return 0;
+}
+
+static CleanupFunc display_cleanup_func = {
+	.cleanup = &display_cleanup,
+	.types = CleanupOnHandoff,
+};
+
+void display_set_ops(DisplayOps *ops)
+{
+	die_if(display_ops, "%s: Display ops already set.\n", __func__);
+	display_ops = ops;
+
+	/* Call stop() when exiting depthcharge. */
+	list_insert_after(&display_cleanup_func.list_node, &cleanup_funcs);
+}
+
+int display_init(void)
+{
+	if (display_ops != NULL && display_ops->init != NULL)
+		return display_ops->init();
+
+	return 0;
+}
+
+int backlight_update(uint8_t enable)
+{
+	if (display_ops != NULL && display_ops->backlight_update != NULL)
+		return display_ops->backlight_update(enable);
+
+	printf("%s called but not implemented.\n", __func__);
+	return 0;
+}
+
