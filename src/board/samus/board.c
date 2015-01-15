83c182721f4ebec0a8673c9da55906f19c9e3c24
Duncan Laurie <dlaurie@chromium.org>
2015-01-14 17:29:12 -0800

    samus: Enable I2S and RT5677 codec support
    Enable sound support on samus with I2S and RT5677 codec.
    
    For the codec the I2C bus controller must be brought out of D3.
    For I2S the DSP must be brought out of D3 and the clock enabled.
    
    BUG=chrome-os-partner:29650
    BRANCH=samus
    TEST=boot on samus in developer mode, check for beep after 30 seconds
    
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/238859
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    (cherry picked from commit add53e6f6fa375f61ac029a1d65658ed77e075b3)
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    
    Change-Id: I7a44b6f43b0db0981efa3489ff454f7a8505fd0e
    Reviewed-on: https://chromium-review.googlesource.com/240841
    Reviewed-by: Shawn N <shawnn@chromium.org>
    Trybot-Ready: Duncan Laurie <dlaurie@chromium.org>
    Tested-by: Duncan Laurie <dlaurie@chromium.org>
    Commit-Queue: Duncan Laurie <dlaurie@chromium.org>

--- src/board/samus/board.c
+++ src/board/samus/board.c
@@ -21,20 +21,79 @@
  */
 
 #include <pci.h>
+#include <libpayload.h>
+#include <sysinfo.h>
 
 #include "base/init_funcs.h"
 #include "base/list.h"
+#include "board/samus/device_nvs.h"
+#include "drivers/bus/i2c/designware.h"
+#include "drivers/bus/i2c/i2c.h"
+#include "drivers/bus/i2s/broadwell/broadwell.h"
+#include "drivers/bus/i2s/broadwell/broadwell-alc5677.h"
+#include "drivers/bus/i2s/i2s.h"
 #include "drivers/ec/cros/lpc.h"
 #include "drivers/flash/flash.h"
 #include "drivers/flash/memmapped.h"
 #include "drivers/gpio/lynxpoint_lp.h"
 #include "drivers/gpio/sysinfo.h"
 #include "drivers/power/pch.h"
+#include "drivers/sound/i2s.h"
+#include "drivers/sound/route.h"
+#include "drivers/sound/rt5677.h"
 #include "drivers/storage/ahci.h"
 #include "drivers/storage/blockdev.h"
 #include "drivers/tpm/lpc.h"
 #include "drivers/tpm/tpm.h"
 #include "vboot/util/flag.h"
+
+enum {
+	/* Power Control and Status to enable/disable D3Hot */
+	PCH_REG_PCS = 0x84,
+	PCH_VAL_PCS_D3HOT = 3,
+
+	/* Power register to enable and disable clock */
+	SIO_REG_PPR_CLOCK = 0x800,
+	SIO_BIT_PPR_CLOCK_EN = (1 << 0),
+
+	/* 400 KHz bus speed */
+	I2C_BUS_SPEED = 400000,
+};
+
+/* Put device in D0 state */
+static void device_enable(int sio_index)
+{
+	device_nvs_t *nvs = lib_sysinfo.acpi_gnvs + DEVICE_NVS_OFFSET;
+	uint32_t reg_pcs = nvs->bar1[sio_index] + PCH_REG_PCS;
+
+	/* Put device in D0 state */
+	writel(readl(reg_pcs) & ~PCH_VAL_PCS_D3HOT, reg_pcs);
+	(void)readl(reg_pcs);
+}
+
+static DesignwareI2c *i2c_enable(int sio_index)
+{
+	device_nvs_t *nvs = lib_sysinfo.acpi_gnvs + DEVICE_NVS_OFFSET;
+	uint32_t reg_ppr = nvs->bar0[sio_index] + SIO_REG_PPR_CLOCK;
+
+	device_enable(sio_index);
+
+	/* Enable clock to the device */
+	writel(readl(reg_ppr) | SIO_BIT_PPR_CLOCK_EN, reg_ppr);
+	(void)readl(reg_ppr);
+
+	return new_designware_i2c(nvs->bar0[sio_index], I2C_BUS_SPEED);
+}
+
+static BdwI2s *i2s_enable(int ssp)
+{
+	device_nvs_t *nvs = lib_sysinfo.acpi_gnvs + DEVICE_NVS_OFFSET;
+
+	device_enable(SIO_NVS_ADSP);
+
+	return new_bdw_i2s(nvs->bar0[SIO_NVS_ADSP], ssp, 16,
+		&broadwell_alc5677_settings);
+}
 
 static int board_setup(void)
 {
@@ -54,6 +113,19 @@ static int board_setup(void)
 	power_set_ops(&pch_power_ops);
 
 	tpm_set_ops(&new_lpc_tpm((void *)(uintptr_t)0xfed40000)->ops);
+
+	/* Setup sound route via I2S to RT5677 codec */
+	BdwI2s *i2s = i2s_enable(0);
+	I2sSource *i2s_source = new_i2s_source(&i2s->ops, 48000, 2, 16000);
+	SoundRoute *sound_route = new_sound_route(&i2s_source->ops);
+
+	DesignwareI2c *i2c0 = i2c_enable(SIO_NVS_I2C0);
+	rt5677Codec *codec = new_rt5677_codec(&i2c0->ops, 0x2c, 16,
+					      48000, 256, 0);
+
+	list_insert_after(&codec->component.list_node,
+			  &sound_route->components);
+	sound_set_ops(&sound_route->ops);
 
 	return 0;
 }
