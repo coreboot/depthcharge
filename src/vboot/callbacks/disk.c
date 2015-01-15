2627987bdaa5c0f878d37a5735ba3360edaf7ae0
Furquan Shaikh <furquan@google.com>
2015-01-15 08:08:07 -0800

    vboot: Use get_all_bdevs API to get list of block devices
    Use the newly added get_all_bdevs function in blockdev.c to obtain
    list of block devices and their count.
    
    BUG=chrome-os-partner:35511
    BRANCH=None
    TEST=Compiles successfully. Boots to kernel prompt on ryu.
    
    Change-Id: I5849681f564f9256e3addd7313975f05300843b4
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/241004
    Trybot-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Randall Spangler <rspangler@chromium.org>
    Commit-Queue: Furquan Shaikh <furquan@chromium.org>

--- src/vboot/callbacks/disk.c
+++ src/vboot/callbacks/disk.c
@@ -45,29 +45,15 @@ VbError_t VbExDiskGetInfo(VbDiskInfo **info_ptr, uint32_t *count,
 {
 	*count = 0;
 
-	// Figure out which devices/controllers to operate on.
-	ListNode *ctrlrs, *devs;
-	if (disk_flags & VB_DISK_FLAG_FIXED) {
-		devs = &fixed_block_devices;
-		ctrlrs = &fixed_block_dev_controllers;
-	} else {
-		devs = &removable_block_devices;
-		ctrlrs = &removable_block_dev_controllers;
-	}
+	blockdev_type_t bd_type;
+	ListNode *devs;
 
-	// Update any controllers that need it.
-	BlockDevCtrlr *ctrlr;
-	list_for_each(ctrlr, *ctrlrs, list_node) {
-		if (ctrlr->ops.update && ctrlr->need_update &&
-		    ctrlr->ops.update(&ctrlr->ops)) {
-			printf("Updating a storage controller failed. "
-			       "Skipping.\n");
-		}
-	}
+	if (disk_flags & VB_DISK_FLAG_FIXED)
+		bd_type = BLOCKDEV_FIXED;
+	else
+		bd_type = BLOCKDEV_REMOVABLE;
 
-	// Count the devices.
-	for (ListNode *node = devs->next; node; node = node->next, (*count)++)
-		;
+	*count = get_all_bdevs(bd_type, &devs);
 
 	// Allocate enough VbDiskInfo structures.
 	VbDiskInfo *disk = NULL;
