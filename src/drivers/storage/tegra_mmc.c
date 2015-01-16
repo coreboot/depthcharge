6cb55450eb3350056e3154d0e2805d9e90585cc0
Furquan Shaikh <furquan@google.com>
2015-01-16 15:27:12 -0800

    tegra_mmc: Add is_bdev_supported function
    BUG=chrome-os-partner:35511
    BRANCH=None
    TEST=Compiles successfully. Verified using fastboot protocol test that
    correct bdev is identified for a ctrlr.
    
    Change-Id: I1b2a3da4b34fbc26c45dd2afc860f93d24c078bd
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/241731
    Trybot-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Commit-Queue: Furquan Shaikh <furquan@chromium.org>

--- src/drivers/storage/tegra_mmc.c
+++ src/drivers/storage/tegra_mmc.c
@@ -572,6 +572,13 @@ static int tegra_mmc_update(BlockDevCtrlrOps *me)
 	return 0;
 }
 
+static int tegra_is_bdev_owned(BlockDevCtrlrOps *me, BlockDev *bdev)
+{
+	TegraMmcHost *host = container_of(me, TegraMmcHost, mmc.ctrlr.ops);
+
+	return (&host->mmc.media->dev == bdev);
+}
+
 TegraMmcHost *new_tegra_mmc_host(uintptr_t ioaddr, int bus_width,
 				 int removable, GpioOps *card_detect,
 				 GpioOps *enable_power)
@@ -579,6 +586,7 @@ TegraMmcHost *new_tegra_mmc_host(uintptr_t ioaddr, int bus_width,
 	TegraMmcHost *ctrlr = xzalloc(sizeof(*ctrlr));
 
 	ctrlr->mmc.ctrlr.ops.update = &tegra_mmc_update;
+	ctrlr->mmc.ctrlr.ops.is_bdev_owned = &tegra_is_bdev_owned;
 	ctrlr->mmc.ctrlr.need_update = 1;
 
 	ctrlr->mmc.voltages = TegraMmcVoltages;
