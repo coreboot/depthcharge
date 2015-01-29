b941f87aebeb33e7708fd45a6ab21f362a5a6dc6
Furquan Shaikh <furquan@google.com>
2015-01-29 11:53:57 -0800

    cmdline arm: Add support for reading cmdline passed in dtb
    If no cmdline is present in default image or bootimg header, then try
    reading the cmdline from dtb, perform cmd substitution if required and
    put the cmdline back into dtb.
    
    BUG=chrome-os-partner:35861
    BRANCH=None
    TEST=Boots bootimg with commandline in dtb.
    
    Change-Id: I148ef5fbbe334caa159982d75b1818c8d78ba6d8
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/244395
    Reviewed-by: Randall Spangler <rspangler@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Commit-Queue: Furquan Shaikh <furquan@chromium.org>
    Trybot-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>

--- src/arch/arm/fit.c
+++ src/arch/arm/fit.c
@@ -24,9 +24,44 @@
 
 #include "arch/arm/boot.h"
 #include "base/device_tree.h"
+#include "boot/commandline.h"
 #include "boot/fit.h"
 #include "config.h"
 #include "vboot/boot.h"
+
+#define CMD_LINE_SIZE	4096
+
+static void update_cmdline(struct boot_info *bi, DeviceTree *tree)
+{
+	const char *path[] = {"chosen", NULL};
+	DeviceTreeNode *node = dt_find_node(tree->root, path, NULL, NULL, 0);
+
+	if (node == NULL)
+		goto fail;
+
+	char *str;
+	size_t size;
+	dt_find_bin_prop(node, "bootargs", (void **)&str, &size);
+
+	if (str == NULL)
+		goto fail;
+
+	static char cmd_line_buf[2 * CMD_LINE_SIZE];
+	struct commandline_info info;
+	memset(&info, 0, sizeof(info));
+
+	if (commandline_subst(str, cmd_line_buf, sizeof(cmd_line_buf), &info))
+		goto fail;
+
+	printf("Adding cmdline : %s\n", cmd_line_buf);
+
+	dt_add_string_prop(node, "bootargs", cmd_line_buf);
+
+	return;
+
+fail:
+	printf("WARNING! No cmd line passed to kernel\n");
+}
 
 int boot(struct boot_info *bi)
 {
@@ -41,6 +76,28 @@ int boot(struct boot_info *bi)
 		printf("A device tree is required to boot on ARM.\n");
 		return 1;
 	}
+
+	/*
+	 * On ARM, there are two different types of images that can be used for
+	 * storing the kernel on disk:
+	 * 1. default image type (FIT)
+	 * 2. fastboot bootimg type
+	 * In case of bootimg type kernels, there are several options of passing
+	 * in command line parameters for the kernel. It could be present either
+	 * in the signed image, or it could be present in the bootimg header or
+	 * it could be passed through the DTB.
+	 *
+	 * If until this point, we still have bi->cmd_line == NULL, it means
+	 * that we need to go and check if it is present in the DTB. If yes,
+	 * then command line substitution needs to be performed and then we need
+	 * to add it back to the DTB.
+	 *
+	 * This step cannot be performed before because we get pointer to the
+	 * device tree after the call to fit_load. Also, fit_load does not make
+	 * any changes to command line in dtb if bi->cmd_line is NULL.
+	 */
+	if (bi->cmd_line == NULL)
+		update_cmdline(bi, tree);
 
 	if (bi->ramdisk_addr && bi->ramdisk_size)
 		fit_add_ramdisk(tree, bi->ramdisk_addr, bi->ramdisk_size);
