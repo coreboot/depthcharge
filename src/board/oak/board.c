a19d8840892b6812e0c3fdfaf7d1a89c5144fbf1
Yidi Lin <yidi.lin@mediatek.com>
2015-02-16 22:04:46 +0800

    Oak: Setup TPM
    BUG=none
    TEST=emerge-oak depthcharge
    BRANCH=none
    
    Change-Id: I0f5cd8b2a3e795fdd694f2b7c26c7c307d567b34
    Signed-off-by: Yidi Lin <yidi.lin@mediatek.com>
    Reviewed-on: https://chromium-review.googlesource.com/250363
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/277718
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>
    Commit-Queue: Furquan Shaikh <furquan@chromium.org>
    Trybot-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>

--- src/board/oak/board.c
+++ src/board/oak/board.c
@@ -27,14 +27,21 @@
 #include "boot/fit.h"
 #include "boot/ramoops.h"
 #include "config.h"
+#include "drivers/bus/i2c/mtk_i2c.h"
 #include "drivers/bus/usb/usb.h"
 #include "drivers/flash/spi.h"
 #include "drivers/sound/i2s.h"
 #include "drivers/sound/max98090.h"
+#include "drivers/tpm/slb9635_i2c.h"
+#include "drivers/tpm/tpm.h"
 
 static int board_setup(void)
 {
 	fit_set_compat("mediatek,mt8173-crosnb");
+
+	MTKI2c *i2c2 = new_mtk_i2c(0x11009000, 0x11000200, 2, 0, 0x20,
+				   ST_MODE, 100, 0);
+	tpm_set_ops(&new_slb9635_i2c(&i2c2->ops, 0x20)->base.ops);
 
 	return 0;
 }
