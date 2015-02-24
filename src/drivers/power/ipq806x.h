d8a5bfc919efe36ebb5ceac5d06273f5b1486dd7
Vadim Bendebury <vbendeb@chromium.org>
2015-02-24 14:47:13 -0800

    ipq806x: power: implement power callbacks
    Depthcharge needs to be able to reboot the device on the vboot layer
    request. This patch introduces power management callbacks for the
    ipq806x SOC.
    
    The reset function is the same as in coreboot, the power off function
    is not yet implemented (it is not even clear if it is be necessary)
    and currently invokes the reset function.
    
    BRANCH=storm
    BUG=chrome-os-partner:36059
    
    TEST=with this config enabled tried booting with corrupted kernel,
         observed the target reboot in recovery mode.
    
    Change-Id: Ia2d62a4ec65a129bc4d6e8b206ac4378e9d7629a
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/253090
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/drivers/power/ipq806x.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#ifndef __DRIVERS_POWER_IPQ806X_H__
+#define __DRIVERS_POWER_IPQ806X_H__
+
+#include "drivers/power/power.h"
+
+#define MSM_CLK_CTL_BASE    ((void *)0x00900000)
+#define MSM_TMR_BASE        ((void *)0x0200A000)
+
+#define APCS_WDT0_EN        (MSM_TMR_BASE + 0x0040)
+#define APCS_WDT0_RST       (MSM_TMR_BASE + 0x0038)
+#define APCS_WDT0_BARK_TIME (MSM_TMR_BASE + 0x004C)
+#define APCS_WDT0_BITE_TIME (MSM_TMR_BASE + 0x005C)
+#define APCS_WDT0_CPU0_WDOG_EXPIRED_ENABLE (MSM_CLK_CTL_BASE + 0x3820)
+
+PowerOps *new_ipq806x_power_ops(void);
+
+#endif /* __DRIVERS_POWER_IPQ806X_H__ */
