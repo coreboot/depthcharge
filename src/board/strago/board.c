212818fe00da02f579784eb37431d9ec46744f0b
Neeraj Tayal <neeraj.tayal@intel.com>
2015-04-16 14:21:49 -0700

    Depthcharge-Strago : Remove SATA support & Enable EC support.
    Remove SATA/AHCI support
    
    BRANCH=none
    BUG=None
    TEST=Build and boot on Strago
    
    Change-Id: I34ffe33c22d59227e6dc65b3bcd7436b2dd2d783
    Signed-off-by: Neeraj Tayal <neeraj.tayal@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/265903
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

--- src/board/strago/board.c
+++ src/board/strago/board.c
@@ -37,7 +37,6 @@
 #include "drivers/sound/max98090.h"
 #include "drivers/storage/blockdev.h"
 #include "drivers/storage/sdhci.h"
-#include "drivers/storage/ahci.h"
 #include "drivers/bus/usb/usb.h"
 #include "drivers/tpm/lpc.h"
 #include "vboot/util/flag.h"
@@ -60,7 +59,7 @@ static int board_setup(void)
 #if CONFIG_DRIVER_EC_CROS
   #if CONFIG_DRIVER_EC_CROS_LPC
 	CrosEcLpcBus *cros_ec_lpc_bus =
-		new_cros_ec_lpc_bus(CROS_EC_LPC_BUS_GENERIC);
+		new_cros_ec_lpc_bus(CROS_EC_LPC_BUS_MEC);
 	cros_ec_set_bus(&cros_ec_lpc_bus->ops);
   #endif
 #endif
@@ -69,11 +68,6 @@ static int board_setup(void)
 
 	power_set_ops(&baytrail_power_ops);
 	tpm_set_ops(&new_lpc_tpm((void *)0xfed40000)->ops);
-
-	/* Add SATA controller support here. */
-	AhciCtrlr *sata;
-	sata = new_ahci_ctrlr(PCI_DEV(0, 0x13, 0));
-	list_insert_after(&sata->ctrlr.list_node, &fixed_block_dev_controllers);
 
 	SdhciHost *emmc, *sd;
 
