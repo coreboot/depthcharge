b90e87f2253ae0cdf328a39cdf636116ccd90d38
Duncan Laurie <dlaurie@chromium.org>
2015-06-05 15:55:55 -0700

    glados: Update mainboard for proto0
    - Reformat the flash layout to take advantage of 16MB:
    0-2MB=ME 2-6MB=RW_A 6-10MB=RW_B 10-12MB=LEGACY 12-16MB=RO
    - Update the board configuration to use sdhci instead of acpi
    - Add the TPM to the board configuration
    - remove the USB driver from the board configuration,
    it will be found based on device class
    
    BUG=chrome-os-partner:40635
    BRANCH=none
    TEST=boot kernel from usb on glados
    
    Change-Id: I78d481522436279652fd154bbdbbb4a4f2c1d30a
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/275712
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/276322
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>
    Commit-Queue: Furquan Shaikh <furquan@chromium.org>
    Trybot-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>

--- src/board/glados/board.c
+++ src/board/glados/board.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Google Inc.
+ * Copyright 2015 Google Inc.
  * Copyright (C) 2015 Intel Corporation.
  *
  * See file CREDITS for list of people who contributed to this
@@ -22,48 +22,63 @@
  */
 
 #include <pci.h>
-
 #include <pci/pci.h>
+#include <libpayload.h>
+#include <sysinfo.h>
+
 #include "base/init_funcs.h"
 #include "base/list.h"
 #include "drivers/ec/cros/lpc.h"
 #include "drivers/flash/flash.h"
 #include "drivers/flash/memmapped.h"
-#include "drivers/gpio/lynxpoint_lp.h"
 #include "drivers/gpio/sysinfo.h"
 #include "drivers/power/pch.h"
-#include "drivers/storage/ahci.h"
 #include "drivers/storage/blockdev.h"
+#include "drivers/storage/sdhci.h"
 #include "drivers/tpm/lpc.h"
 #include "drivers/tpm/tpm.h"
 #include "vboot/util/flag.h"
 #include "drivers/bus/usb/usb.h"
 
+/*
+ * Clock frequencies for the eMMC and SD ports are defined below. The minimum
+ * frequency is the same for both interfaces, the firmware does not run any
+ * interface faster than 52 MHz, but defines maximum eMMC frequency as 200 MHz
+ * for proper divider settings.
+ */
+#define EMMC_SD_CLOCK_MIN	400000
+#define EMMC_CLOCK_MAX		200000000
+#define SD_CLOCK_MAX		52000000
+
 static int board_setup(void)
 {
 	sysinfo_install_flags(NULL);
 
-	LpPchGpio *ec_in_rw = new_lp_pch_gpio_input(25);
-	flag_install(FLAG_ECINRW, &ec_in_rw->ops);
-
+	/* MEC1322 Chrome EC */
 	CrosEcLpcBus *cros_ec_lpc_bus =
-		new_cros_ec_lpc_bus(CROS_EC_LPC_BUS_GENERIC);
+		new_cros_ec_lpc_bus(CROS_EC_LPC_BUS_MEC);
 	cros_ec_set_bus(&cros_ec_lpc_bus->ops);
 
+	/* W25Q128FV SPI Flash */
 	flash_set_ops(&new_mem_mapped_flash(0xff000000, 0x1000000)->ops);
 
-	AhciCtrlr *ahci = new_ahci_ctrlr(PCI_DEV(0, 23, 0));
-	list_insert_after(&ahci->ctrlr.list_node, &fixed_block_dev_controllers);
+	/* SLB9670 SPI TPM */
+	tpm_set_ops(&new_lpc_tpm((void *)(uintptr_t)0xfed40000)->ops);
 
-	uintptr_t UsbMmioBase =
-		pci_read_config32(PCI_DEV(0, 0x14, 0), PCI_BASE_ADDRESS_0);
-	UsbMmioBase &= 0xFFFF0000; /* 32 bits only */
-	UsbHostController *usb_host1 = new_usb_hc(XHCI, UsbMmioBase);
-	list_insert_after(&usb_host1->list_node, &usb_host_controllers);
-
+	/* PCH Power */
 	power_set_ops(&pch_power_ops);
 
-	/* Enable TPM here when present on board. */
+	/* eMMC */
+	SdhciHost *emmc = new_pci_sdhci_host(PCI_DEV(0, 0x1e, 4), 0,
+			EMMC_SD_CLOCK_MIN, EMMC_CLOCK_MAX);
+	list_insert_after(&emmc->mmc_ctrlr.ctrlr.list_node,
+			&fixed_block_dev_controllers);
+
+	/* SD Card */
+	SdhciHost *sd = new_pci_sdhci_host(PCI_DEV(0, 0x1e, 6), 1,
+			EMMC_SD_CLOCK_MIN, SD_CLOCK_MAX);
+	list_insert_after(&sd->mmc_ctrlr.ctrlr.list_node,
+				&removable_block_dev_controllers);
 
 	return 0;
 }
