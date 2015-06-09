8ae06ead36a0659b60c614cae63202d1fb88d243
Jeffy Chen <jeffy.chen@rock-chips.com>
2015-06-09 14:40:39 +0800

    fastboot: add fill_write in dw_mmc
    add fill_write in dw_mmc.
    
    BUG=None
    TEST=Build and boot
    
    Change-Id: Iaafd22dd2c5fc2d964cbb4993111b8919278a92c
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>
    Reviewed-on: https://chromium-review.googlesource.com/276335
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/276617
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Commit-Queue: Furquan Shaikh <furquan@chromium.org>
    Trybot-Ready: Furquan Shaikh <furquan@chromium.org>

--- src/drivers/storage/dw_mmc.c
+++ src/drivers/storage/dw_mmc.c
@@ -376,6 +376,8 @@ static int dwmci_update(BlockDevCtrlrOps *me)
 			host->mmc.media->dev.removable = 1;
 			host->mmc.media->dev.ops.read = &block_mmc_read;
 			host->mmc.media->dev.ops.write = &block_mmc_write;
+			host->mmc.media->dev.ops.fill_write =
+				&block_mmc_fill_write;
 			host->mmc.media->dev.ops.new_stream =
 						&new_simple_stream;
 			list_insert_after(&host->mmc.media->dev.list_node,
@@ -393,6 +395,8 @@ static int dwmci_update(BlockDevCtrlrOps *me)
 		host->mmc.media->dev.removable = 0;
 		host->mmc.media->dev.ops.read = &block_mmc_read;
 		host->mmc.media->dev.ops.write = &block_mmc_write;
+		host->mmc.media->dev.ops.fill_write =
+			&block_mmc_fill_write;
 		host->mmc.media->dev.ops.new_stream = &new_simple_stream;
 		list_insert_after(&host->mmc.media->dev.list_node,
 				  &fixed_block_devices);
