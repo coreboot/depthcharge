302155a6cc0dbee29455baa7afc8f1e0c22b4685
Furquan Shaikh <furquan@google.com>
2015-06-20 15:15:46 -0700

    fastboot: Add support for multiple entrypoints
    Recovery mode with fastboot is supposed to have two entrypoints:
    1. Recovery mode menu
    2. Fastboot mode
    
    All firmware and kernel failures result booting into recovery mode
    menu. On the other hand, fastboot reboot-bootloader, adb
    reboot-bootloader and manual keypress event for fastboot results
    booting into fastboot mode directly.
    
    Currently, recovery mode menu is not integrated in fastboot flow. Its
    a WIP.
    
    CQ-DEPEND=CL:280922
    BUG=None
    BRANCH=None
    TEST=Compiles successfully and verified different entry points on smaug.
    
    Change-Id: I8c1ce9a9a11e20b82dc38f12417a69b8ad72bfa6
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/280924
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Commit-Queue: Furquan Shaikh <furquan@chromium.org>
    Trybot-Ready: Furquan Shaikh <furquan@chromium.org>

--- src/vboot/fastboot.c
+++ src/vboot/fastboot.c
@@ -20,12 +20,17 @@
  * MA 02111-1307 USA
  */
 
+#include <assert.h>
 #include <libpayload.h>
 #include <vboot_nvstorage.h>
+#include <vboot_struct.h>
 
+#include "drivers/ec/cros/commands.h"
+#include "drivers/ec/cros/ec.h"
 #include "drivers/power/power.h"
 #include "fastboot/fastboot.h"
 #include "vboot/stages.h"
+#include "vboot/util/commonparams.h"
 
 fb_ret_type __attribute__((weak)) device_mode_enter(void)
 {
@@ -39,7 +44,59 @@ static void vboot_clear_recovery(void)
 
 static void vboot_set_recovery(void)
 {
-	vboot_update_recovery(VBNV_RECOVERY_RO_MANUAL);
+	vboot_update_recovery(VBNV_RECOVERY_FW_FASTBOOT);
+}
+
+enum {
+	ENTRY_POINT_RECOVERY_MENU,
+	ENTRY_POINT_FASTBOOT_MODE,
+};
+
+/*
+ * Currently, all boards use EC to identify keyboard events. If in the future,
+ * any other mechanism is used, every mechanism needs to have its own
+ * implementation of this routine.
+ */
+uint8_t fastboot_keyboard_mask(void)
+{
+	uint32_t ec_events;
+	const uint32_t kb_fastboot_mask =
+		EC_HOST_EVENT_MASK(EC_HOST_EVENT_KEYBOARD_FASTBOOT);
+
+	cros_ec_get_host_events(&ec_events);
+
+	if (kb_fastboot_mask & ec_events) {
+		cros_ec_clear_host_events(kb_fastboot_mask);
+		return 1;
+	}
+
+	return 0;
+}
+
+uint8_t vboot_get_entrypoint(void)
+{
+	VbSharedDataHeader *vdat;
+	int size;
+
+	assert(find_common_params((void **)&vdat, &size) == 0);
+
+	/*
+	 * Enter fastboot mode when:
+	 * 1. Recovery reason is set to fastboot requested by FW, or
+	 * 2. Recovery reason is set to recovery requested by BCB, or
+	 * 3. Recovery reason is set to manual request and keyboard mask is set
+	 * to fastboot event.
+	 *
+	 * Else, enter recovery-menu to allow user to choose from different
+	 * options.
+	 */
+	if ((vdat->recovery_reason == VBNV_RECOVERY_FW_FASTBOOT) ||
+	    (vdat->recovery_reason == VBNV_RECOVERY_BCB_USER_MODE) ||
+	    ((vdat->recovery_reason == VBNV_RECOVERY_RO_MANUAL) &&
+	     fastboot_keyboard_mask()))
+		return ENTRY_POINT_FASTBOOT_MODE;
+
+	return ENTRY_POINT_RECOVERY_MENU;
 }
 
 void vboot_try_fastboot(void)
@@ -49,6 +106,12 @@ void vboot_try_fastboot(void)
 
 	if (board_should_enter_device_mode() == 0)
 		return;
+
+	uint8_t entry_point = vboot_get_entrypoint();
+
+	/* TBD(furquan/pgeorgi): Add recovery menu */
+	if (entry_point == ENTRY_POINT_RECOVERY_MENU)
+		printf("RECOVERY MENU NOT YET SUPPORTED!\n");
 
 	vboot_clear_recovery();
 
