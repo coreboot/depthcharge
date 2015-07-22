78d29ba1eef65d81df5b8bdc49800836683cdf1d
Jonathan Dixon <joth@google.com>
2015-07-22 16:05:02 -0700

    veyron_rialto: Override FLAG_RECSW
    Copied from Change-Id: If4dab672a35b55230f43ed5f646d39de18843b02
    Since the non-laptop Veyrons have a physical recovery switch,
    override the flag and poll it, using GPIO info passed up from
    coreboot.
    
    This allows us to use the physical recovery button for confirmation
    when entering developer mode.
    
    BUG=chrome-os-partner:43022
    BRANCH=None
    TEST=Used physical recovery button to enter dev mode on rialto
    
    Signed-off-by: Jonathan Dixon <joth@chromium.org>
    Change-Id: Iec6d5c9fc6ba9aa39c846ff8f0273de8f227e4b5
    Reviewed-on: https://chromium-review.googlesource.com/287564
    Reviewed-by: Jason Simmons <jsimmons@chromium.org>
    Reviewed-by: David Hendricks <dhendrix@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/304301
    Reviewed-by: Alexandru Stan <amstan@chromium.org>
    Commit-Queue: David Hendricks <dhendrix@chromium.org>
    Tested-by: David Hendricks <dhendrix@chromium.org>

--- src/board/veyron_rialto/board.c
+++ src/board/veyron_rialto/board.c
@@ -93,6 +93,11 @@ static int board_setup(void)
 	UsbHostController *usb_otg = new_usb_hc(DWC2, 0xff580000);
 	list_insert_after(&usb_otg->list_node, &usb_host_controllers);
 
+	// Read the current value of the recovery button for confirmation
+	// when transitioning between normal and dev mode.
+	flag_replace(FLAG_RECSW, sysinfo_lookup_gpio("recovery",
+				1, new_rk_gpio_input_from_coreboot));
+
 	/* Lid always open for now. */
 	flag_replace(FLAG_LIDSW, new_gpio_high());
 
