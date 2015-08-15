867faeea032fb273c0dab83110bfd05a091e7471
Furquan Shaikh <furquan@google.com>
2015-08-14 18:28:17 -0700

    vbnv: Add wrapper functions to perform read/write operations on vbnv.
    BUG=chrome-os-partner:44172
    BRANCH=None
    TEST=Compiles successfully
    
    Change-Id: I95d42590491e36b786b4d94e86a3520c636e5d10
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/294157
    Trybot-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Commit-Queue: Furquan Shaikh <furquan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/308591
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>

---
+++ src/vboot/vbnv.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <libpayload.h>
+#include <vboot_api.h>
+#include <vboot_nvstorage.h>
+
+#include "vboot/vbnv.h"
+
+uint32_t vbnv_read(uint32_t flag)
+{
+	uint32_t val;
+	VbNvContext context;
+
+	VbExNvStorageRead(context.raw);
+	VbNvSetup(&context);
+
+	VbNvGet(&context, flag, &val);
+
+	VbNvTeardown(&context);
+	if (context.raw_changed)
+		VbExNvStorageWrite(context.raw);
+
+	return val;
+}
+
+void vbnv_write(uint32_t flag, uint32_t val)
+{
+	VbNvContext context;
+
+	VbExNvStorageRead(context.raw);
+	VbNvSetup(&context);
+
+	VbNvSet(&context, flag, val);
+
+	VbNvTeardown(&context);
+	if (context.raw_changed)
+		VbExNvStorageWrite(context.raw);
+}
+
+
