0be8a88a018f59b42b7935cda5acb41833d78ff3
Daisuke Nojiri <dnojiri@chromium.org>
2015-08-21 17:15:37 -0700

    cli: add 'draw info' command
    draw info command prints framebuffer information on console.
    
    BUG=none
    BRANCH=tot
    TEST=ran draw info on Samus
    
    Change-Id: I5df0e1eb07fc441c7e567627c3a26013e3ebe010
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/295740
    Reviewed-by: Randall Spangler <rspangler@chromium.org>

--- src/debug/cli/draw.c
+++ src/debug/cli/draw.c
@@ -1,10 +1,12 @@
 /*
- * Commands for testing image drawing
+ * Commands for testing graphics drawing
  *
  * Copyright (C) 2015 Chromium OS Authors
  */
 
+#include <libpayload.h>
 #include <cbfs.h>
+#include <coreboot_tables.h>
 #include "common.h"
 #include "drivers/video/coreboot_fb.h"
 
@@ -15,24 +17,52 @@ static int do_draw_image(char * const name, unsigned long x, unsigned long y)
 	file = cbfs_get_file(CBFS_DEFAULT_MEDIA, name);
 	if (file == NULL) {
 		printf("File '%s' not found\n", name);
-		return CMD_RET_FAILURE;
+		return -1;
 	}
 
-	if (dc_corebootfb_draw_bitmap(x, y, (void *)file + ntohl(file->offset)))
-		return CMD_RET_FAILURE;
+	return dc_corebootfb_draw_bitmap(x, y,
+					 (void *)file + ntohl(file->offset));
+}
 
-	return CMD_RET_SUCCESS;
+static int do_print_info(void)
+{
+	struct cb_framebuffer *fbinfo = lib_sysinfo.framebuffer;
+	if (!fbinfo) {
+		printf("Invalid address for framebuffer info\n");
+		return -1;
+	}
+
+	unsigned char *fbaddr =
+		(unsigned char *)(uintptr_t)(fbinfo->physical_address);
+
+	printf("physical_address=0x%p\n", fbaddr);
+	printf("x_resolution=%d\n", fbinfo->x_resolution);
+	printf("y_resolution=%d\n", fbinfo->y_resolution);
+	printf("bytes_per_line=%d\n", fbinfo->bytes_per_line);
+	printf("bits_per_pixel=%d\n", fbinfo->bits_per_pixel);
+	printf("red_mask_pos=%d\n", fbinfo->red_mask_pos);
+	printf("red_mask_size=%d\n", fbinfo->red_mask_size);
+	printf("green_mask_pos=%d\n", fbinfo->green_mask_pos);
+	printf("green_mask_size=%d\n", fbinfo->green_mask_size);
+	printf("blue_mask_pos=%d\n", fbinfo->blue_mask_pos);
+	printf("blue_mask_size=%d\n", fbinfo->blue_mask_size);
+
+	return 0;
 }
 
 static int do_draw(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
-	int rv = CMD_RET_FAILURE;
+	int rv = CMD_RET_SUCCESS;
 
-	if (argc == 4) {
+	if (argc == 5 && !strcmp("image", argv[1])) {
 		unsigned long x = strtoul(argv[2], NULL, 10);
 		unsigned long y = strtoul(argv[3], NULL, 10);
 		/* TODO: validate (x, y) fits in the screen */
-		rv = do_draw_image(argv[1], x, y);
+		if (do_draw_image(argv[1], x, y))
+			rv = CMD_RET_FAILURE;
+	} else if (argc == 2 && !strcmp("info", argv[1])) {
+		if (do_print_info())
+			rv = CMD_RET_FAILURE;
 	} else {
 		printf("Syntax error\n");
 		rv = CMD_RET_USAGE;
@@ -42,7 +72,8 @@ static int do_draw(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 }
 
 U_BOOT_CMD(
-	   draw,	4,	1,
-	   "draw image on screen",
-	   "draw <name> <x> <y> - load image from cbfs and draw it at (x, y)"
+	   draw,	5,	1,
+	   "draw image on screen, print screen info, etc.",
+	   "image <name> <x> <y> - load image from cbfs and draw it at (x, y)\n"
+	   "info - print framebuffer information"
 );
