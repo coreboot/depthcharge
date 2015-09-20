6266270b284ea7c299d9bdab5b80bd9fa60cb586
Duncan Laurie <dlaurie@chromium.org>
2015-09-20 10:42:54 -0700

    cli: audio: Add command to simulate developer mode beep
    Add a CLI command to simulate the developer mode beep so it can
    be tested directly without needing to know the magic values to
    supply to the audio command.
    
    BUG=chrome-os-partner:42283
    BRANCH=none
    TEST=enable CONFIG_CLI and test devbeep command
    
    Change-Id: I3550c78c97bef0f5b1f33d7e5b2c56fb55d94805
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/301381
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/debug/cli/audio.c
+++ src/debug/cli/audio.c
@@ -9,28 +9,75 @@
 /* Do not play sounds for longer than this number of milliseconds. */
 #define MAX_DURATION_MS (5 * 1000)
 
+/* Developer mode beep details */
+#define DEV_BEEP_FREQUENCY 400
+#define DEV_BEEP_DURATION  120
+
+static int beep(unsigned long freq, unsigned long duration)
+{
+	int res = sound_start(freq);
+	if (!res) {
+		/* Nonblocking API must be used. */
+		u64 target_time = timer_us(0) + duration * 1000;
+
+		/* Wait in case this was an non-blocking call. */
+		while (timer_us(0) < target_time)
+			;
+
+		res = sound_stop();
+		if (res) {
+			printf("attempt to stop failed with %d\n", res);
+			return CMD_RET_FAILURE;
+		}
+	} else {
+		res = sound_play(duration, freq);
+		if (res) {
+			printf("error: attempt to play failed with %d\n", res);
+			return CMD_RET_FAILURE;
+		}
+	}
+
+	return CMD_RET_SUCCESS;
+}
+
+static int do_devbeep(cmd_tbl_t *cmbtp, int flag, int argc, char * const argv[])
+{
+	u64 target_time;
+
+	/* First beep */
+	beep(DEV_BEEP_FREQUENCY, DEV_BEEP_DURATION);
+
+	/* Wait for duration */
+	target_time = timer_us(0) + (DEV_BEEP_DURATION * 1000);
+	while (timer_us(0) < target_time)
+		;
+
+	/* Second beep */
+	beep(DEV_BEEP_FREQUENCY, DEV_BEEP_DURATION);
+
+	return CMD_RET_SUCCESS;
+}
+
 static int do_audio(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 	unsigned long freq;
 	unsigned long duration;
-	int res;
-	int rv = 0;
 
 	if (argc < 3) {
-		printf("Requred command line parameters missing\n");
-		return 1;
+		printf("Required command line parameters missing\n");
+		return CMD_RET_USAGE;
 	}
 
 	freq = strtoul(argv[1], 0, 10);
 	duration = strtoul(argv[2], 0, 10);
 
 	if ((freq < 20) || (freq > 20000)) {
-		printf("will not try playing sound at %ld Hertz\n", freq);
-		return 1;
+		printf("Invalid frequency %ld Hertz\n", freq);
+		return CMD_RET_FAILURE;
 	}
 
 	if (!duration)
-		return 0;
+		return CMD_RET_SUCCESS;
 
 	if (duration > MAX_DURATION_MS) {
 		printf("Capping duration from %ld to %d milliseconds\n",
@@ -45,30 +92,7 @@ static int do_audio(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		sound_set_volume(volume);
 	}
 
-	res = sound_start(freq);
-	if (!res) {
-		/* Nonblocking API must be used. */
-		u64 target_time = timer_us(0) + duration * 1000;
-
-		/* Wait in case this was an non-blocking call. */
-		while (timer_us(0) < target_time)
-			;
-
-		res = sound_stop();
-		if (res) {
-			printf("attempt to stop failed with %d\n", res);
-			rv = 1;
-		}
-	} else {
-		res = sound_play(duration, freq);
-		if (res) {
-			printf("error: attempt to play failed with %d\n", res);
-			rv = 1;
-		}
-	}
-
-
-	return rv;
+	return beep(freq, duration);
 }
 
 U_BOOT_CMD(
@@ -77,4 +101,10 @@ U_BOOT_CMD(
 	   "\n<freq> <duration> [<volume>]  - play sound of <freq> Hz for "
 	   "<duration>\n"
 	   "                                milliseconds at volume <volume>"
+);
+
+U_BOOT_CMD(
+	   devbeep,	1,	1,
+	   "developer mode timeout beep",
+	   "\n"
 );
