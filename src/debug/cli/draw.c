68b81c84bdc7b6dd5451a2b50f395d651fc1ea58
Daisuke Nojiri <dnojiri@chromium.org>
2015-09-25 08:41:52 -0700

    cli: add 'draw screen' command
    'draw screen' command draws vboot screens, which are identified by
    VB_SCREEN_*.
    
    BUG=none
    BRANCH=tot
    TEST=Tested on Samus
    
    Change-Id: I955c1e41771400591a0693539b3a317768754a32
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/303766
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-by: Randall Spangler <rspangler@chromium.org>

--- src/debug/cli/draw.c
+++ src/debug/cli/draw.c
@@ -7,6 +7,7 @@
 #include <libpayload.h>
 #include <cbfs.h>
 #include <coreboot_tables.h>
+#include <vboot/screens.h>
 #include "common.h"
 #include "drivers/video/display.h"
 #include "drivers/video/coreboot_fb.h"
@@ -121,6 +122,11 @@ static int do_draw_box(char * const argv[])
 	return draw_box(&box, &rgb);
 }
 
+static int do_draw_screen(char * const argv[])
+{
+	return vboot_draw_screen(strtoul(argv[2], NULL, 0));
+}
+
 static int do_draw(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 	int rv = CMD_RET_SUCCESS;
@@ -139,6 +145,9 @@ static int do_draw(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	} else if (argc == 9 && !strcmp("box", argv[1])) {
 		if (do_draw_box(argv))
 			rv = CMD_RET_FAILURE;
+	} else if (argc == 3 && !strcmp("screen", argv[1])) {
+		if (do_draw_screen(argv))
+			rv = CMD_RET_FAILURE;
 	} else {
 		printf("Syntax error\n");
 		rv = CMD_RET_USAGE;
@@ -152,5 +161,6 @@ U_BOOT_CMD(
 	   "draw image on screen, print screen info, etc.",
 	   "image <name> <x> <y> [<width> <height>] - draw image in cbfs at (x, y)\n"
 	   "info - print framebuffer information\n"
-	   "box <x> <y> <width> <height> <red> <green> <blue> - draw a box"
+	   "box <x> <y> <width> <height> <red> <green> <blue> - draw a box\n"
+	   "screen <id> - draw a preprogrammed screen of ID <id>"
 );
