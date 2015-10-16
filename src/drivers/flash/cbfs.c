bb888d6f00d119aea94c077818ad63ef4df4b239
Patrick Georgi <pgeorgi@google.com>
2015-10-16 15:24:56 +0200

    flash driver: provide setup for selecting cbfses from fmap
    The offset needn't be added at all times (in fact, just for the first
    two accesses), so we look for out of bounds conditions to check when
    it's required to add the offset and when it needn't be.
    
    This approach works because the CBFS images' offsets are larger than
    their own size. This restriction (and this imprecise approach) will be
    lifted in follow-up commits.
    
    BUG=chromium:445938
    BRANCH=tot
    TEST=none
    
    Change-Id: I3f60a3a66ed8d6654be5bed1ca1f4b4b2cd5b8d7
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/306600
    Commit-Ready: Patrick Georgi <pgeorgi@chromium.org>
    Tested-by: Patrick Georgi <pgeorgi@chromium.org>
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

--- src/drivers/flash/cbfs.c
+++ src/drivers/flash/cbfs.c
@@ -21,6 +21,7 @@
 
 #include <libpayload.h>
 #include <cbfs.h>
+#include "image/fmap.h"
 #include "drivers/flash/flash.h"
 
 /* flash as CBFS media. */
@@ -32,13 +33,30 @@ static int cbfs_media_open(struct cbfs_media *media)
 
 static int cbfs_media_close(struct cbfs_media *media)
 {
+	if (media->context)
+		free(media->context);
+
 	return 0;
+}
+
+static size_t add_offset(struct cbfs_media *media,
+			size_t offset, size_t count)
+{
+	if (media->context) {
+		FmapArea *area = (FmapArea *)media->context;
+
+		if (offset + count > area->size)
+			return offset;
+		return offset + area->offset;
+	}
+	return offset;
 }
 
 static size_t cbfs_media_read(struct cbfs_media *media,
 			      void *dest, size_t offset,
 			      size_t count)
 {
+	offset = add_offset(media, offset, count);
 	uint8_t *cache = flash_read(offset, count);
 	if (!cache)
 		return 0;
@@ -49,6 +67,7 @@ static size_t cbfs_media_read(struct cbfs_media *media,
 static void *cbfs_media_map(struct cbfs_media *media,
 			    size_t offset, size_t count)
 {
+	offset = add_offset(media, offset, count);
 	void *ptr = flash_read(offset, count);
 	if (!ptr)
 		ptr = CBFS_MEDIA_INVALID_MAP_ADDRESS;
@@ -62,6 +81,31 @@ static void *cbfs_media_unmap(struct cbfs_media *media, const void *address)
 
 int libpayload_init_default_cbfs_media(struct cbfs_media *media)
 {
+	media->context = NULL;
+	media->open = cbfs_media_open;
+	media->close = cbfs_media_close;
+	media->read = cbfs_media_read;
+	media->map = cbfs_media_map;
+	media->unmap = cbfs_media_unmap;
+
+	return 0;
+}
+
+int cbfs_media_from_fmap(struct cbfs_media *media, const char *name)
+{
+	FmapArea *area = malloc(sizeof(*area));
+
+	if (area == NULL) {
+		printf("couldn't allocate memory for FmapArea\n");
+		return -1;
+	}
+	if (fmap_find_area(name, area)) {
+		printf("'%s' not found\n", name);
+		return -1;
+	}
+
+	media->context = area;
+
 	media->open = cbfs_media_open;
 	media->close = cbfs_media_close;
 	media->read = cbfs_media_read;
---
+++ src/drivers/flash/cbfs.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __DRIVERS_FLASH_CBFS_H__
+#define __DRIVERS_FLASH_CBFS_H__
+
+int cbfs_media_from_fmap(struct cbfs_media *media, const char *name);
+
+#endif
