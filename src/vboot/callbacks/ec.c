fe240a57f3689b276fd4cfe1ab8b1b5c6f502e8b
Patrick Georgi <pgeorgi@google.com>
2015-10-16 15:34:35 +0200

    Support fetching EC firmware from CBFS-formatted FW_MAIN_*
    With MULTIPLE_CBFS_INSTANCES enabled, EC firmware and hashes
    come from CBFS instead of its separate fmap region and an indexes
    FW_MAIN_*, respectively.
    
    This is currently implemented as a fallback option when the normal
    "indexed" approach fails. Doesn't hurt to add another try then, and due
    to details in the CBFS format, it's guaranteed that this fallback is
    taken with MULTIPLE_CBFS_INSTANCES.
    
    Once that configuration is in operation, the old code path can be taken
    out.
    
    BUG=chromium:445938
    BRANCH=tot
    TEST=none
    
    Change-Id: I5ac553de3f5dcd0bf59cc1e91aa2e7eed6c23e6e
    Signed-off-by: Patrick Georgi <pgeorgi@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/306602
    Commit-Ready: Patrick Georgi <pgeorgi@chromium.org>
    Tested-by: Patrick Georgi <pgeorgi@chromium.org>
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

--- src/vboot/callbacks/ec.c
+++ src/vboot/callbacks/ec.c
@@ -22,14 +22,77 @@
 
 #include <assert.h>
 #include <libpayload.h>
+#include <cbfs.h>
 #include <vboot_api.h>
 
+#include <stddef.h>
+#include <vb2_api.h>
+
 #include "base/timestamp.h"
+#include "config.h"
 #include "drivers/ec/cros/ec.h"
 #include "drivers/flash/flash.h"
+#include "drivers/flash/cbfs.h"
 #include "image/fmap.h"
 #include "image/index.h"
 #include "vboot/util/flag.h"
+
+/* Returns the CBFS filename for EC firmware images based on the device index
+ * passed in from vboot.
+ */
+static const char *get_fw_filename(int devidx)
+{
+	return devidx == 0 ? "ecrw" : "pdrw";
+}
+
+static struct cbfs_file *get_file_from_cbfs(const char *fmap_name,
+	const char *filename)
+{
+	struct cbfs_media media;
+	struct cbfs_file *file;
+
+	if (!IS_ENABLED(CONFIG_DRIVER_CBFS_FLASH))
+		return NULL;
+
+	if (cbfs_media_from_fmap(&media, fmap_name)) {
+		printf("couldn't access cbfs media\n");
+		return NULL;
+	}
+
+	file = cbfs_get_file(&media, filename);
+	media.close(&media);
+
+	return file;
+}
+
+static const uint8_t *get_file_hash_from_cbfs(const char *fmap_name,
+	const char *filename, int *hash_size)
+{
+	printf("Trying to fetch hash for '%s' from CBFS\n", filename);
+	struct cbfs_file *file = get_file_from_cbfs(fmap_name, filename);
+
+	if (file == NULL) {
+		printf("file not found\n");
+		return NULL;
+	}
+
+	struct cbfs_file_attribute *attr = cbfs_file_find_attr(file,
+		CBFS_FILE_ATTR_TAG_HASH);
+
+	if (attr == NULL) {
+		printf("ECRW found, but without hash\n");
+		return NULL;
+	}
+
+	struct cbfs_file_attr_hash *hash = (struct cbfs_file_attr_hash *)attr;
+
+	if (ntohl(hash->hash_type) != VB2_HASH_SHA256) {
+		printf("hash is not SHA256\n");
+		return NULL;
+	}
+	*hash_size = ntohl(hash->len) - sizeof(*hash);
+	return hash->hash_data;
+}
 
 int VbExTrustEC(int devidx)
 {
@@ -131,13 +194,16 @@ VbError_t VbExEcGetExpectedRW(int devidx, enum VbSelectFirmware_t select,
 			      const uint8_t **image, int *image_size)
 {
 	const char *name;
+	const char *main_name;
 
 	switch (select) {
 	case VB_SELECT_FIRMWARE_A:
 		name = (devidx == 0 ? "EC_MAIN_A" : "PD_MAIN_A");
+		main_name = "FW_MAIN_A";
 		break;
 	case VB_SELECT_FIRMWARE_B:
 		name = (devidx == 0 ? "EC_MAIN_B" : "PD_MAIN_B");
+		main_name = "FW_MAIN_B";
 		break;
 	default:
 		printf("Unrecognized EC firmware requested.\n");
@@ -147,7 +213,17 @@ VbError_t VbExEcGetExpectedRW(int devidx, enum VbSelectFirmware_t select,
 	FmapArea area;
 	if (fmap_find_area(name, &area)) {
 		printf("Didn't find section %s in the fmap.\n", name);
-		return VBERROR_UNKNOWN;
+		/* It may be gone in favor of CBFS based RW sections,
+		 * so look there, too. */
+		const char *filename = get_fw_filename(devidx);
+		struct cbfs_file *file =
+			get_file_from_cbfs(main_name, filename);
+		if (file == NULL)
+			return VBERROR_UNKNOWN;
+		printf("found '%s', sized 0x%x\n", filename, ntohl(file->len));
+		*image = CBFS_SUBHEADER(file);
+		*image_size = ntohl(file->len);
+		return VBERROR_SUCCESS;
 	}
 
 	uint32_t size;
@@ -240,7 +316,10 @@ VbError_t VbExEcGetExpectedRWHash(int devidx, enum VbSelectFirmware_t select,
 	if (!*hash) {
 		printf("Didn't find precalculated hash subsection %d.\n",
 		       devidx + 1);
-		return VBERROR_UNKNOWN;
+		const char *filename = get_fw_filename(devidx);
+		*hash = get_file_hash_from_cbfs(name, filename, hash_size);
+		if (!*hash)
+			return VBERROR_UNKNOWN;
 	}
 
 	printf("EC-RW hash address, size are %p, %d.\n",
