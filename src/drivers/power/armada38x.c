494fdc99e668f726c531bca333f782b5c0e68c75
Ruilin Hao <rlhao@marvell.com>
2015-11-10 23:35:45 -0800

    Cyclone Drivers: Add power driver for armada38x
    It is only a software reset implemented by SOC.
    
    BUG=chrome-os-partner:47463
    TEST=emerge-cyclone depthcharge compiles successfully
    BRANCH=tot
    
    Change-Id: I45e74bbce910ed7427e4c8c2f2d682c91fd7903e
    Signed-off-by: Ruilin Hao <rlhao@marvell.com>
    Reviewed-on: https://chromium-review.googlesource.com/313186
    Commit-Ready: Kan Yan <kyan@google.com>
    Tested-by: Kan Yan <kyan@google.com>
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>

---
+++ src/drivers/power/armada38x.c
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libpayload.h>
+#include "base/container_of.h"
+#include "drivers/power/armada38x.h"
+
+#define MV_MISC_REGS_BASE			(0xF1018200)
+#define CPU_RSTOUTN_MASK_REG			(MV_MISC_REGS_BASE + 0x60)
+#define CPU_SYS_SOFT_RST_REG			(MV_MISC_REGS_BASE + 0x64)
+
+static int armada38x_cold_reboot(struct PowerOps *me)
+{
+	//software reset
+	write32((void *)CPU_RSTOUTN_MASK_REG,
+		(read32((void *)CPU_RSTOUTN_MASK_REG) | (1 << 0)));
+	write32((void *)CPU_SYS_SOFT_RST_REG,
+		(read32((void *)CPU_SYS_SOFT_RST_REG) | (1 << 0)));
+	for (;;)
+		;
+	return 0;
+}
+
+static int armada38x_power_off(struct PowerOps *me)
+{
+	die("Power off not implemented\n");
+}
+
+PowerOps *new_armada38x_power_ops(void)
+{
+	PowerOps *pops = xzalloc(sizeof(*pops));
+
+	pops->cold_reboot = armada38x_cold_reboot;
+	pops->power_off = armada38x_power_off;
+
+	return pops;
+}
---
+++ src/drivers/power/armada38x.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __DRIVERS_POWER_ARMADA38X_H__
+#define __DRIVERS_POWER_ARMADA38X_H__
+
+#include "drivers/power/power.h"
+
+
+PowerOps *new_armada38x_power_ops(void);
+
+#endif /* __DRIVERS_POWER_ARMADA38X_H__ */
