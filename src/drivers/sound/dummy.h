3c717f9e71d066c9fbfdd20211d4c83e2d13aca3
Julius Werner <jwerner@chromium.org>
2016-01-25 15:55:17 -0800

    sound: Add dummy sound driver for devices without a speaker
    The concept of sound is deeply ingrained in vboot1. We now have devices
    that don't have a speaker but still want to have a dev mode delay. It is
    possible to run vboot without ever calling sound_set_ops, but it leads
    to weird errors and kinda only works at all by accident.
    
    This patch adds a "dummy" sound driver that correctly implements the
    depthcharge sound API (and by extension VbExBeep()) for use on those
    boards.
    
    BRANCH=master
    BUG=chrome-os-partner:48657
    TEST=See bug or next CL
    
    Change-Id: I2f5078d66bf683579be4e4720dd5dd73b74fe3cb
    Signed-off-by: Julius Werner <jwerner@chromium.org>
    Signed-off-by: Alexandru M Stan <amstan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/323835
    Commit-Ready: Alexandru Stan <amstan@chromium.org>
    Tested-by: Alexandru Stan <amstan@chromium.org>
    Reviewed-by: Alexandru Stan <amstan@chromium.org>

---
+++ src/drivers/sound/dummy.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __DRIVERS_SOUND_DUMMY_H__
+#define __DRIVERS_SOUND_DUMMY_H__
+
+#include "drivers/sound/sound.h"
+
+SoundOps *new_dummy_sound(void);
+
+#endif /* __DRIVERS_SOUND_DUMMY_H__*/
