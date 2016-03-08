606a3272893c07a2644fe8cb5a2bf4c0e4cf8123
Naveen Krishna Chatradhi <naveenkrishna.ch@intel.com>
2016-03-08 18:21:22 +0530

    board/lars: Remove the workaround for silego unable to see EC reset
    With the new Silego fix on the DVT boards. We can remove the software
    workaround for LARS boards.
    
    BUG=chrome-os-partner:49847
    BRANCH=glados
    TEST=manual testing (in additon to FAFT)
    1) set GBB flag 0x300
    2) enter recovery mode with: dut-control power_state:rec
    3) press Ctrl+D on keyboard to transition to developer mode
    4) also verify that when the GBB flag is not set that if the
    EC is in RW and steps 2-3 are executed that it does not allow
    entry into developer mode
    5) also verify that keyboard controlled recovery mode does clear
    the EC_IN_RW flag properly and does not need GBB flag to be set.
    
    Change-Id: I14dbb7c0731853065dce724a6c3294626c2dd71d
    Signed-off-by: Naveen Krishna Chatradhi <naveenkrishna.ch@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/331366
    Commit-Ready: Naveenkrishna Ch <naveenkrishna.ch@intel.com>
    Tested-by: Naveenkrishna Ch <naveenkrishna.ch@intel.com>
    Reviewed-by: Duncan Laurie <dlaurie@chromium.org>

--- src/board/lars/board.c
+++ src/board/lars/board.c
@@ -21,7 +21,6 @@
  * MA 02111-1307 USA
  */
 
-#include <gbb_header.h>
 #include <pci.h>
 #include <pci/pci.h>
 #include <libpayload.h>
@@ -43,7 +42,6 @@
 #include "drivers/tpm/lpc.h"
 #include "drivers/tpm/tpm.h"
 #include "vboot/util/flag.h"
-#include "vboot/util/commonparams.h"
 #include "drivers/bus/usb/usb.h"
 
 /*
@@ -55,39 +53,9 @@
 #define EMMC_SD_CLOCK_MIN	400000
 #define EMMC_CLOCK_MAX		200000000
 
-/*
- * Workaround for issue where silego is unable to see EC reset to clear the
- * EC_IN_RW state when attempting to enter recovery via servo.  This allows FAFT
- * to transition the system to developer mode.
- */
-static int ec_in_rw_workaround_get_value(GpioOps *me)
-{
-	GoogleBinaryBlockHeader *gbb = cparams.gbb_data;
-
-	if (gbb->flags & GBB_FLAG_FAFT_KEY_OVERIDE) {
-		/* Override is enabled, return 0 for FAFT. */
-		printf("FAFT override enabled, returning 0 for ECINRW flag\n");
-		return 0;
-	}
-
-	/* Override is not enabled, lookup the real GPIO state. */
-	GpioOps *ecinrw = sysinfo_lookup_gpio("EC in RW", 1,
-			    new_skylake_gpio_input_from_coreboot);
-	return ecinrw->get(ecinrw);
-}
-
-GpioOps *ec_in_rw_workaround_gpio(void)
-{
-	GpioOps *ops = xzalloc(sizeof(*ops));
-
-	ops->get = &ec_in_rw_workaround_get_value;
-	return ops;
-}
-
 static int board_setup(void)
 {
 	sysinfo_install_flags(new_skylake_gpio_input_from_coreboot);
-	flag_replace(FLAG_ECINRW, ec_in_rw_workaround_gpio());
 
 	/* MEC1322 Chrome EC */
 	CrosEcLpcBus *cros_ec_lpc_bus =
