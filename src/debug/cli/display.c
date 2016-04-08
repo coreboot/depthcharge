3801174645c880f4cbe977f2d921e7540223a22a
zhuo-hao <zhuo-hao.lee@intel.com>
2016-04-08 15:49:25 +0800

    cli: add more VBOOT screens and change display_screen() to vboot_draw_screen()
    Add 4 VBOOT screens:
      VB_SCREEN_DEVELOPER_TO_NORM
      VB_SCREEN_WAIT
      VB_SCREEN_TO_NORM_CONFIRMED
      VB_SCREEN_OS_BROKEN
    
    Change display_screen() to vboot_draw_screen() because vboot always use this
    API to show screen.
    
    BUG=none
    BRANCH=TOT
    TEST=tried 'display <num>' on Chell, 0~9 screens works correctly
    
    Change-Id: Ib64e53ab323e197028db7bd0ae98fff5dd121fca
    Signed-off-by: Zhuo-hao Lee <zhuo-hao.lee@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/337910
    Reviewed-by: Vadim Bendebury <vbendeb@chromium.org>

--- src/debug/cli/display.c
+++ src/debug/cli/display.c
@@ -1,6 +1,6 @@
 
 #include <vboot_api.h>
-
+#include <vboot/screens.h>
 #include "common.h"
 
 #include "drivers/video/display.h"
@@ -12,20 +12,24 @@ static const enum VbScreenType_t screen_map[] = {
 	VB_SCREEN_RECOVERY_INSERT,
 	VB_SCREEN_RECOVERY_TO_DEV,
 	VB_SCREEN_RECOVERY_NO_GOOD,
+	VB_SCREEN_DEVELOPER_TO_NORM,
+	VB_SCREEN_WAIT,
+	VB_SCREEN_TO_NORM_CONFIRMED,
+	VB_SCREEN_OS_BROKEN,
 	VB_SCREEN_DEVELOPER_EGG,
 };
 
 static int do_display(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 	unsigned display = strtoul(argv[1], 0, 10);
+	uint32_t locale = 0;
 
 	if (display >= ARRAY_SIZE(screen_map)) {
 		printf("Unsupported screen number %d\n", display);
 		return CMD_RET_USAGE;
 	}
 
-	display_screen(screen_map[display]);
-	return CMD_RET_SUCCESS;
+	return vboot_draw_screen(screen_map[display], locale);
 }
 
 U_BOOT_CMD(
@@ -38,5 +42,9 @@ U_BOOT_CMD(
 	   "         3 - recovery USB insert\n"
 	   "         4 - waiting to transition to dev mode\n"
 	   "         5 - bad USB stick\n"
-	   "         6 - easter egg\n"
+	   "         6 - waiting to transition to normal mode\n"
+	   "         7 - waiting EC programming\n"
+	   "         8 - confirm to normal mode\n"
+	   "         9 - verification failure\n"
+	   "        10 - easter egg\n"
 );
