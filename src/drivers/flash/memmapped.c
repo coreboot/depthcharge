30058c5df4abee78305e2e36f370e3d8be10a30c
Furquan Shaikh <furquan@google.com>
2016-06-21 05:29:28 -0700

    drivers/flash: Support mmaped flash with non-zero base offset
    In some boards e.g. reef, the entire flash is not memory mapped. Instead
    only a part of the flash (offset 0x1000 to offset x which covers the
    BIOS region) is mapped in the address space. In order to handle such
    special cases, a new API is required to allow boards to define the
    offset on flash of the base address that is memory mapped. This base
    offset needs to be taken care of while accessing any absolute address
    within the mmaped range. For older boards, this base_offset defaults to
    0.
    
    BUG=chrome-os-partner:54563
    BRANCH=None
    TEST=Compiles successfully and boots to OS on reef.
    
    Change-Id: I8cc54168235ceeaf561c7e77c0b661ed053bcd4b
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/354380
    Commit-Ready: Furquan Shaikh <furquan@chromium.org>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/drivers/flash/memmapped.c
+++ src/drivers/flash/memmapped.c
@@ -24,6 +24,13 @@ static void *mem_mapped_flash_read(FlashOps *me, uint32_t offset, uint32_t size)
 {
 	MemMappedFlash *flash = container_of(me, MemMappedFlash, ops);
 
+	if (offset < flash->base_offset) {
+		printf("Offset out of bounds.\n");
+		return NULL;
+	}
+
+	offset -= flash->base_offset;
+
 	if (offset > flash->size || offset + size > flash->size) {
 		printf("Out of bounds flash access.\n");
 		return NULL;
@@ -32,11 +39,18 @@ static void *mem_mapped_flash_read(FlashOps *me, uint32_t offset, uint32_t size)
 	return (void *)(uintptr_t)(flash->base + offset);
 }
 
-MemMappedFlash *new_mem_mapped_flash(uint32_t base, uint32_t size)
+MemMappedFlash *new_mem_mapped_flash_with_offset(uint32_t base, uint32_t size,
+						 uint32_t base_offset)
 {
 	MemMappedFlash *flash = xzalloc(sizeof(*flash));
 	flash->ops.read = &mem_mapped_flash_read;
 	flash->base = base;
 	flash->size = size;
+	flash->base_offset = base_offset;
 	return flash;
+}
+
+MemMappedFlash *new_mem_mapped_flash(uint32_t base, uint32_t size)
+{
+	return new_mem_mapped_flash_with_offset(base, size, 0);
 }
--- src/drivers/flash/memmapped.h
+++ src/drivers/flash/memmapped.h
@@ -27,8 +27,22 @@ typedef struct MemMappedFlash
 	FlashOps ops;
 	uint32_t base;
 	uint32_t size;
+	/* Offset of mmaped base within the flash. */
+	uint32_t base_offset;
 } MemMappedFlash;
 
 MemMappedFlash *new_mem_mapped_flash(uint32_t base, uint32_t size);
+/*
+ * new_mem_mapped_flash_with_offset accepts an additional parameter base_offset
+ * that identifies the offset in the flash which maps at the provided base
+ * address. In most of the cases base_offset would be 0, indicating that flash
+ * region starting from offset 0 is mapped onto the base address. However, in
+ * some special cases (e.g. reef), flash region from offset 0x1000 is actually
+ * mapped. Thus, base address corresponds to the offset 0x1000 on flash and
+ * hence all addresses generated for mmap access on flash need to be adjusted
+ * according to the base_offset.
+ */
+MemMappedFlash *new_mem_mapped_flash_with_offset(uint32_t base, uint32_t size,
+						 uint32_t base_offset);
 
 #endif /* __DRIVERS_FLASH_MEMMAPPED_H__ */
