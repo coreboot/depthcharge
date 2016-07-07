9e39dad1cf2a69d4eef01f4f6620af3108c505b4
Julius Werner <jwerner@chromium.org>
2016-07-07 16:57:43 -0700

    arm64: power: Add new PSCI PowerOps provider
    ARM64 systems are by design required to support power-off and reboot
    functionality in ARM Trusted Firmware through PSCI calls. Rather than
    reimplement the same functionality in depthcharge for every individual
    board, we can leverage this by just calling the same code with an SMC.
    
    This patch adds rudimentary SMC functionality to depthcharge and uses it
    to implement a PSCI PowerOps provider using the SYSTEM_RESET and
    SYSTEM_OFF PSCI calls to reboot and power down, respectively.
    
    BRANCH=None
    BUG=None
    TEST=Triggered reboot and power-off reasons on Kevin, observed how
    system reacted correctly (with follow-up patch).
    
    Change-Id: I5983b19229cdaa3c8e8fb5e49c778d534d8acbb1
    Signed-off-by: Julius Werner <jwerner@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/358922
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/drivers/power/psci.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __DRIVERS_POWER_PSCI_H__
+#define __DRIVERS_POWER_PSCI_H__
+
+#include "drivers/power/power.h"
+
+PowerOps psci_power_ops;
+
+#endif /* __DRIVERS_POWER_PSCI_H__ */
