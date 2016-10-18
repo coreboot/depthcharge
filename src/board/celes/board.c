a3806ecc309f0254d0451a0e56d9685fef76349b
sh.kim <sh_.kim@samsung.com>
2016-10-18 15:04:31 +0900

    Celes: disable eMMC HS200 in firmware
    Disable HS200 in firmware for Sandisk SDINADB4 issue,
    The eMMC need CMD21 tuning before switch to HS200.
    Adding the tuning in firmware would increase boot time a lot.
    Disable HS200 in firmware would be better,
    Leave to Kernel for enabling HS200.
    
    BUG=chrome-os-partner:58462
    BRANCH=firmware-celes-7287.92.B
    TEST=build, and boot celes
    Signed-off-by: shkim <sh_.kim@samsung.com>
    
    Change-Id: I2cab8a1cf1fa626de69c7d1ae085ac62b4bf09b5
    Reviewed-on: https://chromium-review.googlesource.com/399983
    Tested-by: shkim <sh_.kim@samsung.com>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Commit-Queue: shkim <sh_.kim@samsung.com>

--- src/board/celes/board.c
+++ src/board/celes/board.c
@@ -118,7 +118,7 @@ static int board_setup(void)
 
 	if (nvs->scc_en[SCC_NVS_MMC])
 		emmc = new_mem_sdhci_host((void *)nvs->scc_bar0[SCC_NVS_MMC],
-					  0, emmc_sd_clock_min, emmc_clock_max);
+			SDHCI_PLATFORM_NO_EMMC_HS200, emmc_sd_clock_min, emmc_clock_max);
 	else
 		emmc = new_pci_sdhci_host(PCI_DEV(0, 0x10, 0), 0,
 				emmc_sd_clock_min, emmc_clock_max);
