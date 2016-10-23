f67700fbdd00f1b6a996c9862ba1019a4742d333
Lin Huang <hl@rock-chips.com>
2016-10-23 14:25:54 -0700

    rockchip/rk3399: video: enable edp in depthcharge
    we enabled edp in coreboot, and it will lead to garbage dispaly
    when will reset device in firmware. So move it to depthcharge now.
    
    CQ-DEPEND=CL:402350
    BUG=chrome-os-partner:58675
    BRANCH=none
    TEST=Boot from kevin, and display work
    
    Change-Id: Ied4b2388c25f69433b6e37bf94e05f54b92f1793
    Signed-off-by: Lin Huang <hl@rock-chips.com>
    Reviewed-on: https://chromium-review.googlesource.com/402488
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Commit-Queue: Julius Werner <jwerner@chromium.org>
    Tested-by: Julius Werner <jwerner@chromium.org>

--- src/drivers/video/rk3399.c
+++ src/drivers/video/rk3399.c
@@ -27,9 +27,43 @@
 // from internal_backlight_no_als_ac_brightness
 #define DEFAULT_EC_BL_PWM_DUTY 80
 
+/* video_ctl_1 */
+#define VIDEO_EN				(0x1 << 7)
+
+/* sys_ctl_3 */
+#define STRM_VALID                              (0x1 << 2)
+
+static uint32_t *edp_video_ctl_1 = (uint32_t *)0xff970020;
+static uint32_t *edp_sys_ctl_3 = (uint32_t *)0xff970608;
 static uint32_t *vop0_sys_ctrl = (uint32_t *)0xff900008;
 static uint32_t *vop0_win0_yrgb_mst = (uint32_t *)0xff900040;
 static uint32_t *vop0_reg_cfg_done = (uint32_t *)0xff900000;
+
+static int edp_is_video_stream_on(void)
+{
+	u32 val;
+	u64 start = timer_us(0);
+
+	do {
+		val = readl(edp_sys_ctl_3);
+
+		/* must write value to update STRM_VALID bit status */
+		writel(val, edp_sys_ctl_3);
+		val = readl(edp_sys_ctl_3);
+		if (!(val & STRM_VALID))
+			return 0;
+	} while (timer_us(start) < 100 * 1000);
+
+	return -1;
+}
+
+static int edp_enable(void)
+{
+	/* Enable video at next frame */
+	setbits_le32(edp_video_ctl_1, VIDEO_EN);
+
+	return edp_is_video_stream_on();
+}
 
 /*
  * callback to turn on/off the backlight
@@ -42,6 +76,9 @@ static int rk3399_backlight_update(DisplayOps *me, uint8_t enable)
 static int rockchip_display_init(DisplayOps *me)
 {
 	uintptr_t phys_addr = lib_sysinfo.framebuffer->physical_address;
+
+	if (edp_enable())
+		return -1;
 
 	writel(phys_addr, vop0_win0_yrgb_mst);
 
