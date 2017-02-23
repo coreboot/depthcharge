f410a72b11aedcedd920e67d8d97c6fa01b277c1
Shelley Chen <shchen@chromium.org>
2017-02-22 16:36:57 -0800

    fizz: Add support for new board
    BUG=b:35775024
    BRANCH=None
    TEST=Compiles successfully
    
    Change-Id: I4ae7e9d6dda88abfa13b34e84e8d35a36cc17133
    Signed-off-by: Shelley Chen <shchen@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/448920
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>

---
+++ src/board/fizz/board.c
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <arch/io.h>
+#include <libpayload.h>
+#include <pci.h>
+#include <pci/pci.h>
+#include <sysinfo.h>
+
+#include "base/init_funcs.h"
+#include "base/list.h"
+#include "drivers/ec/cros/lpc.h"
+#include "drivers/flash/flash.h"
+#include "drivers/flash/memmapped.h"
+#include "drivers/gpio/skylake.h"
+#include "drivers/gpio/sysinfo.h"
+#include "drivers/power/pch.h"
+#include "drivers/soc/skylake.h"
+#include "drivers/storage/ahci.h"
+#include "drivers/storage/blockdev.h"
+#include "drivers/storage/sdhci.h"
+
+/*
+ * Clock frequencies for the eMMC and SD ports are defined below. The minimum
+ * frequency is the same for both interfaces, the firmware does not run any
+ * interface faster than 52 MHz, but defines maximum eMMC frequency as 200 MHz
+ * for proper divider settings.
+ */
+#define EMMC_SD_CLOCK_MIN	400000
+#define SD_CLOCK_MAX		52000000
+
+static int board_setup(void)
+{
+	sysinfo_install_flags(new_skylake_gpio_input_from_coreboot);
+
+	/* Chrome EC (eSPI) */
+	CrosEcLpcBus *cros_ec_lpc_bus =
+		new_cros_ec_lpc_bus(CROS_EC_LPC_BUS_GENERIC);
+	CrosEc *cros_ec = new_cros_ec(&cros_ec_lpc_bus->ops, 0, NULL);
+	register_vboot_ec(&cros_ec->vboot, 0);
+
+	/* 16MB SPI Flash */
+	flash_set_ops(&new_mem_mapped_flash(0xff000000, 0x1000000)->ops);
+
+	/* PCH Power */
+	power_set_ops(&skylake_power_ops);
+
+	/* SSD */
+	AhciCtrlr *ahci = new_ahci_ctrlr(PCI_DEV(0, 0x17, 0));
+	list_insert_after(&ahci->ctrlr.list_node, &fixed_block_dev_controllers);
+
+	/* SD Card (if present) */
+	pcidev_t sd_pci_dev = PCI_DEV(0, 0x1e, 6);
+	uint16_t sd_vendor_id = pci_read_config32(sd_pci_dev, REG_VENDOR_ID);
+	if (sd_vendor_id == PCI_VENDOR_ID_INTEL) {
+		SdhciHost *sd = new_pci_sdhci_host(sd_pci_dev, 1,
+					EMMC_SD_CLOCK_MIN, SD_CLOCK_MAX);
+		list_insert_after(&sd->mmc_ctrlr.ctrlr.list_node,
+					&removable_block_dev_controllers);
+	}
+
+	return 0;
+}
+
+INIT_FUNC(board_setup);
