c95ace2cc916bcba1597794e0c5c4391f21ca16c
Shelley Chen <shchen@chromium.org>
2017-05-24 15:10:02 -0700

    fizz: Enable buzzer
    Enable 30 second fw dev screen timeout buzzer.
    As this is edge triggered, creating a separate
    sound driver (gpio_edge_buzzer) for this.
    
    BUG=b:62066805, b:35775024
    BRANCH=None
    TEST=ensure short delay is not enabled in gbb_flags,
         reboot and make sure hear buzzer going off.
    
    Change-Id: I46c7d4834b34dbc9f04aa68ea0f4e219863d0cec
    Signed-off-by: Shelley Chen <shchen@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/514425
    Reviewed-by: Julius Werner <jwerner@chromium.org>

---
+++ src/drivers/sound/gpio_edge_buzzer.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ */
+
+#include <libpayload.h>
+
+#include "base/container_of.h"
+#include "drivers/sound/gpio_edge_buzzer.h"
+
+static int buzzer_play(SoundOps *me, uint32_t msec, uint32_t frequency)
+{
+	GpioEdgeBuzzer *buzzer = container_of(me, GpioEdgeBuzzer, ops);
+	u64 start;
+	int i = 0;
+
+	if (!frequency)
+		return -1;
+
+	start = timer_us(0);
+	while (timer_us(start)/1000 < msec) {
+		buzzer->gpio->set(buzzer->gpio, ++i & 1);
+		mdelay(1);
+	}
+	buzzer->gpio->set(buzzer->gpio, 0);
+
+	return 0;
+}
+
+GpioEdgeBuzzer *new_gpio_edge_buzzer(GpioOps *gpio)
+{
+	GpioEdgeBuzzer *buzzer = xzalloc(sizeof(*buzzer));
+
+	buzzer->gpio = gpio;
+	buzzer->ops.play = buzzer_play;
+
+	return buzzer;
+}
