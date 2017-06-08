487199e380ec421cdc7141dfe6407c432629c010
Marc Jones <marc.jones@scarletltd.com>
2017-06-08 16:42:17 -0600

    amd/fch: Begin power driver
    Duplicate the Intel PCH driver as the starting point for AMD.
    
    BRANCH=none
    BUG=b:62457067
    TEST=Builds clean.
    
    Change-Id: I8ce843838f7ab2880afc8ba79e189eaa3d02c17b
    Signed-off-by: Marshall Dawson <marshalldawson3rd@gmail.com>
    Signed-off-by: Marc Jones <marc.jones@scarletltd.com>
    Reviewed-on: https://chromium-review.googlesource.com/474053
    Commit-Ready: Martin Roth <martinroth@chromium.org>
    Tested-by: Martin Roth <martinroth@chromium.org>
    Reviewed-by: Martin Roth <martinroth@chromium.org>
    Reviewed-by: Stefan Reinauer <reinauer@google.com>

---
+++ src/drivers/power/fch.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2012 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __DRIVERS_POWER_PCH_H__
+#define __DRIVERS_POWER_PCH_H__
+
+#include "drivers/power/power.h"
+
+PowerOps pch_power_ops;
+PowerOps baytrail_power_ops;
+PowerOps braswell_power_ops;
+PowerOps skylake_power_ops;
+PowerOps apollolake_power_ops;
+
+#endif /* __DRIVERS_POWER_PCH_H__ */
