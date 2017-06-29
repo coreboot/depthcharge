bdeb106d4eccb0beba5037bc6acc5613ad08ce37
Subrata Banik <subrata.banik@intel.com>
2017-06-29 16:05:15 +0530

    driver/timer: Add support for Timer driver
    So far only TPM over SPI driver driver was using
    timer functionality from local scope. Moving required
    timer functionality into driver code make its easy for
    other user to access timer function in future.
    
    Change-Id: I9be08b4687d63838ae18c56a9ffd87e1384e5dca
    Signed-off-by: Duncan Laurie <dlaurie@google.com>
    Original-Commit-Id: 81e3a969357921aa44abd331675ebaacb12a0a10
    Original-Change-Id: Idc307506dc7a51533b4cde89b90f0d0906b3cdc3
    Original-Signed-off-by: Subrata Banik <subrata.banik@intel.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/555110
    Original-Reviewed-by: Aaron Durbin <adurbin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/914443

---
+++ src/drivers/timer/timer.h
@@ -0,0 +1,36 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2017 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef __DRIVERS_TIMER_H__
+#define __DRIVERS_TIMER_H__
+
+#define USECS_PER_SEC 1000000
+#define MSECS_PER_SEC 1000
+#define USECS_PER_MSEC (USECS_PER_SEC / MSECS_PER_SEC)
+
+struct stopwatch {
+	uint64_t start;
+	uint64_t expires;
+};
+
+/* Add microseconds to an absolute time. */
+void stopwatch_init_usecs_expire(struct stopwatch *sw, unsigned long us);
+/* Add miliseconds to an absolute time. */
+void stopwatch_init_msecs_expire(struct stopwatch *sw, unsigned long ms);
+/*
+ * Tick and check the stopwatch for expiration. Returns non-zero on exipration.
+ */
+int stopwatch_expired(struct stopwatch *sw);
+
+#endif /* __DRIVERS_TIMER_H__ */
