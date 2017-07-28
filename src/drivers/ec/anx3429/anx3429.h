46a1e1aa02d59f011039b2742f9b88d18fe573e2
Caveh Jalali <caveh@google.com>
2017-07-28 16:44:48 -0700

    anx3429: add skeleton for FW update driver.
    this adds a skeletal "nop" firmware update driver for the anx3429
    TCPC.  the reef board file instantiates the driver, so it's actually
    invoked at boot time.  it just tells vboot that the anx3429 firmware
    is up-to-date and no further interaction takes place.
    
    BRANCH=none
    BUG=b:35586895
    TEST=built & booted new firmware on reef.  no ill effects.
    
    Change-Id: Ia134255ec4998d08322e16d786da3e050e8279c2
    Signed-off-by: Caveh Jalali <caveh@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/592640
    Reviewed-by: Shawn N <shawnn@chromium.org>

---
+++ src/drivers/ec/anx3429/anx3429.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2017 The Chromium OS Authors. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#ifndef __DRIVERS_EC_ANX3429_H__
+#define __DRIVERS_EC_ANX3429_H__
+
+#include "drivers/bus/i2c/cros_ec_tunnel.h"
+#include "drivers/ec/cros/ec.h"
+#include "drivers/ec/vboot_aux_fw.h"
+
+typedef struct Anx3429
+{
+	VbootAuxFwOps fw_ops;
+	CrosECTunnelI2c *bus;
+	int ec_pd_id;
+	int debug_updated;
+
+	/* these are cached from chip regs */
+	struct {
+		uint16_t vendor;
+		uint16_t product;
+		uint16_t device;
+		uint8_t fw_rev;
+	} chip;
+} Anx3429;
+
+Anx3429 *new_anx3429(CrosECTunnelI2c *bus, int ec_pd_id);
+
+#endif /* __DRIVERS_EC_ANX3429_H__ */
