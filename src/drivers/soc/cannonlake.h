902c4b7151800ff10d1993d9cbf30c0ae03a2d20
Nick Vaccaro <nvaccaro@chromium.org>
2017-09-20 14:41:50 -0700

    cannonlake: add soc driver header cannonlake.h
    BUG=b:64395641
    BRANCH=None
    TEST=None
    
    Change-Id: I58c6781c23efafeaac347f4916307b30bd89e4d9
    Signed-off-by: Nick Vaccaro <nvaccaro@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/675751
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

---
+++ src/drivers/soc/cannonlake.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __DRIVERS_SOC_CANNONLAKE_H__
+#define __DRIVERS_SOC_CANNONLAKE_H__
+
+/* I2C Designware Controller runs at 120MHz */
+#define CANNONLAKE_DW_I2C_MHZ	120
+
+#endif /* __DRIVERS_SOC_CANNONLAKE_H__ */
