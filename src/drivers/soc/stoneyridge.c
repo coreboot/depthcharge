4202c7151fda1d414ff49a9095b0dafba25aabbf
Chris Ching <chingcodes@google.com>
2018-02-09 14:02:06 -0700

    drivers/soc/stoneyridge: Clean up unused gpio code
    Dead code now that cr50_irq_status is using the kern gpio driver.
    
    BUG=b:72838769
    BRANCH=none
    TEST=build for grunt
    
    Change-Id: Ibb4018799732526942d0c3b05a3798e43ea6a9a3
    Signed-off-by: Chris Ching <chingcodes@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/912361
    Commit-Ready: Martin Roth <martinroth@chromium.org>
    Tested-by: Martin Roth <martinroth@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@chromium.org>

--- src/drivers/soc/stoneyridge.c
+++ src/drivers/soc/stoneyridge.c
@@ -18,34 +18,6 @@
 
 #include "drivers/soc/stoneyridge.h"
 
-int stoneyridge_get_gpe(int gpe)
-{
-	uint32_t mask, sts;
-	uint64_t start;
-	int rc = 0;
-	const uint64_t timeout_us = 1000;
-
-	if (gpe < 0 || gpe > 31)
-		return -1;
-
-	mask = 1 << gpe;
-
-	/* Wait for GPE status to clear */
-	start = timer_us(0);
-	do {
-		if (timer_us(start) > timeout_us)
-			break;
-
-		sts = readl(EVENT_STATUS);
-		if (sts & mask) {
-			writel(mask, EVENT_STATUS);
-			rc = 1;
-		}
-	} while (sts & mask);
-
-	return rc;
-}
-
 #define CU_PTSC_MSR	0xc0010280
 #define PTSC_FREQ_MHZ	100
 
