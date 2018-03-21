0cc3540706c52e283b3a0a10ce8524b2808825fe
T Michael Turney <mturney@codeaurora.org>
2018-03-21 13:21:04 -0700

    Add new board Cheza
    BRANCH=none
    BUG=none
    TEST=build
    
    Change-Id: If3609d05d48cc3ec7de412472ae8c2cdf7b760ea
    Signed-off-by: T Michael Turney <mturney@codeaurora.org>
    Reviewed-on: https://chromium-review.googlesource.com/967434
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Reviewed-by: Julius Werner <jwerner@chromium.org>

---
+++ src/board/cheza/board.c
@@ -0,0 +1,36 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2018, The Linux Foundation.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <assert.h>
+#include <libpayload.h>
+
+#include "base/init_funcs.h"
+#include "config.h"
+#include "drivers/gpio/gpio.h"
+#include "vboot/util/flag.h"
+#include "boot/fit.h"
+
+static int board_setup(void)
+{
+	/* stub out required GPIOs for vboot */
+	flag_replace(FLAG_LIDSW, new_gpio_high());
+	flag_replace(FLAG_PWRSW, new_gpio_low());
+
+	fit_add_compat("qcom,sdm845-mtp");
+
+	return 0;
+}
+
+INIT_FUNC(board_setup);
