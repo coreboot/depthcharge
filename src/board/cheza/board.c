9d7236a0b08e0dc666f4d17e8b23fcba64fbc4cc
Chandana Kishori Chiluveru <cchiluve@codeaurora.org>
2018-04-09 14:09:57 -0700

    cheza: Add USB support
    BRANCH=none
    BUG=none
    TEST=build
    
    Change-Id: I0cfe91b6ee4b46c59774018bee0026c293536f39
    Signed-off-by: Chandana Kishori Chiluveru <cchiluve@codeaurora.org>
    Reviewed-on: https://chromium-review.googlesource.com/967438
    Commit-Ready: T Michael Turney <mturney@codeaurora.org>
    Tested-by: T Michael Turney <mturney@codeaurora.org>
    Reviewed-by: T Michael Turney <mturney@codeaurora.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>

--- src/board/cheza/board.c
+++ src/board/cheza/board.c
@@ -21,6 +21,7 @@
 #include "drivers/gpio/gpio.h"
 #include "vboot/util/flag.h"
 #include "boot/fit.h"
+#include "drivers/bus/usb/usb.h"
 
 static int board_setup(void)
 {
@@ -29,6 +30,14 @@ static int board_setup(void)
 	flag_replace(FLAG_PWRSW, new_gpio_low());
 
 	fit_add_compat("qcom,sdm845-mtp");
+
+	/* Support primary USB3.0 XHCI controller in firmware. */
+	UsbHostController *usb_host0 = new_usb_hc(XHCI, 0xa600000);
+	list_insert_after(&usb_host0->list_node, &usb_host_controllers);
+
+	/* Support secondary USB3.0 XHCI controller in firmware. */
+	UsbHostController *usb_host1 = new_usb_hc(XHCI, 0xa800000);
+	list_insert_after(&usb_host1->list_node, &usb_host_controllers);
 
 	return 0;
 }
