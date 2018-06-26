d231f43b7cfdac0c625192f38ac857897b293468
Hung-Te Lin <hungte@chromium.org>
2018-06-26 17:13:12 +0800

    board: Add initial config for board 'kukui'.
    Dummy implementation modified from oak (mt8173).
    
    BUG=b:80501386
    TEST=emerge-kukui depthcharge
    BRANCH=none
    
    Change-Id: I9878c71b35b3d569343d809158ab440e2af48472
    Signed-off-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1114575
    Reviewed-by: Joel Kitching <kitching@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

---
+++ src/board/kukui/board.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <assert.h>
+#include <libpayload.h>
+
+#include "base/init_funcs.h"
+
+
+static int board_setup(void)
+{
+	return 0;
+}
+
+INIT_FUNC(board_setup);
