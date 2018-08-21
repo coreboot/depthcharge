2691a5883be005deaa7a0d9a4f2825deb2308bca
Tristan Shieh <tristan.shieh@mediatek.com>
2018-08-21 09:16:21 +0800

    kukui: Add GPIO support
    This patch adds MT8183 GPIO driver for kukui.
    
    BRANCH=None
    BUG=b:80501386
    TEST=Boots correctly on Kukui
    
    Change-Id: Ib64e2f45e725c5955bdd37e896b5af244cf65cb7
    Signed-off-by: Tristan Shieh <tristan.shieh@mediatek.com>
    Reviewed-on: https://chromium-review.googlesource.com/1182907
    Commit-Ready: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Joel Kitching <kitching@chromium.org>

---
+++ src/drivers/gpio/mt8183.c
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2018 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <assert.h>
+#include <libpayload.h>
+
+#include "base/container_of.h"
+#include "drivers/gpio/mt8183.h"
+
+enum {
+	MAX_GPIO_REG_BITS = 32,
+};
+
+static GpioRegs *gpio_reg = (GpioRegs *)0x10005000;
+
+static int mt_set_gpio_out(GpioOps *me, u32 output)
+{
+	MtGpio *gpio = container_of(me, MtGpio, ops);
+	u32 pin = gpio->pin_num;
+	u32 pos, bit;
+	GpioRegs *reg = gpio_reg;
+
+	pos = pin / MAX_GPIO_REG_BITS;
+	bit = pin % MAX_GPIO_REG_BITS;
+
+	if (output == 0)
+		write32(&reg->dout[pos].rst, 1L << bit);
+	else
+		write32(&reg->dout[pos].set, 1L << bit);
+
+	return 0;
+}
+
+static int mt_get_gpio_in(GpioOps *me)
+{
+	MtGpio *gpio = container_of(me, MtGpio, ops);
+	u32 pin = gpio->pin_num;
+	u32 pos, bit, val;
+	GpioRegs *reg = gpio_reg;
+
+	pos = pin / MAX_GPIO_REG_BITS;
+	bit = pin % MAX_GPIO_REG_BITS;
+
+	val = read32(&reg->din[pos].val);
+
+	return (val & (1L << bit)) != 0;
+}
+
+static MtGpio *new_mtk_gpio(u32 pin)
+{
+	die_if(pin > GPIO_MAX, "Bad GPIO pin number %d.\n", pin);
+	MtGpio *gpio = xzalloc(sizeof(*gpio));
+
+	gpio->pin_num = pin;
+
+	return gpio;
+}
+
+GpioOps *new_mtk_gpio_input(u32 pin)
+{
+	MtGpio *gpio = new_mtk_gpio(pin);
+
+	gpio->ops.get = &mt_get_gpio_in;
+	return &gpio->ops;
+}
+
+GpioOps *new_mtk_gpio_output(u32 pin)
+{
+	MtGpio *gpio = new_mtk_gpio(pin);
+
+	gpio->ops.set = &mt_set_gpio_out;
+	return &gpio->ops;
+}
---
+++ src/drivers/gpio/mt8183.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __DRIVERS_GPIO_MT8183_H__
+#define __DRIVERS_GPIO_MT8183_H__
+
+#include <stdint.h>
+
+#include "drivers/gpio/gpio.h"
+
+enum {
+	GPIO_MAX = 180,
+};
+
+typedef struct {
+	uint32_t val;
+	uint32_t set;
+	uint32_t rst;
+	uint32_t align;
+} GpioValRegs;
+
+typedef struct {
+	GpioValRegs dir[6];
+	uint8_t rsv00[160];
+	GpioValRegs dout[6];
+	uint8_t rsv01[160];
+	GpioValRegs din[6];
+} GpioRegs;
+
+typedef struct {
+	GpioOps ops;
+	u32 pin_num;
+} MtGpio;
+
+GpioOps *new_mtk_gpio_input(u32 pin);
+GpioOps *new_mtk_gpio_output(u32 pin);
+
+#endif
