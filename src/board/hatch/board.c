ad7b4e5284ae4fcf1701a7ac7f05e06eb1e4d977
Aamir Bohra <aamir.bohra@intel.com>
2018-12-19 19:09:53 +0530

    hatch: Add board specific storage & spi tpm support
    This implementation adds support NVME, SD card storage support.
    Also adds SPI TPM and chrome ec support.
    
    Change-Id: I96ee747a28f04f4af62f984601338751eac39d1c
    Signed-off-by: Aamir Bohra <aamir.bohra@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/1384446
    Reviewed-by: Shelley Chen <shchen@chromium.org>

--- src/board/hatch/board.c
+++ src/board/hatch/board.c
@@ -20,8 +20,8 @@
 #include "base/init_funcs.h"
 #include "base/list.h"
 #include "config.h"
-#include "drivers/bus/i2c/designware.h"
-#include "drivers/bus/i2c/i2c.h"
+#include "drivers/bus/spi/intel_gspi.h"
+#include "drivers/ec/cros/lpc.h"
 #include "drivers/flash/flash.h"
 #include "drivers/flash/memmapped.h"
 #include "drivers/gpio/gpio.h"
@@ -31,26 +31,51 @@
 #include "drivers/storage/ahci.h"
 #include "drivers/storage/blockdev.h"
 #include "drivers/storage/nvme.h"
-#include "drivers/tpm/cr50_i2c.h"
+#include "drivers/storage/sdhci.h"
+#include "drivers/tpm/spi.h"
 #include "drivers/tpm/tpm.h"
+
+/* Clock frequencies for the SD ports are defined below */
+#define SD_CLOCK_MIN	400000
+#define SD_CLOCK_MAX	52000000
 
 static int cr50_irq_status(void)
 {
-	return cannonlake_get_gpe(GPE0_DW2_18);
+	return cannonlake_get_gpe(GPE0_DW1_21);
+}
+
+static void hatch_setup_tpm(void)
+{
+	if (IS_ENABLED(CONFIG_DRIVER_TPM_SPI)) {
+		/* SPI TPM */
+		const IntelGspiSetupParams gspi0_params = {
+			.dev = PCI_DEV(0, 0x1e, 2),
+			.cs_polarity = SPI_POLARITY_LOW,
+			.clk_phase = SPI_CLOCK_PHASE_FIRST,
+			.clk_polarity = SPI_POLARITY_LOW,
+			.ref_clk_mhz = 120,
+			.gspi_clk_mhz = 1,
+		};
+		tpm_set_ops(&new_tpm_spi(new_intel_gspi(&gspi0_params),
+					 cr50_irq_status)->ops);
+	}
 }
 
 static int board_setup(void)
 {
 	sysinfo_install_flags(NULL);
 
+	/* TPM */
+	hatch_setup_tpm();
+
+	/* Chrome EC (eSPI) */
+	CrosEcLpcBus *cros_ec_lpc_bus =
+		new_cros_ec_lpc_bus(CROS_EC_LPC_BUS_GENERIC);
+	CrosEc *cros_ec = new_cros_ec(&cros_ec_lpc_bus->ops, 0, NULL);
+	register_vboot_ec(&cros_ec->vboot, 0);
+
 	/* 32MB SPI Flash */
 	flash_set_ops(&new_mem_mapped_flash(0xfe000000, 0x2000000)->ops);
-
-	/* H1 TPM on I2C bus 4 @ 400KHz, controller core is 133MHz */
-	DesignwareI2c *i2c4 = new_pci_designware_i2c(
-		PCI_DEV(0, 0x19, 0), 400000, CANNONLAKE_DW_I2C_MHZ);
-	tpm_set_ops(&new_cr50_i2c(&i2c4->ops, 0x50,
-				  &cr50_irq_status)->base.ops);
 
 	/* Cannonlake PCH */
 	power_set_ops(&cannonlake_power_ops);
@@ -59,9 +84,16 @@ static int board_setup(void)
 	AhciCtrlr *ahci = new_ahci_ctrlr(PCI_DEV(0, 0x17, 0));
 	list_insert_after(&ahci->ctrlr.list_node, &fixed_block_dev_controllers);
 
-	/* M.2 2280 SSD x4 */
-	NvmeCtrlr *nvme = new_nvme_ctrlr(PCI_DEV(0, 0x1d, 4));
-	list_insert_after(&nvme->ctrlr.list_node, &fixed_block_dev_controllers);
+	/* NVMe SSD */
+	NvmeCtrlr *nvme = new_nvme_ctrlr(PCI_DEV(0, 0x1d, 0));
+	list_insert_after(&nvme->ctrlr.list_node,
+				&fixed_block_dev_controllers);
+
+	/* SD Card */
+	SdhciHost *sd = new_pci_sdhci_host(PCI_DEV(0, 0x14, 5), 1,
+			SD_CLOCK_MIN, SD_CLOCK_MAX);
+	list_insert_after(&sd->mmc_ctrlr.ctrlr.list_node,
+			&removable_block_dev_controllers);
 
 	return 0;
 }
