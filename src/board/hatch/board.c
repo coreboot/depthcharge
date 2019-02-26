bb29d837f9fb7a4f263a03901e4702f641ef3264
V Sowmya <v.sowmya@intel.com>
2019-02-26 06:04:25 +0530

    board/hatch: Enable boot beep for hatch
    This patch enables the boot beep feature for hatch platform.
    
    BUG=b:123639961
    BRANCH=none
    TEST=Verified the boot beep using devbeep command on hatch.
    
    Change-Id: Ia5d89a22d0e5995bb89c408e64531a0deb8dfbdc
    Signed-off-by: V Sowmya <v.sowmya@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/1488352
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>

--- src/board/hatch/board.c
+++ src/board/hatch/board.c
@@ -34,6 +34,15 @@
 #include "drivers/storage/sdhci.h"
 #include "drivers/tpm/spi.h"
 #include "drivers/tpm/tpm.h"
+#include "drivers/sound/i2s.h"
+#include "drivers/sound/max98357a.h"
+#include "drivers/gpio/cannonlake.h"
+#include "drivers/gpio/gpio.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
+#include "drivers/bus/i2s/cavs_1_8-regs.h"
+
+#define AUD_VOLUME              4000
+#define SDMODE_PIN              GPP_H3
 
 /* Clock frequencies for the SD ports are defined below */
 #define SD_CLOCK_MIN	400000
@@ -79,6 +88,20 @@ static int board_setup(void)
 
 	/* Cannonlake PCH */
 	power_set_ops(&cannonlake_power_ops);
+
+	/* Audio Setup (for boot beep) */
+	GpioOps *sdmode = &new_cannonlake_gpio_output(SDMODE_PIN, 0)->ops;
+
+	I2s *i2s = new_i2s_structure(&max98357a_settings, 16, sdmode,
+			SSP_I2S1_START_ADDRESS);
+	I2sSource *i2s_source = new_i2s_source(&i2s->ops, 48000, 2, AUD_VOLUME);
+	/* Connect the Codec to the I2S source */
+	SoundRoute *sound_route = new_sound_route(&i2s_source->ops);
+	max98357aCodec *speaker_amp = new_max98357a_codec(sdmode);
+
+	list_insert_after(&speaker_amp->component.list_node,
+		&sound_route->components);
+	sound_set_ops(&sound_route->ops);
 
 	/* SATA AHCI */
 	AhciCtrlr *ahci = new_ahci_ctrlr(PCI_DEV(0, 0x17, 0));
