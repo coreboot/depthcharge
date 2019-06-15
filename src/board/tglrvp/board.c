2dc35c7b64726f1fa7a446d8c2971dc911ec01de
Francois Toguo <francois.toguo.fotso@intel.com>
2019-06-15 12:59:21 -0700

    platform/depthcharge: Enable depthcharge for TGL rvp
    Changes for supporting TGL rvp
    
    BRANCH=None
    BUG=None
    TEST=emerge successfully build for TGL
    
    Change-Id: I0596d97d00849a83cb0fadef8cd55fec1b3a9a5d
    Signed-off-by: Francois Toguo <francois.toguo.fotso@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/1661460
    Reviewed-by: Srinidhi N Kaushik <srinidhi.n.kaushik@intel.com>
    Reviewed-by: Wonkyu Kim <wonkyu.kim@intel.com>
    Reviewed-by: Duncan Laurie <dlaurie@google.com>
    Tested-by: Wonkyu Kim <wonkyu.kim@intel.com>
    Commit-Queue: Duncan Laurie <dlaurie@google.com>

---
+++ src/board/tglrvp/board.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2019 Intel Corporation
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+/*
+ * These needs to be included first.
+ * Some of the driver headers would be dependent on these.
+ */
+#include <pci.h>
+#include <pci/pci.h>
+
+#include "base/init_funcs.h"
+#include "base/list.h"
+#include "drivers/flash/flash.h"
+#include "drivers/flash/memmapped.h"
+#include "drivers/storage/ahci.h"
+#include "drivers/gpio/sysinfo.h"
+#include "drivers/power/pch.h"
+#include "drivers/storage/blockdev.h"
+#include "drivers/storage/nvme.h"
+#include <libpayload.h>
+#include <sysinfo.h>
+
+
+static int board_setup(void)
+{
+	uint8_t secondary_bus;
+
+	sysinfo_install_flags(NULL);
+
+	/* 32MB SPI Flash */
+	flash_set_ops(&new_mem_mapped_flash(0xfe000000, 0x2000000)->ops);
+
+	/* PCH Power */
+	power_set_ops(&cannonlake_power_ops);
+
+        /* SATA SSD */
+        AhciCtrlr *ahci = new_ahci_ctrlr(PCI_DEV(0, 0x17, 0));
+        list_insert_after(&ahci->ctrlr.list_node, &fixed_block_dev_controllers);
+
+	/* NVME SSD */
+	secondary_bus = pci_read_config8(PCI_DEV(0, 0x1D, 0),
+			REG_SECONDARY_BUS);
+	NvmeCtrlr *nvme = new_nvme_ctrlr(PCI_DEV(secondary_bus, 0, 0));
+	list_insert_after(&nvme->ctrlr.list_node, &fixed_block_dev_controllers);
+
+	return 0;
+}
+
+INIT_FUNC(board_setup);
