3e8490f75ae7aa28326cd2af99449767a9fc4d4f
T Michael Turney <mturney@codeaurora.org>
2019-06-20 07:31:57 -0700

    trogdor: initial BSP check-in
    Change-Id: Ib91763901e6d51cb58fd5ba3024d97dfe2835370
    Signed-off-by: T Michael Turney <mturney@codeaurora.org>
    Signed-off-by: Ashwin Kumar <ashk@codeaurora.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/1847193
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Commit-Queue: Julius Werner <jwerner@chromium.org>
    Tested-by: Julius Werner <jwerner@chromium.org>

---
+++ src/board/trogdor/board.c
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2019, The Linux Foundation.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <assert.h>
+#include <libpayload.h>
+
+#include "base/init_funcs.h"
+#include "drivers/gpio/gpio.h"
+#include "vboot/util/flag.h"
+#include "boot/fit.h"
+
+static int board_setup(void)
+{
+	/* stub out required GPIOs for vboot */
+	flag_replace(FLAG_LIDSW, new_gpio_high());
+	flag_replace(FLAG_WPSW,  new_gpio_high());
+	flag_replace(FLAG_PWRSW, new_gpio_low());
+
+	return 0;
+}
+
+INIT_FUNC(board_setup);
