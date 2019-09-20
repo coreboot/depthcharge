dde7711336f679a12b2309afc0127e38757770bb
satya priya <skakit@codeaurora.org>
2019-09-20 11:05:02 +0530

    trogdor: Add I2C driver
    Add I2C functionality in depthcharge.
    
    Change-Id: I5cce29ee9e367addf24ab5314c6a5c80fef32c3a
    Signed-off-by: Roja Rani Yarubandi <rojay@codeaurora.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/1918861
    Commit-Queue: Julius Werner <jwerner@chromium.org>
    Tested-by: Julius Werner <jwerner@chromium.org>
    Reviewed-by: Julius Werner <jwerner@chromium.org>

---
+++ src/drivers/bus/i2c/qcom_qupv3_i2c.c
@@ -0,0 +1,69 @@
+/*
+ * This file is part of the depthcharge project.
+ *
+ * Copyright (C) 2018-2019, The Linux Foundation.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "drivers/bus/i2c/qcom_qupv3_i2c.h"
+
+static int i2c_do_xfer(QupRegs *reg_addr, I2cSeg segment, unsigned int prams)
+{
+	unsigned int cmd = segment.read ? 2 : 1;
+	unsigned int master_cmd_reg_val = (cmd << M_OPCODE_SHFT);
+	void *dout = NULL, *din = NULL;
+
+	if (!segment.read) {
+		write32(&reg_addr->tx_trans_len.i2c_tx_trans_len, segment.len);
+		write32(&reg_addr->geni_tx_watermark_reg, TX_WATERMARK);
+		dout = segment.buf;
+	} else {
+		write32(&reg_addr->rx_trans_len.i2c_rx_trans_len, segment.len);
+		din = segment.buf;
+	}
+
+	master_cmd_reg_val |= (prams & M_PARAMS_MSK);
+	write32(&reg_addr->geni_m_cmd0, master_cmd_reg_val);
+
+	return qup_handle_transfer(reg_addr, dout, din, segment.len);
+}
+
+static int i2c_transfer(struct I2cOps *me, I2cSeg *segments, int seg_count)
+{
+	Sc7180I2c *bus = container_of(me, Sc7180I2c, ops);
+	I2cSeg *seg = segments;
+	int ret = 0;
+
+	while (!ret && seg_count--) {
+		/* If stretch bit is set to 1 master will send repeated start
+		 * condition to slave to continue the transfer, otherwise if
+		 * it's set to 0 master will send stop bit to slave to stop
+		 * the transfer
+		 */
+		u32 stretch = (seg_count ? 1 : 0);
+		u32 m_param = 0;
+
+		m_param |= (stretch << 2);
+		m_param |= ((seg->chip & 0x7F) << 9);
+		ret = i2c_do_xfer(bus->reg_addr, *seg, m_param);
+		seg++;
+	}
+	return ret;
+}
+
+Sc7180I2c *new_sc7180_i2c(uintptr_t regs)
+{
+	Sc7180I2c *bus = xzalloc(sizeof(*bus));
+
+	bus->reg_addr = (QupRegs *)regs;
+	bus->ops.transfer = &i2c_transfer;
+	return bus;
+}
---
+++ src/drivers/bus/i2c/qcom_qupv3_i2c.h
@@ -0,0 +1,29 @@
+/*
+ * This file is part of the depthcharge project.
+ *
+ * Copyright (C) 2018-2019, The Linux Foundation.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __DRIVERS_BUS_I2C_SC7180_I2C_H__
+#define __DRIVERS_BUS_I2C_SC7180_I2C_H__
+
+#include "drivers/bus/i2c/i2c.h"
+#include "drivers/soc/qcom_qup_se.h"
+
+typedef struct {
+	I2cOps ops;
+	QupRegs *reg_addr;
+} Sc7180I2c;
+
+Sc7180I2c *new_sc7180_i2c(uintptr_t regs);
+
+#endif /* __DRIVERS_BUS_I2C_SC7180_I2C_H__ */
