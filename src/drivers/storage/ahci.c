f982dd53fa51eaae455f3be03d5a6b94f5e918a0
Jamie Chen <jamie.chen@intel.com>
2019-10-17 16:27:15 +0800

    ahci: skip ahci initialize if mmio base is invalid
    When coreboot disable AHCI, fetch mmio_base will get 0xffffffff.
    Check if ahci mmio_base is 0xffffffff, then return -1 to skip
    AHCI initialize.
    
    TEST=Build and boot kindred with GBB=0x0.
     Use cbmem -t to check time of storage initialization.
     With    this patch is 53ms
     Without this patch is 1090ms
    BUG=b:139329511
    BRANCH=None
    
    Change-Id: I144fc3159e9cfb62c9bd0ae8df9e159b15ac171a
    Signed-off-by: Jamie Chen <jamie.chen@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/1866335
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Tested-by: Kane Chen <kane.chen@intel.corp-partner.google.com>
    Commit-Queue: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/drivers/storage/ahci.c
+++ src/drivers/storage/ahci.c
@@ -491,6 +491,10 @@ static int ahci_ctrlr_init(BlockDevCtrlrOps *me)
 
 	ctrlr->mmio_base = (void *)pci_read_resource(ctrlr->dev, 5);
 	printf("AHCI MMIO base = %p\n", ctrlr->mmio_base);
+	if (ctrlr->mmio_base == (void *)(0xffffffff)) {
+		printf("mmio is invalid, skip AHCI init\n");
+		return -1;
+	}
 
 	// JMicron-specific fixup taken from kernel:
 	// make sure we're in AHCI mode
