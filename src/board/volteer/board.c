72dd74544eaeaa47b664a111b2cac0f4852150a6
Nick Vaccaro <nvaccaro@google.com>
2019-10-18 16:44:43 -0700

    volteer: Add support for new mainboard
    BUG=b:142961277
    BRANCH=master
    TEST="emerge-volteer depthcharge" builds successfully.
    
    Change-Id: I2d5f5455171cec07a39dbf2f35e76a93d532ce9f
    Signed-off-by: Nick Vaccaro <nvaccaro@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/1879878
    Reviewed-by: Furquan Shaikh <furquan@chromium.org>

---
+++ src/board/volteer/board.c
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (C) 2019 Google Inc.  */
+
+/*
+ * These needs to be included first.
+ * Some of the driver headers would be dependent on these.
+ */
+#include <libpayload.h>
+#include <pci.h>
+#include <pci/pci.h>
+#include <sysinfo.h>
+
+#include "base/init_funcs.h"
+#include "base/list.h"
+#include "drivers/bus/spi/intel_gspi.h"
+#include "drivers/ec/cros/lpc.h"
+#include "drivers/flash/flash.h"
+#include "drivers/flash/memmapped.h"
+#include "drivers/gpio/sysinfo.h"
+#include "drivers/power/pch.h"
+//#include "drivers/soc/tigerlake.h"
+#include "drivers/soc/cannonlake.h"
+#include "drivers/storage/ahci.h"
+#include "drivers/storage/blockdev.h"
+#include "drivers/storage/nvme.h"
+#include "drivers/tpm/spi.h"
+#include "drivers/tpm/tpm.h"
+
+static int cr50_irq_status(void)
+{
+	return cannonlake_get_gpe(GPE0_DW0_21); /* GPP_C21 */
+}
+
+static void volteer_setup_tpm(void)
+{
+	/* SPI TPM */
+	const IntelGspiSetupParams gspi0_params = {
+		.dev = PCI_DEV(0, 0x1e, 2),
+		.cs_polarity = SPI_POLARITY_LOW,
+		.clk_phase = SPI_CLOCK_PHASE_FIRST,
+		.clk_polarity = SPI_POLARITY_LOW,
+		.ref_clk_mhz = 100,
+		.gspi_clk_mhz = 1,
+	};
+	tpm_set_ops(&new_tpm_spi(new_intel_gspi(&gspi0_params),
+		cr50_irq_status)->ops);
+}
+
+static int board_setup(void)
+{
+	sysinfo_install_flags(NULL);
+
+	/* TPM */
+	volteer_setup_tpm();
+
+	/* Chrome EC (eSPI) */
+	CrosEcLpcBus *cros_ec_lpc_bus =
+		new_cros_ec_lpc_bus(CROS_EC_LPC_BUS_GENERIC);
+	CrosEc *cros_ec = new_cros_ec(&cros_ec_lpc_bus->ops, 0, NULL);
+	register_vboot_ec(&cros_ec->vboot, 0);
+
+	/* 32MB SPI Flash */
+	flash_set_ops(&new_mem_mapped_flash(0xfe000000, 0x2000000)->ops);
+
+	/* PCH Power */
+	power_set_ops(&cannonlake_power_ops);
+
+	/* NVME SSD */
+	NvmeCtrlr *nvme = new_nvme_ctrlr(PCI_DEV(0, 0x1d, 0));
+	list_insert_after(&nvme->ctrlr.list_node, &fixed_block_dev_controllers);
+
+	/* SATA AHCI */
+	AhciCtrlr *ahci = new_ahci_ctrlr(PCI_DEV(0, 0x17, 0));
+	list_insert_after(&ahci->ctrlr.list_node, &fixed_block_dev_controllers);
+
+	return 0;
+}
+
+INIT_FUNC(board_setup);
