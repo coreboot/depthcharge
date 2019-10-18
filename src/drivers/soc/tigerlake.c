bc3efdbbeeb3eec1a4ae49c2e55d3003f8f95a6c
Shaunak Saha <shaunak.saha@intel.com>
2019-10-18 16:01:18 -0700

    tgl: Add tigerlake_get_gpe() support
    tigerlake_get_gpe which helps to get specific gpe for TGL soc
    
    BUG=b:142961277
    BRANCH=master
    TEST="emerge-volteer depthcharge" builds successfully and boot to kernel.
    
    Change-Id: Ibff04e53c523c8b09bd36b61f8222b6fda44473b
    Signed-off-by: Shaunak Saha <shaunak.saha@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/1869796
    Commit-Queue: caveh jalali <caveh@chromium.org>
    Tested-by: caveh jalali <caveh@chromium.org>
    Reviewed-by: Nick Vaccaro <nvaccaro@google.com>

---
+++ src/drivers/soc/tigerlake.c
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "drivers/soc/common/iomap.h"
+#include "drivers/soc/intel_common.h"
+#include "drivers/soc/tigerlake.h"
+
+static const SocGpeConfig cfg = {
+	.gpe_max = GPE_MAX,
+	.acpi_base = ACPI_BASE_ADDRESS,
+	.gpe0_sts_off = GPE0_STS_OFF,
+};
+
+int tigerlake_get_gpe(int gpe)
+{
+	return soc_get_gpe(gpe, &cfg);
+}
