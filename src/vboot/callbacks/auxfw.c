8407d59ee547cb64d06feb598a4cbd7f563e330c
Tim Wawrzynczak <twawrzynczak@chromium.org>
2019-10-24 16:35:16 -0600

    vboot: Update vboot external APIs to match vboot_reference
    vboot_reference is being updated to eliminate the 'devidx' argument
    from external VbExEc* API functions; do the same here.  There is a minor
    functional change as well that VbExEcGetExpectedImage is no longer in
    vboot_reference, as it simply passed the buffer and size straight to
    depthcharge via VbExEcUpdateImage anyway, so now the update_image EC
    function must get a pointer to the file contents in CBFS instead.
    
    Also all vboot EC and auxfw callbacks have changed the naming convention
    to vb2ex_ec_* / vb2ex_auxfw_*.
    
    Drops support for "PD" software sync.
    
    BUG=b:112198832, b:143094352, chromium:1017093
    BRANCH=none
    TEST=Software sync still works, both in the update and no-update cases.
    
    Cq-Depend: chromium:1872255, chromium:1877065, chromium:1895712
    Change-Id: Ife5f9a5481525804a421f50f9ed6020b795731c5
    Signed-off-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/1910562
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    (cherry picked from commit 39bf1ff90291c892b893cc87ac04861b89233e2d)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/1925909
    Reviewed-by: Shelley Chen <shchen@chromium.org>
    Commit-Queue: Shelley Chen <shchen@chromium.org>
    Tested-by: Shelley Chen <shchen@chromium.org>

---
+++ src/vboot/callbacks/auxfw.c
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright 2019 Google LLC.
+ *
+ * Vboot callbacks for auxfw software sync.
+ */
+
+#include <libpayload.h>
+#include <vb2_api.h>
+
+#include "drivers/ec/vboot_ec.h"
+#include "drivers/ec/vboot_aux_fw.h"
+
+/**
+ * Send the EC the command to close the auxiliary FW tunnels.
+ *
+ * @return VB2_ERROR_UNKNOWN on error, VB2_SUCCESS on success.
+ */
+static vb2_error_t protect_fw_tunnels(void)
+{
+	VbootEcOps *ec = vboot_get_ec(PRIMARY_VBOOT_EC);
+
+	/*
+	 * The entire auxiliary FW update is complete at this point. AP firmware
+	 * won't need to communicate to peripherals past this point, so protect
+	 * the remote bus/tunnel to prevent OS from accessing it later.
+	 */
+	if (ec->protect_tcpc_ports && ec->protect_tcpc_ports(ec)) {
+		printf("Some remote tunnels in EC may be unprotected\n");
+		return VB2_ERROR_UNKNOWN;
+	}
+
+	return VB2_SUCCESS;
+}
+
+/*
+ * Vboot callback to check for severity of auxfw update.
+ */
+vb2_error_t vb2ex_auxfw_check(enum vb2_auxfw_update_severity *severity)
+{
+	return check_vboot_aux_fw(severity);
+}
+
+/*
+ * Vboot callback to update auxfw.
+ */
+vb2_error_t vb2ex_auxfw_update(void)
+{
+	return update_vboot_aux_fw();
+}
+
+/*
+ * Vboot callback to protect auxfw tunnels after sync.
+ */
+vb2_error_t vb2ex_auxfw_finalize(struct vb2_context *ctx)
+{
+	return protect_fw_tunnels();
+}
