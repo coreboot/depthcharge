44cb41bcb25f6b1fa5a1f001981be215d06c3797
Karthikeyan Ramasubramanian <kramasub@google.com>
2020-01-16 16:47:29 -0700

    board/dedede: Set up TPM
    BUG=b:144768001, b:150793146
    BRANCH=None
    TEST=Build and boot the device. Ensure that users can login.
    
    Change-Id: I5e52d7be30b24441113d8d296c3ba633f483eba8
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/2006533
    Tested-by: Karthikeyan Ramasubramanian <kramasub@chromium.org>
    Tested-by: Kirtika Ruchandani <kirtika@chromium.org>
    Reviewed-by: Kirtika Ruchandani <kirtika@chromium.org>
    Commit-Queue: Karthikeyan Ramasubramanian <kramasub@chromium.org>

--- src/board/dedede/board.c
+++ src/board/dedede/board.c
@@ -7,6 +7,7 @@
 
 #include "base/init_funcs.h"
 #include "base/list.h"
+#include "drivers/bus/spi/intel_gspi.h"
 #include "drivers/ec/cros/ec.h"
 #include "drivers/ec/cros/lpc.h"
 #include "drivers/flash/flash.h"
@@ -17,6 +18,7 @@
 #include "drivers/soc/jasperlake.h"
 #include "drivers/storage/blockdev.h"
 #include "drivers/storage/sdhci.h"
+#include "drivers/tpm/spi.h"
 
 /*
  * Clock frequencies for the eMMC and SD ports are defined below. The minimum
@@ -28,12 +30,37 @@
 #define EMMC_CLOCK_MAX          200000000
 #define SD_CLOCK_MAX		52000000
 
+static int cr50_irq_status(void)
+{
+	return jasperlake_get_gpe(GPE0_DW0_04); /* GPP_B4 */
+}
+
+static void dedede_setup_tpm(void)
+{
+	if (CONFIG(DRIVER_TPM_SPI)) {
+		/* SPI TPM */
+		const IntelGspiSetupParams gspi0_params = {
+			.dev = PCI_DEV(0, 0x1e, 2),
+			.cs_polarity = SPI_POLARITY_LOW,
+			.clk_phase = SPI_CLOCK_PHASE_FIRST,
+			.clk_polarity = SPI_POLARITY_LOW,
+			.ref_clk_mhz = 120,
+			.gspi_clk_mhz = 1,
+		};
+		tpm_set_ops(&new_tpm_spi(new_intel_gspi(&gspi0_params),
+					 cr50_irq_status)->ops);
+	}
+}
+
 static int board_setup(void)
 {
 	sysinfo_install_flags(new_jasperlake_gpio_input_from_coreboot);
 
 	/* 32MB SPI Flash */
 	flash_set_ops(&new_mem_mapped_flash(0xfe000000, 0x2000000)->ops);
+
+	/* TPM */
+	dedede_setup_tpm();
 
 	/* Chrome EC (eSPI) */
 	CrosEcLpcBus *cros_ec_lpc_bus =
