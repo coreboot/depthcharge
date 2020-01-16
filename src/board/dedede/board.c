1899663188b9c414ac5557b78afbe60e6aa56106
Karthikeyan Ramasubramanian <kramasub@google.com>
2020-01-16 16:55:38 -0700

    board/dedede: Register EC
    BUG=b:144768001
    BRANCH=None
    TEST=emerge-dedede depthcharge
    
    Change-Id: I3bf33fe07fb9c74d1d24bb330ac63b8b76583323
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/2006534
    Tested-by: Karthikeyan Ramasubramanian <kramasub@chromium.org>
    Reviewed-by: Kirtika Ruchandani <kirtika@chromium.org>
    Reviewed-by: Justin TerAvest <teravest@chromium.org>

--- src/board/dedede/board.c
+++ src/board/dedede/board.c
@@ -6,6 +6,8 @@
  */
 
 #include "base/init_funcs.h"
+#include "drivers/ec/cros/ec.h"
+#include "drivers/ec/cros/lpc.h"
 #include "drivers/flash/flash.h"
 #include "drivers/flash/memmapped.h"
 #include "drivers/gpio/jasperlake.h"
@@ -19,6 +21,12 @@ static int board_setup(void)
 
 	/* 32MB SPI Flash */
 	flash_set_ops(&new_mem_mapped_flash(0xfe000000, 0x2000000)->ops);
+
+	/* Chrome EC (eSPI) */
+	CrosEcLpcBus *cros_ec_lpc_bus =
+		new_cros_ec_lpc_bus(CROS_EC_LPC_BUS_GENERIC);
+	CrosEc *cros_ec = new_cros_ec(&cros_ec_lpc_bus->ops, 0, NULL);
+	register_vboot_ec(&cros_ec->vboot, 0);
 
 	/* PCH Power */
 	power_set_ops(&jasperlake_power_ops);
