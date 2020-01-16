b078a910924f2c0a66dc49da1477bf734004e94b
Karthikeyan Ramasubramanian <kramasub@google.com>
2020-01-16 16:30:45 -0700

    board/dedede: Set up flash
    BUG=b:144768001
    BRANCH=None
    TEST=emerge-dedede depthcharge
    
    Change-Id: I28244a731c58fab5b863a2b6f5e0b713b58b5b9c
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/2006532
    Tested-by: Karthikeyan Ramasubramanian <kramasub@chromium.org>
    Reviewed-by: Justin TerAvest <teravest@chromium.org>
    Reviewed-by: Kirtika Ruchandani <kirtika@chromium.org>
    Commit-Queue: Karthikeyan Ramasubramanian <kramasub@chromium.org>

--- src/board/dedede/board.c
+++ src/board/dedede/board.c
@@ -6,6 +6,8 @@
  */
 
 #include "base/init_funcs.h"
+#include "drivers/flash/flash.h"
+#include "drivers/flash/memmapped.h"
 #include "drivers/gpio/jasperlake.h"
 #include "drivers/gpio/sysinfo.h"
 #include "drivers/power/pch.h"
@@ -14,6 +16,9 @@
 static int board_setup(void)
 {
 	sysinfo_install_flags(new_jasperlake_gpio_input_from_coreboot);
+
+	/* 32MB SPI Flash */
+	flash_set_ops(&new_mem_mapped_flash(0xfe000000, 0x2000000)->ops);
 
 	/* PCH Power */
 	power_set_ops(&jasperlake_power_ops);
