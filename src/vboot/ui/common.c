1ac8379bc841fd2b5a580a5eb532abca3a762d79
Shelley Chen <shchen@google.com>
2020-07-01 16:18:04 -0700

    vboot/ui: Display error dialog box when showing error
    Currently we're using the fallback display for displaying error
    messages.  Tweaking a bit so that we used a dialog box with rounded
    corners and a back button using feedback from UX team.
    
    BUG=b:144969091
    BRANCH=Puff
    TEST=dut-control power_state:rec
         press ctrl-d
         Observe dialog box on the screen pop up
    
    Signed-off-by: Shelley Chen <shchen@google.com>
    Change-Id: Ic915acffd7ac51b657ac4b2775063848221da634
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/2285989
    Tested-by: Shelley Chen <shchen@chromium.org>
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Commit-Queue: Shelley Chen <shchen@chromium.org>

--- src/vboot/ui/common.c
+++ src/vboot/ui/common.c
@@ -146,12 +146,104 @@ vb2_error_t print_fallback_message(const char *str)
 	return rv;
 }
 
+static vb2_error_t print_error_message(const char *str, const char *locale_code)
+{
+	vb2_error_t rv = VB2_SUCCESS;
+	int32_t x, y;
+	int32_t content_width;
+	int32_t box_width;
+	int32_t button_width;
+	char *buf, *end, *line;
+	const enum ui_char_style style = UI_CHAR_STYLE_DEFAULT;
+
+	/* Copy str to buf since strsep() will modify the string. */
+	buf = strdup(str);
+	if (!buf) {
+		UI_ERROR("Failed to malloc string buffer\n");
+		return VB2_ERROR_UI_DRAW_FAILURE;
+	}
+
+	/* Center the box on the screen */
+	x = (UI_SCALE - UI_ERROR_BOX_WIDTH) / 2;
+	y = (UI_SCALE - UI_ERROR_BOX_HEIGHT) / 2;
+	box_width = UI_SCALE - UI_MARGIN_H * 2;
+	content_width = box_width - UI_BOX_PADDING_H * 2;
+
+	/* Clear printing area. */
+	ui_draw_rounded_box(x, y,
+			    UI_ERROR_BOX_WIDTH,
+			    UI_ERROR_BOX_HEIGHT,
+			    &ui_color_error_box,
+			    0,
+			    UI_ERROR_BOX_RADIUS,
+			    0);
+
+	x += UI_ERROR_BOX_PADDING_H;
+	y += UI_ERROR_BOX_PADDING_V;
+
+	/* Insert icon. */
+	struct ui_bitmap bitmap;
+	VB2_TRY(ui_get_bitmap("ic_info.bmp", NULL, 0, &bitmap));
+	VB2_TRY(ui_draw_bitmap(&bitmap, x, y,
+			       UI_ERROR_BOX_ICON_HEIGHT,
+			       UI_ERROR_BOX_ICON_HEIGHT,
+			       PIVOT_H_LEFT | PIVOT_V_TOP,
+			       0));
+
+	/* Insert in the body */
+	y += UI_ERROR_BOX_SECTION_SPACING + UI_ERROR_BOX_ICON_HEIGHT;
+	end = buf;
+	while ((line = strsep(&end, "\n"))) {
+		vb2_error_t line_rv;
+		int32_t width;
+		int32_t height = UI_BOX_TEXT_HEIGHT;
+		/* Ensure the text width is no more than box width */
+		line_rv = ui_get_text_width(line, height, style, &width);
+		if (line_rv) {
+			/* Save the first error in rv */
+			if (rv == VB2_SUCCESS)
+				rv = line_rv;
+			continue;
+		}
+		if (width > content_width)
+			height = height * content_width / width;
+		line_rv = ui_draw_text(line, x, y, height,
+				       PIVOT_H_LEFT | PIVOT_V_TOP, style, 0);
+		y += height + UI_BOX_TEXT_LINE_SPACING;
+		/* Save the first error in rv */
+		if (line_rv && rv == VB2_SUCCESS)
+			rv = line_rv;
+	}
+
+	free(buf);
+
+	/* Insert "Back" button */
+	VB2_TRY(ui_get_bitmap("btn_back.bmp", locale_code, 1, &bitmap));
+	int32_t text_width;
+	VB2_TRY(ui_get_bitmap_width(&bitmap, UI_BUTTON_TEXT_HEIGHT,
+				    &text_width));
+
+	button_width = text_width + (UI_BUTTON_TEXT_PADDING_H * 2);
+	/* x and y are top-left corner of the button */
+	x = (UI_SCALE + UI_ERROR_BOX_WIDTH) / 2 -
+		UI_ERROR_BOX_PADDING_H - button_width;
+	y = (UI_SCALE + UI_ERROR_BOX_HEIGHT) / 2 -
+		UI_ERROR_BOX_PADDING_V - UI_BUTTON_HEIGHT;
+	ui_draw_button("btn_back.bmp", locale_code,
+		       x, y,
+		       button_width,
+		       UI_BUTTON_HEIGHT,
+		       0, 1);
+
+	return rv;
+}
+
 vb2_error_t ui_display_screen(struct ui_state *state,
 			      const struct ui_state *prev_state)
 {
 	vb2_error_t rv;
 	const struct ui_screen_info *screen = state->screen;
-	const char *error_body;
+	const char *error_body = error_map[state->error_code].body;
 
 	VB2_TRY(init_screen());
 
@@ -176,11 +268,10 @@ vb2_error_t ui_display_screen(struct ui_state *state,
 	 * priority and don't print out more error messages.  Also,
 	 * print out the error message to the AP console.
 	 */
-	error_body = error_map[state->error_code].body;
 	if (!rv &&
 	    state->error_code != VB2_UI_ERROR_NONE &&
 	    error_body != NULL) {
-		print_fallback_message(error_body);
+		print_error_message(error_body, state->locale->code);
 		UI_ERROR("%s\n", error_body);
 	}
 
