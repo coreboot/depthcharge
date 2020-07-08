f34446d851e88593ba3bd9bd576e67e00318b8f3
Meng-Huan Yu <menghuan@chromium.org>
2020-07-08 18:28:32 +0800

    minidiag: mem: Support memory test
    The memory test split the task to small pieces, and run a small batch
    each times. Roughly take 50~100ms. Then, it will return the current
    status as string and give the control back to UI for drawing the screen
    and handling user keyboard input.
    
    BRANCH=none
    BUG=b:156693348
    TEST=emerge-hatch depthcharge
    
    Signed-off-by: Meng-Huan Yu <menghuan@chromium.org>
    Change-Id: I965b396c0663650d1c3b389d0b1d1f2cd1ed52f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/2228021
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Queue: Hung-Te Lin <hungte@chromium.org>

---
+++ src/diag/memory.h
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright 2020 Google Inc.
+ */
+
+#ifndef __DIAG_MEMORY_H__
+#define __DIAG_MEMORY_H__
+
+#include <vb2_api.h>
+
+typedef enum {
+	MEMORY_TEST_MODE_QUICK,
+	MEMORY_TEST_MODE_FULL,
+} MemoryTestMode;
+
+/*
+ * Initialize the memory test state with specific test mode.
+ *
+ * Return VB2_SUCCESS if success, VB2_ERROR_EX_DIAG_TEST_INIT_FAILED for failed.
+ */
+vb2_error_t memory_test_init(MemoryTestMode mode);
+
+/*
+ * Run a batch of the test and return the current status to the read only buf.
+ * Otherwise, if the test is finished, it will return the test result.
+ *
+ * Callee will handle the ownership of the buffer. The buffer will be available
+ * until next call.
+ *
+ * Return VB2_SUCCESS if the test finished. VB2_ERROR_EX_DIAG_TEST_RUNNING if
+ * the test is still running. Other for other errors.
+ */
+vb2_error_t memory_test_run(const char **buf);
+
+#endif
