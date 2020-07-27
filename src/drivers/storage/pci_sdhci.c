e11e0e40b51b46bc7fd10a6b584800c4de282014
Raul E Rangel <rrangel@chromium.org>
2020-07-27 14:42:41 -0600

    drivers/storage/pci_sdhci: Call new_mem_sdhci_host to init host
    This allows us to remove all the platform_info parsing from
    `new_pci_sdhci_host`. This reduces the chance of a platform quirk only
    being implemented in one of the drivers.
    
    BUG=b:149101474
    BRANCH=none
    TEST=Boot trembyle and make sure PCI SDHCI still functions.
    
    Signed-off-by: Raul E Rangel <rrangel@chromium.org>
    Change-Id: I235cc7ea4c79cba85bee786dd98c50fd6384de7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/2321608
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-by: Aaron Durbin <adurbin@google.com>

--- src/drivers/storage/pci_sdhci.c
+++ src/drivers/storage/pci_sdhci.c
@@ -43,7 +43,6 @@ SdhciHost *new_pci_sdhci_host(pcidev_t dev, int platform_info, int clock_min,
 {
 	SdhciHost *host;
 	uintptr_t bar;
-	int removable = platform_info & SDHCI_PLATFORM_REMOVABLE;
 
 	if (get_pci_bar(dev, &bar)) {
 		printf("Failed to get BAR for PCI SDHCI %d.%d.%d", PCI_BUS(dev),
@@ -51,41 +50,13 @@ SdhciHost *new_pci_sdhci_host(pcidev_t dev, int platform_info, int clock_min,
 		return NULL;
 	}
 
-	host = xzalloc(sizeof(*host));
+	host = new_mem_sdhci_host(bar, platform_info, clock_min, clock_max,
+				  0 /* clock_base */);
+
 	host->name = xzalloc(SDHCI_NAME_LENGTH);
 
 	snprintf(host->name, SDHCI_NAME_LENGTH, "PCI SDHCI %d.%d.%d",
 		 PCI_BUS(dev), PCI_SLOT(dev), PCI_FUNC(dev));
-
-	host->quirks = SDHCI_QUIRK_NO_SIMULT_VDD_AND_POWER;
-
-	if (platform_info & SDHCI_PLATFORM_NO_EMMC_HS200)
-		host->quirks |= SDHCI_QUIRK_NO_EMMC_HS200;
-
-	if (platform_info & SDHCI_PLATFORM_SUPPORTS_HS400ES)
-		host->quirks |= SDHCI_QUIRK_SUPPORTS_HS400ES;
-
-	if (platform_info & SDHCI_PLATFORM_EMMC_1V8_POWER)
-		host->quirks |= SDHCI_QUIRK_EMMC_1V8_POWER;
-
-	if (platform_info & SDHCI_PLATFORM_CLEAR_TRANSFER_BEFORE_CMD)
-		host->quirks |=
-			SDHCI_QUIRK_CLEAR_TRANSFER_BEFORE_CMD;
-
-	host->clock_f_min = clock_min;
-	host->clock_f_max = clock_max;
-	host->removable = removable;
-	host->ioaddr = (void *)bar;
-
-	if (!removable)
-		/*
-		 * The value translates to 'block access mode, supporting
-		 * 1.7..1.95 and 2.7..3.6 voltage ranges, which is typical for
-		 * eMMC devices.
-		 */
-		host->mmc_ctrlr.hardcoded_voltage = 0x40ff8080;
-
-	add_sdhci(host);
 
 	return host;
 }
