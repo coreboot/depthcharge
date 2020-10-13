209d480eda13c3c6fa1861793a9218f9bf42f8a9
Seunghwan Kim <sh_.kim@samsung.corp-partner.google.com>
2020-10-13 18:07:06 +0900

    drivers/bus/i2s: Add I2S setting for MAX98390 amplifier
    Add I2S setting for MAX98390 amplifier. Just copied MAX98357A setting.
    
    BUG=b:170143676
    BRANCH=None
    TEST=Built and verified BIOS beep sound on nightfury
    
    Signed-off-by: Seunghwan Kim <sh_.kim@samsung.corp-partner.google.com>
    Change-Id: Icb86aa995332d04f593b5f9b7a8f390edbaea9ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/2467501
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Commit-Queue: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Signed-off-by: Martin Roth <martinroth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/2527292
    Tested-by: Martin Roth <martinroth@google.com>
    Commit-Queue: Martin Roth <martinroth@google.com>
    Reviewed-by: Martin Roth <martinroth@google.com>

---
+++ src/drivers/bus/i2s/intel_common/max98390.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 Google Inc.
+ * Copyright (c) 2020, Intel Corporation.
+ *
+ * See file CREDITS for list of people who contributed to this
+ * project.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but without any warranty; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "drivers/bus/i2s/intel_common/max98390.h"
+
+/* I2S controller settings for MAX98390 codec. */
+const I2sSettings max98390_settings = {
+	/* To set MOD bit in SSC0 - definig as network/normal mode */
+	.mode = SSP_IN_NETWORK_MODE,
+	/* To set FRDC bit in SSC0 - timeslot per frame in network mode */
+	.frame_rate_divider_ctrl = FRAME_RATE_CONTROL_STEREO,
+	/* To set EDMYSTOP bit in SSPSP - number of SCLK cycles after data */
+	.ssp_psp_T4 = 2,
+	/* To set SFRMWDTH bit in SSPSP - frame width */
+	.ssp_psp_T6 = 0x18,
+	/* To set TTSA bit n SSTSA - data transmit timeslot */
+	.ssp_active_tx_slots_map = 3,
+	/* To set RTSA bit n SSRSA - data receive timeslot */
+	.ssp_active_rx_slots_map = 3,
+};
