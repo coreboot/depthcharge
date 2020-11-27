b1dbebd7fb4e6fdb0ae8c9ff1119ee8ccef9cbb1
Meng-Huan Yu <menghuan@chromium.org>
2020-11-27 14:25:06 +0800

    memory: Support to exclude MMU allocated memory in libpayload
    BUG=b:171858277
    BRANCH=trogdor
    TEST=emerge-asurada depthcharge
    TEST=pass quick/full memory test on asurada
    TEST=emerge-zork depthcharge # make sure other arch could be built
    
    Cq-Depend: chromium:2577309, chromium:2577108
    Signed-off-by: Meng-Huan Yu <menghuan@chromium.org>
    Change-Id: Id190caf6ea92761f6998607e88cd87349f5869aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/2562866
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/2591834
    Tested-by: Chung-Sheng Wu <chungsheng@chromium.org>
    Commit-Queue: Julius Werner <jwerner@chromium.org>

--- src/vboot/util/memory.c
+++ src/vboot/util/memory.c
@@ -20,6 +20,10 @@
 #include <stdint.h>
 #include <sysinfo.h>
 
+#if CONFIG(LP_ARCH_ARM64)
+#include <arm64/arch/mmu.h>
+#endif
+
 #include "base/ranges.h"
 #include "base/physmem.h"
 #include "image/symbols.h"
@@ -70,10 +74,8 @@ static void remove_range(uint64_t start, uint64_t end, void *data)
 	ranges_sub((Ranges *)data, start, end);
 }
 
-int memory_range_init_and_get_unused(Ranges *ranges)
+static int get_unused_memory(Ranges *ranges)
 {
-	ranges_init(ranges);
-
 	// Process the memory map from coreboot.
 	for (int i = 0; i < lib_sysinfo.n_memranges; i++) {
 		struct memrange *range = &lib_sysinfo.memrange[i];
@@ -104,10 +106,62 @@ int memory_range_init_and_get_unused(Ranges *ranges)
 	return 0;
 }
 
+static int get_mmu_used_memory(Ranges *ranges)
+{
+#if CONFIG(LP_ARCH_ARM64)
+	// In ARM64, we may allocate DMA and framebuffer in libpayload
+	// during MMU initialization.
+	const struct mmu_ranges *usedmem_ranges = mmu_get_used_ranges();
+	for (int i = 0; i < usedmem_ranges->used; i++) {
+		const struct mmu_memrange *entry =
+			&usedmem_ranges->entries[i];
+		uint64_t start = entry->base;
+		uint64_t end = entry->base + entry->size;
+		switch (entry->type) {
+		case TYPE_NORMAL_MEM:
+		case TYPE_DEV_MEM:
+		case TYPE_DMA_MEM:
+			ranges_add(ranges, start, end);
+			break;
+		default:
+			printf("Unrecognized memory type %lld!\n",
+			       entry->type);
+			return 1;
+		}
+	}
+#endif
+	return 0;
+}
+
+int memory_range_init_and_get_unused(Ranges *ranges)
+{
+	ranges_init(ranges);
+
+	int result = get_unused_memory(ranges);
+	if (result)
+		return result;
+
+	// Exclude the memory used in payload for MMU initialization.
+	Ranges payload_used;
+	ranges_init(&payload_used);
+	result = get_mmu_used_memory(&payload_used);
+	if (result) {
+		ranges_teardown(&payload_used);
+		return result;
+	}
+	ranges_for_each(&payload_used, &remove_range, ranges);
+	ranges_teardown(&payload_used);
+
+	return 0;
+}
+
 int memory_wipe_unused(void)
 {
+	// Do not exclude the memory used in payload. We would like to wipe
+	// DMA and framebuffer for ARM64.
 	Ranges ranges;
-	int result = memory_range_init_and_get_unused(&ranges);
+	ranges_init(&ranges);
+	int result = get_unused_memory(&ranges);
 	if (result) {
 		ranges_teardown(&ranges);
 		return result;
