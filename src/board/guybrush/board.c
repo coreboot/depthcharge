759d7b9b2d9b60108df27f159c23a644ee3ce7fd
Martin Roth <martinroth@chromium.org>
2021-01-12 12:14:58 -0700

    Add code for guybrush board
    The guybrush board is based on zork.
    
    The two major pieces that need to be updated are the beep code which
    will be completely different than zork, and the backlight enable which
    doesn't yet have a GPIO assigned.
    
    BUG=b:177225571
    TEST=Build tested only
    BRANCH=None
    
    Change-Id: Ie446d314b628071ecef9600b5d7a16cd00590895
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/2626448
    Reviewed-by: Raul E Rangel <rrangel@chromium.org>
    Reviewed-by: Mathew King <mathewk@chromium.org>
    Commit-Queue: Mathew King <mathewk@chromium.org>
    Tested-by: Martin Roth <martinroth@google.com>
    Auto-Submit: Martin Roth <martinroth@google.com>

--- src/board/guybrush/board.c
+++ src/board/guybrush/board.c
@@ -1,13 +1,142 @@
 // SPDX-License-Identifier: GPL-2.0
-/* Copyright 2020 Google LLC.  */
+/* Copyright 2021 Google LLC.  */
 
 #include <libpayload.h>
 
 #include "base/init_funcs.h"
+#include "base/list.h"
+#include "drivers/bus/i2c/cros_ec_tunnel.h"
+#include "drivers/bus/i2c/designware.h"
+#include "drivers/bus/i2c/i2c.h"
+#include "drivers/ec/cros/lpc.h"
+#include "drivers/ec/anx3429/anx3429.h"
+#include "drivers/ec/ps8751/ps8751.h"
+#include "drivers/flash/flash.h"
+#include "drivers/flash/memmapped.h"
+#include "drivers/gpio/kern.h"
+#include "drivers/gpio/sysinfo.h"
+#include "drivers/power/fch.h"
+#include "drivers/soc/cezanne.h"
+#include "drivers/storage/ahci.h"
+#include "drivers/storage/blockdev.h"
+#include "drivers/storage/sdhci.h"
+#include "drivers/storage/bayhub.h"
+#include "drivers/storage/nvme.h"
+#include "drivers/tpm/cr50_i2c.h"
+#include "drivers/tpm/tpm.h"
+#include "drivers/video/display.h"
+#include "drivers/bus/usb/usb.h"
+#include "pci.h"
+#include "vboot/util/flag.h"
 
+/* SD Controllers */
+#define BH720_PCI_VID		0x1217
+#define BH720_PCI_DID		0x8621
+
+#define GENESYS_PCI_VID		0x17a0
+#define GL9755S_PCI_DID		0x9755
+#define GL9750S_PCI_DID		0x9750
+
+/* Clock frequencies */
+#define MCLK			4800000
+#define LRCLK			8000
+
+/* SPI Flash */
+#define FLASH_SIZE		0x1000000	/* 16MB */
+#define FLASH_START		( 0xffffffff - FLASH_SIZE + 1 )
+
+/* Core boost register */
+#define HW_CONFIG_REG		0xc0010015
+#define   HW_CONFIG_CPBDIS	(1 << 25)
+
+/* cr50 / Ti50 interrupt is attached to GPIO_3 */
+#define CR50_INT		3
+
+/* eDP backlight */
+#define GPIO_BACKLIGHT		129
+
+static int cr50_irq_status(void)
+{
+	static KernGpio *tpm_gpio;
+
+	if (!tpm_gpio)
+		tpm_gpio = new_kern_fch_gpio_latched(CR50_INT);
+
+	return gpio_get(&tpm_gpio->ops);
+}
+
+static int guybrush_backlight_update(DisplayOps *me, uint8_t enable)
+{
+	/* Backlight signal is active low */
+	static KernGpio *backlight;
+
+	if (!backlight)
+		backlight = new_kern_fch_gpio_output(GPIO_BACKLIGHT, !enable);
+	else
+		backlight->ops.set(&backlight->ops, !enable);
+
+	return 0;
+}
+
+static DisplayOps guybrush_display_ops = {
+	.backlight_update = &guybrush_backlight_update,
+};
 
 static int board_setup(void)
 {
+	sysinfo_install_flags(NULL);
+	CrosEcLpcBus *cros_ec_lpc_bus =
+		new_cros_ec_lpc_bus(CROS_EC_LPC_BUS_GENERIC);
+	CrosEc *cros_ec = new_cros_ec(&cros_ec_lpc_bus->ops, NULL);
+	register_vboot_ec(&cros_ec->vboot);
+
+	flag_replace(FLAG_LIDSW, cros_ec_lid_switch_flag());
+	flag_replace(FLAG_PWRSW, cros_ec_power_btn_flag());
+
+	flash_set_ops(&new_mem_mapped_flash(FLASH_START, FLASH_SIZE)->ops);
+
+	// TODO: add audio_setup
+
+	SdhciHost *sd = NULL;
+	pcidev_t pci_dev;
+	if (pci_find_device(BH720_PCI_VID, BH720_PCI_DID, &pci_dev)) {
+		sd = new_bayhub_sdhci_host(pci_dev,
+				SDHCI_PLATFORM_REMOVABLE,
+				0, 0);
+	} else if (pci_find_device(GENESYS_PCI_VID, GL9755S_PCI_DID,
+				&pci_dev)) {
+		sd = new_pci_sdhci_host(pci_dev,
+				SDHCI_PLATFORM_REMOVABLE,
+				0, 0);
+	} else if (pci_find_device(GENESYS_PCI_VID, GL9750S_PCI_DID,
+				&pci_dev)) {
+		sd = new_pci_sdhci_host(pci_dev,
+				SDHCI_PLATFORM_REMOVABLE,
+				0, 0);
+	}
+
+	if (sd) {
+		sd->name = "SD";
+		list_insert_after(&sd->mmc_ctrlr.ctrlr.list_node,
+				&removable_block_dev_controllers);
+	} else {
+		printf("Failed to find SD card reader\n");
+	}
+
+	/* PCI Bridge for NVMe */
+	NvmeCtrlr *nvme = new_nvme_ctrlr(PCI_DEV(0, 0x01, 0x07));
+	list_insert_after(&nvme->ctrlr.list_node,
+				&fixed_block_dev_controllers);
+
+	/* Set up H1 / Dauntless on I2C3 */
+	DesignwareI2c *i2c_h1 = new_designware_i2c(
+		AP_I2C3_ADDR, 400000, AP_I2C_CLK_MHZ);
+	tpm_set_ops(&new_cr50_i2c(&i2c_h1->ops, 0x50,
+				  &cr50_irq_status)->base.ops);
+
+	power_set_ops(&kern_power_ops);
+	display_set_ops(&guybrush_display_ops);
+
 	return 0;
 }
 
