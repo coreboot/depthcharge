18894546c06f1f5e3c247bc6072287251cb4f322
Yidi Lin <yidi.lin@mediatek.com>
2021-04-01 21:11:14 +0800

    cherry: Enable Chrome EC
    BUG=b:177593590
    TEST=boot from USB by `dut-control power_state:rec`
         boot from eMMC by `dut-control power_state:reset`
    
    Change-Id: I3310bd021d3c464d7946cfa96d90ff7f256c3ae8
    Signed-off-by: Yidi Lin <yidi.lin@mediatek.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/2807338
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Commit-Queue: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>

--- src/board/cherry/board.c
+++ src/board/cherry/board.c
@@ -9,17 +9,34 @@
 #include <libpayload.h>
 
 #include "base/init_funcs.h"
+#include "drivers/bus/spi/mtk.h"
 #include "drivers/bus/usb/usb.h"
+#include "drivers/ec/cros/ec.h"
+#include "drivers/ec/cros/spi.h"
+#include "drivers/flash/spi.h"
 #include "drivers/flash/mtk_snfc.h"
 #include "drivers/gpio/mtk_gpio.h"
 #include "drivers/gpio/sysinfo.h"
 #include "drivers/power/psci.h"
 #include "drivers/storage/mtk_mmc.h"
+#include "vboot/util/flag.h"
 
 static int board_setup(void)
 {
 	sysinfo_install_flags(new_mtk_gpio_input);
+	flag_replace(FLAG_LIDSW, cros_ec_lid_switch_flag());
+	if (!CONFIG(DETACHABLE))
+		flag_replace(FLAG_PWRSW, cros_ec_power_btn_flag());
+
 	power_set_ops(&psci_power_ops);
+
+	GpioOps *spi0_cs = new_gpio_not(new_mtk_gpio_output(PAD_SPIM0_CSB));
+	MtkSpi *spi0 = new_mtk_spi(0x1100A000, spi0_cs);
+	CrosEcSpiBus *cros_ec_spi_bus = new_cros_ec_spi_bus(&spi0->ops);
+	GpioOps *ec_int = sysinfo_lookup_gpio("EC interrupt", 1,
+					      new_mtk_gpio_input);
+	CrosEc *cros_ec = new_cros_ec(&cros_ec_spi_bus->ops, ec_int);
+	register_vboot_ec(&cros_ec->vboot);
 
 	/* Setup NOR flash ops */
 	MtkNorFlash *nor_flash = new_mtk_nor_flash(0x1132C000);
