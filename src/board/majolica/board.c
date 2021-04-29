05ef8e872992ae134ee7e9b311cdfbe3af725780
Martin Roth <martinroth@chromium.org>
2021-04-29 13:10:40 -0600

    Majolica: Update build
    - Add NVME boot device
    - Disable EC_VBOOT_SUPPORT, AUX_FW_SOFTWARE_SYNC, & DIAGNOSTIC_UI
    
    BUG=None
    TEST=Builds again.
    
    Change-Id: I6ec90ffba7a35a651ef5b5fc152e835a23d8d983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/2860905
    Tested-by: Martin Roth <martinroth@google.com>
    Auto-Submit: Martin Roth <martinroth@google.com>
    Reviewed-by: Raul E Rangel <rrangel@chromium.org>
    Commit-Queue: Raul E Rangel <rrangel@chromium.org>

--- src/board/majolica/board.c
+++ src/board/majolica/board.c
@@ -7,6 +7,8 @@
 #include "drivers/gpio/sysinfo.h"
 #include "drivers/power/fch.h"
 #include "drivers/soc/cezanne.h"
+#include "drivers/storage/nvme.h"
+#include "pci.h"
 #include "vboot/util/flag.h"
 
 
@@ -32,6 +34,11 @@ static int board_setup(void)
 	sysinfo_install_flags(NULL);
 	flag_replace(FLAG_LIDSW, &fake_gpio_1);
 	flag_replace(FLAG_PWRSW, &fake_gpio_0);
+
+	/* PCI Bridge for NVMe */
+	NvmeCtrlr *nvme = new_nvme_ctrlr(PCI_DEV(0, 0x02, 0x01));
+	list_insert_after(&nvme->ctrlr.list_node,
+				&fixed_block_dev_controllers);
 
 	power_set_ops(&kern_power_ops);
 
