coreboot/depthcharge

commit a912153eedfd1cb8057d32282402f94859211670

tree wide: Use pointer for register space
readX/writeX use a memory pointer instead of an opaque value that
may be of the wrong size.

BUG=b:178785769
BRANCH=none
TEST=builds with other commits

Signed-off-by: Patrick Georgi <pgeorgi@google.com>
Change-Id: Ifd68e94677eb5f0f56ca428733b0242a168be521
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/2659202
Tested-by: Patrick Georgi <pgeorgi@chromium.org>
Reviewed-by: Julius Werner <jwerner@chromium.org>
Commit-Queue: Patrick Georgi <pgeorgi@chromium.org>

src/drivers/bus/soundwire/soundwire.h
@@ -29,13 +29,13 @@ typedef struct {
         /* Soundwire ops structure.*/
         SndwOps ops;
         /* Intel HD Audio Lower Base Address.*/
-        uintptr_t hdabar;
+        void *hdabar;
         /* Audio DSP Lower Base Address.*/
-        uintptr_t dspbar;
+        void *dspbar;
 	/* Sndw Link Number */
 	int sndwlinkindex;
 	/* Sndw Link address */
-	uint32_t sndwlinkaddr;
+	void *sndwlinkaddr;
 } Soundwire;
 
 /*

src/board/samus/board.c
@@ -61,23 +61,23 @@ enum {
 static void device_enable(int sio_index)
 {
 	device_nvs_t *nvs = lib_sysinfo.acpi_gnvs + GNVS_DEVICE_NVS_OFFSET;
-	uint32_t reg_pcs = nvs->bar1[sio_index] + PCH_REG_PCS;
+	void *reg_pcs = (void *)nvs->bar1[sio_index] + PCH_REG_PCS;
 
 	/* Put device in D0 state */
 	write32(reg_pcs, read32(reg_pcs) & ~PCH_VAL_PCS_D3HOT);
-	(void) read32(reg_pcs);
+	(void)read32(reg_pcs);
 }
 
 static DesignwareI2c *i2c_enable(int sio_index)
 {
 	device_nvs_t *nvs = lib_sysinfo.acpi_gnvs + GNVS_DEVICE_NVS_OFFSET;
-	uint32_t reg_ppr = nvs->bar0[sio_index] + SIO_REG_PPR_CLOCK;
+	void *reg_ppr = (void *)nvs->bar0[sio_index] + SIO_REG_PPR_CLOCK;
 
 	device_enable(sio_index);
 
 	/* Enable clock to the device */
 	write32(reg_ppr, read32(reg_ppr) | SIO_BIT_PPR_CLOCK_EN);
-	(void) read32(reg_ppr);
+	(void)read32(reg_ppr);
 
 	return new_designware_i2c(nvs->bar0[sio_index], I2C_BUS_SPEED, 166);
 }

src/drivers/bus/i2s/intel_common/i2s.c
@@ -358,8 +358,8 @@ I2s *new_i2s_structure(const I2sSettings *settings, int bps, GpioOps *sdmode,
 	I2s *bus = xzalloc(sizeof(*bus));
 	pcidev_t lpe_pcidev = PCI_DEV(0, AUDIO_DEV, AUDIO_FUNC);
 
-	bus->lpe_bar0 = pci_read_config32(lpe_pcidev, REG_BAR0) & (~0xf);
-	bus->lpe_bar4 = pci_read_config32(lpe_pcidev, REG_BAR4) & (~0xf);
+	bus->lpe_bar0 = (void *)(uintptr_t)(pci_read_config32(lpe_pcidev, REG_BAR0) & (~0xf));
+	bus->lpe_bar4 = (void *)(uintptr_t)(pci_read_config32(lpe_pcidev, REG_BAR4) & (~0xf));
 	bus->ops.send = &i2s_send;
 	bus->regs = (I2sRegs *)(bus->lpe_bar4 + ssp_i2s_start_address);
 	bus->settings = settings;

src/drivers/bus/i2s/intel_common/i2s.h
@@ -120,9 +120,9 @@ typedef struct {
 	/* I2S ops structure.*/
 	I2sOps ops;
 	/* Intel HD Audio Lower Base Address.*/
-	uintptr_t lpe_bar0;
+	void *lpe_bar0;
 	/* Audio DSP Lower Base Address.*/
-	uintptr_t lpe_bar4;
+	void *lpe_bar4;
 	/* I2S registers.*/
 	struct I2sRegs *regs;
 	/* Configuration-dependent settings. */

src/drivers/bus/soundwire/mipi-sndwregs.h
@@ -146,7 +146,7 @@ typedef union {
 /* Soundwire codec information */
 typedef struct {
 	sndw_codec_id codecid;
-	uint32_t sndwlinkaddr;
+	void *sndwlinkaddr;
 	uint32_t deviceindex;
 } sndw_codec_info;
 

src/drivers/bus/soundwire/soundwire.c
@@ -45,7 +45,7 @@ static const sndw_cmd mipi_sndw_read_endpointid_cmds[] = {
  * pollingmask - The bit mapping for polling.
  * pollingdata - The Data for polling.
  */
-static int poll_status(uint32_t reg, uint32_t pollingmask, uint32_t pollingdata)
+static int poll_status(void *reg, uint32_t pollingmask, uint32_t pollingdata)
 {
 	struct stopwatch sw;
 	uint32_t data;
@@ -103,7 +103,7 @@ static void printrxcmd(uint32_t rxcmd)
  * txcmds - Pointer to send messages.
  * numofmsgs - Size of messages to send.
  */
-static void send(uint32_t sndwlinkaddr, sndw_cmd *txcmds, uint32_t devaddr, uint32_t numofmsgs)
+static void send(void *sndwlinkaddr, sndw_cmd *txcmds, uint32_t devaddr, uint32_t numofmsgs)
 {
 	uint32_t fifofree, txmsg, txindex;
 
@@ -126,7 +126,7 @@ static void send(uint32_t sndwlinkaddr, sndw_cmd *txcmds, uint32_t devaddr, uint
 /*
  * get_fifo_avail - This function returns number of available responses in the Response FIFO.
  */
-static unsigned int get_fifo_avail(uint32_t sndwlinkaddr)
+static unsigned int get_fifo_avail(void *sndwlinkaddr)
 {
 	return (read32(sndwlinkaddr + SNDW_MEM_FIFOSTAT) & SNDW_MEM_FIFOSTAT_AVAIL_MASK)
 			>> SNDW_MEM_FIFOSTAT_AVAIL;
@@ -137,7 +137,7 @@ static unsigned int get_fifo_avail(uint32_t sndwlinkaddr)
  * rxcmds - Pointer to pointer to received messages.
  * rxsize - Size of received messages.
  */
-static int receive(uint32_t sndwlinkaddr, uint32_t **rxcmds, uint32_t *rxsize)
+static int receive(void *sndwlinkaddr, uint32_t **rxcmds, uint32_t *rxsize)
 {
 	uint32_t fifoavailable, i, rxmsg, rxindex;
 	rxindex = 0;
@@ -175,7 +175,7 @@ static int receive(uint32_t sndwlinkaddr, uint32_t **rxcmds, uint32_t *rxsize)
  * sndwlinkaddr - Soundwire controller link address.
  * codecinfo - Pointer to the codec information.
  */
-static int read_endpointid(uint32_t sndwlinkaddr, uint32_t deviceindex,
+static int read_endpointid(void *sndwlinkaddr, uint32_t deviceindex,
 							sndw_codec_info *codecinfo)
 {
 	uint32_t i, fifostatavail, count, rxsize;
@@ -311,7 +311,7 @@ static int enable_sndwcodec(Soundwire *bus, sndw_codec_info *codecinfo)
  * sndwmasterinit - Function initializes Sndw master for enumerating connected codecs.
  * sndwlinkaddr - Soundwire link controller address.
  */
-static int sndwmasterinit(uint32_t sndwlinkaddr)
+static int sndwmasterinit(void *sndwlinkaddr)
 {
 	/* Program MCP_ClockCtrl.Master Clock Divider to configure the Soundwire
 	   clock frequency */
@@ -509,7 +509,7 @@ static int sndw_enable(struct SndwOps *me, sndw_codec_info *codecinfo)
  * txcmd - Message to send.
  * sndwindex - Index of the Soudnwire endpoint device.
  */
-static int sndw_sendwack(uint32_t sndwlinkaddr, sndw_cmd txcmd, uint32_t deviceindex)
+static int sndw_sendwack(void *sndwlinkaddr, sndw_cmd txcmd, uint32_t deviceindex)
 {
 	uint32_t rxsize;
 	uint32_t *rxcmd;
@@ -567,8 +567,8 @@ Soundwire *new_soundwire(int sndwlinkindex)
 	bus->ops.sndw_enable = &sndw_enable;
 	bus->ops.sndw_sendwack = &sndw_sendwack;
 	bus->ops.sndw_disable = &sndw_disable;
-	bus->hdabar = pci_read_config32(lpe_pcidev, REG_BAR0) & (~0xf);
-	bus->dspbar = pci_read_config32(lpe_pcidev, REG_BAR4) & (~0xf);
+	bus->hdabar = (void *)(uintptr_t)(pci_read_config32(lpe_pcidev, REG_BAR0) & (~0xf));
+	bus->dspbar = (void *)(uintptr_t)(pci_read_config32(lpe_pcidev, REG_BAR4) & (~0xf));
 	bus->sndwlinkindex = sndwlinkindex;
 
 	return bus;

src/drivers/soc/picasso.c
@@ -79,7 +79,7 @@ void emmc_set_ios(MmcCtrlr *mmc_ctrlr)
 
 unsigned int emmc_get_platform_info(void)
 {
-	u32 reg = read32(EMMCHC + SDHCI_CAPABILITIES_1);
+	u32 reg = read32((void *)EMMCHC + SDHCI_CAPABILITIES_1);
 	unsigned int platform_info = 0;
 
 	/*

src/drivers/sound/hda_codec.c
@@ -54,7 +54,7 @@
  * Wait 50usec for the codec to indicate it is ready. No response would
  * imply that the codec is non-operative.
  */
-static int wait_for_ready(uint32_t base)
+static int wait_for_ready(void *base)
 {
 	// Use a 50 usec timeout - the Linux kernel uses the same duration.
 	int timeout = 50;
@@ -75,7 +75,7 @@ static int wait_for_ready(uint32_t base)
  * Wait 50usec for the codec to indicate that it accepted the previous
  * command. No response would imply that the code is non-operative.
  */
-static int wait_for_response(uint32_t base, uint32_t *response)
+static int wait_for_response(void *base, uint32_t *response)
 {
 	uint32_t reg32;
 
@@ -107,7 +107,7 @@ static int wait_for_response(uint32_t base, uint32_t *response)
  * Wait for the codec to be ready, write the verb, then wait for the
  * codec to send a valid response.
  */
-static int exec_one_verb(uint32_t base, uint32_t val, uint32_t *response)
+static int exec_one_verb(void *base, uint32_t val, uint32_t *response)
 {
 	if (wait_for_ready(base) == -1)
 		return -1;
@@ -121,7 +121,7 @@ static int exec_one_verb(uint32_t base, uint32_t val, uint32_t *response)
 }
 
 /* Write one verb and ignore the response. */
-static int write_one_verb(uint32_t base, uint32_t val)
+static int write_one_verb(void *base, uint32_t val)
 {
 	return exec_one_verb(base, val, NULL);
 }
@@ -160,26 +160,26 @@ static struct pci_device_id supported[] = {
 };
 
 /* Find the base address to talk to the HDA codec. */
-static uint32_t get_hda_base(void)
+static void *get_hda_base(void)
 {
 	pcidev_t hda_dev;
 
 	for (int i = 0; i < ARRAY_SIZE(supported); i++) {
 		if (pci_find_device(supported[i].vendor, supported[i].device,
 			            &hda_dev)) {
-			return pci_read_resource(hda_dev, 0) & ~0xf;
+			return (void *)(pci_read_resource(hda_dev, 0) & ~0xf);
 		}
 	}
 
 	printf("Audio: Controller not found!\n");
-	return 0;
+	return NULL;
 }
 
 /*
  * Gets the count of sub-nodes and node id they start at for the given
  * super-node.
  */
-static uint32_t get_subnode_info(uint32_t base,
+static uint32_t get_subnode_info(void *base,
 				 uint32_t nid,
 				 uint32_t *num_sub_nodes,
 				 uint32_t *start_sub_node_nid)
@@ -203,7 +203,7 @@ static uint32_t get_subnode_info(uint32_t base,
 }
 
 /* Searches the audio group for a node that supports beeping. */
-static uint32_t find_beep_node_in_group(uint32_t base, uint32_t group_nid)
+static uint32_t find_beep_node_in_group(void *base, uint32_t group_nid)
 {
 	int rc;
 	uint32_t node_count = 0;
@@ -237,7 +237,7 @@ static uint32_t find_beep_node_in_group(uint32_t base, uint32_t group_nid)
 }
 
 /* Checks if the given audio group contains a beep generator. */
-static int audio_group_has_beep_node(uint32_t base, uint32_t nid)
+static int audio_group_has_beep_node(void *base, uint32_t nid)
 {
 	int rc;
 	uint32_t response;
@@ -261,7 +261,7 @@ static int audio_group_has_beep_node(uint32_t base, uint32_t nid)
  * the group contains a beep node, polls each node in the group until it is
  * found.
  */
-static uint32_t get_hda_beep_nid(HdaCodec *codec, uint32_t base)
+static uint32_t get_hda_beep_nid(HdaCodec *codec, void *base)
 {
 	int rc;
 	uint32_t node_count = 0;
@@ -290,10 +290,14 @@ static uint32_t get_hda_beep_nid(HdaCodec *codec, uint32_t base)
 /* Sets the beep generator with the given divisor. Pass 0 to disable beep. */
 static int set_beep_divisor(HdaCodec *codec, uint8_t divider)
 {
-	uint32_t base;
+	void *base;
 	uint32_t beep_nid;
 
 	base = get_hda_base();
+	if (base == NULL) {
+		printf("Audio: Failed to find HDA controller.\n");
+		return -1;
+	}
 	beep_nid = get_hda_beep_nid(codec, base);
 	if (beep_nid <= 0) {
 		printf("Audio: Failed to find a beep-capable node.\n");

commit 7df39176b3b63575f2cb04f12b2c5c7e055bf809

sndw: Use pointer for register space
sndw_sendwack use a memory pointer instead of an opaque value.

Fixes: a912153eed ("tree wide: Use pointer for register space")

Change-Id: I720f97af7d2d70f1cb4dd90dfd1bcdd60cccdbd0
Signed-off-by: Sugnan Prabhu S <sugnan.prabhu.s@intel.com>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3033881
Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

src/drivers/bus/soundwire/soundwire.h
@@ -21,7 +21,7 @@
  */
 typedef struct SndwOps {
 	int (*sndw_enable)(struct SndwOps *me, sndw_codec_info *codecinfo);
-        int (*sndw_sendwack)(uint32_t sndwlinkaddr, sndw_cmd txcmd, uint32_t deviceindex);
+        int (*sndw_sendwack)(void *sndwlinkaddr, sndw_cmd txcmd, uint32_t deviceindex);
 	int (*sndw_disable)(struct SndwOps *me);
 } SndwOps;
 