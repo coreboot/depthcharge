4586e78fd6ac5af44cf9d6644503530d8e7be09d
Malik_Hsu <malik_hsu@wistron.corp-partner.google.com>
2021-08-18 10:24:51 +0800

    primus: Add support for beep
    Add support for the BIOS beep sound.
    
    BUG=b:193377625
    BRANCH=brya
    TEST=emerge-brya coreboot depthcharge, verify it builds without error.
    
    Signed-off-by: Malik_Hsu <malik_hsu@wistron.corp-partner.google.com>
    Change-Id: I9e18608607f66065fc8e7b2d9eb8634c5f25e214
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3102487
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Commit-Queue: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Tested-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/board/brya/primus.c
+++ src/board/brya/primus.c
@@ -6,13 +6,35 @@
 
 #include "base/fw_config.h"
 #include "board/brya/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
 #include "drivers/bus/usb/intel_tcss.h"
 #include "drivers/gpio/alderlake.h"
 #include "drivers/soc/alderlake.h"
 
+#define SDMODE_PIN		GPP_A11
+
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
+
+	if (fw_config_probe(FW_CONFIG(AUDIO, MAX98360_ALC5682I_I2S))) {
+		config = (struct audio_config){
+			.bus = {
+				.i2s.address		= SSP_I2S2_START_ADDRESS,
+				.i2s.enable_gpio	= SDMODE_PIN,
+				.i2s.settings		= &max98357a_settings,
+			},
+			.amp = {
+				.type			= AUDIO_GPIO_AMP,
+				.gpio.enable_gpio	= SDMODE_PIN,
+			},
+			.codec = {
+				.type			= AUDIO_MAX98357,
+			},
+		};
+	}
+
 	return &config;
 }
 
