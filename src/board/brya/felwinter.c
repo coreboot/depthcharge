b5735a4146f59437c6b9c00ca08656ac3f888e9d
Eric Lai <ericr_lai@compal.corp-partner.google.com>
2021-08-30 14:56:46 +0800

    felwinter: update depthcharge setting
    Update typeC and audio setting.
    
    BUG=b:198102945
    TEST=emerge-brya depthcharge
    
    Signed-off-by: Eric Lai <ericr_lai@compal.corp-partner.google.com>
    Change-Id: I6a885004ff5ca40d3f9515603985b17d947de23b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3127968
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Commit-Queue: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/board/brya/felwinter.c
+++ src/board/brya/felwinter.c
@@ -6,17 +6,41 @@
 
 #include "base/fw_config.h"
 #include "board/brya/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
 #include "drivers/bus/usb/intel_tcss.h"
 #include "drivers/gpio/alderlake.h"
+#include "drivers/soc/alderlake.h"
+
+#define SDMODE_PIN		GPP_A11
 
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
+	config = (struct audio_config){
+		.bus = {
+			.i2s.address		= SSP_I2S1_START_ADDRESS,
+			.i2s.enable_gpio	= SDMODE_PIN,
+			.i2s.settings		= &max98357a_settings,
+		},
+		.amp = {
+			.type			= AUDIO_GPIO_AMP,
+			.gpio.enable_gpio	= SDMODE_PIN,
+		},
+		.codec = {
+			.type			= AUDIO_MAX98357,
+		},
+	};
 	return &config;
 }
 
+static const struct tcss_map typec_map[] = {
+	{ .usb2_port = 2, .usb3_port = 1, .ec_port = 1 },
+	{ .usb2_port = 3, .usb3_port = 2, .ec_port = 2 },
+};
+
 const struct tcss_map *variant_get_tcss_map(size_t *count)
 {
-	*count = 0;
-	return NULL;
+	*count = ARRAY_SIZE(typec_map);
+	return typec_map;
 }
