coreboot/depthcharge

commit ff1db358349d05c5ae6cbc43c99f0db96325823d

drivers/soc: Add intel_remap_pcie_rp() function
When the FSP runs, it disables non-hotpluggable PCIe RPs that have no
downstream devices connected. Thus, there can be the case where a slot
no longer has a function #0, but other functions are present. In order
to remain PCI compliant (multi-function devices must have a function 0
present), one RP in the same slot is remapped to function 0
instead. This can be detected programmatically (coreboot does it),
therefore port this logic for depthcharge to use as well.

Signed-off-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
Change-Id: Id601cab12f65f7a4235a0d95586bc6d34f3286d0
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3115371
Reviewed-by: Furquan Shaikh <furquan@chromium.org>

src/drivers/soc/intel_common.c
@@ -14,6 +14,8 @@
 #define GPE0_STS_SIZE 4
 #define GPE0_STS(off, x) ((off) + ((x) * GPE0_STS_SIZE))
 
+#define PCH_PCIE_CFG_LCAP_PN	0x4f /* Root Port Number */
+
 int soc_get_gpe(int gpe, const SocGpeConfig *cfg)
 {
 	int bank;
@@ -44,4 +46,71 @@ int soc_get_gpe(int gpe, const SocGpeConfig *cfg)
 	} while (sts & mask);
 
 	return rc;
+}
+
+static int get_original_pn(pcidev_t rp, const SocPcieRpGroup *groups,
+			   size_t num_groups)
+{
+	unsigned int rp_slot = PCI_SLOT(rp);
+	unsigned int rp_func = PCI_FUNC(rp);
+
+	unsigned int idx = 1;
+	for (size_t i = 0; i < num_groups; i++) {
+		const SocPcieRpGroup *group = &groups[i];
+
+		if (rp_slot != group->slot) {
+			idx += group->count;
+			continue;
+		}
+
+		if (rp_func < rp_start_fn(group))
+			return -1;
+
+		if (rp_func > rp_end_fn(group)) {
+			idx += group->count;
+			continue;
+		}
+
+		idx += rp_func - rp_start_fn(group) + 1;
+		return idx;
+	}
+
+	return -1;
+}
+
+pcidev_t intel_remap_pcie_rp(pcidev_t rp, const SocPcieRpGroup *groups,
+			     size_t num_groups)
+{
+	const int original_pn = get_original_pn(rp, groups, num_groups);
+	const unsigned int lcap_pn = pci_read_config8(rp, PCH_PCIE_CFG_LCAP_PN);
+
+	if ((int)lcap_pn == original_pn)
+		return rp;
+
+	/* Bummer, this device doesn't match; scan PCI config space to
+	 * reconstruct where rp got moved to */
+	for (size_t i = 0; i < num_groups; i++) {
+		const SocPcieRpGroup *group = &groups[i];
+		unsigned int fn;
+
+		for (fn = rp_start_fn(group); fn <= rp_end_fn(group); fn++) {
+			const pcidev_t dev = PCI_DEV(0, group->slot, fn);
+			const uint16_t did = pci_read_config16(dev,
+							       REG_DEVICE_ID);
+			if (did == 0xffff) {
+				/* If function 0 is disabled, then the whole
+				 * slot is disabled, so skip it */
+				if (!fn)
+					break;
+				continue;
+			}
+
+			const uint8_t dev_pn =
+				pci_read_config8(dev, PCH_PCIE_CFG_LCAP_PN);
+			if (dev_pn == original_pn)
+				return dev;
+		}
+	}
+
+	return (pcidev_t)-1;
 }

src/drivers/soc/intel_common.h
@@ -8,7 +8,10 @@
 #ifndef __DRIVERS_SOC_INTEL_COMMON_H__
 #define __DRIVERS_SOC_INTEL_COMMON_H__
 
+#include <stddef.h>
 #include <stdint.h>
+#include <pci.h>
+#include <pci/pci.h>
 
 /* Platform specific GPE configuration */
 typedef struct SocGpeConfig {
@@ -18,5 +21,33 @@ typedef struct SocGpeConfig {
 } SocGpeConfig;
 
 int soc_get_gpe(int gpe, const SocGpeConfig *cfg);
+
+typedef struct {
+	unsigned int slot;
+	unsigned int first_fn;
+	unsigned int count;
+} SocPcieRpGroup;
+
+static inline unsigned int rp_start_fn(const SocPcieRpGroup *group)
+{
+	return group->first_fn;
+}
+
+static inline unsigned int rp_end_fn(const SocPcieRpGroup *group)
+{
+	return group->first_fn + group->count - 1;
+}
+
+/* Since the FSP disables PCIe RPs that it finds no downstream devices on, if
+ * function 0 gets disabled, one of the used RPs will get remapped to function 0
+ * in order to preserve PCI compatibility (a multi-function device must always
+ * have function 0 present). Therefore, this function will return the correct
+ * mapping if it has been remapped, original otherwise.
+ *
+ * Assumes that `groups` are sorted such that the PCIe RP numbers within and
+ * across groups are in increasing order.
+ */
+pcidev_t intel_remap_pcie_rp(pcidev_t rp, const SocPcieRpGroup *groups,
+			     size_t num_groups);
 
 #endif /* __DRIVERS_SOC_INTEL_COMMON_H__ */

commit fbb6b12b2d01001008a74825c8def885d035a124

drivers/soc: Fix intel_remap_pcie_rp
Commit ff1db3583 accidentally left in a `+ 1` when calculating the
original Port Number for the given PCIe RP. Remove the + 1.

BUG=b:195914324
TEST=boot brya to OS

Change-Id: I623de6e62629884775746dbd47f614aeffdede96
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3128690
Tested-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
Tested-by: EricR Lai <ericr_lai@compal.corp-partner.google.com>
Auto-Submit: Tim Wawrzynczak <twawrzynczak@chromium.org>
Reviewed-by: EricR Lai <ericr_lai@compal.corp-partner.google.com>
Reviewed-by: Furquan Shaikh <furquan@chromium.org>
Commit-Queue: Tim Wawrzynczak <twawrzynczak@chromium.org>

src/drivers/soc/intel_common.c
@@ -71,7 +71,7 @@ static int get_original_pn(pcidev_t rp, const SocPcieRpGroup *groups,
 			continue;
 		}
 
-		idx += rp_func - rp_start_fn(group) + 1;
+		idx += rp_func - rp_start_fn(group);
 		return idx;
 	}
 