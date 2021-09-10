3cba85d26579d7d33ebc8b32687694b1bda274bf
Wisley Chen <wisley.chen@quanta.corp-partner.google.com>
2021-09-10 17:43:36 +0600

    Redrix: Update audio setting
    Update audio setting for redrix.
    
    BRANCH=none
    BUG=b:192052098
    TEST=FW_NAME=redrix emerge-brya depthcharge
    
    Signed-off-by: Wisley Chen <wisley.chen@quanta.corp-partner.google.com>
    Change-Id: Ib69e6c8bbed654502f9c661175a256dc33dd83e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3154075
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Tested-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Commit-Queue: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/board/brya/redrix.c
+++ src/board/brya/redrix.c
@@ -8,10 +8,39 @@
 #include "board/brya/include/variant.h"
 #include "drivers/bus/usb/intel_tcss.h"
 #include "drivers/gpio/alderlake.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98390.h"
+
+#define AUD_I2C_ADDR1	0x3a
+#define AUD_I2C_ADDR2	0x3b
+#define AUD_I2C_ADDR3	0x38
+#define AUD_I2C_ADDR4	0x39
+#define SDMODE_PIN	GPP_A11
 
 const struct audio_config *variant_probe_audio_config(void)
 {
-	static struct audio_config config;
+	static struct audio_config config = {
+		.bus = {
+			.i2s.address = SSP_I2S1_START_ADDRESS,
+			.i2s.enable_gpio = {
+				.pad = SDMODE_PIN,
+				.active_low = true
+			},
+			.i2s.settings = &max98390_settings,
+		},
+		.amp = {
+			.type = AUDIO_AMP_NONE,
+		},
+		.codec = {
+			.type = AUDIO_MAX98390,
+			.i2c.ctrlr = I2C0,
+			.i2c.i2c_addr[0] = AUD_I2C_ADDR1,
+			.i2c.i2c_addr[1] = AUD_I2C_ADDR2,
+			.i2c.i2c_addr[2] = AUD_I2C_ADDR3,
+			.i2c.i2c_addr[3] = AUD_I2C_ADDR4,
+		},
+	};
+
 	return &config;
 }
 
