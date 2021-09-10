6cd617c2212a1d626c0ce7b0e3c7d7749454cb31
Kevin Chang <kevin.chang@lcfc.corp-partner.google.com>
2021-09-10 16:52:53 +0800

    Taeko: add support for GL9763E
    support GL9763E as a eMMC boot disk
    
    BRANCH=none
    BUG=b:202192686
    TEST=enable CONFIG_DRIVER_STORAGE_MMC, CONFIG_DRIVER_STORAGE_SDHCI,
    CONFIG_DRIVER_STORAGE_SDHCI_PCI and check eMMC on taeko.
    
    Change-Id: I3581556cc5c13cad0f6770e41569e67e346eb7f5
    Signed-off-by: Kevin Chang <kevin.chang@lcfc.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3153210
    Reviewed-by: YH Lin <yueherngl@chromium.org>

--- src/board/brya/taeko.c
+++ src/board/brya/taeko.c
@@ -10,10 +10,14 @@
 #include "drivers/bus/i2s/intel_common/max98357a.h"
 #include "drivers/bus/usb/intel_tcss.h"
 #include "drivers/gpio/alderlake.h"
-
+#include "drivers/soc/alderlake.h"
+#include "drivers/storage/sdhci_gli.h"
 
 #define SDMODE_PIN		GPP_A11
 #define SDMODE_ENABLE		0
+#define EMMC_CLOCK_MIN		400000
+#define EMMC_CLOCK_MAX		200000000
+
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
@@ -42,6 +46,37 @@ const struct audio_config *variant_probe_audio_config(void)
 
 const struct tcss_map *variant_get_tcss_map(size_t *count)
 {
+	*count = 0;
+	return NULL;
+}
+
+static const struct storage_config storage_configs[] = {
+	{ .media = STORAGE_NVME, .pci_dev = PCH_DEV_PCIE8 },
+	{ .media = STORAGE_EMMC, .pci_dev = PCH_DEV_PCIE8, .emmc = {
+		.platform_flags = SDHCI_PLATFORM_SUPPORTS_HS400ES,
+		.clock_min = EMMC_CLOCK_MIN,
+		.clock_max = EMMC_CLOCK_MAX }},
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	if (!fw_config_is_provisioned()) {
+		*count = ARRAY_SIZE(storage_configs);
+		return storage_configs;
+	} else {
+		if (fw_config_probe(FW_CONFIG(BOOT_NVME_MASK, BOOT_NVME_ENABLED)) &&
+		    fw_config_probe(FW_CONFIG(BOOT_EMMC_MASK, BOOT_EMMC_ENABLED))) {
+			*count = ARRAY_SIZE(storage_configs);
+			return storage_configs;
+		} else if (fw_config_probe(FW_CONFIG(BOOT_NVME_MASK, BOOT_NVME_ENABLED))) {
+			*count = 1;
+			return storage_configs;
+		} else if (fw_config_probe(FW_CONFIG(BOOT_EMMC_MASK, BOOT_EMMC_ENABLED))) {
+			*count = 1;
+			return storage_configs + 1;
+		}
+	}
+
 	*count = 0;
 	return NULL;
 }
