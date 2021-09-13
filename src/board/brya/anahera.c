9d7bdaefd24de8695011c1df0c21a6afe53f5ed1
Wisley Chen <wisley.chen@quanta.corp-partner.google.com>
2021-09-13 19:02:09 +0600

    Anahera: Add audio setting and storage setting
    Add audio setting and storage setting for anahera.
    
    BRANCH=none
    BUG=b:197850509
    TEST=built and boot into OS
    
    Cq-Depend: chromium:3156590
    Change-Id: Ia1e8c2105a5d1df5bed5242374f5c92a72f5cb17
    Signed-off-by: Wisley Chen <wisley.chen@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3156591
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Nick Vaccaro <nvaccaro@google.com>

--- src/board/brya/anahera.c
+++ src/board/brya/anahera.c
@@ -6,17 +6,67 @@
 
 #include "base/fw_config.h"
 #include "board/brya/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
 #include "drivers/bus/usb/intel_tcss.h"
 #include "drivers/gpio/alderlake.h"
+#include "drivers/soc/alderlake.h"
+#include "drivers/storage/sdhci_gli.h"
+
+#define SDMODE_PIN		GPP_A11
+#define SDMODE_ENABLE		0
+#define EMMC_CLOCK_MIN		400000
+#define EMMC_CLOCK_MAX		200000000
 
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
+
+	config = (struct audio_config) {
+		.bus = {
+			.i2s.address		= SSP_I2S1_START_ADDRESS,
+			.i2s.enable_gpio	= {
+				.pad = SDMODE_PIN,
+				.active_low = SDMODE_ENABLE
+			},
+			.i2s.settings		= &max98357a_settings,
+		},
+		.amp = {
+			.type			= AUDIO_GPIO_AMP,
+			.gpio.enable_gpio	= SDMODE_PIN,
+		},
+		.codec = {
+			.type			= AUDIO_MAX98357,
+		},
+	};
 	return &config;
 }
 
+const struct tcss_map typec_map[] = {
+	{ .usb2_port = 1, .usb3_port = 0, .ec_port = 0 },
+	{ .usb2_port = 3, .usb3_port = 2, .ec_port = 1 },
+};
+
 const struct tcss_map *variant_get_tcss_map(size_t *count)
 {
-	*count = 0;
-	return NULL;
+	*count = ARRAY_SIZE(typec_map);
+	return typec_map;
+}
+
+static const struct emmc_config gl9763e_cfg = {
+	.platform_flags = SDHCI_PLATFORM_SUPPORTS_HS400ES,
+	.clock_min = EMMC_CLOCK_MIN,
+	.clock_max = EMMC_CLOCK_MAX,
+};
+
+static const struct storage_config storage_configs[] = {
+	{ .media = STORAGE_NVME, .pci_dev = PCH_DEV_PCIE8 },
+	{ .media = STORAGE_EMMC, .pci_dev = PCH_DEV_PCIE6, .emmc = gl9763e_cfg},
+	{ .media = STORAGE_SDHCI, .pci_dev = PCH_DEV_PCIE7 },
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
 }
