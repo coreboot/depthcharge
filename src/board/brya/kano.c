0ead5997ae076bfe4882489153f128a1a709ec6b
David Wu <david_wu@quanta.corp-partner.google.com>
2021-10-07 19:11:37 +0800

    Kano: Update audio setting
    Update audio setting for kano.
    
    BUG=b:194465448
    TEST=FW_NAME=kano emerge-brya depthcharge
    
    Signed-off-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Change-Id: I8971bcc4e1c3b29fb3e0b005b06bc9a8e3fc30ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3211452
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Commit-Queue: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Tested-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/board/brya/kano.c
+++ src/board/brya/kano.c
@@ -6,13 +6,38 @@
 
 #include "base/fw_config.h"
 #include "board/brya/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
 #include "drivers/bus/usb/intel_tcss.h"
 #include "drivers/gpio/alderlake.h"
 #include "drivers/soc/alderlake.h"
+#include "drivers/sound/max98373.h"
+
+#define AUD_I2C_ADDR		0x32
+#define SDMODE_PIN		GPP_A11
 
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
+
+	config = (struct audio_config){
+		.bus = {
+			.type			= AUDIO_I2S,
+			.i2s.address		= SSP_I2S1_START_ADDRESS,
+			.i2s.enable_gpio	= { .pad =  SDMODE_PIN,
+						    .active_low = false },
+			.i2s.settings		= &max98373_settings,
+		},
+		.amp = {
+			.type			= AUDIO_AMP_NONE,
+			.gpio.enable_gpio	= SDMODE_PIN,
+		},
+		.codec = {
+			.type			= AUDIO_MAX98373,
+			.i2c.ctrlr		= I2C0,
+			.i2c.i2c_addr[0]	= AUD_I2C_ADDR,
+		},
+	};
+
 	return &config;
 }
 
