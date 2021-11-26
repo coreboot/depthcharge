c5a3c0fbc4ae03da34181ccfadf40f53d5285a25
Shon Wang <shon.wang@quanta.corp-partner.google.com>
2021-11-26 11:25:04 +0800

    brya : Update vell depthcharge
    Update vell.c for SSD setting
    
    BUG=b:205908918
    BRANCH=None
    TEST=FW_NAME=vell emerge-brya depthcharge
    
    Signed-off-by: Shon Wang <shon.wang@quanta.corp-partner.google.com>
    Change-Id: I98d75bfb7636d725ffc86db9a314586383b35a0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3303250
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/board/brya/vell.c
+++ src/board/brya/vell.c
@@ -7,9 +7,21 @@
 #include "base/fw_config.h"
 #include "board/brya/include/variant.h"
 #include "drivers/gpio/alderlake.h"
+#include "drivers/soc/alderlake.h"
 
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
 	return &config;
+}
+
+static const struct storage_config storage_configs[] = {
+	{ .media = STORAGE_NVME, .pci_dev = SA_DEV_CPU_RP0 },
+
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
 }
