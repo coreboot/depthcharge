991a905beffcbaf2e910d5a82710063f8b3ab6e9
Eric Lai <ericr_lai@compal.corp-partner.google.com>
2021-12-01 17:05:02 +0800

    felwinter: Change TPM to I2C1
    Follow the latest HW schematic change.
    Note: Once the CL get merged, the proto board no longer worked.
    
    BUG=b:208556921
    BRANCH=none
    TEST=build pass
    
    Signed-off-by: Eric Lai <ericr_lai@compal.corp-partner.google.com>
    Change-Id: Iebeb7fa5e6499f5464d0ac831018855f8febacb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3310459
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Commit-Queue: Anson Tseng <ansontseng@google.com>

--- src/board/brya/felwinter.c
+++ src/board/brya/felwinter.c
@@ -35,3 +35,12 @@ const struct audio_config *variant_probe_audio_config(void)
 	};
 	return &config;
 }
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static const struct tpm_config config = {
+		.pci_dev = PCI_DEV(0, 0x15, 1),
+	};
+
+	return &config;
+}
