cf6673652a5aa5b271d6cf143b7c70bea589bba7
Wisley Chen <wisley.chen@quanta.corp-partner.google.com>
2021-12-02 19:53:18 +0600

    Redrix: Config i2c port for TPM by board version
    BUG=b:205648040
    BRANCH=none
    TEST=USE=redrix emerge-brya depthcharge
    
    Change-Id: I3a35f0585a58c5a82b0001c22694584d5a2af242
    Signed-off-by: Wisley Chen <wisley.chen@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3313216
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Reviewed-by: Nick Vaccaro <nvaccaro@google.com>

--- src/board/brya/redrix.c
+++ src/board/brya/redrix.c
@@ -43,3 +43,15 @@ const struct audio_config *variant_probe_audio_config(void)
 
 	return &config;
 }
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static struct tpm_config config = {
+		.pci_dev = PCI_DEV(0, 0x15, 1),
+	};
+
+	if (lib_sysinfo.board_id < 2)
+		config.pci_dev = PCI_DEV(0, 0x15, 3);
+
+	return &config;
+}
