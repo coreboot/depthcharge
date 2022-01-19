7d039b4166b97152848b39b1fe9321c2f327c5ed
David Wu <david_wu@quanta.corp-partner.google.com>
2022-01-19 14:48:32 +0800

    volmar: update depthcharge setting
    Update audio/tpm and storage setting.
    
    BUG=b:211891086
    BRANCH=none
    TEST=FW_NAME=volmar emerge-brya depthcharge
    
    Change-Id: I15dc69844f846a4ef10ad2343eb45ceaa3c2649e
    Signed-off-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3400561
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Nick Vaccaro <nvaccaro@google.com>

--- src/board/brya/volmar.c
+++ src/board/brya/volmar.c
@@ -6,10 +6,55 @@
 
 #include "base/fw_config.h"
 #include "board/brya/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
 #include "drivers/gpio/alderlake.h"
+#include "drivers/soc/alderlake.h"
+#include "drivers/sound/max98373.h"
+
+#define AUD_I2C_ADDR		0x32
+#define SDMODE_PIN		GPP_A11
 
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
+
+	config = (struct audio_config){
+		.bus = {
+			.type			= AUDIO_I2S,
+			.i2s.address		= SSP_I2S1_START_ADDRESS,
+			.i2s.enable_gpio	= { .pad =  SDMODE_PIN,
+						    .active_low = false },
+			.i2s.settings		= &max98373_settings,
+		},
+		.amp = {
+			.type			= AUDIO_AMP_NONE,
+			.gpio.enable_gpio	= SDMODE_PIN,
+		},
+		.codec = {
+			.type			= AUDIO_MAX98373,
+			.i2c.ctrlr		= I2C0,
+			.i2c.i2c_addr[0]	= AUD_I2C_ADDR,
+		},
+	};
+
+	return &config;
+}
+
+static const struct storage_config storage_configs[] = {
+	{ .media = STORAGE_NVME, .pci_dev = SA_DEV_CPU_RP0 },
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static const struct tpm_config config = {
+		.pci_dev = PCI_DEV(0, 0x15, 1),
+	};
+
 	return &config;
 }
