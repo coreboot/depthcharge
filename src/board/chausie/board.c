edc3531c31de3401dbfa3a9cba969759a94e71bd
Karthikeyan Ramasubramanian <kramasub@chromium.org>
2022-02-09 15:58:57 -0700

    board/chausie: Add chausie board
    Add support for Chausie board to boot out of NVMe storage as well as USB
    disk. PCIe device for NVME storage is chosen based on coreboot config.
    
    BUG=b:214431249
    BRANCH=none
    TEST=cros-workon-skyrim-chausie start depthcharge; emerge-skyrim-chausie
    depthcharge
    
    Change-Id: I72335a2bbf9c4e241a840936eafd97250d11cd4a
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3451279
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

---
+++ src/board/chausie/board.c
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright 2022 Google LLC.  */
+
+#include <libpayload.h>
+
+#include "base/init_funcs.h"
+#include "drivers/gpio/sysinfo.h"
+#include "drivers/power/fch.h"
+#include "drivers/soc/sabrina.h"
+#include "drivers/storage/nvme.h"
+#include "pci.h"
+#include "vboot/util/flag.h"
+
+
+static int get_gpio_0(struct GpioOps *me) {
+	return 0;
+}
+
+static struct GpioOps fake_gpio_0 = {
+	.get = get_gpio_0,
+};
+
+
+static int get_gpio_1(struct GpioOps *me) {
+	return 1;
+}
+
+static struct GpioOps fake_gpio_1 = {
+	.get = get_gpio_1,
+};
+
+static int board_setup(void)
+{
+	sysinfo_install_flags(NULL);
+	flag_replace(FLAG_LIDSW, &fake_gpio_1);
+	flag_replace(FLAG_PWRSW, &fake_gpio_0);
+
+	/* PCI Bridge for NVMe */
+	NvmeCtrlr *nvme = new_nvme_ctrlr(PCI_DEV(0, 0x02, 0x01));
+	list_insert_after(&nvme->ctrlr.list_node,
+				&fixed_block_dev_controllers);
+
+	power_set_ops(&kern_power_ops);
+
+	return 0;
+}
+
+INIT_FUNC(board_setup);
