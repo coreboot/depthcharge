5d78ece9e99b386c52c0efaaa5fa31a7793807c8
Yu-Ping Wu <yupingso@chromium.org>
2022-02-24 18:18:48 +0800

    screenshot: Add support for altfw screen
    Currently screenshot would fail to generate the alternate bootloader
    (altfw) screen because of no bootloaders (vboot's stub
    vb2ex_get_altfw_count() returns 0). Therefore, add a mocking
    payload_get_altfw_list().
    
    BUG=b:221184179
    TEST=make screenshot SCREEN=UI_SCREEN_DEVELOPER_SELECT_ALTFW
    BRANCH=none
    
    Change-Id: I47246aa4d89e36bc9548755b93c40e2d296147cc
    Signed-off-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3488627
    Reviewed-by: Hsuan Ting Chen <roccochen@chromium.org>

---
+++ screenshot/mocks/boot/payload.c
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+#include <vb2_api.h>
+
+#include "base/list.h"
+#include "boot/payload.h"
+
+struct ListNode *payload_get_altfw_list(void)
+{
+	static struct ListNode payload_altfw_head;
+	static int payload_altfw_head_initialized;
+	int i;
+	struct altfw_info *info;
+	const struct altfw_info altfw_infos[] = {
+		[0] = {
+			.filename = "altfw_file0",
+			.name = "altfw 0",
+			.desc = "altfw_desc0",
+		},
+		[1] = {
+			.filename = "altfw_file1",
+			.name = "altfw 1",
+			.desc = "altfw_desc1",
+		},
+	};
+
+	if (payload_altfw_head_initialized)
+		return &payload_altfw_head;
+
+	for (i = ARRAY_SIZE(altfw_infos) - 1; i >= 0; i--) {
+		info = xzalloc(sizeof(*info));
+		*info = altfw_infos[i];
+		info->seqnum = i + 1;  /* Must be positive */
+		list_insert_after(&info->list_node, &payload_altfw_head);
+	}
+
+	payload_altfw_head_initialized = 1;
+	return &payload_altfw_head;
+}
