03c014beb289de4b0af2210cc249a0ab85fdc173
Raihow Shi <raihow_shi@wistron.corp-partner.google.com>
2022-03-02 16:32:16 +0800

    moli: Enable PCIe-to-eMMC support for booting from BH799
    support PCIe-to-eMMC boot.
    
    BUG=b:222221807
    
    Signed-off-by: Raihow Shi <raihow_shi@wistron.corp-partner.google.com>
    Change-Id: I96c3b9f1116b2c30ac580fb45da57e510ef78531
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3499903
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/board/brya/moli.c
+++ src/board/brya/moli.c
@@ -13,6 +13,16 @@ const struct audio_config *variant_probe_audio_config(void)
 	static struct audio_config config;
 	return &config;
 }
+static const struct storage_config storage_configs[] = {
+	{ .media = STORAGE_NVME, .pci_dev = SA_DEV_CPU_RP0 },
+	{ .media = STORAGE_NVME, .pci_dev = PCH_DEV_PCIE11 },
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
 
 const struct tpm_config *variant_get_tpm_config(void)
 {
