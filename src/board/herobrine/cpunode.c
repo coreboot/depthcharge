2b54e5d3f4ffa60984a968cdfe4fcdfe960aa071
Ravi Kumar Bokka <rbokka@codeaurora.org>
2022-03-17 19:36:26 +0530

    herobrine: Update cpu FDT status property based on fusebit
    This patch allows individual CPUs to be disabled in the device tree by
    setting a bit in a fuse register.
    
    BRANCH=none
    BUG=b:213552563
    TEST=Validated on qualcomm sc7280 development board
    
    Signed-off-by: Thogaru Venkat <quic_thogaru@quicinc.com>
    Signed-off-by: Julius Werner <jwerner@chromium.org>
    Change-Id: Ib9e02d8a0d381e7ff1364859e83b63f4c445263a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3581742

---
+++ src/board/herobrine/cpunode.c
@@ -0,0 +1,48 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+
+#include <libpayload.h>
+#include "base/init_funcs.h"
+#include "base/device_tree.h"
+#include "base/list.h"
+
+#define START_BIT 7
+#define END_BIT 14
+
+static int install_cpunode_data(DeviceTreeFixup *fixup, DeviceTree *tree)
+{
+	unsigned int *fuse_addr = (unsigned int *)0x78418C;
+	unsigned int fuse_value = read32(fuse_addr);
+	char cpu_name[8];
+	const char *cpu_path[] = { "cpus", cpu_name, NULL };
+
+	for (int i = START_BIT; i <= END_BIT; i++) {
+		if ((fuse_value >> i) & 0x1) {
+			snprintf(cpu_name, sizeof(cpu_name), "cpu@%u",
+				 (i - START_BIT) * 100);
+			DeviceTreeNode *cpu_node = dt_find_node(tree->root,
+				cpu_path, NULL, NULL, 0);
+			if (!cpu_node) {
+				printf("ERROR: Failed to find node %s\n",
+				       cpu_name);
+				return 1;
+			}
+			dt_add_string_prop(cpu_node, "status", "fail");
+			printf("CPU%d disabled by fuse setting\n",
+			       i - START_BIT);
+		}
+	}
+
+	return 0;
+}
+
+static DeviceTreeFixup cpu_data = {
+	.fixup = &install_cpunode_data
+};
+
+static int cpunode_init(void)
+{
+	list_insert_after(&cpu_data.list_node, &device_tree_fixups);
+	return 0;
+}
+
+INIT_FUNC(cpunode_init);
