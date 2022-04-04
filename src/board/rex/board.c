df60c229dd38de97ed66db3036bd4d787ab57561
Subrata Banik <subratabanik@google.com>
2022-04-04 21:49:38 +0530

    rex: Add initial mainboard support
    This patch adds minimal code from brya to rex.
    Add placeholder for TPM, Audio etc. as applicable based
    on rex schematics.
    
    BUG=b:224325352
    TEST=Able to build rex depthcharge.
    
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Change-Id: If87a64ebda4bb04f16de03102e2b7072a2ba20c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3568906
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Tested-by: Subrata Banik <subratabanik@chromium.org>
    Auto-Submit: Subrata Banik <subratabanik@chromium.org>
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>

---
+++ src/board/rex/board.c
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/*
+ * These needs to be included first.
+ * Some of the driver headers would be dependent on these.
+ */
+#include <pci.h>
+#include <pci/pci.h>
+
+#include "base/init_funcs.h"
+#include "base/list.h"
+#include "board/brya/include/variant.h"
+#include "drivers/bus/i2c/designware.h"
+#include "drivers/bus/i2c/i2c.h"
+#include "drivers/ec/cros/lpc.h"
+#include "drivers/flash/flash.h"
+#include "drivers/gpio/sysinfo.h"
+#include "drivers/gpio/gpio.h"
+#include "drivers/power/pch.h"
+#include "drivers/soc/meteorlake.h"
+#include "vboot/util/flag.h"
+#include <libpayload.h>
+#include <sysinfo.h>
+
+__weak const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	static const struct storage_config storage_configs[] = {
+		{ .media = STORAGE_NVME, .pci_dev = PCI_DEV_PCIE0 },
+	};
+
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
+static void ec_setup(void)
+{
+	CrosEcLpcBus *cros_ec_lpc_bus =
+		new_cros_ec_lpc_bus(CROS_EC_LPC_BUS_GENERIC);
+	CrosEc *cros_ec = new_cros_ec(&cros_ec_lpc_bus->ops, NULL);
+	register_vboot_ec(&cros_ec->vboot);
+}
+
+static void tpm_setup(void)
+{
+	/* Placeholder for TPM setup */
+}
+
+static void audio_setup(void)
+{
+	/* Placeholder for Audio setup */
+}
+
+static int board_setup(void)
+{
+	sysinfo_install_flags(NULL);
+	ec_setup();
+	tpm_setup();
+	power_set_ops(&meteorlake_power_ops);
+	audio_setup();
+
+	return 0;
+}
+
+INIT_FUNC(board_setup);
---
+++ src/board/rex/storage.c
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <pci.h>
+#include <pci/pci.h>
+#include <stdio.h>
+
+#include "base/init_funcs.h"
+#include "base/list.h"
+#include "board/rex/include/variant.h"
+#include "drivers/soc/meteorlake.h"
+#include "drivers/soc/intel_common.h"
+#include "drivers/storage/blockdev.h"
+#include "drivers/storage/nvme.h"
+
+static void setup_nvme(pcidev_t dev)
+{
+	if (!CONFIG(DRIVER_STORAGE_NVME))
+		return;
+
+	NvmeCtrlr *nvme = new_nvme_ctrlr(dev);
+	if (nvme)
+		list_insert_after(&nvme->ctrlr.list_node,
+				  &fixed_block_dev_controllers);
+}
+
+static int configure_storage(void)
+{
+	size_t count;
+	const struct storage_config *config = variant_get_storage_configs(&count);
+	if (!config || !count)
+		return 0;
+
+	for (size_t i = 0; i < count; i++) {
+		const pcidev_t dev = config[i].pci_dev;
+		const SocPcieRpGroup *group;
+		size_t group_count;
+		pcidev_t remapped;
+
+		group = soc_get_rp_group(dev, &group_count);
+		if (group)
+			remapped = intel_remap_pcie_rp(dev, group, group_count);
+		else
+			remapped = dev;
+
+		if (remapped == (pcidev_t)-1) {
+			printf("%s: Failed to remap %2X:%X\n",
+			       __func__, PCI_SLOT(dev), PCI_FUNC(dev));
+			continue;
+		}
+
+		switch (config[i].media) {
+		case STORAGE_NVME:
+			setup_nvme(remapped);
+			break;
+		default:
+			break;
+		}
+	}
+
+	return 0;
+}
+
+INIT_FUNC(configure_storage);
