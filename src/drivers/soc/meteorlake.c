c9664f24260999cc7e0918cb3fc048ae796c6b4e
Subrata Banik <subratabanik@google.com>
2022-04-04 20:49:46 +0530

    meteorlake: Add PCIe Root Port group
    This patch adds Meteor Lake PCIe Root Port Group.
    
    MTL EDS doc 640228.
    
    BUG=b:224325352
    TEST=None
    
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Change-Id: Id97c098246d5732489715c8a7452016ed60e5f78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3568905
    Reviewed-by: Wonkyu Kim <wonkyu.kim@intel.com>
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Tested-by: Subrata Banik <subratabanik@chromium.org>
    Auto-Submit: Subrata Banik <subratabanik@chromium.org>
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>

--- src/drivers/soc/meteorlake.c
+++ src/drivers/soc/meteorlake.c
@@ -5,6 +5,9 @@
 #include "drivers/soc/common/iomap.h"
 #include "drivers/soc/intel_common.h"
 
+#define PCIE_RP_SLOT_0	0x1c
+#define PCIE_RP_SLOT_1	0x06
+
 static const SocGpeConfig cfg = {
 	.gpe_max = GPE_MAX,
 	.acpi_base = ACPI_BASE_ADDRESS,
@@ -14,4 +17,24 @@ static const SocGpeConfig cfg = {
 int meteorlake_get_gpe(int gpe)
 {
 	return soc_get_gpe(gpe, &cfg);
+}
+
+const SocPcieRpGroup mtl_rp_groups[] = {
+	{ .slot = 0x1c, .first_fn = 0, .count = 8 },
+	{ .slot = 0x06, .first_fn = 0, .count = 3 },
+};
+const unsigned int mtl_rp_groups_count = ARRAY_SIZE(mtl_rp_groups);
+
+const SocPcieRpGroup *soc_get_rp_group(pcidev_t dev, size_t *count)
+{
+	switch (PCI_SLOT(dev)) {
+	case PCIE_RP_SLOT_0: /* fallthrough */
+	case PCIE_RP_SLOT_1:
+		*count = mtl_rp_groups_count;
+		return mtl_rp_groups;
+
+	default:
+		*count = 0;
+		return NULL;
+	}
 }
