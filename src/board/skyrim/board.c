fe0a65381904867c2387834dae820b773134287c
Karthikeyan Ramasubramanian <kramasub@chromium.org>
2022-04-07 22:47:01 -0600

    board/skyrim: Enable TPM
    Replace mock TPM with Ti50 TPM2.0
    
    BUG=None
    BRANCH=None
    TEST=Build and boot to OS in Skyrim.
    
    Signed-off-by: Karthikeyan Ramasubramanain <kramasub@google.com>
    Change-Id: I70fac8cd28f1307a3a88127e64df9dbeec3659cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3577982
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Commit-Queue: Raul Rangel <rrangel@chromium.org>

--- src/board/skyrim/board.c
+++ src/board/skyrim/board.c
@@ -5,14 +5,30 @@
 
 #include "base/init_funcs.h"
 #include "base/list.h"
+#include "drivers/bus/i2c/designware.h"
+#include "drivers/bus/i2c/i2c.h"
+#include "drivers/gpio/gpio.h"
 #include "drivers/gpio/kern.h"
 #include "drivers/gpio/sysinfo.h"
 #include "drivers/power/fch.h"
 #include "drivers/soc/sabrina.h"
 #include "drivers/storage/nvme.h"
+#include "drivers/tpm/cr50_i2c.h"
+#include "drivers/tpm/tpm.h"
 #include "pci.h"
 #include "vboot/util/flag.h"
 
+#define CR50_INT_18	18
+
+static int cr50_irq_status(void)
+{
+	static KernGpio *tpm_gpio;
+
+	if (!tpm_gpio)
+		tpm_gpio = new_kern_fch_gpio_latched(CR50_INT_18);
+
+	return gpio_get(&tpm_gpio->ops);
+}
 
 static int get_gpio_0(struct GpioOps *me) {
 	return 0;
@@ -36,6 +52,12 @@ static int board_setup(void)
 	sysinfo_install_flags(NULL);
 	flag_replace(FLAG_LIDSW, &fake_gpio_1);
 	flag_replace(FLAG_PWRSW, &fake_gpio_0);
+
+	/* Set up Dauntless on I2C3 */
+	DesignwareI2c *i2c_ti50 = new_designware_i2c(
+		AP_I2C3_ADDR, 400000, AP_I2C_CLK_MHZ);
+	tpm_set_ops(&new_cr50_i2c(&i2c_ti50->ops, 0x50,
+				  &cr50_irq_status)->base.ops);
 
 	/* PCI Bridge for NVMe */
 	NvmeCtrlr *nvme = new_nvme_ctrlr(PCI_DEV(0, 0x02, 0x3));
