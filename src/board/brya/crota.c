28999708957b98a89fe33aa7b5675254ed1e4eb8
Scott Chao <scott_chao@wistron.corp-partner.google.com>
2022-04-15 09:55:42 +0800

    crota: fix TPM i2c port
    BUG=b:229419355
    BRANCH=none
    TEST=boot to depthcharge without TPM read/ write error
    
    Signed-off-by: Scott Chao <scott_chao@wistron.corp-partner.google.com>
    Change-Id: I5428eb5a3992a1e308f9a52192809192dcc5a265
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3586529
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>

--- src/board/brya/crota.c
+++ src/board/brya/crota.c
@@ -13,3 +13,12 @@ const struct audio_config *variant_probe_audio_config(void)
 	static struct audio_config config;
 	return &config;
 }
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static struct tpm_config config = {
+		.pci_dev = PCI_DEV(0, 0x15, 1),
+	};
+
+	return &config;
+}
