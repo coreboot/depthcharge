b49c87d7b1517e3789346bc13780ef7889b0eaad
David Wu <david_wu@quanta.corp-partner.google.com>
2022-05-03 11:18:37 +0800

    osiris: update depthcharge setting
    Update audio/tpm and storage setting.
    
    BUG=b:224423318
    BRANCH=none
    TEST=FW_NAME=osiris emerge-brya depthcharge
    
    Signed-off-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Change-Id: Ifde29926e26762eaff5e0f1b418c97836d9cb23e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3622694
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    (cherry picked from commit dea12ecbd9f415abed604810c815e29b02ef78e9)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3631723

--- src/board/brya/osiris.c
+++ src/board/brya/osiris.c
@@ -6,10 +6,52 @@
 
 #include "base/fw_config.h"
 #include "board/brya/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
 #include "drivers/gpio/alderlake.h"
+#include "drivers/soc/alderlake.h"
+
+#define SDMODE_PIN		GPP_A11
+#define SDMODE_ENABLE		0
 
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
+	config = (struct audio_config){
+		.bus = {
+			.type			= AUDIO_I2S,
+			.i2s.address		= SSP_I2S1_START_ADDRESS,
+			.i2s.enable_gpio	= {
+						.pad = SDMODE_PIN,
+						.active_low = SDMODE_ENABLE },
+			.i2s.settings		= &max98357a_settings,
+		},
+		.amp = {
+			.type			= AUDIO_GPIO_AMP,
+			.gpio.enable_gpio	= SDMODE_PIN,
+		},
+		.codec = {
+			.type			= AUDIO_MAX98357,
+		},
+	};
+	return &config;
+}
+
+static const struct storage_config storage_configs[] = {
+	{ .media = STORAGE_NVME, .pci_dev = SA_DEV_CPU_RP0 },
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static const struct tpm_config config = {
+		.pci_dev = PCI_DEV(0, 0x15, 1),
+	};
+
 	return &config;
 }
