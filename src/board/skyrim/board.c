f498dbabc39c056077e8f611d0fbb4709a671656
Tim Van Patten <timvp@google.com>
2022-06-16 12:50:22 -0600

    skyrim: Add SD bridge IC Realtek RTS5227S support
    Add SD bridge IC Realtek RTS5227S support for skyrim devices.
    
    BUG=b:220805925
    TEST=Boot skyrim with external SD card
    TEST=Boot skyrim with internal NVMe
    BRANCH=none
    
    Change-Id: I403fddf4c704fd4547c5808a6df75911c5021f7c
    Signed-off-by: Tim Van Patten <timvp@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3710496
    Reviewed-by: Tim Wawrzynczak <twawrzynczak@chromium.org>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>

--- src/board/skyrim/board.c
+++ src/board/skyrim/board.c
@@ -5,6 +5,7 @@
 
 #include "base/init_funcs.h"
 #include "base/list.h"
+#include "board/skyrim/include/variant.h"
 #include "drivers/bus/i2c/designware.h"
 #include "drivers/bus/i2c/i2c.h"
 #include "drivers/ec/cros/lpc.h"
@@ -21,6 +22,19 @@
 
 #define CR50_INT_18	18
 #define EC_SOC_INT_ODL	84
+
+__weak const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	static const struct storage_config storage_configs[] = {
+		/* Coreboot devicetree.cb: gpp_bridge_2 = 02.3 */
+		{ .media = STORAGE_NVME, .pci_dev = PCI_DEV(0, 0x02, 0x3)},
+		/* Coreboot devicetree.cb: gpp_bridge_1 = 02.2 */
+		{ .media = STORAGE_RTKMMC, .pci_dev = PCI_DEV(0, 0x02, 0x02)},
+	};
+
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
 
 static int cr50_irq_status(void)
 {
@@ -55,11 +69,6 @@ static int board_setup(void)
 		AP_I2C3_ADDR, 400000, AP_I2C_CLK_MHZ);
 	tpm_set_ops(&new_cr50_i2c(&i2c_ti50->ops, 0x50,
 				  &cr50_irq_status)->base.ops);
-
-	/* PCI Bridge for NVMe */
-	NvmeCtrlr *nvme = new_nvme_ctrlr(PCI_DEV(0, 0x02, 0x3));
-	list_insert_after(&nvme->ctrlr.list_node,
-				&fixed_block_dev_controllers);
 
 	power_set_ops(&kern_power_ops);
 
---
+++ src/board/skyrim/storage.c
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <pci.h>
+#include <pci/pci.h>
+#include <stdio.h>
+
+#include "base/init_funcs.h"
+#include "base/list.h"
+#include "board/skyrim/include/variant.h"
+#include "drivers/storage/blockdev.h"
+#include "drivers/storage/nvme.h"
+#include "drivers/storage/sdhci.h"
+#include "drivers/storage/rtk_mmc.h"
+
+static void setup_rtkmmc(pcidev_t dev)
+{
+	if (!CONFIG(DRIVER_STORAGE_MMC_RTK))
+		return;
+
+	RtkMmcHost *rtkmmc = probe_pci_rtk_host(dev, RTKMMC_PLATFORM_REMOVABLE);
+	if (rtkmmc) {
+		rtkmmc->name = "rtksd";
+		list_insert_after(&rtkmmc->mmc_ctrlr.ctrlr.list_node,
+				&removable_block_dev_controllers);
+	}
+}
+
+static void setup_nvme(pcidev_t dev)
+{
+	if (!CONFIG(DRIVER_STORAGE_NVME))
+		return;
+
+	/* PCIE O2 Bridge for NVMe */
+	NvmeCtrlr *nvme = NULL;
+	nvme = new_nvme_ctrlr(dev);
+
+	list_insert_after(&nvme->ctrlr.list_node,
+				&fixed_block_dev_controllers);
+}
+
+static int configure_storage(void)
+{
+	size_t count;
+	const struct storage_config *config =
+		variant_get_storage_configs(&count);
+	if (!config || !count)
+		return 0;
+
+	for (size_t i = 0; i < count; i++) {
+		const pcidev_t dev = config[i].pci_dev;
+
+		switch (config[i].media) {
+		case STORAGE_NVME:
+			setup_nvme(dev);
+			break;
+		case STORAGE_RTKMMC:
+			setup_rtkmmc(dev);
+			break;
+		default:
+			break;
+		}
+	}
+
+	return 0;
+}
+
+INIT_FUNC(configure_storage);
