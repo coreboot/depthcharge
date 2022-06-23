fa23c6718e128d59ec2dc4c624946866f0ff38e1
YH Lin <yueherngl@google.com>
2022-06-23 12:49:41 -0700

    depthcharge: add skolas4es variant
    Add the variant file for skolas4es, currently just a copy from brya,
    and to be revised later on.
    
    BUG=b:230772730
    BRANCH=none
    TEST=none
    
    Signed-off-by: YH Lin <yueherngl@chromium.org>
    Change-Id: I4760c2fbd49475c9223595b06fa2c55b590de952
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3722191
    Reviewed-by: Nick Vaccaro <nvaccaro@google.com>
    (cherry picked from commit fec66285da087c1c8a659a45b64dc1b17ef4ccbe)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3726962
    Tested-by: Nick Vaccaro <nvaccaro@google.com>
    Auto-Submit: Nick Vaccaro <nvaccaro@google.com>

---
+++ src/board/brya/skolas4es.c
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+#include <pci.h>
+#include <pci/pci.h>
+
+#include "base/fw_config.h"
+#include "board/brya/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
+#include "drivers/bus/soundwire/cavs_2_5-sndwregs.h"
+#include "drivers/gpio/alderlake.h"
+#include "drivers/soc/alderlake.h"
+
+#define SDMODE_PIN		GPP_A11
+
+const struct audio_config *variant_probe_audio_config(void)
+{
+	static struct audio_config config;
+
+	memset(&config, 0, sizeof(config));
+
+	if (fw_config_probe(FW_CONFIG(AUDIO, MAX98357_ALC5682I_I2S))) {
+		config = (struct audio_config){
+			.bus = {
+				.type			= AUDIO_I2S,
+				.i2s.address		= SSP_I2S2_START_ADDRESS,
+				.i2s.enable_gpio	= { .pad = SDMODE_PIN },
+				.i2s.settings		= &max98357a_settings,
+			},
+			.amp = {
+				.type			= AUDIO_GPIO_AMP,
+				.gpio.enable_gpio	= SDMODE_PIN,
+			},
+			.codec = {
+				.type			= AUDIO_MAX98357,
+			},
+		};
+	} else if (fw_config_probe(FW_CONFIG(AUDIO, MAX98373_ALC5682_SNDW))) {
+		config = (struct audio_config){
+			.bus = {
+				.type			= AUDIO_SNDW,
+				.sndw.link		= AUDIO_SNDW_LINK2,
+			},
+			.codec = {
+				.type			= AUDIO_MAX98373,
+			},
+		};
+	}
+
+	return &config;
+}
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static struct tpm_config config = {
+		.pci_dev = PCI_DEV(0, 0x15, 1),
+	};
+
+	if (lib_sysinfo.board_id < 4)
+		config.pci_dev = PCI_DEV(0, 0x15, 3);
+
+	return &config;
+}
