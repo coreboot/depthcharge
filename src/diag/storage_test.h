76522c1d9f8fb3253df7591c53b090d04d17ca47
Hsuan Ting Chen <roccochen@chromium.org>
2022-06-24 17:04:59 +0800

    diag: Reduce the frequency of dumping storage log
    Sending the GetlogPage too frequently might cause unexpected behavior.
    Introduce the concept of storage delays which prevent calling
    dump_storage_test_log too frequently from the UI loop.
    
    BUG=b:233973191, b:233715472
    BRANCH=brya
    TEST=Test the firmware on vell
    TEST=make clean-screenshot &&
         make screenshot SCREEN=UI_SCREEN_DIAGNOSTICS
    TEST=make clean-unit-tests && make unit-tests -j
    
    Signed-off-by: Hsuan Ting Chen <roccochen@chromium.org>
    Change-Id: Id09ba08bbfb372597e343191f7905f75bd263a49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3723280
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>

--- src/diag/storage_test.h
+++ src/diag/storage_test.h
@@ -10,11 +10,37 @@
 #include "drivers/storage/blockdev.h"
 #include "drivers/storage/info.h"
 
+/* Prevent dumping storage self-test log too frequently */
+#define DIAG_STORAGE_TEST_SHORT_DELAY_MS 200
+#define DIAG_STORAGE_TEST_EXTENDED_DELAY_MS 3000
+#define DIAG_STORAGE_TEST_DEFAULT_DELAY_MS 1000
+
 /*
+ * Dump storage self-test log into the buffer and truncate the text which
+ * exceeds "end".
+ *
+ * The storage test log is retrieved from the block device driver. When this
+ * happens too frequently, the block device might return unexpected result.
+ * Therefore, this function uses a stopwatch to record the timestamp. If the
+ * current call is too close to the previous one, DIAG_TEST_RUNNING will be
+ * returned without retrieving the log.
+ *
+ * @param buf		The buffer to store the self-test log.
+ * @param end		The pointer to the maximum limit of the buffer.
+ *
  * @return DIAG_TEST_SUCCESS on success, or other DiagTestResult enums on
  * failure.
  */
 DiagTestResult diag_dump_storage_test_log(char *buf, const char *end);
+
+/*
+ * Start or stop a storage self-test.
+ *
+ * @param ops		The self-test type.
+ *
+ * @return DIAG_TEST_SUCCESS on success, or other DiagTestResult enums on
+ * failure.
+ */
 DiagTestResult diag_storage_test_control(enum BlockDevTestOpsType ops);
 
 #endif
