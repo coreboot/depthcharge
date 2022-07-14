1eadbc04b396b3926f0f6d444f606afa3936ef5d
Tim Van Patten <timvp@google.com>
2022-07-14 14:59:32 -0600

    board/skyrim: Enable AMD ACP driver for Mendocino
    Enable AMD ACP driver for Mendocino SOCs.
    
    BRANCH=none
    BUG=b:205032348
    TEST=Build with `USE=fwconsole`, `devbeep` in depthcharge CLI
    
    Signed-off-by: Tim Van Patten <timvp@google.com>
    Change-Id: I233b395c920959ee4a5b7aafea006125471ddd1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3835617
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Reviewed-by: Jonathon Murphy <jpmurphy@google.com>

--- src/board/skyrim/board.c
+++ src/board/skyrim/board.c
@@ -3,6 +3,7 @@
 
 #include <libpayload.h>
 
+#include "base/fw_config.h"
 #include "base/init_funcs.h"
 #include "base/list.h"
 #include "board/skyrim/include/variant.h"
@@ -14,6 +15,11 @@
 #include "drivers/gpio/sysinfo.h"
 #include "drivers/power/fch.h"
 #include "drivers/soc/mendocino.h"
+#include "drivers/sound/amd_acp.h"
+#include "drivers/sound/gpio_amp.h"
+#include "drivers/sound/route.h"
+#include "drivers/sound/rt1019.h"
+#include "drivers/sound/rt5682s.h"
 #include "drivers/storage/nvme.h"
 #include "drivers/tpm/google/i2c.h"
 #include "drivers/tpm/tpm.h"
@@ -36,6 +42,14 @@ __weak const struct storage_config *variant_get_storage_configs(size_t *count)
 	return storage_configs;
 }
 
+/* Clock frequencies */
+#define LRCLK			8000
+
+/* Audio Configuration */
+#define AUDIO_I2C_MMIO_ADDR	0xfedc4000
+#define AUDIO_I2C_SPEED		400000
+#define I2C_DESIGNWARE_CLK_MHZ	150
+
 static int cr50_irq_status(void)
 {
 	static KernGpio *tpm_gpio;
@@ -53,6 +67,80 @@ static GpioOps *mkbp_int_ops(void)
 	return new_gpio_not(&mkbp_int_gpio->ops);
 }
 
+__weak enum audio_amp variant_get_audio_amp(void)
+{
+	if (fw_config_probe(FW_CONFIG(AUDIO_DB, AUDIO_DB_C_ALC5682I_A_ALC1019)))
+		return AUDIO_AMP_RT1019;
+	else if (fw_config_probe(
+		FW_CONFIG(AUDIO_DB, AUDIO_DB_C_NAU88L25YGB_A_MAX98360AENL)))
+		return AUDIO_AMP_MAX98360;
+	else
+		return AUDIO_AMP_INVALID;
+}
+
+__weak unsigned int variant_get_en_spkr_gpio(void)
+{
+	return EN_SPKR;
+}
+
+static void setup_amd_acp_i2s(pcidev_t acp_pci_dev)
+{
+	enum audio_amp amp = variant_get_audio_amp();
+	if (amp == AUDIO_AMP_INVALID) {
+		printf("%s: Failed to get audio amp.\n", __func__);
+		return;
+	}
+
+	AmdAcp *acp = new_amd_acp(acp_pci_dev, ACP_OUTPUT_PLAYBACK, LRCLK,
+				  0x1FFF /* Volume */);
+
+	SoundRoute *sound_route = new_sound_route(&acp->sound_ops);
+
+	KernGpio *en_spkr_gpio =
+		new_kern_fch_gpio_output(variant_get_en_spkr_gpio(), 0);
+
+	/* Note: component order matters here. The first thing inserted is the
+	 * last thing enabled. */
+
+	/* If a warm reboot was performed, we need to clear out any settings the
+	 * OS has made to the RT1019. */
+	if (amp == AUDIO_AMP_RT1019) {
+		DesignwareI2c *i2c = new_designware_i2c(
+			(uintptr_t)AUDIO_I2C_MMIO_ADDR,
+			AUDIO_I2C_SPEED, I2C_DESIGNWARE_CLK_MHZ);
+
+		rt1019Codec *speaker_amp =
+			new_rt1019_codec(&i2c->ops, AUD_RT1019_DEVICE_ADDR_R);
+
+		list_insert_after(&speaker_amp->component.list_node,
+				  &sound_route->components);
+
+		GpioAmpCodec *en_spkr = new_gpio_amp_codec(&en_spkr_gpio->ops);
+
+		/* We need to enable the speaker amp before we can send I2C
+		 * transactions. */
+		list_insert_after(&en_spkr->component.list_node,
+				  &sound_route->components);
+	} else if (amp == AUDIO_AMP_MAX98360) {
+		/* Give the MAX98360A time to sample the BCLK */
+		GpioAmpCodec *en_spkr =
+			new_gpio_amp_codec_with_delay(&en_spkr_gpio->ops, 1000);
+
+		/* Enable the speaker amp last since we have to delay to sample
+		 * the clocks. */
+		list_insert_after(&en_spkr->component.list_node,
+				  &sound_route->components);
+	} else {
+		printf("%s: Unknown audio amp: %d\n", __func__, amp);
+		return;
+	}
+
+	/* Enable the ACP so it can start generating the clocks. */
+	list_insert_after(&acp->component.list_node, &sound_route->components);
+
+	sound_set_ops(&sound_route->ops);
+}
+
 static int board_setup(void)
 {
 	CrosEcLpcBus *cros_ec_lpc_bus =
@@ -63,6 +151,10 @@ static int board_setup(void)
 	sysinfo_install_flags(NULL);
 	flag_replace(FLAG_LIDSW, cros_ec_lid_switch_flag());
 	flag_replace(FLAG_PWRSW, cros_ec_power_btn_flag());
+
+	pcidev_t acp_pci_dev;
+	if (pci_find_device(AMD_PCI_VID, AMD_FAM17H_ACP_PCI_DID, &acp_pci_dev))
+		setup_amd_acp_i2s(acp_pci_dev);
 
 	/* Set up Dauntless on I2C3 */
 	DesignwareI2c *i2c_ti50 = new_designware_i2c(
--- src/board/skyrim/include/variant.h
+++ src/board/skyrim/include/variant.h
@@ -6,6 +6,9 @@
 #include <libpayload.h>
 #include <stddef.h>
 
+/* EN_SPKR GPIOs */
+#define EN_SPKR			139
+
 enum storage_media {
 	STORAGE_NVME,
 	STORAGE_RTKMMC
@@ -14,6 +17,12 @@ enum storage_media {
 struct storage_config {
 	enum storage_media media;
 	pcidev_t pci_dev;
+};
+
+enum audio_amp {
+	AUDIO_AMP_RT1019,
+	AUDIO_AMP_MAX98360,
+	AUDIO_AMP_INVALID
 };
 
 const struct storage_config *variant_get_storage_configs(size_t *count);
