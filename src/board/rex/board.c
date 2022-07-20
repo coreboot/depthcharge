c0e09307824b14f94b1eeecb0e8d034065fa4425
Kapil Porwal <kapilporwal@google.com>
2022-07-20 16:13:31 +0000

    board/rex: Add TPM for Rex
    Add TPM device for Rex.
    Device details:
    I2C Controller/Bus = 4
    I2C Slave Address = 0x50
    GPE = GPE0_DW1_03/GPP_E03
    
    BUG=b:224325352
    TEST=Able to build depthcharge without errors.
    
    Signed-off-by: Kapil Porwal <kapilporwal@google.com>
    Change-Id: Ie4fefa0b5e9ff899371c81106e67db990a794af4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3778097
    Tested-by: Subrata Banik <subratabanik@chromium.org>
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    Reviewed-by: Tarun Tuli <taruntuli@google.com>

--- src/board/rex/board.c
+++ src/board/rex/board.c
@@ -18,9 +18,15 @@
 #include "drivers/gpio/gpio.h"
 #include "drivers/power/pch.h"
 #include "drivers/soc/meteorlake.h"
+#include "drivers/tpm/google/i2c.h"
+#include "drivers/tpm/google/switches.h"
+#include "drivers/tpm/tpm.h"
 #include "vboot/util/flag.h"
 #include <libpayload.h>
 #include <sysinfo.h>
+
+#define TPM_I2C_ADDR	0x50
+#define I2C_FS_HZ	400000 /* 400KHz */
 
 __weak const struct storage_config *variant_get_storage_configs(size_t *count)
 {
@@ -41,9 +47,32 @@ static void ec_setup(void)
 	register_vboot_ec(&cros_ec->vboot);
 }
 
+__weak const struct tpm_config *variant_get_tpm_config(void)
+{
+	static const struct tpm_config config = {
+		.pci_dev = PCI_DEV_I2C4,
+	};
+
+	return &config;
+}
+
+static int cr50_irq_status(void)
+{
+	return meteorlake_get_gpe(GPE0_DW1_03); /* GPP_E03 */
+}
+
 static void tpm_setup(void)
 {
-	/* Placeholder for TPM setup */
+	const struct tpm_config *config = variant_get_tpm_config();
+	DesignwareI2c *i2c = new_pci_designware_i2c(config->pci_dev,
+						    I2C_FS_HZ,
+						    METEORLAKE_DW_I2C_MHZ);
+	Cr50I2c *tpm = new_cr50_i2c(&i2c->ops, TPM_I2C_ADDR,
+				    &cr50_irq_status);
+	tpm_set_ops(&tpm->base.ops);
+	if (CONFIG(TPM_GOOGLE_SWITCHES))
+		flag_replace(FLAG_PHYS_PRESENCE,
+				&new_cr50_rec_switch(&tpm->base.ops)->ops);
 }
 
 static void audio_setup(void)
