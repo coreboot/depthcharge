1e7329d2c218ad70f8b5ed4ed3f7ab45ea830355
Kapil Porwal <kapilporwal@google.com>
2022-07-26 09:25:40 +0000

    drivers/soc: Add macros for I2C controllers
    Add macros for 5 I2C controllers.
    
    BUG=b:224325352
    TEST=Able to build depthcharge without errors.
    
    Signed-off-by: Kapil Porwal <kapilporwal@google.com>
    Change-Id: Ic9c2e77165acb4f6673915daa899a52f4705e63d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3784574
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>

--- src/drivers/soc/meteorlake.h
+++ src/drivers/soc/meteorlake.h
@@ -8,7 +8,16 @@
 #include "drivers/soc/common/intel_gpe.h"
 #include "drivers/soc/intel_common.h"
 
+/* I2C Designware Controller runs at 133MHz */
+#define METEORLAKE_DW_I2C_MHZ	133
+
+#define PCI_DEV_I2C0	PCI_DEV(0, 0x15, 0)
+#define PCI_DEV_I2C1	PCI_DEV(0, 0x15, 1)
+#define PCI_DEV_I2C2	PCI_DEV(0, 0x15, 2)
+#define PCI_DEV_I2C3	PCI_DEV(0, 0x15, 3)
 #define PCI_DEV_SATA	PCI_DEV(0, 0x17, 0)
+#define PCI_DEV_I2C4	PCI_DEV(0, 0x19, 0)
+#define PCI_DEV_I2C5	PCI_DEV(0, 0x19, 1)
 #define PCI_DEV_PCIE1	PCI_DEV(0, 0x1c, 0)
 #define PCI_DEV_PCIE3	PCI_DEV(0, 0x1c, 2)
 #define PCI_DEV_PCIE5	PCI_DEV(0, 0x1c, 4)
