15370838da3ca39f31db863bc12a4ab7a7a25283
Raymond Chung <raymondchung@ami.corp-partner.google.com>
2022-07-27 12:49:45 +0800

    Create gaelin variant
    Create a depthcharge target gaelin by copying the template files from
    the reference brask to the appropriate places in the source tree.
    
    (Auto-Generated by create_depthcharge_variant.sh version 1.0.0).
    
    BUG=b:239514438
    BRANCH=None
    TEST=FW_NAME=gaelin emerge-brya depthcharge
    
    Signed-off-by: Raymond Chung <raymondchung@ami.corp-partner.google.com>
    Change-Id: I74122094944318c64dc507645409526fe714d250
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3780064
    Reviewed-by: Zhuohao Lee <zhuohao@chromium.org>

---
+++ src/board/brya/gaelin.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+#include <pci.h>
+#include <pci/pci.h>
+
+#include "base/fw_config.h"
+#include "board/brya/include/variant.h"
+#include "drivers/gpio/alderlake.h"
+
+const struct audio_config *variant_probe_audio_config(void)
+{
+	static struct audio_config config;
+	return &config;
+}
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static const struct tpm_config config = {
+		.pci_dev = PCI_DEV(0, 0x15, 1),
+	};
+
+	return &config;
+}
