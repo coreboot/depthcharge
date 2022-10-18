d05493323002e34a59ad8964227ec1701c48d82a
Hsuan Ting Chen <roccochen@chromium.org>
2022-10-18 13:19:22 +0800

    vboot/ui: Add CrOS diagnostics log event to elog
    Add an exit hook, diagnostics_exit, for diagnostics menu screen.
    
    This hook will:
    * Collect the diagnostics report logs from diag/report service
    * Add the logs to the event log
    
    This hook will run exactly once while exiting the whole diagnostics UI.
    
    Also rename the existing diagnostics item exit hook from
    diagnostics_exit to diagnostics_test_exit to prevent name shadowing.
    
    BUG=b:226554532
    BRANCH=corsola
    TEST=Build locally, launch MiniDiag, and dump with elogtool
    
    Signed-off-by: Hsuan Ting Chen <roccochen@chromium.org>
    Change-Id: I3dac6498d2306fcc047891d76c8d71cac0814034
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3917951
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Commit-Queue: Yu-Ping Wu <yupingso@chromium.org>
    (cherry picked from commit 85f76f13f623d3e4e3a01224a381abf755f1c1bb)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3989706
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Tested-by: Yu-Ping Wu <yupingso@chromium.org>
    Commit-Queue: Hung-Te Lin <hungte@chromium.org>
    Auto-Submit: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-by: Yidi Lin <yidilin@chromium.org>

---
+++ tests/stubs/base/elog.c
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include "base/elog.h"
+
+elog_error_t elog_add_event_raw(uint8_t event_type, void *data,
+				uint8_t data_size)
+{
+	return ELOG_SUCCESS;
+}
--- tests/vboot/ui-diagnostic-test.c
+++ tests/vboot/ui-diagnostic-test.c
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 
+#include <base/elog.h>
 #include <diag/common.h>
 #include <drivers/storage/blockdev.h>
 #include <tests/test.h>
@@ -17,6 +18,34 @@ int ui_is_lid_open(void)
 	return mock();
 }
 
+/* Helper functions */
+
+char expected_event_data[ELOG_MAX_EVENT_DATA_SIZE];
+const char mock_report_data[] = "MOCK_DIAG_REPORT_DUMP";
+
+/* Event size = report size + subtype */
+#define EXPECTED_RAW_EVENT_SIZE (sizeof(mock_report_data) + 1)
+
+#define EXPECT_DIAG_REPORT(_type, _result) do {				       \
+	expect_value(diag_report_start_test, type, (_type));		       \
+	expect_value(diag_report_end_test, result, (_result));		       \
+} while (0)
+
+#define WILL_DUMP_REPORT(_data) do {					       \
+	will_return(diag_report_dump, (_data));				       \
+	will_return(diag_report_dump, sizeof(_data));			       \
+} while (0)
+
+#define EXPECT_DIAG_DUMP_AND_ELOG() do {				       \
+	expect_value(diag_report_dump, size, ELOG_MAX_EVENT_DATA_SIZE - 1);    \
+	expect_value(elog_add_event_raw, event_type,			       \
+		     ELOG_TYPE_CROS_DIAGNOSTICS);			       \
+	expect_value(elog_add_event_raw, data_size,			       \
+		     EXPECTED_RAW_EVENT_SIZE);				       \
+	expect_memory(elog_add_event_raw, data, expected_event_data,	       \
+		      EXPECTED_RAW_EVENT_SIZE);				       \
+} while (0)
+
 /* Tests */
 struct ui_context test_ui_ctx;
 struct vb2_kernel_params test_kparams;
@@ -32,6 +61,16 @@ static int setup_context(void **state)
 	memset(&test_kparams, 0, sizeof(test_kparams));
 	test_ui_ctx.kparams = &test_kparams;
 	*state = &test_ui_ctx;
+
+	diag_report_clear();
+
+	/* Prepare the expected raw value of event log */
+	if (EXPECTED_RAW_EVENT_SIZE > ELOG_MAX_EVENT_DATA_SIZE)
+		fail_msg("The mock_report_data exceeds maximum buffer size");
+	memset(expected_event_data, 0, sizeof(expected_event_data));
+	expected_event_data[0] = ELOG_CROS_DIAGNOSTICS_LOGS;
+	memcpy(expected_event_data + 1, mock_report_data,
+	       sizeof(mock_report_data));
 
 	return 0;
 }
@@ -49,6 +88,8 @@ static void test_diagnostics_screen_disabled_and_hidden(void **state)
 			   BLOCKDEV_TEST_OPS_TYPE_SHORT |
 			   BLOCKDEV_TEST_OPS_TYPE_EXTENDED);
 	will_return_maybe(memory_test_init, DIAG_TEST_PASSED);
+	WILL_DUMP_REPORT(mock_report_data);
+	EXPECT_DIAG_DUMP_AND_ELOG();
 
 	assert_int_equal(vboot_select_and_load_kernel(ui->ctx, ui->kparams),
 			 VB2_REQUEST_SHUTDOWN);
@@ -73,6 +114,8 @@ static void test_diagnostics_screen(void **state)
 	EXPECT_UI_DISPLAY(UI_SCREEN_DIAGNOSTICS, MOCK_IGNORE, 1);
 	EXPECT_UI_DISPLAY(UI_SCREEN_DIAGNOSTICS_STORAGE_HEALTH);
 	EXPECT_UI_DISPLAY_ANY();
+	EXPECT_DIAG_REPORT(ELOG_CROS_DIAG_TYPE_STORAGE_HEALTH,
+			   ELOG_CROS_DIAG_RESULT_PASSED);
 	/* #2: Short storage self-test screen */
 	WILL_PRESS_KEY(UI_KEY_DOWN, 0);
 	WILL_PRESS_KEY(UI_KEY_ENTER, 0);
@@ -80,6 +123,8 @@ static void test_diagnostics_screen(void **state)
 	EXPECT_UI_DISPLAY(UI_SCREEN_DIAGNOSTICS, MOCK_IGNORE, 2);
 	EXPECT_UI_DISPLAY(UI_SCREEN_DIAGNOSTICS_STORAGE_TEST_SHORT);
 	EXPECT_UI_DISPLAY_ANY();
+	EXPECT_DIAG_REPORT(ELOG_CROS_DIAG_TYPE_STORAGE_TEST_SHORT,
+			   ELOG_CROS_DIAG_RESULT_PASSED);
 	/* #3: Extended storage self-test screen */
 	WILL_PRESS_KEY(UI_KEY_DOWN, 0);
 	WILL_PRESS_KEY(UI_KEY_ENTER, 0);
@@ -87,6 +132,8 @@ static void test_diagnostics_screen(void **state)
 	EXPECT_UI_DISPLAY(UI_SCREEN_DIAGNOSTICS, MOCK_IGNORE, 3);
 	EXPECT_UI_DISPLAY(UI_SCREEN_DIAGNOSTICS_STORAGE_TEST_EXTENDED);
 	EXPECT_UI_DISPLAY_ANY();
+	EXPECT_DIAG_REPORT(ELOG_CROS_DIAG_TYPE_STORAGE_TEST_EXTENDED,
+			   ELOG_CROS_DIAG_RESULT_PASSED);
 	/* #4: Quick memory test screen */
 	WILL_PRESS_KEY(UI_KEY_DOWN, 0);
 	WILL_PRESS_KEY(UI_KEY_ENTER, 0);
@@ -94,6 +141,8 @@ static void test_diagnostics_screen(void **state)
 	EXPECT_UI_DISPLAY(UI_SCREEN_DIAGNOSTICS, MOCK_IGNORE, 4);
 	EXPECT_UI_DISPLAY(UI_SCREEN_DIAGNOSTICS_MEMORY_QUICK);
 	EXPECT_UI_DISPLAY_ANY();
+	EXPECT_DIAG_REPORT(ELOG_CROS_DIAG_TYPE_MEMORY_QUICK,
+			   ELOG_CROS_DIAG_RESULT_PASSED);
 	/* #5: Full memory test screen */
 	WILL_PRESS_KEY(UI_KEY_DOWN, 0);
 	WILL_PRESS_KEY(UI_KEY_ENTER, 0);
@@ -101,6 +150,8 @@ static void test_diagnostics_screen(void **state)
 	EXPECT_UI_DISPLAY(UI_SCREEN_DIAGNOSTICS, MOCK_IGNORE, 5);
 	EXPECT_UI_DISPLAY(UI_SCREEN_DIAGNOSTICS_MEMORY_FULL);
 	EXPECT_UI_DISPLAY_ANY();
+	EXPECT_DIAG_REPORT(ELOG_CROS_DIAG_TYPE_MEMORY_FULL,
+			   ELOG_CROS_DIAG_RESULT_PASSED);
 	/* #6: Power off (End of menu) */
 	WILL_PRESS_KEY(UI_KEY_DOWN, 0);
 	WILL_PRESS_KEY(UI_KEY_ENTER, 0);
@@ -118,6 +169,9 @@ static void test_diagnostics_screen(void **state)
 	will_return_always(memory_test_init, DIAG_TEST_PASSED);
 	will_return_always(memory_test_run, DIAG_TEST_PASSED);
 
+	WILL_DUMP_REPORT(mock_report_data);
+	EXPECT_DIAG_DUMP_AND_ELOG();
+
 	assert_int_equal(vboot_select_and_load_kernel(ui->ctx, ui->kparams),
 			 VB2_REQUEST_SHUTDOWN);
 }
@@ -132,6 +186,8 @@ static void test_diagnostics_screen_no_storage_self_test(void **state)
 	will_return_maybe(vb2api_gbb_get_flags, 0);
 	EXPECT_UI_DISPLAY(UI_SCREEN_DIAGNOSTICS, MOCK_IGNORE, MOCK_IGNORE,
 			  0xc, 0x0); /* 0xc = 0b1100 */
+	WILL_DUMP_REPORT(mock_report_data);
+	EXPECT_DIAG_DUMP_AND_ELOG();
 
 	assert_int_equal(vboot_select_and_load_kernel(ui->ctx, ui->kparams),
 			 VB2_REQUEST_SHUTDOWN);
@@ -148,6 +204,8 @@ static void test_diagnostics_screen_no_storage_extended_test(void **state)
 	will_return_maybe(vb2api_gbb_get_flags, 0);
 	EXPECT_UI_DISPLAY(UI_SCREEN_DIAGNOSTICS, MOCK_IGNORE, MOCK_IGNORE,
 			  0x8, 0x0); /* 0x4 = 0b1000 */
+	WILL_DUMP_REPORT(mock_report_data);
+	EXPECT_DIAG_DUMP_AND_ELOG();
 
 	assert_int_equal(vboot_select_and_load_kernel(ui->ctx, ui->kparams),
 			 VB2_REQUEST_SHUTDOWN);
