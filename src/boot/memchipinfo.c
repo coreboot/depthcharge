3628ca1bd88070c2bfa8bb4b4584c207b53a642a
Julius Werner <jwerner@chromium.org>
2022-10-25 17:14:26 -0700

    mem_chip_info: Update to new format
    This patch updates the translation from coreboot's mem_chip_info to the
    kernel device tree according to the updated format in CB:68871 and the
    new DT schema from [1].
    
    [1]: https://lore.kernel.org/lkml/20220930220606.303395-4-jwerner@chromium.org/
    
    BRANCH=none
    BUG=b:177917361
    TEST=none
    
    Signed-off-by: Julius Werner <jwerner@chromium.org>
    Change-Id: I4d954a7a55dbee2e19dd39e65b00ae0dda1e3f89
    Cq-Depend: chromium:4092068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/3980175
    Reviewed-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    (cherry picked from commit c8fc3b19a3312af3172e535a05464673ed6c242c)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4090947
    Auto-Submit: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-by: Chen-Tsung Hsieh <chentsung@chromium.org>
    Commit-Queue: Chen-Tsung Hsieh <chentsung@chromium.org>
    Reviewed-by: Yidi Lin <yidilin@chromium.org>
    Tested-by: Knox Chiou <knoxchiou@chromium.org>

--- src/boot/memchipinfo.c
+++ src/boot/memchipinfo.c
@@ -5,36 +5,101 @@
 #include "base/device_tree.h"
 #include "base/list.h"
 
+static int lpddr_type_num(enum mem_chip_type type)
+{
+	switch (type) {
+	case MEM_CHIP_LPDDR3:
+		return 3;
+	case MEM_CHIP_LPDDR4:
+	case MEM_CHIP_LPDDR4X:
+		return 4;
+	default:
+		return 0;
+	}
+}
+
+static DeviceTreeNode *new_channel(DeviceTree *tree,
+				   struct mem_chip_entry *entry)
+{
+	char name[16];
+	snprintf(name, sizeof(name), "lpddr-channel%d", entry->channel);
+
+	const char *path[] = { name, NULL };
+	DeviceTreeNode *channel = dt_find_node(tree->root, path, NULL, NULL, 1);
+
+	char *compat = xzalloc(24);
+	snprintf(compat, 24, "jedec,lpddr%d-channel",
+		 lpddr_type_num(entry->type));
+	dt_add_string_prop(channel, "compatible", compat);
+	dt_add_u32_prop(channel, "io-width", entry->channel_io_width);
+	dt_add_u32_prop(channel, "#address-cells", 1);
+	dt_add_u32_prop(channel, "#size-cells", 0);
+
+	return channel;
+}
+
+static void new_rank(DeviceTreeNode *channel, struct mem_chip_entry *entry)
+{
+	char name[8];
+	snprintf(name, sizeof(name), "rank@%d", entry->rank);
+
+	const char *path[] = { name, NULL };
+	DeviceTreeNode *rank = dt_find_node(channel, path, NULL, NULL, 1);
+
+	int lpddrnum = lpddr_type_num(entry->type);
+	printf("LPDDR%d chan%d(x%d) rank%d: density %umbits x%d, MF %02x rev %02x%02x\n",
+		lpddrnum, entry->channel, entry->channel_io_width, entry->rank,
+		entry->density_mbits, entry->io_width, entry->manufacturer_id,
+		entry->revision_id[0], entry->revision_id[1]);
+
+	char *compat = xzalloc(32);
+	snprintf(compat, 32, "lpddr%d-%02x,%02x%02x$jedec,lpddr%d", lpddrnum,
+		 entry->manufacturer_id, entry->revision_id[0],
+		 entry->revision_id[1], lpddrnum);
+	dt_add_string_prop(rank, "compatible", compat);
+	/* We manipulate the string *after* adding it to the DT here, which
+	   works because dt_add_string_prop() hooks the raw pointer into the
+	   tree instead of making a deep copy. */
+	char *dollar = strchr(compat, '$');
+	*dollar = '\0';
+
+	dt_add_u32_prop(rank, "reg", entry->rank);
+	dt_add_u32_prop(rank, "density", entry->density_mbits);
+	dt_add_u32_prop(rank, "io-width", entry->io_width);
+
+	uint8_t *revision = xzalloc(8);
+	revision[3] = entry->revision_id[0];
+	revision[7] = entry->revision_id[1];
+	dt_add_bin_prop(rank, "revision-id", revision, 8);
+}
+
 static int install_memchipinfo_data(DeviceTreeFixup *fixup, DeviceTree *tree)
 {
-	struct mem_chip_info *mem_chip_ptr =
-				(struct mem_chip_info *)lib_sysinfo.mem_chip_base;
-	uint8_t *revision = xzalloc(8);
-	uint64_t temp_density = 0;
-	int i;
+	struct mem_chip_info *info = (void *)lib_sysinfo.mem_chip_base;
+	DeviceTreeNode *channel[8] = {0};
 
-	const char *memchip_path[] = { "lpddr3", NULL };
-	DeviceTreeNode *memchip_node = dt_find_node(
-		tree->root, memchip_path, NULL, NULL, 1);
+	if (info->struct_version != MEM_CHIP_STRUCT_VERSION)
+		return 1;
 
-	/* Add a compatible property. */
-	dt_add_string_prop(memchip_node, "compatible", "jedec,lpddr3");
+	for (int i = 0; i < info->num_entries; i++) {
+		if (!lpddr_type_num(info->entries[i].type)) {
+			printf("ERROR:%s: Unknown memory type %d\n",
+			       __func__, info->entries[i].type);
+			continue;
+		}
 
-	/* Add properties. */
-	for (i=0; i<mem_chip_ptr->num_channels; i++)
-		temp_density += mem_chip_ptr->channel[i].density;
+		int c = info->entries[i].channel;
+		if (c >= ARRAY_SIZE(channel)) {
+			printf("ERROR:%s: Too high channel number %d\n",
+			       __func__, c);
+			continue;
+		}
 
-	/* convert density from Mega Bytes to Mega Bits */
-	temp_density *= 8;
-	dt_add_u32_prop(memchip_node, "density", temp_density);
-	dt_add_u32_prop(memchip_node, "io_width",
-			mem_chip_ptr->channel[0].io_width);
-	dt_add_u32_prop(memchip_node, "manufacturer_id",
-			mem_chip_ptr->channel[0].manufacturer_id);
+		if (!channel[c])
+			channel[c] = new_channel(tree, &info->entries[i]);
 
-	revision[3] = mem_chip_ptr->channel[0].revision_id[0];
-	revision[7] = mem_chip_ptr->channel[0].revision_id[1];
-	dt_add_bin_prop(memchip_node, "revision_id", revision, 8);
+		new_rank(channel[c], &info->entries[i]);
+	}
 
 	return 0;
 }
---
+++ tests/boot/memchipinfo.c
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+
+#include "helpers/device_tree.h"
+#include "tests/test.h"
+
+#include "boot/memchipinfo.c"
+
+static DeviceTree tree;
+static DeviceTreeNode root;
+static DeviceTreeFixup fixup;
+
+static int setup(void **state)
+{
+	memset(&tree, 0, sizeof(tree));
+	memset(&root, 0, sizeof(root));
+	memset(&fixup, 0, sizeof(root));
+	tree.root = &root;
+	return 0;
+}
+
+static struct {
+	struct mem_chip_info info;
+	struct mem_chip_entry entries[3];
+} test_info = {
+	.info = {
+		.num_entries = 3,
+		.struct_version = MEM_CHIP_STRUCT_VERSION,
+	},
+	.entries = {
+		{
+			.channel = 0,
+			.rank = 0,
+			.type = MEM_CHIP_LPDDR3,
+			.channel_io_width = 32,
+			.density_mbits = 2048,
+			.io_width = 16,
+			.manufacturer_id = 0x12,
+			.revision_id = { 0x34, 0x56 },
+		},
+		{
+			.channel = 0,
+			.rank = 1,
+			.type = MEM_CHIP_LPDDR3,
+			.channel_io_width = 32,
+			.density_mbits = 1024,
+			.io_width = 16,
+			.manufacturer_id = 0x12,
+			.revision_id = { 0x34, 0x56 },
+		},
+		{
+			.channel = 1,
+			.rank = 0,
+			.type = MEM_CHIP_LPDDR4,
+			.channel_io_width = 16,
+			.density_mbits = 4096,
+			.io_width = 16,
+			.manufacturer_id = 0x78,
+			.revision_id = { 0x9a, 0xbc },
+		},
+	},
+};
+struct sysinfo_t lib_sysinfo = { .mem_chip_base = (uintptr_t)&test_info };
+
+static void test_memchipinfo(void **state)
+{
+	u32 adc, szc;
+
+	assert_int_equal(install_memchipinfo_data(&fixup, &tree), 0);
+
+	adc = szc = 0xff;
+	DeviceTreeNode *c0 = dt_find_node_by_path(
+		&tree, "/lpddr-channel0", &adc, &szc, 0);
+	assert_non_null(c0);
+	assert_int_equal(adc, 1);
+	assert_int_equal(szc, 0);
+	assert_dt_compat_strings(c0, "jedec,lpddr3-channel");
+	assert_dt_u32_prop(c0, "io-width", 32);
+
+	adc = szc = 0xff;
+	DeviceTreeNode *r0 = dt_find_node_by_path(
+		&tree, "/lpddr-channel0/rank@0", &adc, &szc, 0);
+	assert_non_null(r0);
+	assert_int_equal(adc, 1);
+	assert_int_equal(szc, 0);
+	assert_dt_compat_strings(r0, "lpddr3-12,3456", "jedec,lpddr3");
+	assert_dt_u32_prop(r0, "reg", 0);
+	assert_dt_u32_prop(r0, "io-width", 16);
+	assert_dt_u32_prop(r0, "density", 2048);
+
+	adc = szc = 0xff;
+	DeviceTreeNode *r1 = dt_find_node_by_path(
+		&tree, "/lpddr-channel0/rank@1", &adc, &szc, 0);
+	assert_non_null(r1);
+	assert_int_equal(adc, 1);
+	assert_int_equal(szc, 0);
+	assert_dt_compat_strings(r1, "lpddr3-12,3456", "jedec,lpddr3");
+	assert_dt_u32_prop(r1, "reg", 1);
+	assert_dt_u32_prop(r1, "io-width", 16);
+	assert_dt_u32_prop(r1, "density", 1024);
+
+	adc = szc = 0xff;
+	DeviceTreeNode *c1 = dt_find_node_by_path(
+		&tree, "/lpddr-channel1", &adc, &szc, 0);
+	assert_non_null(c1);
+	assert_int_equal(adc, 1);
+	assert_int_equal(szc, 0);
+	assert_dt_compat_strings(c1, "jedec,lpddr4-channel");
+	assert_dt_u32_prop(c1, "io-width", 16);
+
+	adc = szc = 0xff;
+	r0 = dt_find_node_by_path(
+		&tree, "/lpddr-channel1/rank@0", &adc, &szc, 0);
+	assert_non_null(r0);
+	assert_int_equal(adc, 1);
+	assert_int_equal(szc, 0);
+	assert_dt_compat_strings(r0, "lpddr4-78,9abc", "jedec,lpddr4");
+	assert_dt_u32_prop(r0, "reg", 0);
+	assert_dt_u32_prop(r0, "io-width", 16);
+	assert_dt_u32_prop(r0, "density", 4096);
+}
+
+#define TEST(test_function_name) \
+	cmocka_unit_test_setup(test_function_name, setup)
+
+int main(void)
+{
+	const struct CMUnitTest tests[] = {
+		TEST(test_memchipinfo),
+	};
+
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}
