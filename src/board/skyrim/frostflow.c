ada805105651716a65dce37a3f6a2cc799a26b45
Frank Wu <frank_wu@compal.corp-partner.google.com>
2022-11-07 16:41:49 +0800

    frostflow: update storage setting
    Updated storage setting and frostflow can boot from SD card.
    
    BUG=b:257888454
    BRANCH=None
    TEST=emerge-skyrim depthcharge and frostflow can boot from SD card
    
    Signed-off-by: Frank Wu <frank_wu@compal.corp-partner.google.com>
    Change-Id: I038fce943feb4032472deef7068670894cfccd14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4005415
    Reviewed-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Reviewed-by: Tim Van Patten <timvp@google.com>

--- src/board/skyrim/frostflow.c
+++ src/board/skyrim/frostflow.c
@@ -6,20 +6,6 @@
 
 #include "base/fw_config.h"
 #include "board/skyrim/include/variant.h"
-#include "drivers/storage/storage_common.h"
-
-const struct storage_config *variant_get_storage_configs(size_t *count)
-{
-	static const struct storage_config storage_configs[] = {
-		/* Coreboot devicetree.cb: gpp_bridge_2 = 02.3 */
-		{ .media = STORAGE_NVME, .pci_dev = PCI_DEV(0, 0x02, 0x3)},
-		/* Coreboot devicetree.cb: gpp_bridge_1 = 02.2 */
-		{ .media = STORAGE_SDHCI, .pci_dev = PCI_DEV(0, 0x02, 0x02)},
-	};
-
-	*count = ARRAY_SIZE(storage_configs);
-	return storage_configs;
-}
 
 enum audio_amp variant_get_audio_amp(void)
 {
