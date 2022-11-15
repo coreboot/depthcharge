d04366d173ea00aee84d45d6dcf8543ed4db0dd7
Hsuan Ting Chen <roccochen@chromium.org>
2022-11-15 12:16:27 +0800

    Revert "diag: Reduce the frequency of dumping storage log"
    This reverts commit 547b8a7a76c8fb4ca15e4f5690a0bd1c00dd51fb.
    
    BUG=b:252785850
    BRANCH=brya
    TEST=none
    
    Signed-off-by: Hsuan Ting Chen <roccochen@chromium.org>
    Change-Id: I680269db5d272e47792a5c482004428a3f6405e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4027587
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>
    Reviewed-by: Nick Vaccaro <nvaccaro@google.com>
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    Tested-by: Subrata Banik <subratabanik@chromium.org>

--- src/diag/storage_test.h
+++ src/diag/storage_test.h
@@ -11,35 +11,7 @@
 #include "drivers/storage/blockdev.h"
 #include "drivers/storage/info.h"
 
-/* Prevent dumping storage self-test log too frequently */
-#define DIAG_STORAGE_TEST_SHORT_DELAY_MS 200
-#define DIAG_STORAGE_TEST_EXTENDED_DELAY_MS 3000
-#define DIAG_STORAGE_TEST_DEFAULT_DELAY_MS 1000
-
-/*
- * Dump storage self-test log into the buffer and truncate the text which
- * exceeds "end".
- *
- * The storage test log is retrieved from the block device driver. When this
- * happens too frequently, the block device might return unexpected result.
- * Therefore, this function uses a stopwatch to record the timestamp. If the
- * current call is too close to the previous one, DIAG_TEST_RUNNING will be
- * returned without retrieving the log.
- *
- * @param buf		The buffer to store the self-test log.
- * @param end		The pointer to the maximum limit of the buffer.
- *
- * @return VB2_SUCCESS on success, or other vb2_error_t on failure.
- */
 vb2_error_t diag_dump_storage_test_log(char *buf, const char *end);
-
-/*
- * Start or stop a storage self-test.
- *
- * @param ops		The self-test type.
- *
- * @return VB2_SUCCESS on success, or other vb2_error_t on failure.
- */
 vb2_error_t diag_storage_test_control(enum BlockDevTestOpsType ops);
 
 #endif
