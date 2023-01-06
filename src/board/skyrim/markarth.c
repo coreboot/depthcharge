d844799e83515b43cdd2b55a3841469f59b51ad2
John Su <john_su@compal.corp-partner.google.com>
2023-01-07 01:49:43 +0800

    markarth: update storage and audio beep setting
    Support eMMC via PCIe to eMMC bridge: Bayhub BH799BB
    Support audio beep: RT1019
    
    BUG=b:263216451
    BRANCH=None
    TEST=emerge-skyrim depthcharge
    
    Change-Id: I065f21f1458f6b7f6acd8d136feecf37556c183c
    Signed-off-by: John Su <john_su@compal.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4141184
    Reviewed-by: Frank Wu <frank_wu@compal.corp-partner.google.com>
    Reviewed-by: Chao Gui <chaogui@google.com>
    Reviewed-by: Tim Van Patten <timvp@google.com>

--- src/board/skyrim/markarth.c
+++ src/board/skyrim/markarth.c
@@ -6,3 +6,22 @@
 
 #include "base/fw_config.h"
 #include "board/skyrim/include/variant.h"
+#include "drivers/storage/storage_common.h"
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	static const struct storage_config storage_configs[] = {
+		/* Coreboot devicetree.cb: gpp_bridge_2 = 02.3 */
+		{ .media = STORAGE_NVME, .pci_dev = PCI_DEV(0, 0x02, 0x3)},
+		/* Coreboot devicetree.cb: gpp_bridge_1 = 02.2 */
+		{ .media = STORAGE_NVME, .pci_dev = PCI_DEV(0, 0x02, 0x02)},
+	};
+
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
+enum audio_amp variant_get_audio_amp(void)
+{
+	return AUDIO_AMP_RT1019;
+}
