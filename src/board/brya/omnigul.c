943687a1cfcf0ba717e979b313cb100d2db7fd88
jamie_chen <jamie_chen@compal.corp-partner.google.com>
2023-01-17 13:43:20 +0800

    omnigul: Add NVMe and UFS storage support
    Add NVMe and UFS storage support.
    
    BUG=b:263223008
    BRANCH=None
    TEST=FW_NAME=omnigul emerge-brya depthcharge
    Change-Id: I3523ad9cfd6cb0844c72f8a39e1e9ee0a824dd45
    Signed-off-by: jamie_chen <jamie_chen@compal.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4167116
    Reviewed-by: Kyle Lin <kylelinck@google.com>
    (cherry picked from commit 7e1e4037a9e46a9cbf502b2b20cdc9db1a84cf94)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4211988
    Commit-Queue: Kyle Lin <kylelinck@google.com>

--- src/board/brya/omnigul.c
+++ src/board/brya/omnigul.c
@@ -7,9 +7,30 @@
 #include "base/fw_config.h"
 #include "board/brya/include/variant.h"
 #include "drivers/gpio/alderlake.h"
+#include "drivers/soc/alderlake.h"
+#include "drivers/storage/storage_common.h"
 
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
+	return &config;
+}
+
+static const struct storage_config storage_configs[] = {
+	{ .media = STORAGE_NVME, .pci_dev = PCH_DEV_PCIE10 },
+	{ .media = STORAGE_UFS, .pci_dev = PCH_DEV_UFS1 },
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static const struct tpm_config config = {
+		.pci_dev = PCI_DEV(0, 0x15, 1),
+	};
 	return &config;
 }
