a45545fa4983fc6f7f143907bbff27138cc97e0a
Yidi Lin <yidilin@chromium.org>
2023-02-01 21:36:34 +0800

    drivers/bus/i2c: support MT8188 I2C
    Add Kconfig option DRIVER_BUS_I2C_MT8188 and the register definition for
    MT8188.
    
    BUG=b:267272136
    TEST=build pass
    BRANCH=none
    
    Change-Id: Ibc5558449a5f364108c6c79ed6528c89c975795d
    Signed-off-by: Yidi Lin <yidilin@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4210878
    Commit-Queue: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-by: Rex-BC Chen <rex-bc.chen@mediatek.com>

--- src/drivers/bus/i2c/mtk_i2c.h
+++ src/drivers/bus/i2c/mtk_i2c.h
@@ -65,6 +65,8 @@ check_member(mtk_i2c_dma_regs, dma_tx_len, 0x24);
 #include "drivers/bus/i2c/mt8173.h"
 #elif CONFIG(DRIVER_BUS_I2C_MT8183)
 #include "drivers/bus/i2c/mt8183.h"
+#elif CONFIG(DRIVER_BUS_I2C_MT8188)
+#include "drivers/bus/i2c/mt8188.h"
 #else
 #error "Unsupported I2C for MediaTek"
 #endif
