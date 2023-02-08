e621eaeb058f47c7a0aa414cfa29584ab4b482cb
Caveh Jalali <caveh@chromium.org>
2023-02-07 16:59:15 -0800

    drivers/ec/cros: Add commands_api.h
    This adds a header file to encapsulate the EC API header file (exported
    by the EC) to define the EC API for depthcharge.
    
    BRANCH=none
    BUG=b:258110734
    TEST='emerge-brya depthcharge' passes
    
    Cq-Depend: chromium:5095278
    Change-Id: Ieb1cf86afdd22a75c993d7d39e9ee83466a4afa5
    Signed-off-by: Caveh Jalali <caveh@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4230134
    Reviewed-by: Daisuke Nojiri <dnojiri@chromium.org>
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5092001
    Tested-by: Yang Wu <wuyang5@huaqin.corp-partner.google.com>
    Commit-Queue: Knox Chiou <knoxchiou@chromium.org>

---
+++ src/drivers/ec/cros/commands_api.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __CROS_EC_COMMANDS_API_H
+#define __CROS_EC_COMMANDS_API_H
+
+#include "drivers/ec/cros/ec.h"
+
+#define CROS_EC_COMMAND_INFO CrosEc
+#define CROS_EC_COMMAND ec_command
+
+#include <ec_cmd_api.h>
+
+#endif /* __CROS_EC_COMMANDS_API_H */
