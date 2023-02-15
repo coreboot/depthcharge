3aeffc4a3fc045363533ed5e40a11a32b7337f2a
Eric Lai <eric_lai@quanta.corp-partner.google.com>
2023-02-15 15:27:22 +0800

    brya: add hades board
    Copied from agah.
    
    BUG=b:269387218
    BRANCH=None
    TEST=NA
    
    Change-Id: I8faed32aa63b933df2e9d231d1dc84cf1a2765e9
    Signed-off-by: Eric Lai <eric_lai@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4253309
    Reviewed-by: Tarun Tuli <taruntuli@google.com>
    Reviewed-by: YH Lin <yueherngl@chromium.org>

---
+++ src/board/brya/hades.c
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+#include <pci.h>
+#include <pci/pci.h>
+
+#include "base/fw_config.h"
+#include "board/brya/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
+#include "drivers/gpio/alderlake.h"
+#include "drivers/soc/alderlake.h"
+#include "drivers/storage/storage_common.h"
+
+#define SDMODE_PIN	GPP_A11
+#define SDMODE_ENABLE	0
+
+const struct audio_config *variant_probe_audio_config(void)
+{
+	static struct audio_config config = {
+		.bus = {
+			.type = AUDIO_I2S,
+			.i2s.address = SSP_I2S1_START_ADDRESS,
+			.i2s.enable_gpio = {
+				.pad = SDMODE_PIN,
+				.active_low = SDMODE_ENABLE
+			},
+			.i2s.settings = &max98357a_settings,
+		},
+		.amp = {
+			.type = AUDIO_GPIO_AMP,
+			.gpio.enable_gpio = SDMODE_PIN,
+		},
+		.codec = {
+			.type = AUDIO_MAX98357,
+		},
+	};
+
+	return &config;
+}
+static const struct storage_config storage_configs[] = {
+	{ .media = STORAGE_NVME, .pci_dev = PCH_DEV_PCIE8 },
+	{ .media = STORAGE_SDHCI, .pci_dev = PCH_DEV_PCIE7 },
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
