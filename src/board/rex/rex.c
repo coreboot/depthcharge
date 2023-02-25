fe99d2e6aa0042dad3a96586cd69bdff7aae81be
Kapil Porwal <kapilporwal@google.com>
2023-02-26 01:27:21 +0530

    board/rex: Enable SoundWire codecs
    BUG=b:269497731
    TEST=Verified boot beep with SoundWire DB on google/rex
    
    Change-Id: I832ab68bd55884869d023500eae8d4c8bd2c52c9
    Signed-off-by: Kapil Porwal <kapilporwal@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4289029
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    Commit-Queue: Kapil Porwal <kapilporwal@chromium.org>
    Tested-by: Kapil Porwal <kapilporwal@chromium.org>

--- src/board/rex/rex.c
+++ src/board/rex/rex.c
@@ -4,17 +4,19 @@
 #include <pci.h>
 #include <pci/pci.h>
 
+#include "base/fw_config.h"
 #include "board/rex/include/variant.h"
 #include "drivers/bus/i2s/cavs-regs.h"
 #include "drivers/bus/i2s/intel_common/max98357a.h"
+#include "drivers/bus/soundwire/cavs_2_5-sndwregs.h"
 #include "drivers/gpio/meteorlake.h"
 #include "drivers/soc/meteorlake.h"
 
 #define SDMODE_PIN GPP_D4
 
-const struct audio_config *variant_probe_audio_config(void)
+static void initialize_i2s_config(struct audio_config *config)
 {
-	static struct audio_config config = {
+	*config = (struct audio_config) {
 		.bus = {
 			.type			= AUDIO_I2S,
 			.i2s.address		= SSP_I2S0_START_ADDRESS,
@@ -29,6 +31,31 @@ const struct audio_config *variant_probe_audio_config(void)
 			.type			= AUDIO_MAX98357,
 		},
 	};
+}
+
+static void initialize_soundwire_config(struct audio_config *config)
+{
+	*config = (struct audio_config) {
+		.bus = {
+			.type			= AUDIO_SNDW,
+			.sndw.link		= AUDIO_SNDW_LINK2,
+		},
+		.codec = {
+			.type			= AUDIO_MAX98363,
+		},
+	};
+}
+
+const struct audio_config *variant_probe_audio_config(void)
+{
+	static struct audio_config config;
+
+	memset(&config, 0, sizeof(config));
+
+	if (fw_config_probe(FW_CONFIG(AUDIO, MAX98360_ALC5682I_I2S)))
+		initialize_i2s_config (&config);
+	else if (fw_config_probe(FW_CONFIG(AUDIO, MAX98363_CS42L42_SNDW)))
+		initialize_soundwire_config (&config);
 
 	return &config;
 }
