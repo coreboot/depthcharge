d72bc73f596e3ccca97b6351d9a6b74e5a7b3dc6
Dtrain Hsu <dtrain_hsu@compal.corp-partner.google.com>
2023-03-09 15:11:38 +0800

    omnigul: Enable beep function
    Enable beep function in depthcharge.
    
    BUG=b:272408547
    BRANCH=firmware-brya-14505.B
    TEST=Test on Omnigul and got beep sound in dev mode.
    
    Change-Id: Ic93074834737c71e57c076b72b0511b690637c5d
    Signed-off-by: Dtrain Hsu <dtrain_hsu@compal.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4322575
    Reviewed-by: Kyle Lin <kylelinck@google.com>
    Commit-Queue: Kyle Lin <kylelinck@google.com>
    (cherry picked from commit d697e5ee1e3572683283683e5c3fb08d73c1ae27)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4328371

--- src/board/brya/omnigul.c
+++ src/board/brya/omnigul.c
@@ -6,13 +6,34 @@
 
 #include "base/fw_config.h"
 #include "board/brya/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
 #include "drivers/gpio/alderlake.h"
 #include "drivers/soc/alderlake.h"
 #include "drivers/storage/storage_common.h"
 
+#define SDMODE_PIN		GPP_A11
+#define SDMODE_ENABLE		0
+
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
+	config = (struct audio_config){
+		.bus = {
+			.type			= AUDIO_I2S,
+			.i2s.address		= SSP_I2S1_START_ADDRESS,
+			.i2s.enable_gpio	= { .pad = SDMODE_PIN,
+					.active_low = SDMODE_ENABLE },
+			.i2s.settings		= &max98357a_settings,
+		},
+		.amp = {
+			.type			= AUDIO_GPIO_AMP,
+			.gpio.enable_gpio	= SDMODE_PIN,
+		},
+		.codec = {
+			.type			= AUDIO_MAX98357,
+		},
+	};
 	return &config;
 }
 
