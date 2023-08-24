83ad18b1ce6583a9142f892fcb23221418f34b83
Tyler Wang <tyler.wang@quanta.corp-partner.google.com>
2023-08-24 10:36:32 +0800

    Karis: Add audio beep for ALC5650
    Add rt5650 support.
    
    BUG=b:295112765
    BRANCH=none
    TEST=(1) emerge-rex depthcharge
         (2) Test on karis, heard beep sound in depthcharge.
    
    Change-Id: I6e666a22fbe0c90301f0d5b1492e42cecfe83d6e
    Signed-off-by: Tyler Wang <tyler.wang@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4809619
    Reviewed-by: Kapil Porwal <kapilporwal@chromium.org>
    Reviewed-by: Mac Chiang <mac.chiang@intel.com>

--- src/board/rex/karis.c
+++ src/board/rex/karis.c
@@ -6,13 +6,36 @@
 
 #include "base/fw_config.h"
 #include "board/rex/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
 #include "drivers/gpio/meteorlake.h"
 #include "drivers/soc/meteorlake.h"
 #include "drivers/storage/storage_common.h"
 
+static void initialize_i2s_config(struct audio_config *config)
+{
+	*config = (struct audio_config) {
+		.bus = {
+			.type			= AUDIO_I2S,
+			.i2s.address		= SSP_I2S0_START_ADDRESS,
+			.i2s.settings		= &max98357a_settings,
+		},
+		.amp = {
+			.type			= AUDIO_AMP_NONE,
+		},
+		.codec = {
+			.type			= AUDIO_RT5650,
+			.i2c[0].ctrlr		= PCI_DEV_I2C0,
+			.i2c[0].i2c_addr[0]	= 0x1a,
+		},
+	};
+}
+
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
+	memset(&config, 0, sizeof(config));
+	initialize_i2s_config(&config);
 	return &config;
 }
 
