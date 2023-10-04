9a857e9472d72ceabf27f4fe844b09ff4f558733
Morris Hsu <morris-hsu@quanta.corp-partner.google.com>
2023-10-04 15:41:16 +0800

    dochi: update depthcharge setting
    Update audio/tpm and storage setting.
    
    BUG=b:298328847, b:299570339
    TEST=FW_NAME=dochi emerge-brya depthcharge
    
    Change-Id: I594fe26d45ebdead57225ce27b2b4d3d974b13b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4911113
    Reviewed-by: Nick Vaccaro <nvaccaro@google.com>
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Reviewed-by: Bob Moragues <moragues@chromium.org>
    Tested-by: Morris Hsu <morris-hsu@quanta.corp-partner.google.com>
    Commit-Queue: Bob Moragues <moragues@chromium.org>
    (cherry picked from commit 1b09449bf9383eef9f4c422df0f73e2efa2df820)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4918892
    Tested-by: David Wu <david_wu@quanta.corp-partner.google.com>

--- src/board/brya/dochi.c
+++ src/board/brya/dochi.c
@@ -6,10 +6,52 @@
 
 #include "base/fw_config.h"
 #include "board/brya/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
 #include "drivers/gpio/alderlake.h"
+#include "drivers/soc/alderlake.h"
+#include "drivers/storage/storage_common.h"
+
+static void initialize_i2s_config(struct audio_config *config)
+{
+	*config = (struct audio_config) {
+		.bus = {
+			.type			= AUDIO_I2S,
+			.i2s.address		= SSP_I2S1_START_ADDRESS,
+			.i2s.settings		= &max98357a_settings,
+		},
+		.amp = {
+			.type			= AUDIO_AMP_NONE,
+		},
+		.codec = {
+			.type			= AUDIO_RT5650,
+		},
+	};
+}
 
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
+	memset(&config, 0, sizeof(config));
+	initialize_i2s_config(&config);
+	return &config;
+}
+
+static const struct storage_config storage_configs[] = {
+	{ .media = STORAGE_NVME, .pci_dev = SA_DEV_CPU_RP0 },
+	{ .media = STORAGE_UFS, .pci_dev = PCH_DEV_UFS1 },
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static const struct tpm_config config = {
+		.pci_dev = PCI_DEV(0, 0x15, 1),
+	};
 	return &config;
 }
