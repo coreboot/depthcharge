c429e2c4c19c6da19c6eaf42b515a690d4919db3
Eran Mitrani <mitrani@google.com>
2023-11-01 13:40:56 -0700

    deku: Configure storage
    Deku uses PCIe rp11 for NVMe storage.
    
    BUG=b:305793886
    BRANCH=None
    TEST=built FW image correctly
    
    Change-Id: I93aa38ce8a40b5ea80223f62007d17195084c95a
    Signed-off-by: Eran Mitrani <mitrani@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/4997991
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>

--- src/board/rex/deku.c
+++ src/board/rex/deku.c
@@ -13,3 +13,13 @@ const struct audio_config *variant_probe_audio_config(void)
 	static struct audio_config config;
 	return &config;
 }
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	static const struct storage_config storage_configs[] = {
+		{ .media = STORAGE_NVME, .pci_dev = PCI_DEV_PCIE11 },
+	};
+
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
