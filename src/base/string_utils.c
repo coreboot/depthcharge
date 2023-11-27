2d3fc0d623e787bbcf2dfa3006afe2a0b1411963
Jan Dabros <dabros@google.com>
2023-11-27 08:54:20 +0000

    Add support for bootconfig handling
    The boot configuration expands kernel cmdline to support providing
    additional key=value data. The main difference in the format comparing
    to commandline is usage of '\n' as a separator instead of space.
    Bootconfig section is provided to the kernel at the very end of a
    ramdisk.
    
    Bootconfig is also used by android starting from version 12. Parameters
    may be created at build time and runtime. Build time params are inserted
    into vendor_boot partition, one needs to copy strings from there and
    append trailer with metadata. Runtime parameters are modified/filled by
    bootloader before handoff to the kernel.
    
    BUG=b:309426555,b:309426818
    TEST=With consecutive CLs applied build FW and boot android
    BRANCH=main
    
    Change-Id: Ida668d443e30566715e263f6aedfe14e315ebd57
    Signed-off-by: Jan Dabros <dabros@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5716688
    Commit-Queue: Grzegorz Bernacki <bernacki@google.com>
    Tested-by: Grzegorz Bernacki <bernacki@google.com>

---
+++ src/base/string_utils.c
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+#include <string.h>
+
+char *set_key_value_in_separated_list(const char *src, const char *key,
+				      const char *value, const char *separator)
+{
+	char *tmp;
+	char *dst;
+	int str_idx;
+
+	/*
+	 * Get enough space for the worst case, where we need to insert key,
+	 * value and separator, '=' and NULL terminator before potential new param.
+	 */
+	dst = calloc(1, strlen(src) + strlen(separator) + strlen(key) +
+			strlen(value) + 2);
+	if (dst == NULL)
+		return dst;
+
+	tmp = strstr(src, key);
+	if (tmp == NULL) {
+		/* If there is no "<key>", add "<key>=<value>" string */
+		sprintf(dst, "%s%s%s=%s", src, separator, key, value);
+		return dst;
+	}
+
+	/* 1. Copy old content up to and including "key=" */
+	str_idx = (tmp - src) + strlen(key) + 1;
+	memcpy(dst, src, str_idx);
+
+	/* 2. Copy value for the key */
+	memcpy(dst + str_idx, value, strlen(value));
+
+	tmp = strstr(tmp, separator);
+	if (tmp == NULL) {
+		/* No space after <key><separator> in src, what means we don't
+		 * have more to copy */
+		return dst;
+	}
+
+	/* 3. Copy rest of the old string if necessary */
+	memcpy(dst + strlen(value) + str_idx, tmp, strlen(tmp) + 1);
+
+	return dst;
+}
---
+++ src/boot/bootconfig.c
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+
+#include "base/string_utils.h"
+#include "boot/bootconfig.h"
+
+static char *bootconfig_set_key_value(const char *src, char *key, char *value)
+{
+	return set_key_value_in_separated_list(src, key, value, "\n");
+}
+
+/*
+ * Calculate checksum for bootconfig params.
+ *
+ * @param addr pointer to the start of the buffer.
+ * @param size size of the buffer in bytes.
+ * @return checksum result.
+ */
+static uint32_t checksum(const unsigned char *const buffer, size_t size) {
+	uint32_t sum = 0;
+
+	for (size_t i = 0; i < size; i++)
+		sum += buffer[i];
+
+	return sum;
+}
+
+/*
+ * Add boot config trailer.
+ */
+static void append_bootconfig_trailer(void *bootc_start, size_t params_size)
+{
+	uint8_t *trailer;
+	uint32_t sum;
+
+	trailer = (uint8_t *)bootc_start + params_size;
+
+	/* size */
+	memcpy(trailer, &params_size, BOOTCONFIG_SIZE_BYTES);
+
+	/* checksum */
+	sum = checksum((unsigned char *)bootc_start, params_size);
+	memcpy(trailer + BOOTCONFIG_SIZE_BYTES, &sum,
+	       BOOTCONFIG_CHECKSUM_BYTES);
+
+	/* magic at the end of trailer */
+	memcpy(trailer + BOOTCONFIG_SIZE_BYTES + BOOTCONFIG_CHECKSUM_BYTES,
+	       BOOTCONFIG_MAGIC, BOOTCONFIG_MAGIC_BYTES);
+}
+
+/**
+ * Add new string to bootconfig parameters, update trailer structure.
+ *
+ * In case provided key already exists, update its value.
+ *
+ * @param key - pointer to the string comprising key
+ * @param value - pointer to the string comprising value for key
+ * @param bootc_start - pointer to the bootconfig section
+ * @param bootc_size - current size of bootconfig structure
+ * @param buffer_size - size allocated for bootconfig structure
+ *
+ * @param return: New size of bootconfig params section after update,
+ *                -1 on error
+ */
+int append_bootconfig_params(char *key, char *value, void *bootc_start,
+			     size_t bootc_size, size_t buffer_size)
+{
+	size_t size;
+	char *bootc_str = (char *)bootc_start;
+	char *updated_str;
+
+	if (bootc_start == NULL || key == NULL || value == NULL)
+		return -1;
+
+	/*
+	 * If bootconfig structure doesn't have trailer (its current size is
+	 * 0), we will need to construct it from scratch after adding key=value
+	 * string. Shadow `bootc_size` real value, so that algorithm below will
+	 * take care of it.
+	 */
+	if (bootc_size == 0)
+		bootc_size = BOOTCONFIG_TRAILER_BYTES;
+
+	/*
+	 * Firstly, we need to append NULL terminator to bootconfig entries to
+	 * convert it to string acceptable by API used below. Trailer can be
+	 * overridden as we will reconstruct it anyway.
+	 */
+	bootc_str[bootc_size - BOOTCONFIG_TRAILER_BYTES] = '\0';
+
+	updated_str = bootconfig_set_key_value(bootc_str, key, value);
+
+	/* Check if caller has enough space allocated */
+	if (buffer_size < strlen(updated_str + BOOTCONFIG_TRAILER_BYTES)) {
+		free(updated_str);
+		return -1;
+	}
+
+	/* Don't include string terminator */
+	memcpy((void *)bootc_str, updated_str, strlen(updated_str));
+	append_bootconfig_trailer(bootc_start, strlen(updated_str));
+
+	size = strlen(updated_str) + BOOTCONFIG_TRAILER_BYTES;
+	free(updated_str);
+
+	return size;
+}
+
+/**
+ * Parse bootconfig section created at build time (usually on vendor_boot
+ * partition) and based on this create eventual bootconfig structure at the
+ * provided target address.
+ *
+ * @param bootc_start - Start address of bootconfig section in memory (usually right
+ *               after ramdisks)
+ * @param bootc_params - pointer to the buffer with bootconfig parameters
+ * @param params_size - size in bytes of bootc_params
+ *
+ * @return Size of the whole bootconfig structure,
+ *         -1 on error
+ */
+int parse_build_time_bootconfig(void *bootc_start, void *bootc_params,
+				size_t params_size)
+{
+	if (bootc_start == NULL || bootc_params == NULL)
+		return -1;
+
+	if (params_size == 0)
+		return 0;
+
+	memcpy(bootc_start, bootc_params, params_size);
+	append_bootconfig_trailer(bootc_start, params_size);
+
+	return params_size + BOOTCONFIG_TRAILER_BYTES;
+}
