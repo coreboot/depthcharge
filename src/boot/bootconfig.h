d2061dd01e33929f4aa9e3547a4cf702ffee6521
Jan Dabros <dabros@google.com>
2023-11-27 08:54:20 +0000

    Add support for bootconfig handling
    The boot configuration expands kernel cmdline to support providing
    additional key=value data. The main difference in the format comparing
    to commandline is usage of '\n' as a separator instead of space.
    Bootconfig section is provided to the kernel at the very end of a
    ramdisk.
    
    Bootconfig is also used by android starting from version 12. Parameters
    may be created at build time and runtime. Build time params are inserted
    into vendor_boot partition, one needs to copy strings from there and
    append trailer with metadata. Runtime parameters are modified/filled by
    bootloader before handoff to the kernel.
    
    BUG=b:309426555,b:309426818
    TEST=With consecutive CLs applied build FW and boot android
    BRANCH=main
    
    Change-Id: Ida668d443e30566715e263f6aedfe14e315ebd57
    Signed-off-by: Jan Dabros <dabros@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5716688
    Commit-Queue: Grzegorz Bernacki <bernacki@google.com>
    Tested-by: Grzegorz Bernacki <bernacki@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5720969
    Reviewed-by: Jakub Czapiga <czapiga@google.com>
    Tested-by: Kornel Dulęba <korneld@google.com>
    Tested-by: Patryk Busse <pbusse@google.com>
    Commit-Queue: Kornel Dulęba <korneld@google.com>

---
+++ src/boot/bootconfig.h
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#ifndef __BOOT_BOOTCONFIG_H__
+#define __BOOT_BOOTCONFIG_H__
+
+#define BOOTCONFIG_MAGIC "#BOOTCONFIG\n"
+#define BOOTCONFIG_MAGIC_BYTES 12
+#define BOOTCONFIG_SIZE_BYTES 4
+#define BOOTCONFIG_CHECKSUM_BYTES 4
+#define BOOTCONFIG_TRAILER_BYTES (BOOTCONFIG_MAGIC_BYTES + \
+				  BOOTCONFIG_SIZE_BYTES + \
+				  BOOTCONFIG_CHECKSUM_BYTES)
+
+/**
+ * Add new string to bootconfig parameters, update trailer structure.
+ *
+ * In case provided key already exists, update its value.
+ *
+ * @param key - pointer to the string comprising key
+ * @param value - pointer to the string comprising value for key
+ * @param bootc_start - pointer to the bootconfig section
+ * @param bootc_size - current size of bootconfig structure
+ * @param buffer_size - size allocated for bootconfig structure
+ *
+ * @param return: New size of bootconfig params section after update,
+ *                -1 on error
+ */
+int append_bootconfig_params(char *key, char *value, void *bootc_start,
+			     size_t bootc_size, size_t buffer_size);
+
+/**
+ * Parse bootconfig section created at build time (usually on vendor_boot
+ * partition) and based on this create eventual bootconfig structure at the
+ * provided target address.
+ *
+ * @param bootc_start - Start address of bootconfig section in memory (usually right
+ *               after ramdisks)
+ * @param bootc_params - pointer to the buffer with bootconfig parameters
+ * @param params_size - size in bytes of bootc_params
+ *
+ * @return Size of the whole bootconfig structure,
+ *         -1 on error
+ */
+int parse_build_time_bootconfig(void *bootc_start, void *bootc_params,
+				size_t params_size);
+
+#endif /* __BOOT_BOOTCONFIG_H__ */
