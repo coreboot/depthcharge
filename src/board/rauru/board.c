521c37d51aec37a0a8914c0c37671eb32c210623
Jarried Lin <jarried.lin@mediatek.corp-partner.google.com>
2023-12-28 12:19:59 +0800

    rauru: Add new board 'rauru'
    TEST=build pass
    BUG=b:317009620
    
    (cherry picked from commit 58b35b51a7b9d514e69afac595192d7bb2536f4a)
    
    Original-Signed-off-by: Jarried Lin <jarried.lin@mediatek.corp-partner.google.com>
    Change-Id: I27796640ef4c3bdedf6970c281133b63a4a35065
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5832788
    Original-Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Original-Reviewed-by: Yidi Lin <yidilin@google.com>
    GitOrigin-RevId: 58b35b51a7b9d514e69afac595192d7bb2536f4a
    Cr-Build-Id: 8736283101312505153
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8736283101312505153
    Copybot-Job-Name: android-main-depthcharge-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5861813
    Commit-Queue: Grzegorz Bernacki <bernacki@google.com>
    Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Reviewed-by: Grzegorz Bernacki <bernacki@google.com>

---
+++ src/board/rauru/board.c
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#include <assert.h>
+#include <libpayload.h>
+
+#include "base/init_funcs.h"
+#include "drivers/bus/usb/usb.h"
+#include "drivers/flash/mtk_snfc.h"
+#include "drivers/gpio/mtk_gpio.h"
+#include "drivers/gpio/sysinfo.h"
+#include "drivers/power/psci.h"
+
+static int board_setup(void)
+{
+	sysinfo_install_flags(new_mtk_gpio_input);
+	power_set_ops(&psci_power_ops);
+
+	/* Set up NOR flash ops */
+	MtkNorFlash *nor_flash = new_mtk_nor_flash(0x16340000);
+	flash_set_ops(&nor_flash->ops);
+
+	/* TODO: Set up SD card */
+	/* TODO: Set up USB */
+	return 0;
+}
+
+INIT_FUNC(board_setup);
