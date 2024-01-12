4c37c04d74e2190ea565a7f90548a33d03284e83
Yu-Ping Wu <yupingso@chromium.org>
2024-01-12 08:34:06 +0800

    tests/drivers/input/mkbp/mkbp-test: Simulate EC's MKBP FIFO
    Currently we use cmocka's mocking framework to implement the mock
    cros_ec_get_next_event(). However, that has one major problem -- we
    can't control when a mock return value (determined by will_return())
    will be used within the test function. For example, consider the
    following test code:
    
     will_return(cros_ec_get_next_event, 1);
     will_return(cros_ec_get_next_event, 2);
     assert_false(mkbp_keyboard.havekey());
    
     will_return(cros_ec_get_next_event, 3);
     assert_true(mkbp_keyboard.havekey());
    
    Even if the test passes, we still don't know whether the return value 2
    is consumed by the first or second mkbp_keyboard.havekey() call, and
    cmocka doesn't support checking that.
    
    The situation will become worse if we want to write tests for button
    long press. In that case, timer_us() will also needs to be mocked, and
    we will need to additionally synchronize the return values of the mock
    cros_ec_get_next_event() and mock timer_us(). Again, that's impossible
    with the current cmocka's API.
    
    Therefore, to allow adding test cases for button long press, modify the
    mock cros_ec_get_next_event() to not using cmocka's mocking objects.
    Instead, we simulate EC's MKBP FIFO by maintaining a mocked event queue.
    
    BUG=none
    TEST=make tests/drivers/input/mkbp/mkbp-test
    BRANCH=none
    
    Change-Id: I9d06afc85e82c43e6c0ae311ff755599132e94fc
    Signed-off-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5188600
    Reviewed-by: Yidi Lin <yidilin@chromium.org>
    Reviewed-by: Hsuan Ting Chen <roccochen@chromium.org>

--- tests/drivers/input/mkbp/mkbp-test.c
+++ tests/drivers/input/mkbp/mkbp-test.c
@@ -5,20 +5,6 @@
 
 #include "drivers/input/mkbp/mkbp.c"
 
-static int setup(void **state)
-{
-	/* A hack to reset the static variable 'combo_detected' in
-	   mkbp_add_button(), by simulating button release. */
-	prev_bitmap = 0x1;
-	mkbp_add_button(NULL, 0, 0x0);
-	/* Reset global variables. */
-	prev_bitmap = 0;
-	memset(key_fifo, 0, sizeof(key_fifo));
-	fifo_offset = 0;
-	fifo_size = 0;
-	return 0;
-}
-
 /* Mock functions. */
 
 int cros_ec_interrupt_pending(void)
@@ -37,37 +23,73 @@ int cros_ec_clear_host_events(uint32_t events)
 	return 0;
 }
 
+#define MOCK_EC_FIFO_SIZE 64
+static struct ec_response_get_next_event_v1
+	mock_ec_event_fifo[MOCK_EC_FIFO_SIZE];
+static size_t mock_ec_event_fifo_start, mock_ec_event_fifo_end;
+
+static uint32_t mock_buttons;
+
 int cros_ec_get_next_event(struct ec_response_get_next_event_v1 *e)
 {
-	e->event_type = mock_type(uint8_t);
+	if (mock_ec_event_fifo_start == mock_ec_event_fifo_end)
+		return -EC_RES_UNAVAILABLE;
+
+	memcpy(e, &mock_ec_event_fifo[mock_ec_event_fifo_start],
+	       sizeof(struct ec_response_get_next_event_v1));
+	mock_ec_event_fifo_start++;
+
 	if (e->event_type != EC_MKBP_EVENT_BUTTON)
 		return -EC_RES_UNAVAILABLE;
-	e->data.buttons = mock_type(uint32_t);
 	return sizeof(e->event_type) + sizeof(e->data.buttons);
+}
+
+/* Setup function. */
+
+static int setup(void **state)
+{
+	/* A hack to reset the static variable 'combo_detected' in
+	   mkbp_add_button(), by simulating button release. */
+	prev_bitmap = 0x1;
+	mkbp_add_button(NULL, 0, 0x0);
+	/* Reset global variables. */
+	prev_bitmap = 0;
+	memset(key_fifo, 0, sizeof(key_fifo));
+	fifo_offset = 0;
+	fifo_size = 0;
+	/* Reset mock variables. */
+	memset(mock_ec_event_fifo, 0, sizeof(mock_ec_event_fifo));
+	mock_ec_event_fifo_start = 0;
+	mock_ec_event_fifo_end = 0;
+	mock_buttons = 0;
+	return 0;
 }
 
 /* Test functions. */
 
-#define WILL_GET_BUTTON_EVENT(buttons) do { \
-	will_return(cros_ec_get_next_event, EC_MKBP_EVENT_BUTTON); \
-	will_return(cros_ec_get_next_event, buttons); \
-} while (0)
+static void add_button_event_to_fifo(uint32_t buttons)
+{
+	if (mock_ec_event_fifo_end >= MOCK_EC_FIFO_SIZE)
+		fail_msg("Mock EC event fifo is full");
 
-#define WILL_HAVE_NO_EVENT() will_return(cros_ec_get_next_event, -1)
+	struct ec_response_get_next_event_v1 *event =
+		&mock_ec_event_fifo[mock_ec_event_fifo_end];
+	event->event_type = EC_MKBP_EVENT_BUTTON;
+	event->data.buttons = buttons;
+	mock_ec_event_fifo_end++;
+}
 
-#define WILL_PRESS_BUTTON(buttons, button_bit) ({ \
-	uint32_t _test_buttons = buttons; \
-	_test_buttons |= 1U << button_bit; \
-	WILL_GET_BUTTON_EVENT(_test_buttons); \
-	_test_buttons; \
-})
+static void press_button(unsigned int button_bit)
+{
+	mock_buttons |= 1U << button_bit;
+	add_button_event_to_fifo(mock_buttons);
+}
 
-#define WILL_RELEASE_BUTTON(buttons, button_bit) ({ \
-	uint32_t _test_buttons = buttons; \
-	_test_buttons &= ~(1U << button_bit); \
-	WILL_GET_BUTTON_EVENT(_test_buttons); \
-	_test_buttons; \
-})
+static void release_button(unsigned int button_bit)
+{
+	mock_buttons &= ~(1U << button_bit);
+	add_button_event_to_fifo(mock_buttons);
+}
 
 #define ASSERT_GETCHAR(c) do { \
 	assert_true(mkbp_keyboard.havekey()); \
@@ -78,34 +100,27 @@ int cros_ec_get_next_event(struct ec_response_get_next_event_v1 *e)
 
 static void test_power_short_press(void **state)
 {
-	uint32_t b = 0;
-	b = WILL_PRESS_BUTTON(b, EC_MKBP_POWER_BUTTON);
-	WILL_HAVE_NO_EVENT();
+	press_button(EC_MKBP_POWER_BUTTON);
 	ASSERT_NO_MORE_CHAR();
 
 	/* Single button is reported on release. */
-	b = WILL_RELEASE_BUTTON(b, EC_MKBP_POWER_BUTTON);
-	WILL_HAVE_NO_EVENT();
+	release_button(EC_MKBP_POWER_BUTTON);
 	ASSERT_GETCHAR(UI_BUTTON_POWER_SHORT_PRESS);
 	ASSERT_NO_MORE_CHAR();
 }
 
 static void test_vol_up_short_press(void **state)
 {
-	uint32_t b = 0;
-	b = WILL_PRESS_BUTTON(b, EC_MKBP_VOL_UP);
-	b = WILL_RELEASE_BUTTON(b, EC_MKBP_VOL_UP);
-	WILL_HAVE_NO_EVENT();
+	press_button(EC_MKBP_VOL_UP);
+	release_button(EC_MKBP_VOL_UP);
 	ASSERT_GETCHAR(UI_BUTTON_VOL_UP_SHORT_PRESS);
 	ASSERT_NO_MORE_CHAR();
 }
 
 static void test_vol_down_short_press(void **state)
 {
-	uint32_t b = 0;
-	b = WILL_PRESS_BUTTON(b, EC_MKBP_VOL_DOWN);
-	b = WILL_RELEASE_BUTTON(b, EC_MKBP_VOL_DOWN);
-	WILL_HAVE_NO_EVENT();
+	press_button(EC_MKBP_VOL_DOWN);
+	release_button(EC_MKBP_VOL_DOWN);
 	ASSERT_GETCHAR(UI_BUTTON_VOL_DOWN_SHORT_PRESS);
 	ASSERT_NO_MORE_CHAR();
 }
@@ -113,31 +128,26 @@ static void test_vol_down_short_press(void **state)
 static void test_vol_up_down_combo(void **state)
 {
 	/* Combo is reported on button press instead of release. */
-	uint32_t b = 0;
-	b = WILL_PRESS_BUTTON(b, EC_MKBP_VOL_UP);
-	b = WILL_PRESS_BUTTON(b, EC_MKBP_VOL_DOWN);
-	WILL_HAVE_NO_EVENT();
+	press_button(EC_MKBP_VOL_UP);
+	press_button(EC_MKBP_VOL_DOWN);
 	ASSERT_GETCHAR(UI_BUTTON_VOL_UP_DOWN_COMBO_PRESS);
 	ASSERT_NO_MORE_CHAR();
 
 	/* Combo isn't reported again on release. */
-	b = WILL_RELEASE_BUTTON(b, EC_MKBP_VOL_DOWN);
-	b = WILL_RELEASE_BUTTON(b, EC_MKBP_VOL_UP);
-	WILL_HAVE_NO_EVENT();
+	release_button(EC_MKBP_VOL_DOWN);
+	release_button(EC_MKBP_VOL_UP);
 	ASSERT_NO_MORE_CHAR();
 }
 
 static void test_undefined_combo(void **state)
 {
-	uint32_t b = 0;
-	b = WILL_PRESS_BUTTON(b, EC_MKBP_VOL_UP);
+	press_button(EC_MKBP_VOL_UP);
 
 	/* Power button shouldn't be reported when holding another button. */
-	b = WILL_PRESS_BUTTON(b, EC_MKBP_POWER_BUTTON);
-	b = WILL_RELEASE_BUTTON(b, EC_MKBP_POWER_BUTTON);
+	press_button(EC_MKBP_POWER_BUTTON);
+	release_button(EC_MKBP_POWER_BUTTON);
 
-	b = WILL_RELEASE_BUTTON(b, EC_MKBP_VOL_UP);
-	WILL_HAVE_NO_EVENT();
+	release_button(EC_MKBP_VOL_UP);
 	ASSERT_NO_MORE_CHAR();
 }
 
