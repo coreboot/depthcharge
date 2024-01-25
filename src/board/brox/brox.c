4fdb3debdefa78584e8c7d877ac239e3c3b0a506
Tristan Honscheid <honscheid@google.com>
2024-01-24 17:35:52 -0700

    pdc: Choose PDC FW edition based on CBI data
    Query the EC CBI data to determine which firmware edition shall be
    flashed to the PDC. Add a mechanism so board code can specify what FW
    edition is needed.
    
    BUG=b:303712961
    BRANCH=None
    TEST=Flash to brox DUT
    
    Change-Id: Ic2c12b96fcf87f616f95d16b3abf2783d2ae95c6
    Signed-off-by: Tristan Honscheid <honscheid@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5237931
    Reviewed-by: Karthikeyan Ramasubramanian <kramasub@google.com>

--- src/board/brox/brox.c
+++ src/board/brox/brox.c
@@ -6,9 +6,26 @@
 
 #include "base/fw_config.h"
 #include "board/brox/include/variant.h"
+#include "drivers/ec/rts5453/rts5453.h"
 #include "drivers/gpio/alderlake.h"
 #include "drivers/soc/alderlake.h"
 #include "drivers/storage/storage_common.h"
+
+/* Override of func in src/drivers/ec/rts5453/rts5453.c */
+void board_rts5453_get_image_paths(const char **image_path, const char **hash_path)
+{
+	if (fw_config_probe(FW_CONFIG(RETIMER, RETIMER_BYPASS))) {
+		*image_path = "rts5453_retimer_bypass.bin";
+		*hash_path = "rts5453_retimer_bypass.hash";
+	} else if (fw_config_probe(FW_CONFIG(RETIMER, RETIMER_JHL8040))) {
+		*image_path = "rts5453_retimer_jhl8040.bin";
+		*hash_path = "rts5453_retimer_jhl8040.hash";
+	} else {
+		/* Unknown configuration. Don't perform an update. */
+		*image_path = NULL;
+		*hash_path = NULL;
+	}
+}
 
 const struct audio_config *variant_probe_audio_config(void)
 {
---
+++ src/drivers/ec/rts5453/rts5453.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright 2024 Google LLC.  */
+
+/**
+ * @brief Board-provided function used by RTS5453 driver to obtain the FW
+ *        binary and hash file to flash to the PDC. Board logic is used to
+ *        determine the desired image to support multiple variations. A
+ *        default implementation of this function returns NULL for both
+ *        paths, which causes no update to occur.
+ *
+ * @param image_path Output pointer to a FW binary path string (or NULL)
+ * @param hash_path Output pointer to a FW hash path string (or NULL)
+ */
+void board_rts5453_get_image_paths(const char **image_path,
+				   const char **hash_path);
