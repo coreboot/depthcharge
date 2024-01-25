9151c14b4a3c41a0111998813fa9d9dd8f1f3728
YH Lin <yueherngl@google.com>
2024-01-25 21:30:51 +0000

    Create xol variant
    Create a depthcharge target xol by copying the template files from
    skolas to the appropriate places in the source tree.
    
    Note the xol variant is currently just a skeleton and need to be
    revised later on.
    
    BUG=b:319506033
    BRANCH=brya
    TEST=FW_NAME=xol emerge-brya depthcharge
    
    Signed-off-by: YH Lin <yueherngl@google.com>
    Change-Id: I228e4a5c40d8932ee72a8f711ce8460d2b35c8aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5241810
    Reviewed-by: Nick Vaccaro <nvaccaro@google.com>
    Commit-Queue: YH Lin <yueherngl@chromium.org>
    Tested-by: YH Lin <yueherngl@chromium.org>
    (cherry picked from commit f0de054d5495932f854aaea4e29efcdede473df7)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5246054
    Tested-by: Nick Vaccaro <nvaccaro@google.com>
    Auto-Submit: Nick Vaccaro <nvaccaro@google.com>
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>
    Commit-Queue: Nick Vaccaro <nvaccaro@google.com>
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>

---
+++ src/board/brya/xol.c
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+#include <pci.h>
+#include <pci/pci.h>
+
+#include "base/fw_config.h"
+#include "board/brya/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
+#include "drivers/bus/soundwire/cavs_2_5-sndwregs.h"
+#include "drivers/gpio/alderlake.h"
+#include "drivers/soc/alderlake.h"
+#include "drivers/sound/nau8318.h"
+
+const struct audio_config *variant_probe_audio_config(void)
+{
+	static struct audio_config config;
+
+	memset(&config, 0, sizeof(config));
+
+	return &config;
+}
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static struct tpm_config config = {
+		.pci_dev = PCI_DEV(0, 0x15, 1),
+	};
+
+	if (lib_sysinfo.board_id < 4)
+		config.pci_dev = PCI_DEV(0, 0x15, 3);
+
+	return &config;
+}
