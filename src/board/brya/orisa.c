9760b5a186fbc7872b3eff0a96489cb4529586f6
Amanda Huang <amanda_hwang@compal.corp-partner.google.com>
2024-06-05 20:06:46 +0800

    orisa: Configure depthcharge for orisa
    Add audio and storage support. Use GPP_A17
    as GSC irq.
    
    BUG=b:345112878
    BRANCH=firmware-nissa-15217.B
    TEST=emerge-nissa depthcharge
    
    Change-Id: Ieadfcce6d2b01cab59e9646d406f058c71b819b5
    Signed-off-by: Amanda Huang <amanda_hwang@compal.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5642243
    Reviewed-by: Shou-Chieh Hsu <shouchieh@chromium.org>
    Commit-Queue: Shou-Chieh Hsu <shouchieh@chromium.org>

--- src/board/brya/orisa.c
+++ src/board/brya/orisa.c
@@ -7,9 +7,72 @@
 #include "base/fw_config.h"
 #include "board/brya/include/variant.h"
 #include "drivers/gpio/alderlake.h"
+#include "drivers/soc/alderlake.h"
+#include "drivers/storage/sdhci.h"
+#include "drivers/storage/storage_common.h"
+
+#define EMMC_CLOCK_MIN		400000
+#define EMMC_CLOCK_MAX		200000000
+
+#define EC_PCH_INT_ODL		GPD2
+
+#define PCI_DEV_I2C0		PCI_DEV(0, 0x15, 0)
 
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
+
+	config = (struct audio_config){
+		.bus = {
+			.type = AUDIO_HDA,
+		},
+		.codec = {
+			.type = AUDIO_ALC256,
+		},
+	};
+
 	return &config;
+}
+
+int gsc_irq_status(void)
+{
+	return alderlake_get_gpe(GPE0_DW0_17); /* GPP_A17 */
+}
+
+static const struct storage_config storage_configs[] = {
+	{
+		.media = STORAGE_EMMC,
+		.pci_dev = PCH_DEV_EMMC,
+		.emmc = {
+			.platform_flags = SDHCI_PLATFORM_SUPPORTS_HS400ES,
+			.clock_min = EMMC_CLOCK_MIN,
+			.clock_max = EMMC_CLOCK_MAX
+			},
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_EMMC)
+	},
+	{
+		.media = STORAGE_UFS,
+		.pci_dev = PCH_DEV_UFS1,
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_UFS)
+	},
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static const struct tpm_config config = {
+		.pci_dev = PCI_DEV_I2C0,
+	};
+
+	return &config;
+}
+
+const int variant_get_ec_int(void)
+{
+	return EC_PCH_INT_ODL;
 }
