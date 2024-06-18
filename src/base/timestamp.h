2a33a6b1e09d3556d6aa986cb27b57a9a639f49d
Kapil Porwal <kapilporwal@google.com>
2024-06-18 20:17:44 +0000

    base/timestamp: Add function to get tick frequency
    Port timestamp_tick_freq_mhz() from coreboot.
    
    BUG=b:305902595
    TEST=Verify build and boot on Screebo
    
    Change-Id: I0303bab1b14f5e8cbc62342070013460b748e2dc
    Signed-off-by: Kapil Porwal <kapilporwal@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5640813
    Tested-by: Kapil Porwal <kapilporwal@chromium.org>
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    Commit-Queue: Kapil Porwal <kapilporwal@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6063883
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Tested-by: Konrad Adamczyk <konrada@google.com>

--- src/base/timestamp.h
+++ src/base/timestamp.h
@@ -47,5 +47,7 @@ void timestamp_add(enum timestamp_id id, uint64_t ts_time);
 void timestamp_add_now(enum timestamp_id id);
 void timestamp_mix_in_randomness(u8 *buffer, size_t size);
 uint64_t get_us_since_boot(void);
+/* Returns timestamp tick frequency in MHz. */
+int timestamp_tick_freq_mhz(void);
 
 #endif /* __BASE_TIMESTAMP_H__ */
