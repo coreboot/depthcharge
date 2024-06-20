f69b3a5d9a8dbc41480a2ec16b83ac7ddd19a978
Amanda Huang <amanda_hwang@compal.corp-partner.google.com>
2024-06-20 23:40:30 +0800

    orisa: Support HDA Boot beep functionality
    Add support for boot beep using HDA codec. Also, add
    the ALDERLAKE_N PCH device ID to HDA driver.
    
    BUG=None
    BRANCH=firmware-nissa-15217.B
    TEST=Build & verified boot beep functionality on Brox
    
    Change-Id: Ide734f747a093f560c71c971f492306355673f11
    Signed-off-by: Amanda Huang <amanda_hwang@compal.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5644375
    Reviewed-by: Shou-Chieh Hsu <shouchieh@chromium.org>
    Commit-Queue: Shou-Chieh Hsu <shouchieh@chromium.org>

--- src/drivers/sound/intel_audio_setup.h
+++ src/drivers/sound/intel_audio_setup.h
@@ -14,6 +14,7 @@ enum audio_bus_type {
 	AUDIO_I2S,
 	AUDIO_SNDW,
 	AUDIO_PWM,
+	AUDIO_HDA,
 };
 
 enum audio_codec_type {
@@ -24,6 +25,7 @@ enum audio_codec_type {
 	AUDIO_CS35L53,
 	AUDIO_MAX98396,
 	AUDIO_RT5650,
+	AUDIO_ALC256,
 };
 
 enum audio_amp_type {
