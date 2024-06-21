1969d421eef21c3403c00b9414a6eea94d045e62
Subrata Banik <subratabanik@google.com>
2024-06-22 00:37:49 +0530

    fatcat: Add initial mainboard support
    This patch adds minimal code from rex to fatcat.
    
    TODO: Add placeholder for TPM, Audio etc. as applicable based
    on fatcat schematics.
    
    BUG=b:347669091
    TEST=Able to build fatcat depthcharge.
    
    Change-Id: I62936719e8f6c454940b2a3f689cf1a3e941b5d7
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5644377
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Tested-by: Subrata Banik <subratabanik@chromium.org>

---
+++ src/board/fatcat/board.c
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/*
+ * These needs to be included first.
+ * Some of the driver headers would be dependent on these.
+ */
+#include <commonlib/list.h>
+#include <pci.h>
+#include <pci/pci.h>
+
+#include "base/init_funcs.h"
+#include "board/rex/include/variant.h"
+#include "drivers/bus/i2c/designware.h"
+#include "drivers/bus/i2c/i2c.h"
+#include "drivers/ec/cros/lpc.h"
+#include "drivers/flash/flash.h"
+#include "drivers/flash/memmapped.h"
+#include "drivers/gpio/sysinfo.h"
+#include "drivers/gpio/gpio.h"
+#include "drivers/gpio/meteorlake.h"
+#include "drivers/power/pch.h"
+#include "drivers/soc/meteorlake.h"
+#include "drivers/storage/storage_common.h"
+#include "drivers/tpm/google/i2c.h"
+#include "drivers/tpm/google/switches.h"
+#include "drivers/tpm/tpm.h"
+#include "vboot/util/flag.h"
+#include <libpayload.h>
+#include <sysinfo.h>
+
+#define EC_PCH_INT_ODL	GPP_A17
+#define I2C_FS_HZ	400000 /* 400KHz */
+
+__weak const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	static const struct storage_config storage_configs[] = {
+		{ .media = STORAGE_NVME, .pci_dev = PCI_DEV_PCIE9 },
+		{ .media = STORAGE_SDHCI, .pci_dev = PCI_DEV_PCIE7 },
+	};
+
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
+__weak const int variant_get_ec_int(void)
+{
+	return EC_PCH_INT_ODL;
+}
+
+static GpioOps *mkbp_int_ops(void)
+{
+	GpioCfg *mkbp_int_gpio = new_platform_gpio_input(variant_get_ec_int());
+	/* Active-low, has to be inverted */
+	return new_gpio_not(&mkbp_int_gpio->ops);
+}
+
+static void ec_setup(void)
+{
+	CrosEcLpcBus *cros_ec_lpc_bus =
+		new_cros_ec_lpc_bus(CROS_EC_LPC_BUS_GENERIC);
+	CrosEc *cros_ec = new_cros_ec(&cros_ec_lpc_bus->ops, mkbp_int_ops());
+	register_vboot_ec(&cros_ec->vboot);
+}
+
+__weak const struct tpm_config *variant_get_tpm_config(void)
+{
+	static const struct tpm_config config = {
+		.pci_dev = PCI_DEV_I2C4,
+	};
+
+	return &config;
+}
+
+static int gsc_irq_status(void)
+{
+	return meteorlake_get_gpe(GPE0_DW1_03); /* GPP_E03 */
+}
+
+static void tpm_setup(void)
+{
+	const struct tpm_config *config = variant_get_tpm_config();
+	DesignwareI2c *i2c = new_pci_designware_i2c(config->pci_dev,
+						    I2C_FS_HZ,
+						    METEORLAKE_DW_I2C_MHZ);
+	GscI2c *tpm = new_gsc_i2c(&i2c->ops, GSC_I2C_ADDR,
+				    &gsc_irq_status);
+	tpm_set_ops(&tpm->base.ops);
+	if (CONFIG(TPM_GOOGLE_SWITCHES))
+		flag_replace(FLAG_PHYS_PRESENCE,
+				&new_gsc_rec_switch(&tpm->base.ops)->ops);
+}
+
+__weak const struct audio_config *variant_probe_audio_config(void)
+{
+	static struct audio_config config;
+
+	printf("%s Empty stub. Please add varaint audio config\n", __func__);
+	return &config;
+}
+
+static int board_setup(void)
+{
+	sysinfo_install_flags(NULL);
+	ec_setup();
+	tpm_setup();
+	power_set_ops(&meteorlake_power_ops);
+	flash_set_ops(&new_mmap_flash()->ops);
+	common_audio_setup(variant_probe_audio_config());
+
+	return 0;
+}
+
+INIT_FUNC(board_setup);
---
+++ src/board/fatcat/include/variant.h
@@ -0,0 +1,12 @@
+#ifndef _VARIANT_H_
+#define _VARIANT_H_
+
+#include <libpayload.h>
+
+#include "drivers/sound/intel_audio_setup.h"
+
+struct tpm_config {
+	pcidev_t pci_dev;
+};
+
+#endif /* _VARIANT_H_ */
