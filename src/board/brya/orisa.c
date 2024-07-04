41715576cec5d22ffae0c28602860803091e4560
Amanda Huang <amanda_hwang@compal.corp-partner.google.com>
2024-07-04 19:37:51 +0800

    orisa: Choose PDC FW edition based on CBI data
    Query the EC CBI data to determine which firmware edition shall be
    flashed to the PDC. Add a mechanism so board code can specify what FW
    edition is needed.
    
    BUG=b:334793686
    BRANCH=firmware-nissa-15217.B
    TEST=Flash to orisa DUT
    
    (cherry picked from commit 2c60bafbe5ae59f76e6136539de8c35cc51b8751)
    
    Cq-Depend: chromium:5682802,chromium:5666191,chrome-internal:7470665
    Change-Id: If139d24d7435c2b7420b490306fb74cf30860191
    Original-Signed-off-by: Amanda Huang <amanda_hwang@compal.corp-partner.google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5699705
    Original-Reviewed-by: Shou-Chieh Hsu <shouchieh@chromium.org>
    Original-Reviewed-by: Ivan Chen <yulunchen@google.com>
    Original-Commit-Queue: Ivan Chen <yulunchen@google.com>
    Original-Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    GitOrigin-RevId: 2c60bafbe5ae59f76e6136539de8c35cc51b8751
    Cr-Build-Id: 8740088176436538961
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8740088176436538961
    Copybot-Job-Name: android-main-depthcharge-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5771900
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>
    Tested-by: Jonathon Murphy <jpmurphy@google.com>
    Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>

--- src/board/brya/orisa.c
+++ src/board/brya/orisa.c
@@ -6,6 +6,7 @@
 
 #include "base/fw_config.h"
 #include "board/brya/include/variant.h"
+#include "drivers/ec/rts5453/rts5453.h"
 #include "drivers/gpio/alderlake.h"
 #include "drivers/soc/alderlake.h"
 #include "drivers/storage/sdhci.h"
@@ -17,6 +18,23 @@
 #define EC_PCH_INT_ODL		GPD2
 
 #define PCI_DEV_I2C0		PCI_DEV(0, 0x15, 0)
+
+/* Override of func in src/drivers/ec/rts5453/rts5453.c */
+void board_rts5453_get_image_paths(const char **image_path, const char **hash_path)
+{
+	if (fw_config_probe(FW_CONFIG(PDC_CONTROL, PDC_RTS_BYPASS))) {
+		*image_path = "rts5453_retimer_bypass.bin";
+		*hash_path = "rts5453_retimer_bypass.hash";
+	} else if (fw_config_probe(FW_CONFIG(PDC_CONTROL, PDC_TI_BYPASS))) {
+		/* Will update path for TI PDC AU later */
+		*image_path = NULL;
+		*hash_path = NULL;
+	} else {
+		/* Unknown configuration. Don't perform an update. */
+		*image_path = NULL;
+		*hash_path = NULL;
+	}
+}
 
 const struct audio_config *variant_probe_audio_config(void)
 {
