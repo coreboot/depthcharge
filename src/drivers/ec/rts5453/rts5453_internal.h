3abe504b789e216b54642baff26639516390286c
Tristan Honscheid <honscheid@google.com>
2024-07-16 09:17:19 -0600

    rts5453: Enforce supported upgrade paths
    Not every RTS5453 PDC FW version can be upgraded to every other. Add
    a function that encodes the upgrade compatibility matrix and check it
    before proceeding with the update. If an update is not supported, simply
    skip it and print a warning. This prevents a boot loop due to wrong FW
    continually being rejected by the PDC.
    
    Add a unit test to verify the logic in test_rtk_upgrade_compatibility()
    
    See linked bug for a spreadsheet with the compatibility matrix.
    
    BUG=b:353510274
    BRANCH=None
    TEST=emerge-brox depthcharge
    TEST=make tests/drivers/rts5453/rts5453-test
    
    (cherry picked from commit 9f832243f50b67594c0b75142184764feed236a0)
    
    Change-Id: Ib6942e5bf2128940c37c298c06fb1db3b39cd867
    Original-Signed-off-by: Tristan Honscheid <honscheid@google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5714069
    Original-Reviewed-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Original-Reviewed-by: Keith Short <keithshort@chromium.org>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5758795
    Original-Tested-by: Ivan Chen <yulunchen@google.com>
    Original-Commit-Queue: Ivan Chen <yulunchen@google.com>
    GitOrigin-RevId: 9f832243f50b67594c0b75142184764feed236a0
    Cr-Build-Id: 8738276238912292609
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8738276238912292609
    Copybot-Job-Name: android-nissa-depthcharge-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5777324
    Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Commit-Queue: Łukasz Majczak <lmajczak@google.com>
    Reviewed-by: Łukasz Majczak <lmajczak@google.com>

---
+++ src/drivers/ec/rts5453/rts5453_internal.h
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright 2024 Google LLC.  */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/** Type for storing a 3-byte PDC FW version tuple. Bits 23:16 = Major,
+ *  15:8 = Minor, 7:0 = Patch versions. Simplifies comparisons of two
+ *  version values.
+ */
+typedef uint32_t pdc_fw_ver_t;
+
+/**
+ * @brief Convert a three part PDC FW version into a single integer for
+ *        comparison.
+ */
+#define PDC_FWVER_TO_INT(MAJOR, MINOR, PATCH)                                                  \
+	(pdc_fw_ver_t)(((MAJOR) & 0xFF) << 16 | ((MINOR) & 0xFF) << 8 | ((PATCH) & 0xFF))
+
+#define PDC_FWVER_MAJOR(VER) (((VER) >> 16) & 0xFF)
+#define PDC_FWVER_MINOR(VER) (((VER) >> 8) & 0xFF)
+#define PDC_FWVER_PATCH(VER) ((VER) & 0xFF)
+
+#define PDC_FWVER_TYPE_RELEASE (0)
+#define PDC_FWVER_TYPE_TEST (1)
+#define PDC_FWVER_TYPE_BASE (2)
+
+bool rts545x_check_update_compatibility(pdc_fw_ver_t current, pdc_fw_ver_t new);
