1c27e3750dc738a1764ee6157ea40ae0ee56e934
Tristan Honscheid <honscheid@google.com>
2024-07-16 09:17:19 -0600

    rts5453: Enforce supported upgrade paths
    Not every RTS5453 PDC FW version can be upgraded to every other. Add
    a function that encodes the upgrade compatibility matrix and check it
    before proceeding with the update. If an update is not supported, simply
    skip it and print a warning. This prevents a boot loop due to wrong FW
    continually being rejected by the PDC.
    
    Add a unit test to verify the logic in test_rtk_upgrade_compatibility()
    
    See linked bug for a spreadsheet with the compatibility matrix.
    
    BUG=b:353510274
    BRANCH=None
    TEST=emerge-brox depthcharge
    TEST=make tests/drivers/rts5453/rts5453-test
    
    (cherry picked from commit 883f5602571e8a712d45347d76210b61faffdbd3)
    
    Change-Id: Ib6942e5bf2128940c37c298c06fb1db3b39cd867
    Original-Signed-off-by: Tristan Honscheid <honscheid@google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5714069
    Original-Reviewed-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Original-Reviewed-by: Keith Short <keithshort@chromium.org>
    GitOrigin-RevId: 883f5602571e8a712d45347d76210b61faffdbd3
    Cr-Build-Id: 8740088176436538961
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8740088176436538961
    Copybot-Job-Name: android-main-depthcharge-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5771909
    Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Tested-by: Jonathon Murphy <jpmurphy@google.com>
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>

---
+++ tests/drivers/rts5453/rts5453-test.c
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright 2024 Google LLC.  */
+
+#include <tests/test.h>
+
+#include "drivers/ec/rts5453/rts5453_internal.h"
+
+#define ARRAY_SIZE(ARR) (sizeof((ARR)) / sizeof((ARR)[0]))
+
+struct rtk_upgrade_test_case {
+	pdc_fw_ver_t current;
+	pdc_fw_ver_t new;
+	bool success;
+} upgrade_test_cases[] = {
+	/* From < 0.20 */
+	{PDC_FWVER_TO_INT(0, 19, 1), PDC_FWVER_TO_INT(0, 19, 1), true},
+	{PDC_FWVER_TO_INT(0, 19, 1), PDC_FWVER_TO_INT(0, 20, 1), true},
+	{PDC_FWVER_TO_INT(0, 19, 1), PDC_FWVER_TO_INT(0, 21, 1), false},
+	{PDC_FWVER_TO_INT(0, 19, 1), PDC_FWVER_TO_INT(1, 21, 1), true},
+	{PDC_FWVER_TO_INT(0, 19, 1), PDC_FWVER_TO_INT(1, 22, 1), true},
+	{PDC_FWVER_TO_INT(0, 19, 1), PDC_FWVER_TO_INT(1, 23, 1), false},
+	/* No upgrade to base FW (2.x) in DC */
+	{PDC_FWVER_TO_INT(0, 19, 1), PDC_FWVER_TO_INT(2, 0, 1), false},
+	{PDC_FWVER_TO_INT(0, 19, 1), PDC_FWVER_TO_INT(2, 1, 1), false},
+	{PDC_FWVER_TO_INT(0, 19, 1), PDC_FWVER_TO_INT(2, 2, 1), false},
+
+	/* From == 0.20 */
+	{PDC_FWVER_TO_INT(0, 20, 1), PDC_FWVER_TO_INT(0, 19, 1), true},
+	{PDC_FWVER_TO_INT(0, 20, 1), PDC_FWVER_TO_INT(0, 20, 1), true},
+	{PDC_FWVER_TO_INT(0, 20, 1), PDC_FWVER_TO_INT(0, 21, 1), true},
+	{PDC_FWVER_TO_INT(0, 20, 1), PDC_FWVER_TO_INT(1, 21, 1), true},
+	{PDC_FWVER_TO_INT(0, 20, 1), PDC_FWVER_TO_INT(1, 22, 1), true},
+	{PDC_FWVER_TO_INT(0, 20, 1), PDC_FWVER_TO_INT(1, 23, 1), true},
+	/* No upgrade to base FW (2.x) in DC */
+	{PDC_FWVER_TO_INT(0, 20, 1), PDC_FWVER_TO_INT(2, 0, 1), false},
+	{PDC_FWVER_TO_INT(0, 20, 1), PDC_FWVER_TO_INT(2, 1, 1), false},
+	{PDC_FWVER_TO_INT(0, 20, 1), PDC_FWVER_TO_INT(2, 2, 1), false},
+
+	/* From > 0.20 */
+	{PDC_FWVER_TO_INT(0, 21, 1), PDC_FWVER_TO_INT(0, 19, 1), true},
+	{PDC_FWVER_TO_INT(0, 21, 1), PDC_FWVER_TO_INT(0, 20, 1), true},
+	{PDC_FWVER_TO_INT(0, 21, 1), PDC_FWVER_TO_INT(0, 21, 1), true},
+	{PDC_FWVER_TO_INT(0, 21, 1), PDC_FWVER_TO_INT(1, 21, 1), true},
+	{PDC_FWVER_TO_INT(0, 21, 1), PDC_FWVER_TO_INT(1, 22, 1), true},
+	{PDC_FWVER_TO_INT(0, 21, 1), PDC_FWVER_TO_INT(1, 23, 1), true},
+	/* No upgrade to base FW (2.x) in DC */
+	{PDC_FWVER_TO_INT(0, 21, 1), PDC_FWVER_TO_INT(2, 0, 1), false},
+	{PDC_FWVER_TO_INT(0, 21, 1), PDC_FWVER_TO_INT(2, 1, 1), false},
+	{PDC_FWVER_TO_INT(0, 21, 1), PDC_FWVER_TO_INT(2, 2, 1), false},
+
+	/* From < 1.22 */
+	{PDC_FWVER_TO_INT(1, 21, 1), PDC_FWVER_TO_INT(0, 19, 1), true},
+	{PDC_FWVER_TO_INT(1, 21, 1), PDC_FWVER_TO_INT(0, 20, 1), true},
+	{PDC_FWVER_TO_INT(1, 21, 1), PDC_FWVER_TO_INT(0, 21, 1), false},
+	{PDC_FWVER_TO_INT(1, 21, 1), PDC_FWVER_TO_INT(1, 21, 1), true},
+	{PDC_FWVER_TO_INT(1, 21, 1), PDC_FWVER_TO_INT(1, 22, 1), true},
+	{PDC_FWVER_TO_INT(1, 21, 1), PDC_FWVER_TO_INT(1, 23, 1), false},
+	/* No upgrade to base FW (2.x) in DC */
+	{PDC_FWVER_TO_INT(1, 21, 1), PDC_FWVER_TO_INT(2, 0, 1), false},
+	{PDC_FWVER_TO_INT(1, 21, 1), PDC_FWVER_TO_INT(2, 1, 1), false},
+	{PDC_FWVER_TO_INT(1, 21, 1), PDC_FWVER_TO_INT(2, 2, 1), false},
+
+	/* From == 1.22 */
+	{PDC_FWVER_TO_INT(1, 22, 1), PDC_FWVER_TO_INT(0, 19, 1), true},
+	{PDC_FWVER_TO_INT(1, 22, 1), PDC_FWVER_TO_INT(0, 20, 1), true},
+	{PDC_FWVER_TO_INT(1, 22, 1), PDC_FWVER_TO_INT(0, 21, 1), false},
+	{PDC_FWVER_TO_INT(1, 22, 1), PDC_FWVER_TO_INT(1, 21, 1), true},
+	{PDC_FWVER_TO_INT(1, 22, 1), PDC_FWVER_TO_INT(1, 22, 1), true},
+	{PDC_FWVER_TO_INT(1, 22, 1), PDC_FWVER_TO_INT(1, 23, 1), false},
+	/* No upgrade to base FW (2.x) in DC */
+	{PDC_FWVER_TO_INT(1, 22, 1), PDC_FWVER_TO_INT(2, 0, 1), false},
+	{PDC_FWVER_TO_INT(1, 22, 1), PDC_FWVER_TO_INT(2, 1, 1), false},
+	{PDC_FWVER_TO_INT(1, 22, 1), PDC_FWVER_TO_INT(2, 2, 1), false},
+
+	/* From > 1.22 */
+	{PDC_FWVER_TO_INT(1, 23, 1), PDC_FWVER_TO_INT(0, 19, 1), true},
+	{PDC_FWVER_TO_INT(1, 23, 1), PDC_FWVER_TO_INT(0, 20, 1), true},
+	{PDC_FWVER_TO_INT(1, 23, 1), PDC_FWVER_TO_INT(0, 21, 1), true},
+	{PDC_FWVER_TO_INT(1, 23, 1), PDC_FWVER_TO_INT(1, 21, 1), true},
+	{PDC_FWVER_TO_INT(1, 23, 1), PDC_FWVER_TO_INT(1, 22, 1), true},
+	{PDC_FWVER_TO_INT(1, 23, 1), PDC_FWVER_TO_INT(1, 23, 1), true},
+	/* No upgrade to base FW (2.x) in DC */
+	{PDC_FWVER_TO_INT(1, 23, 1), PDC_FWVER_TO_INT(2, 0, 1), false},
+	{PDC_FWVER_TO_INT(1, 23, 1), PDC_FWVER_TO_INT(2, 1, 1), false},
+	{PDC_FWVER_TO_INT(1, 23, 1), PDC_FWVER_TO_INT(2, 2, 1), false},
+
+	/* From == 2.0 */
+	{PDC_FWVER_TO_INT(2, 0, 1), PDC_FWVER_TO_INT(0, 19, 1), true},
+	{PDC_FWVER_TO_INT(2, 0, 1), PDC_FWVER_TO_INT(0, 20, 1), true},
+	{PDC_FWVER_TO_INT(2, 0, 1), PDC_FWVER_TO_INT(0, 21, 1), true},
+	{PDC_FWVER_TO_INT(2, 0, 1), PDC_FWVER_TO_INT(1, 21, 1), true},
+	{PDC_FWVER_TO_INT(2, 0, 1), PDC_FWVER_TO_INT(1, 22, 1), true},
+	{PDC_FWVER_TO_INT(2, 0, 1), PDC_FWVER_TO_INT(1, 23, 1), true},
+	/* No upgrade to base FW (2.x) in DC */
+	{PDC_FWVER_TO_INT(2, 0, 1), PDC_FWVER_TO_INT(2, 0, 1), false},
+	{PDC_FWVER_TO_INT(2, 0, 1), PDC_FWVER_TO_INT(2, 1, 1), false},
+	{PDC_FWVER_TO_INT(2, 0, 1), PDC_FWVER_TO_INT(2, 2, 1), false},
+
+	/* From == 2.1 */
+	{PDC_FWVER_TO_INT(2, 1, 1), PDC_FWVER_TO_INT(0, 19, 1), true},
+	{PDC_FWVER_TO_INT(2, 1, 1), PDC_FWVER_TO_INT(0, 20, 1), true},
+	{PDC_FWVER_TO_INT(2, 1, 1), PDC_FWVER_TO_INT(0, 21, 1), false},
+	{PDC_FWVER_TO_INT(2, 1, 1), PDC_FWVER_TO_INT(1, 21, 1), true},
+	{PDC_FWVER_TO_INT(2, 1, 1), PDC_FWVER_TO_INT(1, 22, 1), true},
+	{PDC_FWVER_TO_INT(2, 1, 1), PDC_FWVER_TO_INT(1, 23, 1), false},
+	/* No upgrade to base FW (2.x) in DC */
+	{PDC_FWVER_TO_INT(2, 1, 1), PDC_FWVER_TO_INT(2, 0, 1), false},
+	{PDC_FWVER_TO_INT(2, 1, 1), PDC_FWVER_TO_INT(2, 1, 1), false},
+	{PDC_FWVER_TO_INT(2, 1, 1), PDC_FWVER_TO_INT(2, 2, 1), false},
+
+	/* From == 2.2 */
+	{PDC_FWVER_TO_INT(2, 2, 1), PDC_FWVER_TO_INT(0, 19, 1), true},
+	{PDC_FWVER_TO_INT(2, 2, 1), PDC_FWVER_TO_INT(0, 20, 1), true},
+	{PDC_FWVER_TO_INT(2, 2, 1), PDC_FWVER_TO_INT(0, 21, 1), true},
+	{PDC_FWVER_TO_INT(2, 2, 1), PDC_FWVER_TO_INT(1, 21, 1), true},
+	{PDC_FWVER_TO_INT(2, 2, 1), PDC_FWVER_TO_INT(1, 22, 1), true},
+	{PDC_FWVER_TO_INT(2, 2, 1), PDC_FWVER_TO_INT(1, 23, 1), true},
+	/* No upgrade to base FW (2.x) in DC */
+	{PDC_FWVER_TO_INT(2, 2, 1), PDC_FWVER_TO_INT(2, 0, 1), false},
+	{PDC_FWVER_TO_INT(2, 2, 1), PDC_FWVER_TO_INT(2, 1, 1), false},
+	{PDC_FWVER_TO_INT(2, 2, 1), PDC_FWVER_TO_INT(2, 2, 1), false},
+};
+
+static void test_rtk_upgrade_compatibility(void **state)
+{
+	for (int i = 0; i < ARRAY_SIZE(upgrade_test_cases); i++) {
+		pdc_fw_ver_t curr = upgrade_test_cases[i].current;
+		pdc_fw_ver_t new = upgrade_test_cases[i].new;
+		bool expected = upgrade_test_cases[i].success;
+		bool actual = rts545x_check_update_compatibility(curr, new);
+
+		if (actual != expected) {
+			fail_msg("Upgrade from %u.%u.%u to %u.%u.%u should %s but %s",
+				 PDC_FWVER_MAJOR(curr), PDC_FWVER_MINOR(curr),
+				 PDC_FWVER_PATCH(curr), PDC_FWVER_MAJOR(new),
+				 PDC_FWVER_MINOR(new), PDC_FWVER_PATCH(new),
+				 expected ? "succeed" : "fail",
+				 actual ? "succeeded" : "failed");
+		}
+	}
+}
+
+int main(int argc, char **argv)
+{
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(test_rtk_upgrade_compatibility),
+	};
+
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}
