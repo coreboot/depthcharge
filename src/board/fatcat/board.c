11b722d5f1a53c4b05a997017fb966fdd14374c8
Anil Kumar <anil.kumar.k@intel.corp-partner.google.com>
2024-07-15 21:03:53 -0700

    board/fatcat: Add support for Pantherlake
    BUG=b:347669091
    TEST=Verified with fatcat mainboard on pantherlake reference board
    
    Change-Id: Ia8549c23b8badd8082c3dee1c4715164a690b8fc
    Signed-off-by: Anil Kumar <anil.kumar.k@intel.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5711302
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    Tested-by: Subrata Banik <subratabanik@chromium.org>

--- src/board/fatcat/board.c
+++ src/board/fatcat/board.c
@@ -17,9 +17,9 @@
 #include "drivers/flash/memmapped.h"
 #include "drivers/gpio/sysinfo.h"
 #include "drivers/gpio/gpio.h"
-#include "drivers/gpio/meteorlake.h"
+#include "drivers/gpio/pantherlake.h"
 #include "drivers/power/pch.h"
-#include "drivers/soc/meteorlake.h"
+#include "drivers/soc/pantherlake.h"
 #include "drivers/storage/storage_common.h"
 #include "drivers/tpm/google/i2c.h"
 #include "drivers/tpm/google/switches.h"
@@ -35,7 +35,6 @@ __weak const struct storage_config *variant_get_storage_configs(size_t *count)
 {
 	static const struct storage_config storage_configs[] = {
 		{ .media = STORAGE_NVME, .pci_dev = PCI_DEV_PCIE9 },
-		{ .media = STORAGE_SDHCI, .pci_dev = PCI_DEV_PCIE7 },
 	};
 
 	*count = ARRAY_SIZE(storage_configs);
@@ -56,8 +55,17 @@ static GpioOps *mkbp_int_ops(void)
 
 static void ec_setup(void)
 {
-	CrosEcLpcBus *cros_ec_lpc_bus =
-		new_cros_ec_lpc_bus(CROS_EC_LPC_BUS_GENERIC);
+	/* Chrome EC (eSPI) */
+	if (!CONFIG(DRIVER_EC_CROS_LPC))
+		return;
+
+	CrosEcLpcBus *cros_ec_lpc_bus;
+	CrosEcLpcBusVariant variant = CROS_EC_LPC_BUS_GENERIC;
+
+	if (CONFIG(CROS_EC_ENABLE_MEC))
+		variant = CROS_EC_LPC_BUS_MEC;
+
+	cros_ec_lpc_bus = new_cros_ec_lpc_bus(variant);
 	CrosEc *cros_ec = new_cros_ec(&cros_ec_lpc_bus->ops, mkbp_int_ops());
 	register_vboot_ec(&cros_ec->vboot);
 }
@@ -65,7 +73,7 @@ static void ec_setup(void)
 __weak const struct tpm_config *variant_get_tpm_config(void)
 {
 	static const struct tpm_config config = {
-		.pci_dev = PCI_DEV_I2C4,
+		.pci_dev = PCI_DEV_I2C3,
 	};
 
 	return &config;
@@ -73,7 +81,7 @@ __weak const struct tpm_config *variant_get_tpm_config(void)
 
 static int gsc_irq_status(void)
 {
-	return meteorlake_get_gpe(GPE0_DW1_03); /* GPP_E03 */
+	return pantherlake_get_gpe(GPE0_DW1_15); /* GPP_D15 */
 }
 
 static void tpm_setup(void)
@@ -81,7 +89,7 @@ static void tpm_setup(void)
 	const struct tpm_config *config = variant_get_tpm_config();
 	DesignwareI2c *i2c = new_pci_designware_i2c(config->pci_dev,
 						    I2C_FS_HZ,
-						    METEORLAKE_DW_I2C_MHZ);
+						    PANTHERLAKE_DW_I2C_MHZ);
 	GscI2c *tpm = new_gsc_i2c(&i2c->ops, GSC_I2C_ADDR,
 				    &gsc_irq_status);
 	tpm_set_ops(&tpm->base.ops);
@@ -103,7 +111,7 @@ static int board_setup(void)
 	sysinfo_install_flags(NULL);
 	ec_setup();
 	tpm_setup();
-	power_set_ops(&meteorlake_power_ops);
+	power_set_ops(&pantherlake_power_ops);
 	flash_set_ops(&new_mmap_flash()->ops);
 	common_audio_setup(variant_probe_audio_config());
 
--- src/board/fatcat/fatcat.c
+++ src/board/fatcat/fatcat.c
@@ -1,5 +1,17 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include <libpayload.h>
+#include "drivers/soc/pantherlake.h"
+#include "drivers/storage/storage_common.h"
 
 /* TODO: Implement override for `variant_probe_audio_config` */
+/* TODO: b/357011633 - add FW_CONFIG for storage selection */
+static const struct storage_config storage_configs[] = {
+        { .media = STORAGE_NVME, .pci_dev = PCI_DEV_PCIE5 },
+        { .media = STORAGE_NVME, .pci_dev = PCI_DEV_PCIE1 },
+};
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+        *count = ARRAY_SIZE(storage_configs);
+        return storage_configs;
+}
