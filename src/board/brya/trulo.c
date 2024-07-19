bc571183d217635decabe771ae69770584655175
Subrata Banik <subratabanik@google.com>
2024-07-19 14:06:16 +0530

    board/brya: Add minimal code to boot trulo
    This patch adds audio, GSC support for Trulo.
    
    Additionally, configured IRQ pins:
    - Use GPP_A17 as GSC irq.
    - Use GPD2 as EC irq.
    
    BUG=b:351976770
    TEST=Builds successfully for google/trulo.
    
    (cherry picked from commit 87084fe59992502583dda9d231f5bbf546780513)
    
    Change-Id: I53586d76fc6ba20db533eff1d243a758c3dc4efe
    Original-Signed-off-by: Subrata Banik <subratabanik@google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5724668
    Original-Auto-Submit: Subrata Banik <subratabanik@chromium.org>
    Original-Tested-by: Subrata Banik <subratabanik@chromium.org>
    Original-Reviewed-by: Dinesh Gehlot <digehlot@google.com>
    Original-Commit-Queue: Subrata Banik <subratabanik@chromium.org>
    GitOrigin-RevId: 87084fe59992502583dda9d231f5bbf546780513
    Cr-Build-Id: 8740088176436538961
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8740088176436538961
    Copybot-Job-Name: android-main-depthcharge-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5774504
    Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Tested-by: Jonathon Murphy <jpmurphy@google.com>
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>

--- src/board/brya/trulo.c
+++ src/board/brya/trulo.c
@@ -7,9 +7,42 @@
 #include "base/fw_config.h"
 #include "board/brya/include/variant.h"
 #include "drivers/gpio/alderlake.h"
+#include "drivers/soc/alderlake.h"
 
+#define EC_PCH_INT_ODL		GPD2
+
+#define PCI_DEV_I2C0		PCI_DEV(0, 0x15, 0)
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
+
+	config = (struct audio_config){
+		.bus = {
+			.type = AUDIO_HDA,
+		},
+		.codec = {
+			.type = AUDIO_ALC256,
+		},
+	};
+
 	return &config;
+}
+
+int cr50_irq_status(void)
+{
+	return alderlake_get_gpe(GPE0_DW0_17); /* GPP_A17 */
+}
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static const struct tpm_config config = {
+		.pci_dev = PCI_DEV_I2C0,
+	};
+
+	return &config;
+}
+
+const int variant_get_ec_int(void)
+{
+	return EC_PCH_INT_ODL;
 }
