26e8ad23525b804c16e7c70cc56ce9aa2936106d
Subrata Banik <subratabanik@google.com>
2024-07-19 14:15:35 +0530

    board/brya/trulo: Add storage support
    1. Add STORAGE_UNKNOWN fw_config to enable all storage devices,
    this is used for the first boot in factory.
    
    2. Add STORAGE_EMMC fw_config to boot from eMMC.
    
    3. Add STORAGE_UFS fw_config to boot from UFS.
    
    BUG=b:351976770
    TEST=Builds successfully for google/trulo.
    
    Disallow-Recycled-Builds: all
    Change-Id: Ib28582995ba1c57af938795c7705ae1f3a65ba01
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5724551
    Tested-by: Subrata Banik <subratabanik@chromium.org>
    Auto-Submit: Subrata Banik <subratabanik@chromium.org>
    Reviewed-by: Dinesh Gehlot <digehlot@google.com>
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>

--- src/board/brya/trulo.c
+++ src/board/brya/trulo.c
@@ -8,6 +8,11 @@
 #include "board/brya/include/variant.h"
 #include "drivers/gpio/alderlake.h"
 #include "drivers/soc/alderlake.h"
+#include "drivers/storage/sdhci.h"
+#include "drivers/storage/storage_common.h"
+
+#define EMMC_CLOCK_MIN		400000
+#define EMMC_CLOCK_MAX		200000000
 
 #define EC_PCH_INT_ODL		GPD2
 
@@ -31,6 +36,45 @@ const struct audio_config *variant_probe_audio_config(void)
 int cr50_irq_status(void)
 {
 	return alderlake_get_gpe(GPE0_DW0_17); /* GPP_A17 */
+}
+
+static const struct storage_config storage_configs[] = {
+	{
+		.media = STORAGE_EMMC,
+		.pci_dev = PCH_DEV_EMMC,
+		.emmc = {
+			.platform_flags = SDHCI_PLATFORM_SUPPORTS_HS400ES,
+			.clock_min = EMMC_CLOCK_MIN,
+			.clock_max = EMMC_CLOCK_MAX
+			},
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_EMMC)
+	},
+	{
+		.media = STORAGE_UFS,
+		.pci_dev = PCH_DEV_UFS1,
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_UFS)
+	},
+	{
+		.media = STORAGE_EMMC,
+		.pci_dev = PCH_DEV_EMMC,
+		.emmc = {
+				.platform_flags = SDHCI_PLATFORM_SUPPORTS_HS400ES,
+				.clock_min = EMMC_CLOCK_MIN,
+				.clock_max = EMMC_CLOCK_MAX
+		},
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_UNKNOWN)
+	},
+	{
+		.media = STORAGE_UFS,
+		.pci_dev = PCH_DEV_UFS1,
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_UNKNOWN)
+	},
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
 }
 
 const struct tpm_config *variant_get_tpm_config(void)
