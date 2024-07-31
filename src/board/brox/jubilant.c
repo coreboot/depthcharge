b5c066f61129cd231ffb45cd8147af9f01b64207
Ren Kuo <ren.kuo@quanta.corp-partner.google.com>
2024-07-31 20:33:00 +0800

    Jubilant: remove retimer settigns
    Remove undefined fw_config retimer settings and
    update jubilant's codec setting.
    
    BUG=b:348543712
    BRANCH=None
    TEST=FW_NAME=jubilant emerge-brox depthcharge
    
    (cherry picked from commit 67c5d94d0fe95c500e4628ef2cae732c112027e7)
    
    Change-Id: I9ccad3d027294d218c59ce535f05600cc81b2f2d
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5749772
    Original-Reviewed-by: Bob Moragues <moragues@chromium.org>
    Original-Tested-by: Ren Kuo <ren.kuo@quanta.corp-partner.google.com>
    Original-Commit-Queue: Bob Moragues <moragues@chromium.org>
    Original-Reviewed-by: YH Lin <yueherngl@chromium.org>
    Original-Reviewed-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    GitOrigin-RevId: 67c5d94d0fe95c500e4628ef2cae732c112027e7
    Cr-Build-Id: 8740088176436538961
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8740088176436538961
    Copybot-Job-Name: android-main-depthcharge-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5774514
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Tested-by: Jonathon Murphy <jpmurphy@google.com>

--- src/board/brox/jubilant.c
+++ src/board/brox/jubilant.c
@@ -6,7 +6,6 @@
 
 #include "base/fw_config.h"
 #include "board/brox/include/variant.h"
-#include "drivers/ec/rts5453/rts5453.h"
 #include "drivers/gpio/alderlake.h"
 #include "drivers/soc/alderlake.h"
 #include "drivers/storage/storage_common.h"
@@ -22,22 +21,6 @@ const struct storage_config *variant_get_storage_configs(size_t *count)
 	return storage_configs;
 }
 
-/* Override of func in src/drivers/ec/rts5453/rts5453.c */
-void board_rts5453_get_image_paths(const char **image_path, const char **hash_path)
-{
-	if (fw_config_probe(FW_CONFIG(RETIMER, RETIMER_BYPASS))) {
-		*image_path = "rts5453_retimer_bypass.bin";
-		*hash_path = "rts5453_retimer_bypass.hash";
-	} else if (fw_config_probe(FW_CONFIG(RETIMER, RETIMER_JHL8040))) {
-		*image_path = "rts5453_retimer_jhl8040.bin";
-		*hash_path = "rts5453_retimer_jhl8040.hash";
-	} else {
-		/* Unknown configuration. Don't perform an update. */
-		*image_path = NULL;
-		*hash_path = NULL;
-	}
-}
-
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
@@ -47,7 +30,7 @@ const struct audio_config *variant_probe_audio_config(void)
 			.type = AUDIO_HDA,
 		},
 		.codec = {
-			.type = AUDIO_ALC256,
+			.type = AUDIO_RT1019,
 		},
 	};
 
