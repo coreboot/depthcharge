d9f95deb5c215bea6aa957fd67d567c8b120e6e9
Morris Hsu <morris-hsu@quanta.corp-partner.google.com>
2024-08-08 14:53:08 +0800

    jubilant: update audio settings
    Update audio settings for beep sound.
    
    BUG=b:358231086
    TEST=emerge-brox depthcharge
         Heard beep sound in depthcharge
    
    (cherry picked from commit c2bcd3ec93b775c7483e5f6bdd108338abf675ca)
    
    Change-Id: Ib2f036190707edf7507fe2b6d0a4571e5f35a83a
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5770176
    Original-Reviewed-by: Tyler Wang <tyler.wang@quanta.corp-partner.google.com>
    Original-Commit-Queue: Bob Moragues <moragues@chromium.org>
    Original-Reviewed-by: Bob Moragues <moragues@chromium.org>
    Original-Tested-by: Morris Hsu <morris-hsu@quanta.corp-partner.google.com>
    GitOrigin-RevId: c2bcd3ec93b775c7483e5f6bdd108338abf675ca
    Cr-Build-Id: 8739544595176752177
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8739544595176752177
    Copybot-Job-Name: android-main-depthcharge-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5787237
    Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>

--- src/board/brox/jubilant.c
+++ src/board/brox/jubilant.c
@@ -6,9 +6,13 @@
 
 #include "base/fw_config.h"
 #include "board/brox/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
 #include "drivers/gpio/alderlake.h"
 #include "drivers/soc/alderlake.h"
 #include "drivers/storage/storage_common.h"
+
+#define EN_SPK_PIN    GPP_F20
 
 static const struct storage_config storage_configs[] = {
 	{ .media = STORAGE_NVME, .pci_dev = SA_DEV_CPU_RP0 },
@@ -25,12 +29,19 @@ const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
 
-	config = (struct audio_config){
+	config = (struct audio_config) {
 		.bus = {
-			.type = AUDIO_HDA,
+			.type			= AUDIO_I2S,
+			.i2s.address		= SSP_I2S1_START_ADDRESS,
+			.i2s.enable_gpio	= { .pad = EN_SPK_PIN },
+			.i2s.settings		= &max98357a_settings,
+		},
+		.amp = {
+			.type			= AUDIO_GPIO_AMP,
+			.gpio.enable_gpio	= EN_SPK_PIN,
 		},
 		.codec = {
-			.type = AUDIO_RT1019,
+			.type			= AUDIO_RT1019,
 		},
 	};
 
