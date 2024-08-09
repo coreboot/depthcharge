0a912e6cba17f3c380eaae662806e3d16e98cced
Tristan Honscheid <honscheid@google.com>
2024-08-09 13:02:29 -0600

    drivers/ec: Add initial PD Sync boilerplate for TPS6699X PDC
    Add a new driver to support firmware update sequence for TI TPS6699X
    PDC chip.
    
    BUG=b:356672047
    BRANCH=None
    TEST=emerge-brox depthcharge, flash to DUT, chip is detected.
    
    (cherry picked from commit e5eb7753444181b7e0ac7ec8bfc788f76b756f1b)
    
    Change-Id: Ie5a3d0be94dfbcd4ca348f97b69efa10df7bf809
    Original-Signed-off-by: Tristan Honscheid <honscheid@google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5555653
    Original-Reviewed-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    GitOrigin-RevId: e5eb7753444181b7e0ac7ec8bfc788f76b756f1b
    Cr-Build-Id: 8735377134521380449
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8735377134521380449
    Copybot-Job-Name: android-main-depthcharge-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5886604
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-by: Grzegorz Bernacki <bernacki@google.com>
    Commit-Queue: Yu-Ping Wu <yupingso@chromium.org>
    Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>

---
+++ src/drivers/ec/tps6699x/tps6699x.h
@@ -0,0 +1,53 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright 2024 Google LLC.  */
+
+#ifndef __DRIVERS_EC_TPS6699X_H_
+#define __DRIVERS_EC_TPS6699X_H_
+
+#include "drivers/bus/i2c/cros_ec_tunnel.h"
+#include "drivers/ec/common/pdc_utils.h"
+#include "drivers/ec/vboot_auxfw.h"
+#include "vb2_api.h"
+
+#if (TPS6699X_DEBUG > 0)
+#define debug(msg, args...) printf("%s: " msg, __func__, ##args)
+#else
+#define debug(msg, args...)
+#endif
+
+typedef struct Tps6699x {
+	/** This driver's API implementation, passed to Vboot */
+	VbootAuxfwOps fw_ops;
+	/** EC I2C tunnel used to access this chip */
+	CrosECTunnelI2c *bus;
+	/** these are cached from chip regs */
+	struct {
+		/** PDC's USB Vendor ID */
+		uint16_t vid;
+		/** PDC's USB Product ID */
+		uint16_t pid;
+		/** Reported FW version */
+		pdc_fw_ver_t fw_version;
+		/** I2C target address of this chip, use with Tps6699x::bus */
+		uint8_t i2c_addr;
+	} chip_info;
+	/** PD chip index as used by the EC */
+	int ec_pd_id;
+	/** Internally-used chip name for logging */
+	char chip_name[16];
+	/** I2C bus speed before FWUP to restore to original settings */
+	uint16_t saved_i2c_speed_khz;
+	/** Pointer to memory-mapped firmware binary file. The file is opened
+	 *  handled by vboot and the pointer and size are provided through
+	 *  VbootAuxfwOps::update_image().
+	 */
+	/** Pointer to memory-mapped firmware binary and its length */
+	struct {
+		const uint8_t *image;
+		size_t size;
+	} fw_image;
+	/** Set true once the update completed */
+	bool has_updated;
+} Tps6699x;
+
+#endif /* __DRIVERS_EC_TPS6699X_H_ */
