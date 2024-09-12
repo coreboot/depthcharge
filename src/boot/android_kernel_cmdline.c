2441efaa92582caf19116b661ba44572f6c89993
Karthikeyan Ramasubramanian <kramasub@google.com>
2024-09-12 23:04:11 +0000

    boot: Append android.boottime to cmdline
    This change eventually propogates the firmware boottime to Android
    system properties.
    
    BUG=b:313627346
    BRANCH=None
    TEST=Build Kano BIOS image and boot to OS. Ensure that the firmware
    boottime is exported to Android system properties.
    cbmem -t
    Total Time: 1,810,989
    brya:/ # getprop ro.boot.boottime
    firmware:1783976000
    
    (cherry picked from commit bfaef5a7fdcac2763bc65893969e185104f967e5)
    
    Change-Id: Id8bc3597cc07682de0805de6799fa50efc6a6014
    Original-Signed-off-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5860313
    Original-Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    Original-Commit-Queue: Jonathon Murphy <jpmurphy@google.com>
    GitOrigin-RevId: bfaef5a7fdcac2763bc65893969e185104f967e5
    Cr-Build-Id: 8735014748122464833
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8735014748122464833
    Copybot-Job-Name: android-main-fw-nissa-depthcharge-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5908846
    Reviewed-by: Łukasz Majczak <lmajczak@google.com>
    Commit-Queue: Łukasz Majczak <lmajczak@google.com>
    Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>

--- src/boot/android_kernel_cmdline.c
+++ src/boot/android_kernel_cmdline.c
@@ -4,6 +4,7 @@
 #include <libpayload.h>
 
 #include "base/init_funcs.h"
+#include "base/timestamp.h"
 #include "base/vpd_util.h"
 #include "boot/android_kernel_cmdline.h"
 #include "boot/commandline.h"
@@ -12,6 +13,11 @@
 #define SERIAL_NUM_PROPERTY_NAME "androidboot.serialno="
 #define SERIAL_NUM_PROPERTY_NAME_LEN strlen(SERIAL_NUM_PROPERTY_NAME)
 #define MAX_SERIAL_NUM_LENGTH CB_MAX_SERIALNO_LENGTH
+
+#define BOOTTIME_PROPERTY_NAME "androidboot.boottime=firmware:"
+#define BOOTTIME_PROPERTY_NAME_LEN strlen(BOOTTIME_PROPERTY_NAME)
+/* 20 characters are sufficient for max uint64 18446744073709551615 */
+#define MAX_BOOTTIME_LENGTH 32
 
 static void append_serial_num_to_android_kernel_cmdline(void)
 {
@@ -26,6 +32,17 @@ static void append_serial_num_to_android_kernel_cmdline(void)
 	commandline_append(key_val);
 }
 
+static void append_boot_time_to_android_kernel_cmdline(void)
+{
+	/* Need to record boot time in ns as per
+	   https://android.googlesource.com/platform/system/core/+/HEAD/init/README.md#boot-timing */
+	uint64_t boot_time_ns = get_us_since_pre_cpu_reset() * NSECS_PER_USEC;
+	char key_val[BOOTTIME_PROPERTY_NAME_LEN + MAX_BOOTTIME_LENGTH];
+
+	snprintf(key_val, sizeof(key_val), "%s%lld", BOOTTIME_PROPERTY_NAME, boot_time_ns);
+	commandline_append(key_val);
+}
+
 void android_kernel_cmdline_fixup(struct vb2_kernel_params *kparams)
 {
 	/* Fixup kernel cmdline only for Android GKI images */
@@ -33,4 +50,5 @@ void android_kernel_cmdline_fixup(struct vb2_kernel_params *kparams)
 		return;
 
 	append_serial_num_to_android_kernel_cmdline();
+	append_boot_time_to_android_kernel_cmdline();
 }
