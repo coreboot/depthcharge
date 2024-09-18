12f00ad00e6dc39b8227567c31c332f443ccc164
Karthikeyan Ramasubramanian <kramasub@google.com>
2024-09-18 22:11:25 +0000

    base/timestamp: Get boot time including pre-cpu reset
    get_us_since_boot() gets the time elapsed since the base_timestamp i.e.
    time since the 1st timestamp as recorded by cpu. This does not include
    the pre-cpu reset time. Add a new helper to get the time elapsed since
    pre-cpu reset.
    
    BUG=b:313627346
    BRANCH=None
    TEST=Build Kano BIOS firmware and boot to OS. Ensure that the measured
    boot time includes the pre-cpu reset time and matches with the output of
    cbmem timestamp.
    
    Change-Id: I5fdf5a386417c2b03fea09ee7a6f9c01fa04b018
    Original-Signed-off-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5873763
    Original-Commit-Queue: Jonathon Murphy <jpmurphy@google.com>
    Original-Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Original-Reviewed-by: Tim Van Patten <timvp@google.com>
    GitOrigin-RevId: f1e86d93468054b1ed89acb31721c306ae864382
    Cr-Build-Id: 8735830119939728337
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8735830119939728337
    Copybot-Job-Name: android-main-fw-nissa-depthcharge-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5889174
    Commit-Queue: Łukasz Majczak <lmajczak@google.com>
    Reviewed-by: Patryk Busse <pbusse@google.com>
    Reviewed-by: Kornel Dulęba <korneld@google.com>
    Tested-by: Łukasz Majczak <lmajczak@google.com>

--- src/base/timestamp.h
+++ src/base/timestamp.h
@@ -43,5 +43,6 @@ void timestamp_add(enum timestamp_id id, uint64_t ts_time);
 void timestamp_add_now(enum timestamp_id id);
 void timestamp_mix_in_randomness(u8 *buffer, size_t size);
 uint64_t get_us_since_boot(void);
+uint64_t get_us_since_pre_cpu_reset(void);
 
 #endif /* __BASE_TIMESTAMP_H__ */
