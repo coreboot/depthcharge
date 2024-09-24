d07c3688e07b7346da20a2268062104c842495a8
Karthikeyan Ramasubramanian <kramasub@google.com>
2024-09-24 16:10:07 +0000

    Android Bringup: See http://go/android-fw-sync  boot: Move android_kernel_cmdline to android_bootconfig_params
    This will reduce polluting the kernel commandline parameters and
    leverage bootconfig to add any current and future boot parameters.
    
    PORT NOTE: Changed vb2_kernel_parms in initial cherry-pick to
    VbSelectAndLoadKernelParams given we don't know about vb2_kernel_parms
    in this firmware branch.
    
    BUG=None
    BRANCH=firmware-android-brya-14505.782.B
    TEST=Build Kano BIOS image and boot to OS. Ensure that the migrated
    parameters are visible in Android system properties and got removed from
    kernel commandline.
    int appenbrya:/ # getprop ro.boot.boottime
    firmware:1860575000
    brya:/ # getprop ro.boot.serialno
    N9K7SWW0012280119D7600
    brya:/ # cat /proc/cmdline | grep boottime
    1|brya:/ # cat /proc/cmdline | grep serialno
    
    Change-Id: I8a6b11b0f864167e61234eba7ada6d76578cc57b
    Original-Signed-off-by: Karthikeyan Ramasubramanian <kramasub@google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5885018
    Original-Reviewed-by: Tim Van Patten <timvp@google.com>
    Original-Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>
    GitOrigin-RevId: 8ee8ccd5f730665e75048fc6e960656b63daeb95
    Cr-Build-Id: 8734018180574106017
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8734018180574106017
    Copybot-Job-Name: android-main-fw-brya-depthcharge-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/5923443
    Tested-by: Nick Vaccaro <nvaccaro@google.com>
    Reviewed-by: Eran Mitrani <mitrani@google.com>
    Commit-Queue: Nick Vaccaro <nvaccaro@google.com>

---
+++ src/boot/android_bootconfig_params.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef _ANDROID_BOOTCONFIG_PARAMS_H_
+#define _ANDROID_BOOTCONFIG_PARAMS_H_
+
+#include <vboot_api.h>
+
+/*
+ * Append androidboot bootconfig parameters to bootconfig section.
+ *
+ * @param kparams - pointer to vboot kernel parameter structure that is used as
+ *                  a basis to access current and total size of bootconfig section
+ * @param bootc_start - pointer to the bootconfig section
+ *
+ * Return: Return 0 on success, -1 in case of errors
+ */
+int append_android_bootconfig_params(struct VbSelectAndLoadKernelParams *kparams,
+				     void *bootc_start);
+
+#endif /* _ANDROID_BOOTCONFIG_PARAMS_H_ */
