1d95f811db1e62e88b7071e52437ee1cc1500586
Pranava Y N <pranavayn@google.com>
2024-11-07 16:53:38 +0530

    fatcat: Add storage device selection based on fw_config
    This patch enables storage device on NVME Gen5 slot and selects storage
    devices based on the fw_config.
    
    1. STORAGE_UNKNOWN fw_config enables all storage devices.
    2. STORAGE_NVME_GEN4 fw_config to boot from NVME Gen4 slot.
    3. STORAGE_NVME_GEN5 fw_config to boot from NVME Gen5 slot.
    
    BUG=b:357011633,b:377654686
    TEST=Able to boot fatcat from both Gen4 and Gen5 slots.
    
    Change-Id: I88a12d5af4d0b8aa3e5b847e8af24b0b1f390b02
    Signed-off-by: Pranava Y N <pranavayn@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6000898
    Tested-by: Subrata Banik <subratabanik@chromium.org>
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>

--- src/board/fatcat/fatcat.c
+++ src/board/fatcat/fatcat.c
@@ -1,15 +1,45 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include <libpayload.h>
+
+#include "base/fw_config.h"
 #include "drivers/soc/pantherlake.h"
 #include "drivers/storage/storage_common.h"
 
 /* TODO: Implement override for `variant_probe_audio_config` */
-/* TODO: b/357011633 - add FW_CONFIG for storage selection */
 static const struct storage_config storage_configs[] = {
-        { .media = STORAGE_NVME, .pci_dev = PCI_DEV_PCIE5 },
-        { .media = STORAGE_NVME, .pci_dev = PCI_DEV_PCIE1 },
+	{
+		.media = STORAGE_NVME,
+		.pci_dev = PCI_DEV_PCIE5,
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_NVME_GEN4)
+	},
+	{
+		.media = STORAGE_NVME,
+		.pci_dev = PCI_DEV_PCIE1,
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_NVME_GEN4)
+	},
+	{
+		.media = STORAGE_NVME,
+		.pci_dev = PCI_DEV_PCIE9,
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_NVME_GEN5)
+	},
+	{
+		.media = STORAGE_NVME,
+		.pci_dev = PCI_DEV_PCIE5,
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_UNKNOWN)
+	},
+	{
+		.media = STORAGE_NVME,
+		.pci_dev = PCI_DEV_PCIE1,
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_UNKNOWN)
+	},
+	{
+		.media = STORAGE_NVME,
+		.pci_dev = PCI_DEV_PCIE9,
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_UNKNOWN)
+	},
 };
+
 const struct storage_config *variant_get_storage_configs(size_t *count)
 {
         *count = ARRAY_SIZE(storage_configs);
