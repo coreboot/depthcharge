bfc7a21307dc9d7946ed062c1ed5924271a1fdfe
Aamir Bohra <aamirbohra@google.com>
2024-11-07 15:19:19 +0000

    boot: Add HWID to bootconfig
    Allow hwid information to be passed as bootconfig. This will be used to
    set the ro.boot.product.hardware.id system property. The ro.boot.* props
    needs to be set via bootloader.
    
    BUG=b:364474483
    BRANCH=None
    TEST=Verify getprop ro.boot.product.hardware.id is set to device HWID.
    
    Change-Id: I9a178d3e16809283a2c0dcce2100f70ce574b22a
    Signed-off-by: Aamir Bohra <aamirbohra@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6002457
    Reviewed-by: Tomasz Michalec <tmichalec@google.com>
    Commit-Queue: Grzegorz Bernacki <bernacki@google.com>
    Reviewed-by: Grzegorz Bernacki <bernacki@google.com>
    Reviewed-by: Jan Dąbroś <dabros@google.com>

--- src/boot/android_bootconfig_params.c
+++ src/boot/android_bootconfig_params.c
@@ -2,6 +2,8 @@
 
 #include <libpayload.h>
 #include <vb2_android_bootimg.h>
+#include <vb2_api.h>
+#include <lp_vboot.h>
 
 #include "base/init_funcs.h"
 #include "base/timestamp.h"
@@ -20,6 +22,21 @@
 
 #define DISPLAY_ORIENTATION_KEY_STR "androidboot.surface_flinger.primary_display_orientation"
 #define MAX_DISPLAY_ORIENTATION_LENGTH sizeof("ORIENTATION_xxx")
+
+#define HWID_KEY_STR "androidboot.product.hardware.id"
+
+static int append_hwid(void *bootc_start, size_t bootc_size, size_t buf_size)
+{
+	char hwid[VB2_GBB_HWID_MAX_SIZE];
+	uint32_t hwid_size = sizeof(hwid);
+
+	if (vb2api_gbb_read_hwid(vboot_get_context(), hwid, &hwid_size)) {
+		printf("No HWID in GBB\n");
+		return -1;
+	}
+	return append_bootconfig_params(HWID_KEY_STR, hwid,
+					bootc_start, bootc_size, buf_size);
+}
 
 static int append_serial_num(void *bootc_start, size_t bootc_size, size_t buf_size)
 {
@@ -69,6 +86,7 @@ enum bootconfig_param_index {
 	SERIAL_NUM,
 	BOOTTIME,
 	DISPLAY_ORIENTATION,
+	HWID,
 };
 
 static struct {
@@ -89,6 +107,11 @@ static struct {
 	[DISPLAY_ORIENTATION] = {
 		.name = DISPLAY_ORIENTATION_KEY_STR,
 		.append = append_display_orientation,
+		.exists = false,
+	},
+	[HWID] = {
+		.name = HWID_KEY_STR,
+		.append = append_hwid,
 		.exists = false,
 	},
 };
