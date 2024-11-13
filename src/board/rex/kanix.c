0cab0c484667671de68e53f72a7bb7fb664eac6b
Tyler Wang <tyler.wang@quanta.corp-partner.google.com>
2024-11-13 09:46:15 +0800

    kanix: Enable support for NVME Android Bringup: See http://go/android-fw-sync
    BRANCH=none
    BUG=b:368501705
    TEST=emerge-rex depthcharge pass
    
    (cherry picked from commit a332a4110dcdd8983dac28a11ed8093a018bee51)
    
    Change-Id: I7526f2ede6bcc076b75ffbe435206edc3c726847
    Original-Signed-off-by: Tyler Wang <tyler.wang@quanta.corp-partner.google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6011828
    Original-Reviewed-by: Kapil Porwal <kapilporwal@chromium.org>
    Original-Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    GitOrigin-RevId: a332a4110dcdd8983dac28a11ed8093a018bee51
    Cr-Build-Id: 8731376278489575233
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8731376278489575233
    Copybot-Job-Name: android-main-depthcharge-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6018843
    Reviewed-by: Jonathon Murphy <jpmurphy@google.com>
    Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Commit-Queue: Jonathon Murphy <jpmurphy@google.com>

--- src/board/rex/kanix.c
+++ src/board/rex/kanix.c
@@ -7,9 +7,21 @@
 #include "base/fw_config.h"
 #include "board/rex/include/variant.h"
 #include "drivers/gpio/meteorlake.h"
+#include "drivers/soc/meteorlake.h"
+#include "drivers/storage/storage_common.h"
 
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
 	return &config;
+}
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	static const struct storage_config storage_configs[] = {
+		{ .media = STORAGE_NVME, .pci_dev = PCI_DEV_PCIE10 },
+	};
+
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
 }
