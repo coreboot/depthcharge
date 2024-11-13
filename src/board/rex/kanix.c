70aa79165e69ff5d18aba59a690795c95aed6ab6
Tyler Wang <tyler.wang@quanta.corp-partner.google.com>
2024-11-13 10:15:16 +0800

    Kanix: Update audio settings Android Bringup: See http://go/android-fw-sync
    BRANCH=none
    BUG=b:368501705
    TEST=emerge-rex depthcharge pass
    
    (cherry picked from commit 544b54a4eafb41e4f050b099376dda445ad9ea39)
    
    Change-Id: Id8b32e8ed9c5a78c228c733082302bf271e2e6a0
    Original-Signed-off-by: Tyler Wang <tyler.wang@quanta.corp-partner.google.com>
    Original-Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6016086
    Original-Reviewed-by: Kapil Porwal <kapilporwal@chromium.org>
    GitOrigin-RevId: 544b54a4eafb41e4f050b099376dda445ad9ea39
    Cr-Build-Id: 8731376278489575233
    Cr-Build-Url: https://cr-buildbucket.appspot.com/build/8731376278489575233
    Copybot-Job-Name: android-main-depthcharge-copybot-downstream
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6018844
    Tested-by: ChromeOS Prod (Robot) <chromeos-ci-prod@chromeos-bot.iam.gserviceaccount.com>
    Commit-Queue: Jonathon Murphy <jpmurphy@google.com>
    Reviewed-by: Jonathon Murphy <jpmurphy@google.com>

--- src/board/rex/kanix.c
+++ src/board/rex/kanix.c
@@ -6,13 +6,38 @@
 
 #include "base/fw_config.h"
 #include "board/rex/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
 #include "drivers/gpio/meteorlake.h"
 #include "drivers/soc/meteorlake.h"
 #include "drivers/storage/storage_common.h"
 
+#define EN_SPK_PIN	GPP_D4
+
+static void initialize_i2s_config(struct audio_config *config)
+{
+	*config = (struct audio_config) {
+		.bus = {
+			.type			= AUDIO_I2S,
+			.i2s.address		= SSP_I2S0_START_ADDRESS,
+			.i2s.enable_gpio	= { .pad = EN_SPK_PIN },
+			.i2s.settings		= &max98357a_settings,
+		},
+		.amp = {
+			.type			= AUDIO_GPIO_AMP,
+			.gpio.enable_gpio	= EN_SPK_PIN,
+		},
+		.codec = {
+			.type			= AUDIO_RT1019,
+		}
+	};
+}
+
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
+	memset(&config, 0, sizeof(config));
+	initialize_i2s_config(&config);
 	return &config;
 }
 
