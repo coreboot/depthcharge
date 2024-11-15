4c29d93763868c59d964721e2778a9b6c212365c
Ian Feng <ian_feng@compal.corp-partner.google.com>
2024-11-15 14:40:22 +0800

    francka: Add depthcharge variant for francka
    Create a depthcharge target francka by copying the template
    files from fatcat. Add storage support and TPM device.
    
    Device details:
    GSC I2C Controller/Bus = 1
    GSC GPE = GPE0_DW0_11/GPP_H11
    EC interrupt = GPP_B05
    
    BUG=b:376245884
    BRANCH=none
    TEST=emerge-fatcat depthcharge
    
    Change-Id: I0be05fa6133d3f3770866879666c3a5b54c2b7cf
    Signed-off-by: Ian Feng <ian_feng@compal.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6022382
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>

---
+++ src/board/fatcat/francka.c
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+
+#include "board/fatcat/include/variant.h"
+#include "drivers/gpio/pantherlake.h"
+#include "drivers/soc/pantherlake.h"
+#include "drivers/storage/storage_common.h"
+
+#define EC_PCH_INT_ODL		GPP_B05
+
+int gsc_irq_status(void)
+{
+	return pantherlake_get_gpe(GPE0_DW0_11); /* GPP_H11 */
+}
+
+static const struct storage_config storage_configs[] = {
+	{ .media = STORAGE_NVME, .pci_dev = PCI_DEV_PCIE9 },
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static const struct tpm_config config = {
+		.pci_dev = PCI_DEV_I2C1,
+	};
+
+	return &config;
+}
+
+const int variant_get_ec_int(void)
+{
+	return EC_PCH_INT_ODL;
+}
