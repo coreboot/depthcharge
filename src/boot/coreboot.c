a3653b50a9a5e6d093601df979f1e7a7bd2c6704
Chen-Yu Tsai <wenst@chromium.org>
2024-11-26 16:46:19 +0800

    coreboot: Insert #address-cells and #size-cells for firmware node
    Device nodes with children that contain address regions should have
    the "#address-cells" and "#size-cells" properties.
    
    The Linux Kernel recently enabled checking for these properties and
    emitting big warnings [1] if they are missing. A patch was posted to
    add the offending device into an exclusion list [2]. However the
    firmware should be fixed to do the right thing.
    
    [1] https://lore.kernel.org/all/Z0NUdoG17EwuCigT@sashalap/
    [2] https://lore.kernel.org/all/20241125113151.107812-1-krzysztof.kozlowski@linaro.org/
    
    BRANCH=none
    BUG=b:380991384
    TEST=Boot Linux and check /proc/device-tree/firmware has the two properties
    
    Change-Id: I7ca66da3fa73c679ab66092ad8d612c2542e0f6b
    Signed-off-by: Chen-Yu Tsai <wenst@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6051580
    Reviewed-by: Julius Werner <jwerner@chromium.org>

--- src/boot/coreboot.c
+++ src/boot/coreboot.c
@@ -31,6 +31,10 @@ static int install_coreboot_data(struct device_tree_fixup *fixup,
 	struct device_tree_node *firmware_node = dt_find_node(tree->root,
 		firmware_path, &addr_cells, &size_cells, 1);
 
+	// Need to add '#address-cells' and '#size-cells'
+	dt_add_u32_prop(firmware_node, "#address-cells", addr_cells);
+	dt_add_u32_prop(firmware_node, "#size-cells", size_cells);
+
 	// Need to add 'ranges' to the intermediate node to make 'reg' work.
 	dt_add_bin_prop(firmware_node, "ranges", NULL, 0);
 
