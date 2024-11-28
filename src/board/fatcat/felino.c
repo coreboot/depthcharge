ff92292688cd19c29e46644f472173db9208ff2f
Hualin Wei <weihualin@huaqin.corp-partner.google.com>
2024-11-28 13:47:58 +0800

    felino: Add depthcharge variant for felino
    Create a depthcharge target francka by copying the template
    files from francka. Add audio config, TPM device and TI PDC
    FW update support for felino.
    
    BUG=b:379797598
    TEST=emerge-fatcat depthcharge
    
    Change-Id: I66cf7c1693b0af8d396ad9b4c570ed13aa146b2b
    Signed-off-by: Hualin Wei <weihualin@huaqin.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6056670
    Reviewed-by: Weimin Wu <wuweimin@huaqin.corp-partner.google.com>
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    Commit-Queue: YH Lin <yueherngl@chromium.org>
    Reviewed-by: YH Lin <yueherngl@chromium.org>

---
+++ src/board/fatcat/felino.c
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+
+#include "board/fatcat/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
+#include "base/fw_config.h"
+#include "drivers/gpio/pantherlake.h"
+#include "drivers/soc/pantherlake.h"
+#include "drivers/storage/storage_common.h"
+
+#define EC_PCH_INT_ODL		GPP_E03
+
+const struct audio_config *variant_probe_audio_config(void)
+{
+	static struct audio_config config;
+
+	config = (struct audio_config){
+		.bus = {
+			.type			= AUDIO_I2S,
+			.i2s.address	= SSP_I2S1_START_ADDRESS,
+			.i2s.settings	= &max98357a_settings,
+		},
+		.amp = {
+			.type			= AUDIO_AMP_NONE,
+		},
+		.codec = {
+			.type			= AUDIO_RT5650,
+			.i2c[0].ctrlr		= PCI_DEV_I2C3,
+			.i2c[0].i2c_addr[0]	= 0x1a,
+		},
+	};
+
+	return &config;
+}
+
+int gsc_irq_status(void)
+{
+	return pantherlake_get_gpe(GPE0_DW0_11); /* GPP_H11 */
+}
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static const struct tpm_config config = {
+		.pci_dev = PCI_DEV_I2C1,
+	};
+
+	return &config;
+}
+
+const int variant_get_ec_int(void)
+{
+	return EC_PCH_INT_ODL;
+}
+
+/* Override of func in src/drivers/ec/tps6699x/tps6699x.c */
+void board_tps6699x_get_image_paths(const char **image_path, const char **hash_path)
+{
+	if (fw_config_probe(FW_CONFIG(PDC_CONTROL, PDC_TI))) {
+		/* Will update path for TI PDC AU later */
+		*image_path = "tps6699x.bin";
+		*hash_path = "tps6699x.hash";
+	} else {
+		/* Unknown configuration. Don't perform an update. */
+		*image_path = NULL;
+		*hash_path = NULL;
+	}
+}
