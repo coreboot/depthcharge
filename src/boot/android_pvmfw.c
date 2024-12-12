99cd2be815e64d97376a15eb17c998f7d9ec98bd
Bartłomiej Grzesik <bgrzesik@google.com>
2024-12-12 14:58:20 +0100

    pvmfw: Add reserved memory entry with GSC boot params
    This patch adds a new entry to the pvmfw configuration space with GSC
    boot parameters. The entry holds the reserved memory blobs with
    additional information. This information consists of name of the VM that
    the reserved memory blob will copied into and DTB compatible string that
    will be used to inform the VM about it's location in the guest memory.
    
    BUG=b:410735713
    TEST=pvmfw receives the correct data and is able to parse it
    BRANCH=main
    
    Change-Id: I0c1cb792d9c459a946cab7d661b19d0d7ee9e03e
    Signed-off-by: Bartłomiej Grzesik <bgrzesik@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6459286
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>

--- src/boot/android_pvmfw.c
+++ src/boot/android_pvmfw.c
@@ -8,7 +8,7 @@
 #include "boot/android_pvmfw.h"
 
 #define ANDROID_PVMFW_CFG_MAGIC "pvmf"
-#define ANDROID_PVMFW_CFG_V1_2 ((1 << 16) | (2 & 0xffff))
+#define ANDROID_PVMFW_CFG_V1_3 ((1 << 16) | (3 & 0xffff))
 #define ANDROID_PVMFW_CFG_BLOB_ALIGN 8
 
 struct pvmfw_config_entry {
@@ -16,7 +16,7 @@ struct pvmfw_config_entry {
 	uint32_t size;
 };
 
-struct pvmfw_config_v1_2 {
+struct pvmfw_config_v1_3 {
 	/* Header */
 	uint8_t magic[4];
 	uint32_t version;
@@ -31,10 +31,70 @@ struct pvmfw_config_v1_2 {
 	struct pvmfw_config_entry da_dtbo;
 	/* Entry 3: VM reference DTB */
 	struct pvmfw_config_entry ref_dtb;
+	/* Entry 4: VM reserved memory blobs */
+	struct pvmfw_config_entry reserved_mem;
 
 	/* Entries' blobs */
 	uint8_t blobs[];
 };
+
+/* pvmfw's entry 4: Reserved memory blob header */
+struct pvmfw_cfg_rmem_hdr {
+	uint32_t vm_name_offset;
+	uint32_t blob_offset;
+	uint32_t blob_size;
+	uint32_t compat_offset;
+	uint32_t flags;
+};
+
+#define PVMFW_CFG_RESERVED_MEM_NO_MAP 0x1
+
+#define GSC_EARLY_ENTROPY_SIZE 64
+#define GSC_SESSION_KEY_SEED_SIZE 32
+#define GSC_AUTH_TOKEN_SIZE 32
+
+/* The value of com.android.virt.name property in Trusty image's AVB vbmeta */
+static const char desktop_trusty_name[] = "desktop-trusty";
+/* Reserved memory blobs VM DTB compatible strings */
+static const char entropy_compat_str[] = "google,early-entropy";
+static const char session_compat_str[] = "google,session-key-seed";
+static const char auth_token_compat_str[] = "google,auth-token-key-seed";
+
+/* pvmfw's entry 4: Reserved memory blobs with headers */
+struct pvmfw_cfg_rmem {
+	/* The number of reserved blobs in the entry */
+	uint32_t count;
+
+#define PVMFW_CFG_RESERVED_MEM_BLOB_COUNT 3
+	/*
+	 * The headers for each reserved blob in the entry, all store the
+	 * offset and size of blob in the entry.
+	 */
+	struct {
+		struct pvmfw_cfg_rmem_hdr entropy;
+		struct pvmfw_cfg_rmem_hdr session;
+		struct pvmfw_cfg_rmem_hdr auth;
+	} hdrs;
+
+	/*
+	 * The payload of reserved blobs entry. The offsets and sizes of
+	 * the fields are stored in pvmfw_cfg_rmem_hdr headers above in runtime.
+	 */
+	struct reserved_blobs {
+		uint8_t early_entropy[GSC_EARLY_ENTROPY_SIZE];
+		uint8_t session_key_seed[GSC_SESSION_KEY_SEED_SIZE];
+		uint8_t auth_token_key_seed[GSC_AUTH_TOKEN_SIZE];
+
+		/*
+		 * It is required that string table is at the end of reserved
+		 * blobs entry.
+		 */
+		uint8_t desktop_trusty_name[sizeof(desktop_trusty_name)];
+		uint8_t entropy_compat[sizeof(entropy_compat_str)];
+		uint8_t session_compat[sizeof(session_compat_str)];
+		uint8_t auth_token_compat[sizeof(auth_token_compat_str)];
+	} blobs;
+} __attribute__((packed));
 
 /**
  * BootParam CBOR object flatten into raw C structure.
@@ -55,21 +115,21 @@ struct pvmfw_boot_params_cbor_v1 {
 		 * 0x40 - .size 64
 		 */
 		uint8_t cbor_labels_entropy[4];
-		uint8_t early_entropy[64];
+		uint8_t early_entropy[GSC_EARLY_ENTROPY_SIZE];
 		/*
 		 * 0x02 - uint = 2	SessionKeySeed label
 		 * 0x58 - bstr
 		 * 0x20 - .size 32
 		 */
 		uint8_t cbor_labels_session[3];
-		uint8_t session_key_seed[32];
+		uint8_t session_key_seed[GSC_SESSION_KEY_SEED_SIZE];
 		/*
 		 * 0x03 - uint = 3	AuthTokenKeySeed label
 		 * 0x58 - bstr
 		 * 0x20 - .size 32
 		 */
 		uint8_t cbor_labels_auth[3];
-		uint8_t auth_token_key_seed[32];
+		uint8_t auth_token_key_seed[GSC_AUTH_TOKEN_SIZE];
 	} boot_params;
 
 	/*
@@ -81,12 +141,69 @@ struct pvmfw_boot_params_cbor_v1 {
 	uint8_t android_dice_handover[];
 } __attribute__((packed));
 
+static void copy_reserved_mem(struct pvmfw_boot_params_cbor_v1 *v1,
+			      struct pvmfw_cfg_rmem *reserved)
+{
+	struct reserved_blobs *blobs = &reserved->blobs;
+
+	/* Clear the size of entry that will be used */
+	memset(reserved, 0, sizeof(struct pvmfw_cfg_rmem));
+
+	/* Set the count of the reserved memory entiries */
+	reserved->count = PVMFW_CFG_RESERVED_MEM_BLOB_COUNT;
+
+	/* Copy VM name to reserved memory blobs string table */
+	memcpy(blobs->desktop_trusty_name, desktop_trusty_name, sizeof(desktop_trusty_name));
+
+	/* Setup the reserved memory header for early entropy */
+	reserved->hdrs.entropy = (struct pvmfw_cfg_rmem_hdr){
+		.vm_name_offset = offsetof(struct reserved_blobs, desktop_trusty_name),
+		.blob_offset = offsetof(struct reserved_blobs, early_entropy),
+		.blob_size = GSC_EARLY_ENTROPY_SIZE,
+		.compat_offset = offsetof(struct reserved_blobs, entropy_compat),
+		.flags = PVMFW_CFG_RESERVED_MEM_NO_MAP,
+	};
+
+	/* Copy early entropy received from GSC to reserved memory blobs */
+	memcpy(blobs->early_entropy, v1->boot_params.early_entropy, GSC_EARLY_ENTROPY_SIZE);
+	memcpy(blobs->entropy_compat, entropy_compat_str, sizeof(entropy_compat_str));
+
+	/* Setup the reserved memory header for session key seed */
+	reserved->hdrs.session = (struct pvmfw_cfg_rmem_hdr){
+		.vm_name_offset = offsetof(struct reserved_blobs, desktop_trusty_name),
+		.blob_offset = offsetof(struct reserved_blobs, session_key_seed),
+		.blob_size = GSC_SESSION_KEY_SEED_SIZE,
+		.compat_offset = offsetof(struct reserved_blobs, session_compat),
+		.flags = PVMFW_CFG_RESERVED_MEM_NO_MAP,
+	};
+
+	/* Copy session key seed received from GSC to reserved memory blobs */
+	memcpy(blobs->session_key_seed, v1->boot_params.session_key_seed,
+	       GSC_SESSION_KEY_SEED_SIZE);
+	memcpy(blobs->session_compat, session_compat_str, sizeof(session_compat_str));
+
+	/* Setup the reserved memory header for auth token key seed */
+	reserved->hdrs.auth = (struct pvmfw_cfg_rmem_hdr){
+		.vm_name_offset = offsetof(struct reserved_blobs, desktop_trusty_name),
+		.blob_offset = offsetof(struct reserved_blobs, auth_token_key_seed),
+		.blob_size = GSC_AUTH_TOKEN_SIZE,
+		.compat_offset = offsetof(struct reserved_blobs, auth_token_compat),
+		.flags = PVMFW_CFG_RESERVED_MEM_NO_MAP,
+	};
+
+	/* Copy auth token key seed received from GSC to first reserved memory */
+	memcpy(blobs->auth_token_key_seed, v1->boot_params.auth_token_key_seed,
+	       GSC_AUTH_TOKEN_SIZE);
+	memcpy(blobs->auth_token_compat, auth_token_compat_str, sizeof(auth_token_compat_str));
+}
+
 /**
  * Parse BootParam CBOR object and extract relevant data.
  */
 static int parse_boot_params(const void *blob, size_t size, struct pvmfw_config_v1_3 *cfg,
 			     size_t max_blobs_size)
 {
+	size_t offset_blobs;
 	const char *field;
 	struct pvmfw_boot_params_cbor_v1 *v1 = (void *)blob;
 
@@ -143,7 +260,7 @@ static int parse_boot_params(const void *blob, size_t size, struct pvmfw_config_
 	 * The remaining bytes of BootParams structure are AndroidDiceHandover
 	 * bytes.
 	 *
-	 * Fill the entry 0 location. The remaining entries are
+	 * Fill the entry 0 and entry 4 location. The remaining entries are
 	 * not used so leave them as zeros.
 	 *
 	 * Place handover right after config header.
@@ -160,8 +277,26 @@ static int parse_boot_params(const void *blob, size_t size, struct pvmfw_config_
 	/* Copy handover data into place. */
 	memcpy(&cfg->blobs[0], v1->android_dice_handover, cfg->dice_handover.size);
 
+	/*
+	 * Align the entry offsets to pvmfw config alignment bytes to make
+	 * sure we won't run into unaligned memory access later.
+	 * The size of the entire entry contains both headers and blobs.
+	 */
+	offset_blobs = ALIGN_UP(cfg->dice_handover.size, ANDROID_PVMFW_CFG_BLOB_ALIGN);
+	cfg->reserved_mem.offset = offsetof(struct pvmfw_config_v1_3, blobs) + offset_blobs;
+	cfg->reserved_mem.size = sizeof(struct pvmfw_cfg_rmem);
+
+	/* Make sure that there is enough space for reserved memory in the buffer */
+	if (offset_blobs + cfg->reserved_mem.size > max_blobs_size) {
+		printf("Not enough space in the destination buffer for reserved memory.\n");
+		return -1;
+	}
+
+	/* Copy headers and blobs data into place. */
+	copy_reserved_mem(v1, (struct pvmfw_cfg_rmem *)&cfg->blobs[offset_blobs]);
+
 	/* Calculate the total size of the config with blobs, using the last used entry */
-	cfg->total_size = cfg->dice_handover.offset + cfg->dice_handover.size;
+	cfg->total_size = cfg->reserved_mem.offset + cfg->reserved_mem.size;
 
 	return 0;
 mismatch:
@@ -176,7 +311,7 @@ int setup_android_pvmfw(void *buffer, size_t buffer_size, size_t *pvmfw_size,
 {
 	int ret;
 	uintptr_t pvmfw_addr, pvmfw_max, cfg_addr;
-	struct pvmfw_config_v1_2 *cfg;
+	struct pvmfw_config_v1_3 *cfg;
 
 	/* Set the boundaries of the pvmfw buffer */
 	pvmfw_addr = (uintptr_t)buffer;
