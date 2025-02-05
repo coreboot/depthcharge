2befdb2b732e0e39b5c1be1b2caa6e894d3756a5
Yang Wu <wuyang5@huaqin.corp-partner.google.com>
2025-02-05 17:03:03 +0800

    rauru: Distinguish the RTS545x firmware binary for rauru boards
    Distinguish the RTS545x firmware binary based on project name. We use
    file path 'rts5453_GOOG0600' for Hylia.
    
    BUG=b:385253091
    TEST=Verify that the PDC firmware can be updated successfully.
    BRANCH=rauru
    
    Cq-Depend: chrome-internal:7971560
    Change-Id: Ifef29d25b6105011d5f46aaa4e9a1d1dd3060029
    Signed-off-by: Yang Wu <wuyang5@huaqin.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6234664
    Reviewed-by: Yidi Lin <yidilin@google.com>
    Commit-Queue: ChromeOS Auto Retry <chromeos-auto-retry@chromeos-bot.iam.gserviceaccount.com>
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>

---
+++ src/board/rauru/hylia.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+
+#include "drivers/ec/rts5453/rts5453.h"
+
+/* Override of func in src/drivers/ec/rts5453/rts5453.c */
+void board_rts5453_get_image_paths(const char **image_path,
+		const char **hash_path, struct ec_response_pd_chip_info_v2 *r)
+{
+	*image_path = "rts5453_GOOG0600.bin";
+	*hash_path = "rts5453_GOOG0600.hash";
+}
