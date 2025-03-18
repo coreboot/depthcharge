be8e7e57a4a06ea6f0c42f9471e47e1586caea2d
Dolan Liu <liuyong5@huaqin.corp-partner.google.com>
2025-03-18 16:31:03 +0800

    felino: Enable SD card
    Enable support for Realtek Card Reader (RTS5250A)
    BUG=b:404409600
    TEST=emerge-fatcat depthcharge coreboot chromeos-bootimage,
    and recognized SD card in OS.
    
    Change-Id: Icb7f35a55bba5d03e708a47466b4cfe20aa165db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6366300
    Reviewed-by: Jianeng Ceng <cengjianeng@huaqin.corp-partner.google.com>
    Commit-Queue: Dolan Liu <liuyong5@huaqin.corp-partner.google.com>
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Reviewed-by: Tongtong Pan <pantongtong@huaqin.corp-partner.google.com>
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Tested-by: Dolan Liu <liuyong5@huaqin.corp-partner.google.com>

--- src/board/fatcat/felino.c
+++ src/board/fatcat/felino.c
@@ -33,6 +33,17 @@ const struct audio_config *variant_probe_audio_config(void)
 	return &config;
 }
 
+static const struct storage_config storage_configs[] = {
+	{ .media = STORAGE_NVME, .pci_dev = PCI_DEV_PCIE9 },
+	{ .media = STORAGE_RTKMMC, .pci_dev = PCI_DEV_PCIE2 },
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
 int gsc_irq_status(void)
 {
 	return pantherlake_get_gpe(GPE0_DW2_15); /* GPP_F15 */
