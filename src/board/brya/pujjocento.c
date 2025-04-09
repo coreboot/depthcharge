f8fb59835e44f167a12e7bcf6a03832fed7e70ec
Kun Liu <liukun11@huaqin.corp-partner.google.com>
2025-04-09 20:29:36 +0800

    Create pujjocento variant
    Create a depthcharge target pujjocento by copying the template files
    from the reference brya to the appropriate places in the source tree.
    
    (Auto-Generated by create_depthcharge_variant.sh version 1.0.0).
    
    BUG=b:409254508
    BRANCH=None
    TEST=FW_NAME=pujjocento emerge-nissa depthcharge
    
    Change-Id: Ib6fa253871618bccf638a0e2ae9da75b6420e41a
    Signed-off-by: Kun Liu <liukun11@huaqin.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6440983
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Reviewed-by: David Cross <davidmcross@google.com>
    Reviewed-by: Ashley Shen <shenashley@google.com>
    Commit-Queue: Ashley Shen <shenashley@google.com>

---
+++ src/board/brya/pujjocento.c
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+#include <pci.h>
+#include <pci/pci.h>
+#include "board/brya/include/variant.h"
+#include "drivers/ec/rts5453/rts5453.h"
+#include "drivers/gpio/alderlake.h"
+#include "drivers/soc/alderlake.h"
+#include "drivers/storage/sdhci.h"
+#include "drivers/storage/storage_common.h"
+
+#define EC_PCH_INT_ODL		GPD2
+
+/* Override of func in src/drivers/ec/rts5453/rts5453.c */
+void board_rts5453_get_image_paths(const char **image_path, const char **hash_path,
+				   int ec_pd_id, struct ec_response_pd_chip_info_v2 *r)
+{
+	*image_path = NULL;
+	*hash_path = NULL;
+}
+
+const struct audio_config *variant_probe_audio_config(void)
+{
+	static struct audio_config config;
+
+	config = (struct audio_config){
+		.bus = {
+			.type = AUDIO_HDA,
+		},
+		.codec = {
+			.type = AUDIO_ALC256,
+		},
+	};
+
+	return &config;
+}
+
+int cr50_irq_status(void)
+{
+	return alderlake_get_gpe(GPE0_DW0_13); /* GPP_A13 */
+}
+
+static const struct storage_config storage_configs[] = {
+	{
+		.media = STORAGE_UFS,
+		.pci_dev = PCH_DEV_UFS1,
+	},
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static const struct tpm_config config = {
+		.pci_dev = PCH_DEV_I2C0,
+	};
+
+	return &config;
+}
+
+const int variant_get_ec_int(void)
+{
+	return EC_PCH_INT_ODL;
+}
