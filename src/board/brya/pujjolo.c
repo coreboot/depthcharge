1ce8141ac55625155fd52bd886e73d7869708f5f
Luca Lai <luca.lai@lcfc.corp-partner.google.com>
2025-05-28 13:27:20 +0800

    Modify pujjolo variant
    Modify function parameters of the board_rts5453_get_image_paths
    
    BUG=b:395763555
    BRANCH=None
    TEST=FW_NAME=pujjolo emerge-nissa depthcharge
    
    Change-Id: I45faa3ebf5f49873e2925cf155af660b3b2951d6
    Signed-off-by: Luca Lai <luca.lai@lcfc.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6595850
    Reviewed-by: Kyle Lin <kylelinck@google.com>
    Commit-Queue: Kyle Lin <kylelinck@google.com>
    Reviewed-by: Ashley Shen <shenashley@google.com>

--- src/board/brya/pujjolo.c
+++ src/board/brya/pujjolo.c
@@ -13,7 +13,8 @@
 #define EC_PCH_INT_ODL		GPD2
 
 /* Override of func in src/drivers/ec/rts5453/rts5453.c */
-void board_rts5453_get_image_paths(const char **image_path, const char **hash_path)
+void board_rts5453_get_image_paths(const char **image_path, const char **hash_path,
+					int ec_pd_id, struct ec_response_pd_chip_info_v2 *r)
 {
 	*image_path = NULL;
 	*hash_path = NULL;
