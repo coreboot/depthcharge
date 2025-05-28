b56241ae509b79337f4fa599503ffda7bd575d48
Tony Huang <tony-huang@quanta.corp-partner.google.com>
2025-05-28 15:11:11 +0800

    Create caboc variant
    Create a depthcharge target caboc by copying the files from
    the reference brox to the appropriate places in the source tree.
    
    BUG=b:420796212
    BRANCH=firmware-brox-16080.B
    TEST=FW_NAME=caboc emerge-brox depthcharge
    
    Change-Id: I1848ee941d8eeb561651f62ee138aafdcfc90d5d
    Signed-off-by: Tony Huang <tony-huang@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6596131
    Reviewed-by: Oscar Liu <oscarliu@google.com>
    Reviewed-by: Derek Huang <derekhuang@google.com>

---
+++ src/board/brox/caboc.c
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+#include <pci.h>
+#include <pci/pci.h>
+
+#include "base/fw_config.h"
+#include "board/brox/include/variant.h"
+#include "drivers/ec/rts5453/rts5453.h"
+#include "drivers/gpio/alderlake.h"
+#include "drivers/soc/alderlake.h"
+#include "drivers/storage/storage_common.h"
+
+static const struct storage_config storage_configs[] = {
+	{ .media = STORAGE_NVME, .pci_dev = SA_DEV_CPU_RP0 },
+	{ .media = STORAGE_UFS, .pci_dev = PCH_DEV_UFS1 },
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
+const struct audio_config *variant_probe_audio_config(void)
+{
+	static struct audio_config config;
+
+	config = (struct audio_config){
+		.bus = {
+			.type = AUDIO_HDA,
+		},
+		.codec = {
+			.type = AUDIO_ALC256,
+		},
+	};
+
+	return &config;
+}
