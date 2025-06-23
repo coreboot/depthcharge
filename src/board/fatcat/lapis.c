771fb4a9ebeccf0edcd6af2845e4205d1d71ab74
Hualin Wei <weihualin@huaqin.corp-partner.google.com>
2025-06-23 17:16:46 +0800

    lapis: Add depthcharge variant for lapis
    Create a depthcharge target fatcat by copying the template
    files from fatcat. Add storage device, TPM device and TI PDC
    FW update support for lapis.
    
    BUG=b:438785495
    TEST=emerge-fatcat depthcharge
    
    Change-Id: I799f6d0fff7675dcb6c9fd83c49ca33cf6b4618b
    Signed-off-by: Hualin Wei <weihualin@huaqin.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6658464
    Reviewed-by: Weimin Wu <wuweimin@huaqin.corp-partner.google.com>
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Reviewed-by: YH Lin <yueherngl@chromium.org>

---
+++ src/board/fatcat/lapis.c
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+#include "drivers/ec/rts5453/rts5453.h"
+#include "board/fatcat/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
+#include "base/fw_config.h"
+#include "drivers/gpio/pantherlake.h"
+#include "drivers/soc/pantherlake.h"
+#include "drivers/storage/storage_common.h"
+#include "drivers/bus/soundwire/soundwire.h"
+
+#define EC_SOC_INT_ODL		GPP_E07
+
+static const struct storage_config storage_configs[] = {
+	{ .media = STORAGE_NVME, .pci_dev = PCI_DEV_PCIE9 },
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
+int gsc_irq_status(void)
+{
+	return pantherlake_get_gpe(GPE0_DW2_02); /* GPP_E02 */
+}
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static const struct tpm_config config = {
+		.pci_dev = PCI_DEV_I2C3,
+	};
+
+	return &config;
+}
+
+const int variant_get_ec_int(void)
+{
+	return EC_SOC_INT_ODL;
+}
