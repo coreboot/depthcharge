2c21e8b4809018d39a2f8ea8c6f3f5844b4a57d9
Ivy Jian <ivy.jian@quanta.corp-partner.google.com>
2025-07-30 10:41:49 +0800

    fatcat: Add PDC FW update support for kinmen
    BRANCH=none
    BUG=b:429488469
    TEST=Successfully updated PDC firmware to the latest version.
    
    Change-Id: Ib3f8bd18b53bd5025bca62aecbf0ba1c054b9348
    Signed-off-by: Ivy Jian <ivy.jian@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6799877
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Tested-by: Subrata Banik <subratabanik@chromium.org>
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>

--- src/board/fatcat/kinmen.c
+++ src/board/fatcat/kinmen.c
@@ -3,6 +3,7 @@
 #include <libpayload.h>
 
 #include "drivers/bus/soundwire/cavs_2_5-sndwregs.h"
+#include "drivers/ec/tps6699x/tps6699x.h"
 #include "drivers/gpio/pantherlake.h"
 #include "drivers/soc/pantherlake.h"
 #include "drivers/storage/storage_common.h"
@@ -40,4 +41,11 @@ const struct storage_config *variant_get_storage_configs(size_t *count)
 int gsc_irq_status(void)
 {
 	return pantherlake_get_gpe(GPE0_DW2_02); /* GPP_E02 */
+}
+
+/* Override of func in src/drivers/ec/tps6699x/tps6699x.c */
+void board_tps6699x_get_image_paths(const char **image_path, const char **hash_path)
+{
+	*image_path = "tps6699x_GOOG0700.bin";
+	*hash_path = "tps6699x_GOOG0700.hash";
 }
