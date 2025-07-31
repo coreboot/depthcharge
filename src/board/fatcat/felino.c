4cd714c1f8a5beb98d357b19053aed09f2fcfc7e
Subrata Banik <subratabanik@google.com>
2025-07-31 19:10:00 +0530

    board/fatcat/felino: Use common audio functions
    This commit refactors the audio detection logic in fatcat/felino.c
    to use the common functions audio_get_type() and audio_compare_codec_id()
    defined in intel_audio_setup.c.
    
    Previously, felino.c had its own, duplicated versions of these functions
    (variant_get_audio_config() and variant_compare_codec_id()). This
    change removes the redundant code and replaces it with calls to the
    centralized functions. This improves code reuse and simplifies
    maintenance.
    
    The new approach also improves the clarity of the code by using the
    standard audio_codec_type enum instead of a local variant_audio_config
    enum. The logic for selecting the codec is now more straightforward,
    directly using the type returned by audio_get_type().
    
    TEST=Able to hear boot beep while booting google/felino.
    
    Change-Id: Ib9e33846ea840256b18696c533d7f7aece3480bc
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6804769
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>
    Reviewed-by: Kapil Porwal <kapilporwal@chromium.org>
    Tested-by: Subrata Banik <subratabanik@chromium.org>

--- src/board/fatcat/felino.c
+++ src/board/fatcat/felino.c
@@ -8,6 +8,7 @@
 #include "drivers/ec/tps6699x/tps6699x.h"
 #include "drivers/gpio/pantherlake.h"
 #include "drivers/soc/pantherlake.h"
+#include "drivers/sound/intel_audio_setup.h"
 #include "drivers/sound/rt1320_sndw.h"
 #include "drivers/sound/rt7xx_sndw.h"
 #include "drivers/sound/sndw_common.h"
@@ -15,66 +16,15 @@
 
 #define EC_SOC_INT_ODL		GPP_E03
 
-enum variant_audio_config {
-	AUDIO_UNKNOWN,
-	AUDIO_ALC712,
-	AUDIO_ALC1320,
-};
-
-/*
- * variant_compare_codec_id - Helper to compare a codec info ID with a reference ID.
- * @info: Pointer to the sndw_codec_info containing the ID to compare.
- * @ref_id: Pointer to the reference sndw_codec_id to compare against.
- *
- * This function iterates through the ID fields and returns true if they all match,
- * false otherwise.
- */
-static bool variant_compare_codec_id(const sndw_codec_info *info, const sndw_codec_id *ref_id)
-{
-	for (int i = 0; i < SNDW_DEV_ID_NUM; i++) {
-		if (ref_id->id[i] != info->codecid.id[i])
-			return false;
-	}
-	return true;
-}
-
-static bool variant_audio_config_alc712(const sndw_codec_info *info)
-{
-	return variant_compare_codec_id(info, &rt712_id);
-}
-
-static bool variant_audio_config_alc1320(const sndw_codec_info *info)
-{
-	return variant_compare_codec_id(info, &rt1320_id);
-}
-
-static enum variant_audio_config variant_get_audio_config(unsigned int link)
-{
-	Soundwire *bus = new_soundwire(link);
-	sndw_codec_info info;
-
-	if (sndw_probe_and_enable_codec(bus, &info)) {
-		printf("Failed to enable codec.\n");
-		return AUDIO_UNKNOWN;
-	}
-
-	if (variant_audio_config_alc1320(&info))
-		return AUDIO_ALC1320;
-	else if (variant_audio_config_alc712(&info))
-		return AUDIO_ALC712;
-	else
-		return AUDIO_UNKNOWN;
-}
-
 const struct audio_config *variant_probe_audio_config(void)
 {
 	static struct audio_config config;
 
 	if (fw_config_probe(FW_CONFIG(AUDIO, AUDIO_ALC712_SNDW))) {
 		unsigned int link = AUDIO_SNDW_LINK3;
-		enum variant_audio_config audio_variant = variant_get_audio_config(link);
+		enum audio_codec_type audio_type = audio_get_type(link);
 
-		if (audio_variant == AUDIO_UNKNOWN)
+		if (audio_type == AUDIO_CODEC_NONE)
 			return NULL;
 
 		config = (struct audio_config){
@@ -83,7 +33,7 @@ const struct audio_config *variant_probe_audio_config(void)
 				.sndw.link	= link,
 			},
 			.codec = {
-				.type	= (audio_variant == AUDIO_ALC712) ? AUDIO_RT712 : AUDIO_RT1320,
+				.type	= audio_type,
 			},
 		};
 	}
