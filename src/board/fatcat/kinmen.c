9326a553812512061a5250905e43712698f9d31d
Ivan Chen <yulunchen@google.com>
2025-08-27 12:55:08 +0800

    kinmen: Update PIDs used for kinmen
    Define PID 0x5071, which is allocated for TPS6699x on fatcat,
    and add 0x506c into ec_aux_fw_chip_list to support PDC FW
    updating from version earlier than GOOG0720.
    
    BRANCH=none
    BUG=b:439693466
    TEST=Flash to DUT and PDC FW could be updated
    
    Signed-off-by: Ivan Chen <yulunchen@google.com>
    Change-Id: I1c0ccf70b0d646e1ef5d5b1a4161afbb748fae0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6887994
    Commit-Queue: Yu-Ping Wu <yupingso@chromium.org>
    Reviewed-by: Yu-Ping Wu <yupingso@chromium.org>

--- src/board/fatcat/kinmen.c
+++ src/board/fatcat/kinmen.c
@@ -3,6 +3,7 @@
 #include <libpayload.h>
 
 #include "drivers/bus/soundwire/cavs_2_5-sndwregs.h"
+#include "drivers/ec/cros/ec.h"
 #include "drivers/ec/tps6699x/tps6699x.h"
 #include "drivers/gpio/pantherlake.h"
 #include "drivers/soc/pantherlake.h"
@@ -43,7 +44,17 @@ int gsc_irq_status(void)
 	return pantherlake_get_gpe(GPE0_DW2_02); /* GPP_E02 */
 }
 
-/* Override of func in src/drivers/ec/tps6699x/tps6699x.c */
+/* Override of functions in src/drivers/ec/tps6699x/tps6699x.c */
+void board_tps6699x_register(void)
+{
+	static CrosEcAuxfwChipInfo tps6699x_info = {
+		.vid = CONFIG_DRIVER_EC_TPS6699X_VID,
+		.pid = 0x506c,
+		.new_chip_aux_fw_ops = new_tps6699x_from_chip_info,
+	};
+	list_insert_after(&tps6699x_info.list_node, &ec_aux_fw_chip_list);
+}
+
 void board_tps6699x_get_image_paths(const char **image_path, const char **hash_path)
 {
 	*image_path = "tps6699x_GOOG0700.bin";
