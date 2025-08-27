2bd77d41104edc06aa3793723658436a6a26f378
Ren Kuo <ren.kuo@quanta.corp-partner.google.com>
2025-08-27 15:34:13 +0800

    Moonstone: Add depthcharge variant
    Create a depthcharge target Moonstone by copying the template
    files from kinmen.
    
    BUG=b:441010542
    TEST=emerge-fatcat depthcharge
    
    Change-Id: I5b60f839e2a3920237c9a25f58d7cb96334b60ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6888294
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Reviewed-by: Subrata Banik <subratabanik@chromium.org>
    Tested-by: Ren Kuo <ren.kuo@quanta.corp-partner.google.com>
    Commit-Queue: YH Lin <yueherngl@chromium.org>

---
+++ src/board/fatcat/moonstone.c
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+
+#include "drivers/bus/soundwire/cavs_2_5-sndwregs.h"
+#include "drivers/ec/tps6699x/tps6699x.h"
+#include "drivers/gpio/pantherlake.h"
+#include "drivers/soc/pantherlake.h"
+#include "drivers/storage/storage_common.h"
+#include "drivers/sound/intel_audio_setup.h"
+
+const struct audio_config *variant_probe_audio_config(void)
+{
+	static struct audio_config config;
+	config = (struct audio_config){
+		.bus = {
+			.type = AUDIO_SNDW,
+			.sndw.link = AUDIO_SNDW_LINK3,
+		},
+		.codec = {
+			.type = AUDIO_RT721,
+		},
+	};
+
+	return &config;
+}
+
+static const struct storage_config storage_configs[] = {
+	{
+		.media = STORAGE_NVME,
+		.pci_dev = PCI_DEV_PCIE9,
+	},
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
+int gsc_irq_status(void)
+{
+	return pantherlake_get_gpe(GPE0_DW2_02); /* GPP_E02 */
+}
+
+/* Override of func in src/drivers/ec/tps6699x/tps6699x.c */
+void board_tps6699x_get_image_paths(const char **image_path, const char **hash_path)
+{
+	*image_path = "tps6699x_GOOG0700.bin";
+	*hash_path = "tps6699x_GOOG0700.hash";
+}
