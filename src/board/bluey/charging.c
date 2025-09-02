30b0d88a168b05c024db263b83f9f386651a2d61
Subrata Banik <subratabanik@google.com>
2025-09-02 08:46:52 +0000

    board/bluey: Enable charging applet for low-battery charging
    This commit introduces a new function, launch_charger_applet(), to
    support off-mode and low-battery charging on the Bluey board.
    
    The launch_charger_applet() function polls the EC for powerbtn event,
    and if the AC adapter is present, it continues to charge
    the battery. The charging loop is designed to:
     - Reboot: Exit the loop if the power button is pressed, allowing the
               system to boot normally during consecutive boot (tear-down
               ADSP-lite related instances while performing reboot).
     - Power-off:
         - Power off the system once the battery reaches full capacity.
         - Power off the system if the AC adapter is removed.
    
    This feature is initiated from INIT_FUNC only when the system
    boots in a low-battery state, as determined by a check against the EC.
    
    TODO: Launch charger applet even for off-mode charging.
    
    BUG=b:439819922
    TEST=Able to put google/bluey into charging loop and charger till 100%.
    
    Change-Id: Ic8ff046f954dab9c5d7ba3dde57970fa095d02e7
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6880007
    Reviewed-by: Kapil Porwal <kapilporwal@chromium.org>
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>
    Auto-Submit: Subrata Banik <subratabanik@chromium.org>
    Tested-by: Subrata Banik <subratabanik@chromium.org>

--- src/board/bluey/charging.c
+++ src/board/bluey/charging.c
@@ -3,6 +3,7 @@
 #include <libpayload.h>
 #include "base/cleanup_funcs.h"
 #include "base/init_funcs.h"
+#include "drivers/ec/cros/ec.h"
 #include "drivers/soc/qcom_spmi.h"
 
 #define PMIC_CORE_REGISTERS_ADDR 0x0C500000
@@ -17,6 +18,8 @@
 #define SMB2_CHGR_CHRG_EN_CMD ((SMB2_SLAVE_ID << 16) | SCHG_CHGR_CHARGING_ENABLE_CMD)
 
 #define CHRG_DISABLE 0x00
+
+#define DELAY_CHARGING_APPLET_MS 2000 /* 2ms */
 
 static void disable_battery_charging(void)
 {
@@ -52,3 +55,80 @@ static int board_cleanup_install(void)
 }
 
 INIT_FUNC(board_cleanup_install);
+
+static bool board_boot_in_low_battery_mode(void)
+{
+	return lib_sysinfo.boot_mode & CB_BOOT_MODE_LOW_BATTERY;
+}
+
+static void clear_ec_power_button_input(void)
+{
+	cros_ec_clear_host_events(EC_HOST_EVENT_MASK(EC_HOST_EVENT_POWER_BUTTON));
+}
+
+static int detect_ec_power_button_input(void)
+{
+	const uint32_t pwr_btn_mask = EC_HOST_EVENT_MASK(EC_HOST_EVENT_POWER_BUTTON);
+	uint32_t events;
+
+	if ((cros_ec_get_host_events(&events) == 0) && (events & pwr_btn_mask)) {
+		cros_ec_clear_host_events(pwr_btn_mask);
+		return 1;
+	}
+
+	return 0;
+}
+
+static int detect_ec_ac_disconnect_input(void)
+{
+	const uint32_t ac_disc_mask = EC_HOST_EVENT_MASK(EC_HOST_EVENT_AC_DISCONNECTED);
+	uint32_t events;
+
+	if ((cros_ec_get_host_events(&events) == 0) && (events & ac_disc_mask)) {
+		cros_ec_clear_host_events(ac_disc_mask);
+		return 1;
+	}
+
+	return 0;
+}
+
+static int launch_charger_applet(void)
+{
+	if (!CONFIG(DRIVER_EC_CROS) || !board_boot_in_low_battery_mode())
+		return 0;
+
+	printf("Inside %s. Initiating charging\n", __func__);
+
+	/* clear any pending power button press event */
+	clear_ec_power_button_input();
+
+	do {
+		/*
+		 * Read the charger status, bail out if not present or not
+		 * charging or with critical temperature then issue a shutdown
+		 */
+		if (detect_ec_ac_disconnect_input())
+			cros_ec_ap_poweroff();
+
+		/*
+		 * Sample the Power Button press to reset the device to ensure a fresh boot
+		 * to OS. This is required to avoid any kind of tear-down due to ADSP-lite
+		 * being loaded and need some clean up before loading ADSP firmware by
+		 * linux kernel.
+		 */
+		if (detect_ec_power_button_input())
+			cros_ec_ap_reset();
+
+		/* Add static delay before reading the charging applet pre-requisites */
+		mdelay(DELAY_CHARGING_APPLET_MS );
+	} while (true);
+
+	return 0;
+}
+
+/*
+ * Launch charging applet for below boot reasons:
+ *  - boot in low-battery mode
+ *  - boot in off-mode charging
+ */
+INIT_FUNC(launch_charger_applet);
