ed4a7726a991dab5400d664f20d76a1aea3ae2c7
Tony Huang <tony-huang@quanta.corp-partner.google.com>
2025-09-05 09:52:34 +0800

    brox: Enable PDC FW update for caboc
    According to CL:6915338, set PID to 0x507e for PDC FW update.
    
    BUG=b:442604598
    BRANCH=firmware-brox-16080.B
    TEST=emerge-brox depthcharge
         check PDC FW update successfully
    
    Change-Id: I2c3cbd97a6acc45806a9f0d73a2b308dc1b9b324
    Signed-off-by: Tony Huang <tony-huang@quanta.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6918614
    Reviewed-by: Oscar Liu <oscarliu@google.com>
    Commit-Queue: Oscar Liu <oscarliu@google.com>
    Reviewed-by: Ivan Chen <yulunchen@google.com>

--- src/board/brox/caboc.c
+++ src/board/brox/caboc.c
@@ -37,3 +37,11 @@ const struct audio_config *variant_probe_audio_config(void)
 
 	return &config;
 }
+
+/* Override of func in src/drivers/ec/rts5453/rts5453.c */
+void board_rts5453_get_image_paths(const char **image_path, const char **hash_path,
+				   int ec_pd_id, struct ec_response_pd_chip_info_v2 *r)
+{
+	*image_path = "rts5453vb_GOOG0L01.bin";
+	*hash_path = "rts5453vb_GOOG0L01.hash";
+}
