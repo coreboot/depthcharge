5b16389a326bc515fb5705385d35ca35c7b5120a
Tongtong Pan <pantongtong@huaqin.corp-partner.google.com>
2025-09-22 18:39:15 +0800

    lapis: Add support for CS35L56 beep function
    Add support for boot beep using CS35L56
    
    BUG=b:444302678
    TEST=emerge-fatcat depthcharge
    
    Change-Id: I934b71d3e301feabbcfd0068f240d3bb8c6de3e8
    Signed-off-by: Tongtong Pan <pantongtong@huaqin.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/6965065
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Reviewed-by: Weimin Wu <wuweimin@huaqin.corp-partner.google.com>
    Reviewed-by: Ivan Chen <yulunchen@google.com>

--- src/board/fatcat/lapis.c
+++ src/board/fatcat/lapis.c
@@ -29,6 +29,23 @@ int gsc_irq_status(void)
 	return pantherlake_get_gpe(GPE0_DW2_02); /* GPP_E02 */
 }
 
+const struct audio_config *variant_probe_audio_config(void)
+{
+	static struct audio_config config;
+	if (CONFIG(DRIVER_SOUND_CS35L56_SNDW)) {
+	config = (struct audio_config){
+		.bus = {
+			.type = AUDIO_SNDW,
+			.sndw.link = AUDIO_SNDW_LINK2
+		},
+		.codec = {
+			.type = AUDIO_CS35L56,
+		},
+	};
+	}
+	return &config;
+}
+
 const struct tpm_config *variant_get_tpm_config(void)
 {
 	static const struct tpm_config config = {
