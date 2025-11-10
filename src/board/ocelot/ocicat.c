68bebb922d4dbe12143542e3316c06523d8a4e94
chou.pierce <chou.pierce@inventec.corp-partner.google.com>
2025-11-10 10:37:50 +0800

    ocelot: Add depthcharge variant for ocicat
    Create a depthcharge target ocicat by copying the template
    files from ocelot.
    
    BUG=b:457879750
    BRANCH=main
    TEST=emerge-ocelot depthcharge
    
    Change-Id: I6ec37a68d8b7005b88de0ab6ff5640606d55682a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/7136127
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Reviewed-by: Minho Park <minhopark@google.com>
    Commit-Queue: ChromeOS Auto Retry <chromeos-auto-retry@chromeos-bot.iam.gserviceaccount.com>
    Tested-by: Pierce Chou <chou.pierce@inventec.corp-partner.google.com>

---
+++ src/board/ocelot/ocicat.c
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+
+#include "base/fw_config.h"
+#include "drivers/bus/soundwire/cavs_2_5-sndwregs.h"
+#include "drivers/ec/rts5453/rts5453.h"
+#include "drivers/soc/pantherlake.h"
+#include "drivers/storage/storage_common.h"
+#include "drivers/sound/intel_audio_setup.h"
+
+static const struct storage_config storage_configs[] = {
+	{
+		.media = STORAGE_NVME,
+		.pci_dev = PCI_DEV_PCIE1,
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_NVME_GEN4)
+	},
+	{
+		.media = STORAGE_NVME,
+		.pci_dev = PCI_DEV_PCIE5,
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_NVME_GEN4)
+	},
+	{
+		.media = STORAGE_UFS,
+		.pci_dev = PCI_DEV_UFS,
+		.ufs = {
+			.ref_clk_freq = UFS_REFCLKFREQ_38_4
+		},
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_UFS)
+	},
+	{
+		.media = STORAGE_NVME,
+		.pci_dev = PCI_DEV_PCIE1,
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_UNKNOWN)
+	},
+	{
+		.media = STORAGE_NVME,
+		.pci_dev = PCI_DEV_PCIE5,
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_UNKNOWN)
+	},
+	{
+		.media = STORAGE_UFS,
+		.pci_dev = PCI_DEV_UFS,
+		.ufs = {
+			.ref_clk_freq = UFS_REFCLKFREQ_38_4
+		},
+		.fw_config = FW_CONFIG(STORAGE, STORAGE_UNKNOWN)
+	},
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
+/* Override of func in src/drivers/ec/rts5453/rts5453.c */
+void board_rts5453_get_image_paths(const char **image_path, const char **hash_path,
+				   int ec_pd_id, struct ec_response_pd_chip_info_v2 *r)
+{
+		*image_path = NULL;
+		*hash_path = NULL;
+}
