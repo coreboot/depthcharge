9cb0e7c49b9ca4876564434b93f623ce56b25240
Zheng Li <lizheng@huaqin.corp-partner.google.com>
2026-01-28 21:09:42 +0800

    Pujjocento/Pujjoteenlo: Support RTS5452G-CG PD FW update
    GOOG0N01 bypass retimer
    GOOG0O01 TUSB1044
    
    BUG=b:471731004
    BRANCH=None
    TEST=emerge-nissa depthcharge chromeos-bootimage
    
    Cq-Depend:chromium:7509348,chrome-internal:8971678
    Change-Id: Ia144a82f4a41989abb275885e95fe340f92af730
    Signed-off-by: Zheng Li <lizheng@huaqin.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/7526407
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Reviewed-by: Ashley Shen <shenashley@google.com>
    Reviewed-by: Kun Liu <liukun11@huaqin.corp-partner.google.com>
    Commit-Queue: Ashley Shen <shenashley@google.com>

--- src/board/brya/pujjocento.c
+++ src/board/brya/pujjocento.c
@@ -11,6 +11,10 @@
 #include "drivers/storage/storage_common.h"
 
 #define EC_PCH_INT_ODL		GPD2
+#define PDC_RTS5453_BYPASS		"GOOG0901"
+#define PDC_RTS5453_TUSB546		"GOOG0F00"
+#define PDC_RTS5453VB_BYPASS	"GOOG0N01"
+#define PDC_RTS5453VB_TUSB546	"GOOG0O01"
 
 /* Override of func in src/drivers/ec/rts5453/rts5453.c */
 void board_rts5453_get_image_paths(const char **image_path, const char **hash_path,
@@ -19,13 +23,29 @@ void board_rts5453_get_image_paths(const char **image_path, const char **hash_pa
 	switch (ec_pd_id) {
 	case 0:
 		/* bypass retimer */
-		*image_path = "rts5453_GOOG0901.bin";
-		*hash_path = "rts5453_GOOG0901.hash";
+		if (!strcmp(r->fw_name_str, PDC_RTS5453_BYPASS)) {
+			*image_path = "rts5453_GOOG0901.bin";
+			*hash_path = "rts5453_GOOG0901.hash";
+		} else if (!strcmp(r->fw_name_str, PDC_RTS5453VB_BYPASS)) {
+			*image_path = "rts5453vb_GOOG0N01.bin";
+			*hash_path = "rts5453vb_GOOG0N01.hash";
+		} else {
+			*image_path = NULL;
+			*hash_path = NULL;
+		}
 		break;
 	case 1:
 		/* TUSB546/1044 */
-		*image_path = "rts5453_GOOG0F00.bin";
-		*hash_path = "rts5453_GOOG0F00.hash";
+		if (!strcmp(r->fw_name_str, PDC_RTS5453_TUSB546)) {
+			*image_path = "rts5453_GOOG0F00.bin";
+			*hash_path = "rts5453_GOOG0F00.hash";
+		} else if (!strcmp(r->fw_name_str, PDC_RTS5453VB_TUSB546)) {
+			*image_path = "rts5453vb_GOOG0O01.bin";
+			*hash_path = "rts5453vb_GOOG0O01.hash";
+		} else {
+			*image_path = NULL;
+			*hash_path = NULL;
+		}
 		break;
 	default:
 		printf("Unknown ec_pd_id %d\n", ec_pd_id);
@@ -81,15 +101,26 @@ const int variant_get_ec_int(void)
 void board_rts545x_register(void)
 {
 	/*
-	 * Pujjocento/Pujjoteenlo's PDC FW 1.71.1 contains the wrong PID 0x5072.
-	 * To allow updating from 1.71.1 to newer versions,
-	 * register a fake chip with the wrong PID.
-	 */
+	* Pujjocento/Pujjoteenlo's PDC FW 1.71.1 contains the wrong PID 0x5072.
+	* To allow updating from 1.71.1 to newer versions,
+	* register a fake chip with the wrong PID.
+	*
+	* Add rts5453vb_pid 0x5078 to allow PD controller RTS5452G-CG updates.
+	*/
 	const uint32_t old_pid = 0x5072;
-	static CrosEcAuxfwChipInfo rts5453_info = {
-		.vid = CONFIG_DRIVER_EC_RTS545X_VID,
-		.pid = old_pid,
-		.new_chip_aux_fw_ops = new_rts545x_from_chip_info,
+	const uint32_t rts5453vb_pid = 0x5078;
+	static CrosEcAuxfwChipInfo rts5453_infos[] = {
+		{
+			.vid = CONFIG_DRIVER_EC_RTS545X_VID,
+			.pid = old_pid,
+			.new_chip_aux_fw_ops = new_rts545x_from_chip_info,
+		},
+		{
+			.vid = CONFIG_DRIVER_EC_RTS545X_VID,
+			.pid = rts5453vb_pid,
+			.new_chip_aux_fw_ops = new_rts545x_from_chip_info,
+		},
 	};
-	list_insert_after(&rts5453_info.list_node, &ec_aux_fw_chip_list);
+	for (int i = 0; i < ARRAY_SIZE(rts5453_infos); i++)
+		list_insert_after(&rts5453_infos[i].list_node, &ec_aux_fw_chip_list);
 }
