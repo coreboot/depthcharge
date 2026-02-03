16b3407a167dc56ee30f6a0155db102187b4c3f3
mikelee <mike.lee@lcfc.corp-partner.google.com>
2026-02-03 13:42:21 +0800

    skywalker/mace: add GOOG0W00/GOOG0U00 PDC FW update support
    Add PDC chip mapping for config_id 0W/0U to correctly identify
    RTS5452P-VB with IT5205 retimer on skywalker/mace.
    
    BUG=b:480858786
    BRANCH=Skywalker
    TEST=emerge-skywalker depthcharge chromeos-bootimage
         check PDC FW update successfully
    
    Cq-Depend: 7532331
    Change-Id: I38142ae4d3622912255f2474e367611a13d56723
    Signed-off-by: mikelee <mike.lee@lcfc.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/7532428
    Reviewed-by: Ant Liao <antliao@google.com>
    Commit-Queue: ChromeOS Auto Retry <chromeos-auto-retry@chromeos-bot.iam.gserviceaccount.com>
    Reviewed-by: Ivan Chen <yulunchen@google.com>

--- src/board/skywalker/mace.c
+++ src/board/skywalker/mace.c
@@ -4,7 +4,18 @@
 
 int override_pdc_chip(struct pdc_chip *pdc)
 {
-	if (pdc->retimer == PDC_RETIMER_PS8747)
+	if (pdc->chip == PDC_RTS5452P &&
+	    pdc->retimer == PDC_RETIMER_PS8747) {
 		pdc->config.variant = '1'; /* 0B01 */
-	return 0;
+		return 0;
+	}
+	if (pdc->chip == PDC_RTS5452P_VB) {
+		if (pdc->retimer == PDC_RETIMER_PS8747 ||
+		pdc->retimer == PDC_RETIMER_IT5205) {
+			pdc->config.variant = '0'; /* 0U00/0W00 */
+			return 0;
+		}
+	}
+
+return 0;
 }
