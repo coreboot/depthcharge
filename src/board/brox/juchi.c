2c0b3c0457f558c0935ff4caee33991d7b119ef7
David Wu <david_wu@quanta.corp-partner.google.com>
2026-02-04 20:05:43 +0800

    Create juchi variant
    Create a depthcharge target juchi by copying the files from
    jubilant.
    
    BUG=b:481602501
    TEST=FW_NAME=juchi emerge-brox depthcharge
    
    Change-Id: I04da9db789a66a226becedab0ba389058a5c5c9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/7545417
    Reviewed-by: YH Lin <yueherngl@chromium.org>
    Tested-by: David Wu <david_wu@quanta.corp-partner.google.com>
    Reviewed-by: Bob Moragues <moragues@chromium.org>
    Commit-Queue: Kyle Lin <kylelinck@google.com>
    Reviewed-by: Kyle Lin <kylelinck@google.com>

---
+++ src/board/brox/juchi.c
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+#include <pci.h>
+#include <pci/pci.h>
+
+#include "base/fw_config.h"
+#include "board/brox/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
+#include "drivers/ec/rts5453/rts5453.h"
+#include "drivers/gpio/alderlake.h"
+#include "drivers/soc/alderlake.h"
+#include "drivers/storage/storage_common.h"
+
+#define EN_SPK_PIN    GPP_F20
+
+static const struct storage_config storage_configs[] = {
+	{ .media = STORAGE_NVME, .pci_dev = SA_DEV_CPU_RP0 },
+	{ .media = STORAGE_UFS, .pci_dev = PCH_DEV_UFS1 },
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
+/* Override of func in src/drivers/ec/rts5453/rts5453.c */
+void board_rts5453_get_image_paths(const char **image_path, const char **hash_path,
+				   int ec_pd_id, struct ec_response_pd_chip_info_v2 *r)
+{
+	*image_path = "rts5453_retimer_bypass.bin";
+	*hash_path = "rts5453_retimer_bypass.hash";
+}
+const struct audio_config *variant_probe_audio_config(void)
+{
+	static struct audio_config config;
+
+	config = (struct audio_config) {
+		.bus = {
+			.type			= AUDIO_I2S,
+			.i2s.address		= SSP_I2S1_START_ADDRESS,
+			.i2s.enable_gpio	= { .pad = EN_SPK_PIN },
+			.i2s.settings		= &max98357a_settings,
+		},
+		.amp = {
+			.type			= AUDIO_GPIO_AMP,
+			.gpio.enable_gpio	= EN_SPK_PIN,
+		},
+		.codec = {
+			.type			= AUDIO_RT1019,
+		},
+	};
+
+	return &config;
+}
