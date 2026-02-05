40c325a665023dbcf01640f17553b486934c586c
Grzegorz Bernacki <bernacki@google.com>
2026-02-05 14:57:08 +0000

    drivers: storage: Remove legacy MTD support
    MTD storage support is no longer required following the removal of
    the 'purin' board (CL:4892113). Remove the MTD driver stack
    and associated stream handling logic.
    
    BUG=b:514271116
    BRANCH=none
    TEST=make unit-tests
    
    Change-Id: I3b78e8599c6c3f52a24e221e8d728b4938060ee4
    Signed-off-by: Grzegorz Bernacki <bernacki@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/7797335
    Reviewed-by: Julius Werner <jwerner@chromium.org>
    Reviewed-by: Jakub "Kuba" Czapiga <czapiga@google.com>

--- src/drivers/storage/mtd/nand/ipq_nand.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DRIVERS_STORAGE_MTD_NAND_IPQ_NAND_H__
-#define __DRIVERS_STORAGE_MTD_NAND_IPQ_NAND_H__
-
-#include "drivers/storage/mtd/mtd.h"
-
-MtdDevCtrlr *new_ipq_nand(void *ebi2nd_base);
-
-#endif
