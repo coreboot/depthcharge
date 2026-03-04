817f0ddee1cb45bc53bcc71e135abe7af4655720
Hirokazu Honda <hiroh@chromium.org>
2026-03-04 14:06:51 -0800

    vboot: widevine: Add stable provisioning for OPTEE
    This adds the key derivation from the stable seed, which are new
    ways used in non ChromeOS devices. The legacy key derivation are
    left as is for ChromeOS. Remove the unnecessary build configuration.
    
    Specifically, this change:
    - Removes CONFIG_WIDEVINE_PROVISION_LEGACY.
    - Updates the NV data block to include a stable seed.
    - Implements HMAC-SHA256 derivation for stable HUK and Device Keys.
    - Adds SMC interfaces to register the GSC counter key, DRM device
      key, and stable HUK with the secure environment.
    
    BRANCH=main
    BUG=b:461433992
    TEST=Confirm the keys are in DT in OP-TEE
    
    Change-Id: Ieddc57721c0851ddfa02ede174ee291c7aae903f
    Signed-off-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/7633153
    Reviewed-by: Julius Werner <jwerner@chromium.org>

--- src/vboot/widevine/root_of_trust.c
+++ src/vboot/widevine/root_of_trust.c
@@ -16,6 +16,9 @@
 
 #define CROS_OEM_SMC_DRM_SET_HARDWARE_UNIQUE_KEY_FUNC_ID 0xC300C051
 #define CROS_OEM_SMC_DRM_SET_ROOT_OF_TRUST_FUNC_ID 0xC300C052
+#define CROS_OEM_SMC_DRM_SET_GSC_COUNTER_KEY_FUNC_ID 0xC300C053
+#define CROS_OEM_SMC_DRM_SET_DRM_DEVICE_KEY_FUNC_ID 0xC300C054
+#define CROS_OEM_SMC_DRM_SET_STABLE_HARDWARE_UNIQUE_KEY_FUNC_ID 0xC300C055
 
 #define CROS_OEM_QTEE_SMC_GSC_SET_ROOT_OF_TRUST_FUNC_ID 0x72000801
 #define CROS_OEM_QTEE_SMC_DRM_SET_ROOT_OF_TRUST_FUNC_ID 0x72000802
@@ -30,12 +33,9 @@
  */
 struct widevine_nv_block {
 	uint8_t rot_seed[WIDEVINE_SEED_LEN];
-	uint8_t reserved[WIDEVINE_SEED_LEN];
+	uint8_t stable_seed[WIDEVINE_SEED_LEN];
 	uint8_t counter_key[WIDEVINE_SEED_LEN];
 };
-
-static struct widevine_nv_block widevine_block_cache;
-static bool widevine_cache_valid = false;
 
 // Widevine RoT
 static const uint8_t widevine_rot_context[] = {'W', 'i', 'd', 'e', 'v', 'i',
@@ -45,25 +45,20 @@ static const uint8_t widevine_rot_context[] = {'W', 'i', 'd', 'e', 'v', 'i',
 static const uint8_t widevine_huk_context[] = {'W', 'i', 'd', 'e', 'v', 'i',
 					       'n', 'e', ' ', 'H', 'U', 'K'};
 
+#define WIDEVINE_STABLE_SEED_VERSION_CONTEXT '1'
+
+// Widevine new HUK (for stable seed)
+static const uint8_t widevine_stable_huk_context[] = {'W', 'i', 'd', 'e', 'v',
+						   'i', 'n', 'e', ' ', 'H', 'U', 'K',
+						   WIDEVINE_STABLE_SEED_VERSION_CONTEXT};
+// Widevine device key
+static const uint8_t widevine_device_key_context[] = {'W', 'i', 'd', 'e', 'v',
+					       'i', 'n', 'e', ' ', 'D', 'V', 'K',
+						   WIDEVINE_STABLE_SEED_VERSION_CONTEXT};
+
 _Static_assert(CONFIG(WIDEVINE_PROVISION_OPTEE) !=
-	           CONFIG(WIDEVINE_PROVISION_QTEE),
-	           "Exactly one of WIDEVINE_PROVISION_OPTEE or WIDEVINE_PROVISION_QTEE must be set");
-
-static uint32_t fill_widevine_cache(void)
-{
-	if (widevine_cache_valid)
-		return TPM_SUCCESS;
-
-	uint32_t ret = TlclRead(WIDEVINE_NV_ROT_SEED_INDEX, &widevine_block_cache,
-			 sizeof(widevine_block_cache));
-
-	if (ret == TPM_SUCCESS)
-		widevine_cache_valid = true;
-	else
-		printf("GSC seed read failed: %#x\n", ret);
-
-	return ret;
-}
+		   CONFIG(WIDEVINE_PROVISION_QTEE),
+		   "Exactly one of WIDEVINE_PROVISION_OPTEE or WIDEVINE_PROVISION_QTEE must be set");
 
 /**
  * Checks if a retrieved seed is valid.
@@ -80,59 +75,49 @@ static uint32_t validate_seed(const uint8_t *seed, size_t len, const char *name)
 	}
 
 	if (test_00 == 0 || test_ff == 0xff) {
-		printf("Widevine %s seed is invalid (all %s)\n", name, test_ff ? "0xff" : "zeros");
+		printf("Widevine %s seed is invalid (all %s)\n", name,
+			test_ff ? "0xff" : "zeros");
 		return TPM_E_READ_EMPTY;
 	}
 
 	return TPM_SUCCESS;
 }
 
-static uint32_t read_rot_seed(uint8_t *dest)
+static uint32_t read_seeds(struct widevine_nv_block *block)
 {
-	uint32_t ret = fill_widevine_cache();
+	uint32_t ret = TlclRead(WIDEVINE_NV_ROT_SEED_INDEX, block, sizeof(*block));
+
+	if (ret != TPM_SUCCESS) {
+		printf("GSC seed read failed: %#x\n", ret);
+		return ret;
+	}
+
+	ret = validate_seed(block->rot_seed, WIDEVINE_SEED_LEN, "RoT");
 	if (ret != TPM_SUCCESS)
 		return ret;
 
-	memcpy(dest, widevine_block_cache.rot_seed, WIDEVINE_SEED_LEN);
-	return validate_seed(dest, WIDEVINE_SEED_LEN, "RoT");
-}
-
-static uint32_t read_counter_key(uint8_t *dest)
-{
-	uint32_t ret = fill_widevine_cache();
+	ret = validate_seed(block->stable_seed, WIDEVINE_SEED_LEN, "Stable");
 	if (ret != TPM_SUCCESS)
 		return ret;
 
-	memcpy(dest, widevine_block_cache.counter_key, WIDEVINE_SEED_LEN);
-	return validate_seed(dest, WIDEVINE_SEED_LEN, "Counter");
+	ret = validate_seed(block->counter_key, WIDEVINE_SEED_LEN, "Counter");
+	if (ret != TPM_SUCCESS)
+		return ret;
+	return TPM_SUCCESS;
 }
 
-static void register_widevine_optee_huk(uint8_t huk[WIDEVINE_SEED_LEN])
+static void register_widevine_optee_data(uint8_t data[WIDEVINE_SEED_LEN],
+				   uint32_t func_id, const char *name)
 {
 	int ret;
-
-	ret = widevine_write_smc_data(
-		CROS_OEM_SMC_DRM_SET_HARDWARE_UNIQUE_KEY_FUNC_ID, huk,
-		WIDEVINE_SEED_LEN);
+	ret = widevine_write_smc_data(func_id, data, WIDEVINE_SEED_LEN);
 
 	if (ret)
-		printf("write TF-A widevine OPTEE HUK failed: %#x\n", ret);
+		printf("write TF-A widevine OPTEE %s failed: %#x\n", name, ret);
 }
 
-static void register_widevine_optee_rot(uint8_t rot[WIDEVINE_SEED_LEN])
-{
-	int ret;
-
-	ret = widevine_write_smc_data(
-		CROS_OEM_SMC_DRM_SET_ROOT_OF_TRUST_FUNC_ID, rot,
-		WIDEVINE_SEED_LEN);
-
-	if (ret)
-		printf("write TF-A widevine OPTEE ROT failed: %#x\n", ret);
-}
-
-static void register_widevine_qtee_seed(uint8_t seed[WIDEVINE_SEED_LEN], uint32_t func_id,
-	 const char *name)
+static void register_widevine_qtee_data(uint8_t seed[WIDEVINE_SEED_LEN],
+				   uint32_t func_id, const char *name)
 {
 	size_t elements = WIDEVINE_SEED_LEN / sizeof(uint64_t);
 	uint64_t qword_keys[elements];
@@ -147,66 +132,103 @@ static void register_widevine_qtee_seed(uint8_t seed[WIDEVINE_SEED_LEN], uint32_
 		printf("write TF-A widevine QTEE %s failed: %#x\n", name, ret);
 }
 
-static void register_widevine_qtee_rot(uint8_t rot[WIDEVINE_SEED_LEN])
+static uint32_t derive_and_register_seed_optee(struct vb2_context *ctx,
+				   uint8_t *seed, const uint8_t *context, size_t context_len,
+				   uint32_t func_id, const char *name)
 {
-	register_widevine_qtee_seed(rot, CROS_OEM_QTEE_SMC_DRM_SET_ROOT_OF_TRUST_FUNC_ID, "ROT");
+	struct vb2_hash hash;
+	if (vb2_hmac_calculate(vb2api_hwcrypto_allowed(ctx), VB2_HASH_SHA256,
+			       seed, WIDEVINE_SEED_LEN,
+			       context, context_len, &hash)) {
+		printf("Failed to calculate hmac for %s\n", name);
+		return TPM_E_INTERNAL_ERROR;
+	}
+
+	register_widevine_optee_data(hash.sha256, func_id, name);
+	return TPM_SUCCESS;
 }
 
-static void register_widevine_qtee_counter(uint8_t counter[WIDEVINE_SEED_LEN])
+static uint32_t derive_and_register_seed_qtee(struct vb2_context *ctx,
+				   uint8_t *seed, const uint8_t *context, size_t context_len,
+				   uint32_t func_id, const char *name)
 {
-	register_widevine_qtee_seed(counter, CROS_OEM_QTEE_SMC_GSC_SET_ROOT_OF_TRUST_FUNC_ID,
-			 "Counter");
+	struct vb2_hash hash;
+	if (vb2_hmac_calculate(vb2api_hwcrypto_allowed(ctx), VB2_HASH_SHA256,
+			       seed, WIDEVINE_SEED_LEN,
+				   context, context_len, &hash)) {
+		printf("Failed to calculate hmac for %s\n", name);
+		return TPM_E_INTERNAL_ERROR;
+	}
+
+	register_widevine_qtee_data(hash.sha256, func_id, name);
+	return TPM_SUCCESS;
+}
+
+uint32_t prepare_optee_root_of_trust(struct vb2_context *ctx,
+					struct widevine_nv_block *block)
+{
+	if (derive_and_register_seed_optee(ctx, block->rot_seed,
+				   widevine_rot_context, sizeof(widevine_rot_context),
+				   CROS_OEM_SMC_DRM_SET_ROOT_OF_TRUST_FUNC_ID, "ROT")) {
+		return TPM_E_INTERNAL_ERROR;
+	}
+	if (derive_and_register_seed_optee(ctx, block->rot_seed,
+				   widevine_huk_context, sizeof(widevine_huk_context),
+				   CROS_OEM_SMC_DRM_SET_HARDWARE_UNIQUE_KEY_FUNC_ID, "HUK")) {
+		return TPM_E_INTERNAL_ERROR;
+	}
+	if (derive_and_register_seed_optee(ctx, block->stable_seed,
+				   widevine_stable_huk_context,
+				   sizeof(widevine_stable_huk_context),
+				   CROS_OEM_SMC_DRM_SET_STABLE_HARDWARE_UNIQUE_KEY_FUNC_ID,
+				   "Stable HUK")) {
+		return TPM_E_INTERNAL_ERROR;
+	}
+	if (derive_and_register_seed_optee(ctx, block->stable_seed,
+				   widevine_device_key_context,
+				   sizeof(widevine_device_key_context),
+				   CROS_OEM_SMC_DRM_SET_DRM_DEVICE_KEY_FUNC_ID,
+				   "Device Key")) {
+		return TPM_E_INTERNAL_ERROR;
+	}
+	register_widevine_optee_data(block->counter_key,
+				     CROS_OEM_SMC_DRM_SET_GSC_COUNTER_KEY_FUNC_ID,
+				     "Counter Key");
+	return TPM_SUCCESS;
+}
+
+uint32_t prepare_qtee_root_of_trust(struct vb2_context *ctx,
+				       struct widevine_nv_block *block)
+{
+	if (derive_and_register_seed_qtee(ctx, block->rot_seed,
+				   widevine_rot_context, sizeof(widevine_rot_context),
+				   CROS_OEM_QTEE_SMC_DRM_SET_ROOT_OF_TRUST_FUNC_ID, "ROT")) {
+		return TPM_E_INTERNAL_ERROR;
+	}
+	register_widevine_qtee_data(block->counter_key,
+				      CROS_OEM_QTEE_SMC_GSC_SET_ROOT_OF_TRUST_FUNC_ID,
+				     "Counter Key");
+	return TPM_SUCCESS;
 }
 
 uint32_t prepare_widevine_root_of_trust(struct vb2_context *ctx)
 {
-	uint8_t rot_seed[WIDEVINE_SEED_LEN];
-	struct vb2_hash rot;
-	struct vb2_hash huk;
+	struct widevine_nv_block block;
 	uint32_t ret;
 
-	ret = read_rot_seed(rot_seed);
+	ret = read_seeds(&block);
 	if (ret != TPM_SUCCESS) {
-		printf("read_rot_seed() failed: %#x\n", ret);
+		printf("read_seeds() failed: %#x\n", ret);
 		/* Continue the the zero UDS for the development process. */
-		memset(rot_seed, 0, WIDEVINE_SEED_LEN);
+		memset(block.rot_seed, 0, WIDEVINE_SEED_LEN);
+		memset(block.stable_seed, 0, WIDEVINE_SEED_LEN);
+		memset(block.counter_key, 0, WIDEVINE_SEED_LEN);
 	}
 
-	if (vb2_hmac_calculate(vb2api_hwcrypto_allowed(ctx), VB2_HASH_SHA256,
-			       rot_seed, sizeof(rot_seed), widevine_rot_context,
-			       sizeof(widevine_rot_context), &rot)) {
-		printf("Failed to calculate hmac for RoT\n");
-		return TPM_E_INTERNAL_ERROR;
-	}
+	if (CONFIG(WIDEVINE_PROVISION_OPTEE))
+		ret = prepare_optee_root_of_trust(ctx, &block);
+	else
+		ret = prepare_qtee_root_of_trust(ctx, &block);
 
-	if (CONFIG(WIDEVINE_PROVISION_OPTEE)) {
-		if (vb2_hmac_calculate(vb2api_hwcrypto_allowed(ctx), VB2_HASH_SHA256,
-				       rot_seed, sizeof(rot_seed), widevine_huk_context,
-				       sizeof(widevine_huk_context), &huk)) {
-			printf("Failed to calculate hmac for HUK\n");
-			return TPM_E_INTERNAL_ERROR;
-		}
-		register_widevine_optee_huk(huk.sha256);
-		register_widevine_optee_rot(rot.sha256);
-	} else {
-		register_widevine_qtee_rot(rot.sha256);
-	}
-
-	/* Need special handing for non-CrOS devices */
-	if (CONFIG(WIDEVINE_PROVISION_LEGACY))
-		return TPM_SUCCESS;
-
-	uint8_t counter_key[WIDEVINE_SEED_LEN];
-
-	ret = read_counter_key(counter_key);
-	if (ret != TPM_SUCCESS) {
-		printf("read_counter_key() failed: %#x\n", ret);
-		/* Continue the the zero UDS for the development process. */
-		memset(counter_key, 0, WIDEVINE_SEED_LEN);
-	}
-
-	if (CONFIG(WIDEVINE_PROVISION_QTEE))
-		register_widevine_qtee_counter(counter_key);
-
-	return TPM_SUCCESS;
+	return ret;
 }
