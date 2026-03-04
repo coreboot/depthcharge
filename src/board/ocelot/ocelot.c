91ff25dec4c52627f04614d31c5265217bd756cd
Pranava Y N <pranavayn@google.com>
2026-03-04 11:02:56 +0530

    ocelotite: Enable PDC FW update for ocelotite
    This patch selects the TI PDC driver for ocelotite variant. It also adds
    the image and hash paths for the TI PDC FW for updates.
    
    BUG=b:478026867
    TEST=Verify depthcharge is able to update PDC FW successfully on
    ocelotite/SKU3
    
    Cq-Depend: chrome-internal:9071116
    Change-Id: Icdba38b425697d04dcc870aff31a64e7d56c508e
    Signed-off-by: Pranava Y N <pranavayn@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/7631536
    Reviewed-by: Ivan Chen <yulunchen@google.com>

--- src/board/ocelot/ocelot.c
+++ src/board/ocelot/ocelot.c
@@ -113,3 +113,10 @@ void board_rts5453_get_image_paths(const char **image_path, const char **hash_pa
 	*image_path = "rts5453_GOOG0H00.bin";
 	*hash_path = "rts5453_GOOG0H00.hash";
 }
+
+void board_tps6699x_get_image_paths(const char **image_path, const char **hash_path,
+				    int ec_pd_id, struct ec_response_pd_chip_info_v2 *r)
+{
+	*image_path = "tps6699x_GOOG0J00.bin";
+	*hash_path = "tps6699x_GOOG0J00.hash";
+}
