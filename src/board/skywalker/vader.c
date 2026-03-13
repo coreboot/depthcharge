1913aef72524f8be264779d3416d90412ff9a155
John Su <john_su@compal.corp-partner.google.com>
2026-03-13 13:33:42 +0800

    skywalker/vader: add Realtek PDC FW update support
    Add PDC chip mapping for config_id to correctly identify the
    RTS5452P-VB with PS8747 retimer.
    
    Additionally, revert the RTS5452P FW switch from GOOG0B03 back to
    GOOG0B00 on Vader.
    
    BUG=b:473708874
    BRANCH=skywalker
    TEST=emerge-skywalker depthcharge chromeos-bootimage
    
    Change-Id: Ie0a0f6e4446f72b4722886ac9784640b6d307d45
    Signed-off-by: John Su <john_su@compal.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/7665557
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Reviewed-by: Ashley Shen <shenashley@google.com>

--- src/board/skywalker/vader.c
+++ src/board/skywalker/vader.c
@@ -6,7 +6,12 @@ int override_pdc_chip(struct pdc_chip *pdc)
 {
 	if (pdc->chip == PDC_RTS5452P &&
 	    pdc->retimer == PDC_RETIMER_PS8747) {
-		pdc->config.variant = '3'; /* 0B03 */
+		pdc->config.variant = '0'; /* 0B00 */
+		return 0;
+	}
+	if (pdc->chip == PDC_RTS5452P_VB &&
+		pdc->retimer == PDC_RETIMER_PS8747) {
+		pdc->config.variant = '0'; /* 0U00 */
 		return 0;
 	}
 	return 0;
