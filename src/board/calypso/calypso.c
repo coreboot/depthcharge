f8477cb26cbc75facefac83a5029f0c330aed7ef
Subrata Banik <subratabanik@google.com>
2026-04-05 13:06:58 +0530

    board/calypso: Add `calypso` as varaint
    BUG=b:496650089
    TEST=ABle to build google/calypso.
    
    Change-Id: I86da85101ff6c0b10653be24c7728867e45b620c
    Signed-off-by: Subrata Banik <subratabanik@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/7725430
    Auto-Submit: Subrata Banik <subratabanik@chromium.org>
    Reviewed-by: Kapil Porwal <kapilporwal@chromium.org>
    Tested-by: Subrata Banik <subratabanik@chromium.org>
    Commit-Queue: Subrata Banik <subratabanik@chromium.org>

---
+++ src/board/calypso/calypso.c
@@ -0,0 +1,35 @@
+/*
+* This file is part of the coreboot project.
+ *
+ * Copyright (C) 2026, The Linux Foundation.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <libpayload.h>
+#include "drivers/soc/calypso.h"
+#include "variant.h"
+
+#define NVME_PCI_DEV PCI_DEV(0x1, 0, 0)
+
+pcidev_t variant_get_nvme_pcidev()
+{
+	return NVME_PCI_DEV;
+}
+
+uintptr_t variant_get_ec_spi_base()
+{
+	return QUP_SERIAL22_BASE;
+}
+
+uintptr_t variant_get_gsc_i2c_base()
+{
+	return QUP_SERIAL1_BASE;
+}
