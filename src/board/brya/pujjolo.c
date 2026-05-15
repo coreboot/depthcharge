795ce7689b86da2a0bf75889ad90ff85ec5ee549
Wayby Zhai <wayby.zhai@lcfc.corp-partner.google.com>
2026-05-15 09:43:49 +0800

    Pujjolo/Pujjoquince: Support RTS5452G PD FW update
    Support RTS5452G PD FW update and replace FW_CONFIG PDC FW
    selection with GOOGLE CONFIG ID(8 bits).
    
    Changes:
    - Use pdc_fw_map table to match config_id from fw_name_str.
    
    - Ignore patching revision.Only need to match the first 6 chars and variant index.
    
    - Register RTS5453P-VB PID 0x5078 in board_rts545x_register()
    
    BUG=b:504775758
    BRANCH=None
    TEST=FW_NAME=pujjolo emerge-nissa coreboot-private-files
    coreboot-private-files-chipset-adln depthcharge chromeos-bootimage coreboot
    
    Cq-Depend: chrome-internal:9296822
    Cq-Depend: chrome-internal:9298423
    Change-Id: I149017be6849221a3963c192cc9641f3b878c084
    Signed-off-by: Wayby Zhai <wayby.zhai@lcfc.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/7851606
    Reviewed-by: Ashley Shen <shenashley@google.com>
    Commit-Queue: Ivan Chen <yulunchen@google.com>
    Reviewed-by: Ivan Chen <yulunchen@google.com>

--- src/board/brya/pujjolo.c
+++ src/board/brya/pujjolo.c
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 
+#include <assert.h>
 #include <libpayload.h>
 #include <pci.h>
 #include <pci/pci.h>
@@ -9,35 +10,50 @@
 #include "drivers/soc/alderlake.h"
 #include "drivers/storage/sdhci.h"
 #include "drivers/storage/storage_common.h"
-#include "base/fw_config.h"
 
 #define EC_PCH_INT_ODL		GPD2
+#define MAX_PDC_PORT_NUM	2
+#define PDC_FW_NAME_REVISION_INDEX	6
+#define PDC_FW_NAME_VARIANT_INDEX	7
+
+static const struct pdc_fw_map {
+	const char *config_id;
+	const char *image;
+	const char *hash;
+} pdc_fw_map[] = {
+	{ "GOOG0901", "rts5453_GOOG0901.bin", "rts5453_GOOG0901.hash" },
+	{ "GOOG0F01", "rts5453_GOOG0F01.bin", "rts5453_GOOG0F01.hash" },
+	{ "GOOG0F02", "rts5453_GOOG0F02.bin", "rts5453_GOOG0F02.hash" },
+	{ "GOOG0N01", "rts5453vb_GOOG0N01.bin", "rts5453vb_GOOG0N01.hash" },
+	{ "GOOG0O01", "rts5453vb_GOOG0O01.bin", "rts5453vb_GOOG0O01.hash" },
+	{ "GOOG0O02", "rts5453vb_GOOG0O02.bin", "rts5453vb_GOOG0O02.hash" },
+};
 
 /* Override of func in src/drivers/ec/rts5453/rts5453.c */
 void board_rts5453_get_image_paths(const char **image_path, const char **hash_path,
 					int ec_pd_id, struct ec_response_pd_chip_info_v2 *r)
 {
-	switch (ec_pd_id) {
-	case 0:
-		/* bypass retimer */
-		*image_path = "rts5453_GOOG0901.bin";
-		*hash_path = "rts5453_GOOG0901.hash";
-		break;
-	case 1:
-		if (fw_config_probe(FW_CONFIG(PDC, PDC_PUJJOLO))) {
-			/* TUSB546/1044 for pujjolo*/
-			*image_path = "rts5453_GOOG0F01.bin";
-			*hash_path = "rts5453_GOOG0F01.hash";
-		} else {
-			/* TUSB546/1044 for pujjoquince*/
-			*image_path = "rts5453_GOOG0F02.bin";
-			*hash_path = "rts5453_GOOG0F02.hash";
-		}
-		break;
-	default:
-		printf("Unknown ec_pd_id %d\n", ec_pd_id);
-	}
+	assert(ec_pd_id >= 0 && ec_pd_id < MAX_PDC_PORT_NUM);
+	*image_path = NULL;
+	*hash_path = NULL;
 
+	for (int i = 0; i < ARRAY_SIZE(pdc_fw_map); i++) {
+		const struct pdc_fw_map *m = &pdc_fw_map[i];
+		/*
+		 * PDC FW 16.10.4 to 16.12.4 change revision fields
+		 * (e.g. GOOG0O01 -> GOOG0O11). Ignore patching revision.
+		 * Only need to match the first 6 chars and variant index.
+		 */
+		if (!strncmp(m->config_id, r->fw_name_str,
+			     PDC_FW_NAME_REVISION_INDEX) &&
+		    m->config_id[PDC_FW_NAME_VARIANT_INDEX] ==
+			    r->fw_name_str[PDC_FW_NAME_VARIANT_INDEX]) {
+			*image_path = m->image;
+			*hash_path = m->hash;
+			return;
+		}
+	}
+	printf("Unknown PDC name (ec_pd_id %d): %s\n", ec_pd_id, r->fw_name_str);
 }
 
 const struct audio_config *variant_probe_audio_config(void)
@@ -91,12 +107,23 @@ void board_rts545x_register(void)
 	/*
 	 * Pujjolo/Pujjoquince's PDC FW might contain wrong PID 0x5072.
 	 * Register it to allow updating to the newer versions.
+	 *
+	 * Add rts5453vb_pid 0x5078 to allow PD controller RTS5453P-VB updates.
 	 */
 	const uint32_t old_pid = 0x5072;
-	static CrosEcAuxfwChipInfo rts5453_info = {
-		.vid = CONFIG_DRIVER_EC_RTS545X_VID,
-		.pid = old_pid,
-		.new_chip_aux_fw_ops = new_rts545x_from_chip_info,
+	const uint32_t rts5453vb_pid = 0x5078;
+	static CrosEcAuxfwChipInfo rts5453_infos[] = {
+		{
+			.vid = CONFIG_DRIVER_EC_RTS545X_VID,
+			.pid = old_pid,
+			.new_chip_aux_fw_ops = new_rts545x_from_chip_info,
+		},
+		{
+			.vid = CONFIG_DRIVER_EC_RTS545X_VID,
+			.pid = rts5453vb_pid,
+			.new_chip_aux_fw_ops = new_rts545x_from_chip_info,
+		},
 	};
-	list_insert_after(&rts5453_info.list_node, &ec_aux_fw_chip_list);
+	for (int i = 0; i < ARRAY_SIZE(rts5453_infos); i++)
+		list_insert_after(&rts5453_infos[i].list_node, &ec_aux_fw_chip_list);
 }
