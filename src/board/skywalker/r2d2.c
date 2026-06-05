091f9a84c646c9a2f559d72d7cb37614c5f0c1e1
Motoki Miyata <miyata.motoki1@fujitsu.corp-partner.google.com>
2026-06-05 16:10:06 +0900

    skywalker: Enable PDC FW Update for R2D2
    Select GOOG0B04 for R2D2 PDC firmware.
    
    FW GOOG0B04 for RTS54542P + PS8747
    This firmware contains modification to make PDC to change
    USB-C MUX Rx Equalization settings (facing USB-C connector)
    from 2.4dB to 12.8dB.
    
    BUG=b:508378912
    TEST=cros_workon-skywalker start sys-boot/depthcharge
    TEST=emerge-skywalker depthcharge
    TEST=DUT boots properly with PDC software sync enabled
    BRANCH=skywalker
    
    Change-Id: I1fe93fb5bd5add1ea29bbfc62775bf0749cf9bae
    Signed-off-by: Motoki Miyata <miyata.motoki1@fujitsu.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/7902463
    Reviewed-by: Ashley Shen <shenashley@google.com>
    Reviewed-by: Yoshikazu Konishi <konishi.yoshi@fujitsu.corp-partner.google.com>
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Commit-Queue: Ashley Shen <shenashley@google.com>

---
+++ src/board/skywalker/r2d2.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include "board/skywalker/include/variant.h"
+
+int override_pdc_chip(struct pdc_chip *pdc)
+{
+	if (pdc->chip == PDC_RTS5452P &&
+	    pdc->retimer == PDC_RETIMER_PS8747)
+		pdc->config.variant = '4'; /* 0B04 */
+	return 0;
+}
