8781f5ef230a0b679bf2d53f519b7323e2adf826
Qinghong Zeng <zengqinghong@huaqin.corp-partner.google.com>
2026-06-25 14:14:51 +0800

    ocelot: Add depthcharge variant for hekla
    Create a depthcharge target hekla by copying the template
    files from ocelot. Add storage device, TPM device and TI PDC
    FW update support for hekla.
    
    BUG=b:524857392
    TEST=emerge-ocelot depthcharge
    
    Change-Id: Ia4c482f94040b1c1fc644d3e76afb76b7f27d16b
    Signed-off-by: Qinghong Zeng <zengqinghong@huaqin.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/depthcharge/+/8002015
    Reviewed-by: Ivan Chen <yulunchen@google.com>
    Reviewed-by: Derek Huang <derekhuang@google.com>
    Reviewed-by: Weimin Wu <wuweimin@huaqin.corp-partner.google.com>

---
+++ src/board/ocelot/hekla.c
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <libpayload.h>
+#include "drivers/ec/tps6699x/tps6699x.h"
+#include "board/fatcat/include/variant.h"
+#include "drivers/bus/i2s/cavs-regs.h"
+#include "drivers/bus/i2s/intel_common/max98357a.h"
+#include "base/fw_config.h"
+#include "drivers/ec/cros/lpc.h"
+#include "drivers/gpio/pantherlake.h"
+#include "drivers/soc/pantherlake.h"
+#include "drivers/storage/storage_common.h"
+#include "drivers/bus/soundwire/soundwire.h"
+
+#define EC_SOC_INT_ODL		GPP_E08
+
+static const struct storage_config storage_configs[] = {
+	{ .media = STORAGE_NVME, .pci_dev = PCI_DEV_PCIE1 },
+};
+
+const struct storage_config *variant_get_storage_configs(size_t *count)
+{
+	*count = ARRAY_SIZE(storage_configs);
+	return storage_configs;
+}
+
+int gsc_irq_status(void)
+{
+	return pantherlake_get_gpe(GPE0_DW2_14); /* GPP_E14 */
+}
+
+const struct audio_config *variant_probe_audio_config(void)
+{
+	static struct audio_config config;
+	if (CONFIG(DRIVER_SOUND_CS35L56_SNDW)) {
+	config = (struct audio_config){
+		.bus = {
+			.type = AUDIO_SNDW,
+			.sndw.link = AUDIO_SNDW_LINK2
+		},
+		.codec = {
+			.type = AUDIO_CS35L56,
+		},
+	};
+	}
+	return &config;
+}
+
+const struct tpm_config *variant_get_tpm_config(void)
+{
+	static const struct tpm_config config = {
+		.pci_dev = PCI_DEV_I2C1,
+	};
+
+	return &config;
+}
+
+const int variant_get_ec_int(void)
+{
+	return EC_SOC_INT_ODL;
+}
+
+const CrosEcLpcBusVariant variant_get_ec_lpc_bus(void)
+{
+	return CROS_EC_LPC_BUS_RTK;
+}
+
+/* Override of func in src/drivers/ec/tps6699x/tps6699x.c */
+void board_tps6699x_get_image_paths(const char **image_path, const char **hash_path,
+				    int ec_pd_id, struct ec_response_pd_chip_info_v2 *r)
+{
+	*image_path = "tps6699x_GOOG0I00.bin";
+	*hash_path = "tps6699x_GOOG0I00.hash";
+}
